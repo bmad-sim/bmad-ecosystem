0e600cc7bc733a61a7a390c8cf486d8edfeadf88
David Sagan <dcs16@cornell.edu>
2002-07-23 17:15:46 +0000

    Added dcslib_struct and dcslib_interface

--- modules/bmad.f90
+++ modules/bmad.f90
@@ -1,8 +1,12 @@
+#include "CESR_platform.inc"
+
 module bmad
 
   use bmad_struct
   use bmad_interface
   use runge_kutta_mod
   use make_mat6_mod
+  use dcslib_struct
+  use dcslib_interface
 
 end module
