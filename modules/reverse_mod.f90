9aa5815c3cf405111278f2e1c92c9dad1bf11f17
David Sagan <dcs16@cornell.edu>
2003-01-03 22:14:53 +0000

    New module.

---
+++ modules/reverse_mod.f90
@@ -0,0 +1,192 @@
+#include "CESR_platform.inc"
+
+module reverse_mod
+
+  use bmad_struct
+  use bmad_interface
+
+contains
+
+!+
+! Subroutine ring_reverse (ring_in, ring_rev)
+!
+! Subroutine to construct a ring structure with the elements in reversed order.
+! This may be used for backward tracking through the ring. 
+!
+! The correspondence between elements in the two rings is as follows:
+!     ring_rev%ele_(ring%n_ele_ring+1-i) = ring_in%ele_(i)  
+!                                                for 0 < i <= ring%n_ele_ring
+!     ring_rev%ele_(i)                   = ring_in%ele_(i)   
+!                                                for ring%n_ele_ring < i 
+!
+! All longitudial quantities (for example, the value of ks for a solenoid) 
+! are flipped in sign for the reversed ring. 
+! This means that the appropriate transformation from particle coordinates
+! in one ring the corrisponding coordinates in the other is:
+!     (x, P_x, y, P_y, z, P_z) -> (x, -P_x, y, -P_y, -z, P_z)
+!
+! Modules needed:
+!   use bmad
+!
+! Input:
+!   ring_in -- Ring_struct: Input ring.
+!
+! Output:
+!   ring_rev -- Ring_struct: Ring with the elements in reversed order.
+!-
+
+subroutine ring_reverse (ring_in, ring_rev)
+
+  implicit none
+
+  type (ring_struct), intent(in) :: ring_in
+  type (ring_struct), intent(out), target :: ring_rev
+  type (ele_struct), pointer :: ele
+  type (control_struct), pointer :: con
+
+  integer i, i1, i2, nr, ix_con(n_control_maxx)
+
+! transfer
+
+  ring_rev = ring_in
+
+  nr = ring_rev%n_ele_ring
+  ring_rev%ele_(1:nr) = ring_rev%ele_(nr:1:-1)
+
+! flip longitudinal stuff, maps
+
+  do i = 1, ring_rev%n_ele_max
+    call reverse_ele (ring_rev%ele_(i))
+  enddo
+
+! correct control information
+
+  do i = 1, ring_rev%n_control_array
+    con => ring_rev%control_(i)
+    if (con%ix_slave <= nr) con%ix_slave = nr+1-con%ix_slave
+    if (con%ix_lord <= nr)  con%ix_lord  = nr+1-con%ix_lord
+  enddo
+
+! slaves of a super lord must be in assending sequence.
+! ix_con keeps track of the switching
+
+  forall (i = 1:n_control_maxx) ix_con(i) = i 
+
+  do i = nr+1, ring_rev%n_ele_max
+    ele => ring_rev%ele_(i)
+    if (ele%control_type == super_lord$) then
+      i1 = ele%ix1_slave
+      i2 = ele%ix2_slave
+      ring_rev%control_(i1:i2) = ring_rev%control_(i2:i1:-1)
+      ix_con(i1:i2) = ix_con(i2:i1:-1)
+    endif
+  enddo
+
+  ring_rev%ic_(:) = ix_con(ring_rev%ic_(:))
+
+  call check_ring_controls (ring_rev, .true.)
+
+end subroutine
+
+!--------------------------------------------------------------------------
+!+
+! Subroutine ele_reverse (ele)
+!
+! Subroutine to "reverse" an element for backward tracking.
+!
+! All longitudial quantities (for example, the value of ks for a solenoid) 
+! are flipped in sign for the reversed ring. 
+! This means that the appropriate transformation that corresponds to the 
+! reverse transformation is:
+!     (x, P_x, y, P_y, z, P_z) -> (x, -P_x, y, -P_y, -z, P_z)
+!
+! Modules needed:
+!   use bmad
+!
+! Input:
+!   ele -- Ele_struct: Input element.
+!
+! Output:
+!   ele -- Ele_struct: Reversed element.
+!-
+
+subroutine ele_reverse (ele)
+
+  type (ele_struct) ele
+
+  integer i, j, sum245
+
+  real(rdef) tempp
+
+! flip longitudinal attributes
+
+  ele%value(x_pitch$) = -ele%value(x_pitch$)
+  ele%value(y_pitch$) = -ele%value(y_pitch$)
+
+  select case (ele%key)
+
+  case (solenoid$, sol_quad$)
+    ele%value(ks$) = -ele%value(ks$)
+
+  case (rfcavity$)
+    ele%value(lag$) = -ele%value(lag$)
+
+  case (sbend$)
+    tempp = ele%value(e1$)
+    ele%value(e1$) = ele%value(e2$)
+
+  end select
+
+! The inverse of the Taylor map is: 
+!   M * T^(-1) * M
+! where: 
+!   T = the input taylor series
+!   M = The map (x, P_x, y, P_y, z, P_z) -> (x, -P_x, y, -P_y, -z, P_z)
+
+  if (.not. associated(ele%taylor(1)%term)) return
+
+  call taylor_inverse (ele%taylor, ele%taylor)
+
+! Apply M to the right
+
+  do i = 1, 6
+    do j = 1, size(ele%taylor(i)%term)
+      sum245 = ele%taylor(i)%term(j)%exp(2) + ele%taylor(i)%term(j)%exp(4) + &
+                                                ele%taylor(i)%term(j)%exp(5)
+      if (mod(sum245, 2) == 1) ele%taylor(i)%term(j)%coef = &
+                                                  -ele%taylor(i)%term(j)%coef
+    end do
+  end do
+
+! Apply M to the left
+
+  ele%taylor(2)%term(:)%coef = -ele%taylor(2)%term(:)%coef
+  ele%taylor(4)%term(:)%coef = -ele%taylor(4)%term(:)%coef
+  ele%taylor(5)%term(:)%coef = -ele%taylor(5)%term(:)%coef
+
+! kill any gen_field
+
+if (associated(ele%gen_field)) call kill (ele%gen_field)
+
+! reverse mat6
+
+  call mat_inverse (ele%mat6, ele%mat6)
+  ele%vec0 = -matmul(ele%mat6, ele%vec0)
+
+  ele%mat6(2,:) = -ele%mat6(2,:)
+  ele%mat6(4,:) = -ele%mat6(4,:)
+  ele%mat6(5,:) = -ele%mat6(5,:)
+
+  ele%vec0(2) = -ele%vec0(2)
+  ele%vec0(4) = -ele%vec0(4)
+  ele%vec0(5) = -ele%vec0(5)
+
+  ele%mat6(:,2) = -ele%mat6(:,2)
+  ele%mat6(:,4) = -ele%mat6(:,4)
+  ele%mat6(:,5) = -ele%mat6(:,5)
+
+end subroutine
+
+end module
+
+
