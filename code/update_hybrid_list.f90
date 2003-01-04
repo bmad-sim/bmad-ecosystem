4c2515ec321e92cb76d75f36e37fc4affeb1de7a
David Sagan <dcs16@cornell.edu>
2003-01-04 00:44:23 +0000

    Eliminated fixed array and made subroutine recursive.

--- code/update_hybrid_list.f90
+++ code/update_hybrid_list.f90
@@ -1,25 +1,35 @@
 !+
-! Subroutine UPDATE_HYBRID_LIST (RING, N_IN, USE_ELE)
+! Subroutine update_hybrid_list (ring, n_in, use_ele)
 !
-! USE_ELE is a list of elements that should not be hyberdized in
-! MAKE_HYBRID_RING. If an element is to be used then its controls/controllers
-! should also be added to the list. This routine adds the control/controller
-! elements for USE_ELE(N_IN).
+! Subroutine to add elements to the use_ele list needed by the routine
+! make_hybrid_ring.
+!
+! Use_ele is a list of elements that should not be hyberdized. This list
+! is used by the subroutine make_hybrid_ring. If an element is to be
+! used (not hyberdized) then the associated lord and slave elements need
+! to be also added to the use_ele list. This routine does that bookkeeping.
+! for a single element use_ele(n_in).
+!
+! Modules needed:
+!   use bmad
 !
 ! Input:
-!     RING -- Ring_struct: Input ring.
-!     N_IN -- Integer: USE_ELE(N_IN) is the element whose controlls/controllers
-!             are to be added to USE_ELE.
+!   ring -- Ring_struct: Input ring structure.
+!   n_in -- Integer: use_ele(n_in) is the element whose associated lord and
+!             slave elements are to be added to use_ele.
 !
 ! Output:
-!     USE_ELE(:) -- Logical array: list RING elements to be not hyberdized
-!                   with MAKE_HYBRID_RING.
+!   USE_ELE(:) -- Logical: list of ring elements to be not hyberdized.
+!                   This is used with make_hybrid_ring.
 !
-! Note: If USE_ELE(N_IN) = .false. then no updating is done
+! Note: If use_ele(n_in) = .false. then no updating is done
 !-
 
 !$Id$
 !$Log$
+!Revision 1.5  2003/01/04 00:44:23  dcs
+!Eliminated fixed array and made subroutine recursive.
+!
 !Revision 1.4  2002/07/16 20:44:03  dcs
 !*** empty log message ***
 !
@@ -32,55 +42,41 @@
 
 #include "CESR_platform.inc"
 
+recursive subroutine update_hybrid_list (ring, n_in, use_ele)
 
+  use bmad_struct
+  use bmad_interface
 
-subroutine update_hybrid_list (ring, n_in, use_ele)
-
-  use bmad
-  implicit none      
+  implicit none
 
   type (ring_struct)  ring
 
   logical use_ele(:)
 
-  integer use_ix(100), ixu
-  integer n, ix, n_in, i, j
+  integer ix, n_in, i, j
 
 ! see if any work needs to be done
 
   if (.not. use_ele(n_in)) return
-  if (ring%ele_(n_in)%n_slave+ring%ele_(n_in)%n_lord == 0) return
 
 ! now go through and put controlled elements in the list and make sure
 ! all appropriate controllers are on the list
 
-  ixu = 1
-  use_ix(1) = n_in
-
-  do while (ixu /= 0)
-
-    n = use_ix(ixu)
-    ixu = ixu - 1
-
-    do i = ring%ele_(n)%ix1_slave, ring%ele_(n)%ix2_slave
-      ix = ring%control_(i)%ix_slave
-      if (.not. use_ele(ix)) then
-        use_ele(ix) = .true.
-        ixu = ixu + 1
-        use_ix(ixu) = ix
-      endif
-    enddo
-
-    do i = ring%ele_(n)%ic1_lord, ring%ele_(n)%ic2_lord
-      j= ring%ic_(i)
-      ix = ring%control_(j)%ix_lord
-      if (.not. use_ele(ix)) then
-        use_ele(ix) = .true.
-        ixu = ixu + 1
-        use_ix(ixu) = ix
-      endif
-    enddo
+  do i = ring%ele_(n_in)%ix1_slave, ring%ele_(n_in)%ix2_slave
+    ix = ring%control_(i)%ix_slave
+    if (.not. use_ele(ix)) then
+      use_ele(ix) = .true.
+      call update_hybrid_list (ring, ix, use_ele)
+    endif
   enddo
 
-  return
-  end
+  do i = ring%ele_(n_in)%ic1_lord, ring%ele_(n_in)%ic2_lord
+    j= ring%ic_(i)
+    ix = ring%control_(j)%ix_lord
+    if (.not. use_ele(ix)) then
+      use_ele(ix) = .true.
+      call update_hybrid_list (ring, ix, use_ele)
+    endif
+  enddo
+
+end subroutine
