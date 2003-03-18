e028c50e10f223ea55f758908ad0e7f6ec7a05aa
David Sagan <dcs16@cornell.edu>
2003-03-18 20:38:34 +0000

    Checks for twiss_propagate1 error.

--- code/twiss_propagate_all.f90
+++ code/twiss_propagate_all.f90
@@ -1,23 +1,31 @@
 !+
-! Subroutine TWISS_PROPAGATE_ALL (RING)
+! Subroutine twiss_propagate_all (ring)
 !
 ! Subroutine to propagate the twiss parameters from the start to the end.
+!
+! Note: ring%ele_()%x Twiss parameters are associated with the "A" mode and
+! the ring%ele_()%y Twiss parameters are associated with the "B" mode.
 !
 ! Modules Needed:
 !   use bmad
 !
 ! Input:
-!     RING.ELE_(0) -- Ring_struct: Twiss parameters at the start
-!     STATUS       -- Common block status structure
-!       .TYPE_OUT     -- Logical: If .true. then will type a message if the
-!                        modes are flipped.
+!   ring.ELE_(0) -- Ring_struct: Twiss parameters at the start
+!   bmad_status -- Common block status structure:
+!       %type_out -- If True then will type a message if the modes are flipped.
+!       %exit_on_error -- If True then stop if there is an error.
 !
 ! Output:
-!     RING     -- Ring_struct: Ring
+!   ring         -- Ring_struct: Ring
+!   bmad_status -- Common block status structure:
+!       %ok         -- Set False if an input beta is zero. True otherwise
 !-
 
 !$Id$
 !$Log$
+!Revision 1.5  2003/03/18 20:38:34  dcs
+!Checks for twiss_propagate1 error.
+!
 !Revision 1.4  2003/01/27 14:40:47  dcs
 !bmad_version = 56
 !
@@ -26,11 +34,8 @@
 !
 !Revision 1.2  2001/09/27 18:32:00  rwh24
 !UNIX compatibility updates
-!
 
 #include "CESR_platform.inc"
-
-
 
 subroutine twiss_propagate_all (ring)
 
@@ -55,9 +60,11 @@ subroutine twiss_propagate_all (ring)
 
   temp_type_out = bmad_status%type_out
   if (ring%param%symmetry == mobius_symmetry$) bmad_status%type_out = .false.
+  bmad_status%ok = .true.
 
   do n = 1, n_use
     call twiss_propagate1 (ring%ele_(n-1), ring%ele_(n))
+    if (.not. bmad_status%ok) return
   enddo
 
   bmad_status%type_out = temp_type_out
--- code/twiss_propagate_many.f90
+++ code/twiss_propagate_many.f90
@@ -4,9 +4,20 @@
 ! Subroutine to propagate the Twiss parameters from one point in the 
 ! ring to another.
 !
+! Note: ring%ele_()%x Twiss parameters are associated with the "A" mode and
+! the ring%ele_()%y Twiss parameters are associated with the "B" mode.
+!
+! Note: Starting and ending points are just after the elements with index
+!   IX_START and IX_END. For example, if DIRECTION = +1 then the first element
+!   propagateed through is element ix_start+1. If DIRECTION = -1 then the first
+!   element propagateed through is element ix_start.
+!
+! Note: If needed the subroutine will propagate through from the end of the 
+!   ring to the beginning (or vice versa) to get to the end point.
+!   Also: If IX_START = IX_END then the subroutine will propagate 1 full turn.
+!
 ! Modules Needed:
-!   use bmad_struct
-!   use bmad_interface
+!   use bmad
 !
 ! Input:
 !   ring             -- Ring_struct: Ring to propagate through.
@@ -15,6 +26,9 @@
 !   direction        -- Integer: Direction to propagate.
 !                            = +1  -> Propagate forward
 !                            = -1  -> Propagate backward
+!   bmad_status -- Common block status structure:
+!       %type_out -- If True then will type a message if the modes are flipped.
+!       %exit_on_error -- If True then stop if there is an error.
 !
 ! Output:
 !   ring          -- Ring_struct:
@@ -22,15 +36,8 @@
 !     %ele_(i)%y       -- Y Twiss for i^th element.
 !     %ele_(i)%c_mat   -- C coupling matrix for i^th element.
 !     %ele_(i)%gamma_c -- Coupling gamma factor for i^th element.
-!
-! Note: Starting and ending points are just after the elements with index
-!   IX_START and IX_END. For example, if DIRECTION = +1 then the first element
-!   propagateed through is element ix_start+1. If DIRECTION = -1 then the first
-!   element propagateed through is element ix_start.
-!
-! Note: If needed the subroutine will propagate through from the end of the ring
-!   to the beginning (or vice versa) to get to the end point.
-!   Also: If IX_START = IX_END then the subroutine will propagate 1 full turn.
+!   bmad_status -- Common block status structure:
+!       %ok         -- Set False if an input beta is zero. True otherwise
 !-
 
 subroutine twiss_propagate_many (ring, ix_start, ix_end, direction)
@@ -47,6 +54,8 @@ subroutine twiss_propagate_many (ring, ix_start, ix_end, direction)
 
 !
 
+  bmad_status%ok = .true.
+
   select case (direction)
 
   case (+1)
@@ -54,12 +63,14 @@ subroutine twiss_propagate_many (ring, ix_start, ix_end, direction)
     if (ix_start < ix_end) then
       do i = ix_start, ix_end-1
         call twiss_propagate1 (ring%ele_(i), ring%ele_(i+1))
+        if (.not. bmad_status%ok) return
       enddo
       return
     endif
 
     do i = ix_start, ring%n_ele_ring - 1
       call twiss_propagate1 (ring%ele_(i), ring%ele_(i+1))
+      if (.not. bmad_status%ok) return
     enddo
 
     if (ix_start /= 0) then
@@ -71,6 +82,7 @@ subroutine twiss_propagate_many (ring, ix_start, ix_end, direction)
 
     do i = 0, ix_end-1
       call twiss_propagate1 (ring%ele_(i), ring%ele_(i+1))
+      if (.not. bmad_status%ok) return
     enddo
 
 !
