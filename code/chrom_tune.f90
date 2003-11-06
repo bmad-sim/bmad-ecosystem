2bdf57fa4d49ab082e148f41e3544df49d7f402e
David Sagan <dcs16@cornell.edu>
2003-11-06 16:28:44 +0000

    Add argument.

--- code/chrom_tune.f90
+++ code/chrom_tune.f90
@@ -1,5 +1,5 @@
 !+
-! Subroutine chrom_tune (ring, delta_e, target_x, target_y, err_flag)
+! Subroutine chrom_tune (ring, delta_e, target_x, target_y, err_tol, err_flag)
 !
 ! Subroutine to set the sextupole strengths so that the ring has the desired
 ! chormaticities.
@@ -15,6 +15,9 @@
 !                    If 0 then default of 1.0e-4 is used.
 !   target_x -- Real(rp): Target X Chromaticity
 !   target_y -- Real(rp): Target Y Chromaticity
+!   err_tol  -- Real(rp): Max allowable Error:
+!                    Error = | X_Target - X_Actual | + | Y_Target -Y_Actual |
+!               A good number is: err_tol = 0.05_rp
 !
 ! Output:
 !   ring     -- Ring_struct: Ring with sextupole set
@@ -31,7 +34,7 @@
 
 #include "CESR_platform.inc"
 
-subroutine chrom_tune(ring,delta_e,target_x,target_y,err_flag)
+subroutine chrom_tune(ring, delta_e, target_x, target_y, err_tol, err_flag)
   
   use bmad_struct
   use bmad_interface
@@ -40,55 +43,55 @@ subroutine chrom_tune(ring,delta_e,target_x,target_y,err_flag)
   implicit none
   
   type (ring_struct) ring
-  type (coord_struct), allocatable :: co_(:)
   type (modes_struct) mode
 
   integer i, j, n_sex, n_x_sex, n_y_sex
   integer, pointer :: ix_sex(:)
   integer, allocatable :: ix_x_sex(:), ix_y_sex(:)
+  integer, allocatable :: sex_type(:)
+  integer ix
 
   real(rp), allocatable :: sex_y_values(:), sex_x_values(:)
   real(rp) target_x, target_y, chrom_x, chrom_y
   real(rp) delta_x, delta_y, d_chrom, chrom_x0
   real(rp) chrom_y0, step_x, step_y, chrom_(2,1), matrix(2,2)
-  real(rp) delta_e
+  real(rp) delta_e, err_tol
  
   logical err_flag, debug
-  logical, allocatable :: is_x_sex(:)
 
-!
+!                      
  
-  allocate (co_(0:ring%n_ele_max))
-
   debug = .false.
 
-  n_sex = 1
-  n_x_sex = 1
-  n_y_sex = 1
-
   call elements_locator(sextupole$, ring, ix_sex)
-
-  allocate (is_x_sex(size(ix_sex)))
+  allocate (sex_type(size(ix_sex)))
 
   do i = 1, size(ix_sex)
-     if ((ring%ele_(ix_sex(i))%x%beta) > & 
+    ix = ix_sex(i)
+    if (ring%ele_(ix)%value(tilt$) /= 0) then
+      sex_type(i) = n_plane$  ! do not use tilted sextupoles 
+    elseif ((ring%ele_(ix_sex(i))%x%beta) > & 
                       (ring%ele_(ix_sex(i))%y%beta)) then
-        is_x_sex(i) = .true.
-     else
-        is_x_sex(i) = .false.
-     end if
+      sex_type(i) = x_plane$
+    else
+      sex_type(i) = y_plane$
+    end if
   end do
 
-  allocate (ix_x_sex(count(is_x_sex)), ix_y_sex(count(.not. is_x_sex)))
-  allocate (sex_x_values(size(ix_x_sex)), sex_y_values(size(ix_y_sex)))
-  ix_x_sex = pack(ix_sex, mask = is_x_sex)
-  ix_y_sex = pack(ix_sex, mask = .not. is_x_sex)
+  n_sex = 0
+  n_x_sex = count(sex_type == x_plane$)
+  n_y_sex = count(sex_type == y_plane$)
+
+  allocate (ix_x_sex(n_x_sex), ix_y_sex(n_y_sex))
+  allocate (sex_x_values(n_x_sex), sex_y_values(n_y_sex))
+  ix_x_sex = pack(ix_sex, mask = (sex_type == x_plane$))
+  ix_y_sex = pack(ix_sex, mask = (sex_type == y_plane$))
 
   delta_x = 0.1
   delta_y = 0.1
 
 !
-
+        
   do j = 1, 100
 
     sex_x_values(:) = ring%ele_(ix_x_sex(:))%value(k2$)
@@ -96,24 +99,24 @@ subroutine chrom_tune(ring,delta_e,target_x,target_y,err_flag)
 
     call chrom_calc(ring, delta_e, chrom_x0, chrom_y0)
 
-    if (debug .and. j == 1) then
-      print*, 'Target Chromaticities'
-      print*, target_x, target_y
-      print*, 'Initial Chromaticities'
-      print*, chrom_x0, chrom_y0
+    if (debug) then
+      print *
+      print '(1x, a, 2f10.4)', 'Target Chrom:    ', target_x, target_y
+      print '(1x, a, 2f10.4)', 'Chrom Now:       ', chrom_x0, chrom_y0
+      print '(1x, a, 2f10.4)', 'Chrom diff (N-T):', &
+                                 chrom_x0-target_x, chrom_y0-target_y
     end if
 
     d_chrom = abs(target_x - chrom_x0) + abs(target_y - chrom_y0)
 
-    if (d_chrom < 1.0e-3) then
+    if (d_chrom < err_tol) then
       if (debug) then
         print *, 'Number of iterations:', j
-        print *, 'Final Chromaticities, x & y:'
-        print *, chrom_x0, chrom_y0
+        print '(1x, a, 2f10.4)', 'Final Chromaticities:', chrom_x0, chrom_y0
       end if
       deallocate (ix_x_sex, ix_y_sex, ix_sex)
-      deallocate (sex_x_values, sex_y_values, is_x_sex)
-      deallocate (co_)
+      deallocate (sex_x_values, sex_y_values)
+      err_flag = .false.
       return
     end if
 
@@ -143,7 +146,9 @@ subroutine chrom_tune(ring,delta_e,target_x,target_y,err_flag)
 
 !
   
-  print*, 'ERROR IN CHROM_TUNE:  TUNING SEXTUPOLES FAILED!'
+  print *, 'ERROR IN CHROM_TUNE:  TUNING SEXTUPOLES FAILED!'
+  deallocate (ix_x_sex, ix_y_sex, ix_sex)
+  deallocate (sex_x_values, sex_y_values)
   err_flag = .true.
 
 end subroutine
