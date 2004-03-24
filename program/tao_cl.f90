30b87eb2a6d05e3dae28f99bbc237a252b93f744
cesrulib <cesrulib@cornell.edu>
2004-03-24 17:33:10 +0000

    Initial revision

---
+++ program/tao_cl.f90
@@ -0,0 +1,31 @@
+!+
+! Program tao_cl
+!
+! Command Line version of tao.
+!-
+
+program tao_cl
+
+  use tao_mod
+
+  implicit none
+
+  type (tao_super_universe_struct) s
+  logical end_flag, found
+  character(100) cmd_line
+
+! init
+
+  call tao_init (s)
+
+! loop over commands.
+
+  end_flag = .false.
+
+  do
+    call tao_get_user_input (s, cmd_line)
+    call tao_stub_command (s, cmd_line, found)
+    if (.not. found) call tao_command (s, cmd_line)
+  enddo
+
+end program
