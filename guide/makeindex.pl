7c957d62b9847845ccc5664b4e59f11de3905502
David Sagan <dcs16@cornell.edu>
2004-06-12 21:35:07 +0000

    perl script to substitute "\_" for "_" in the index.

---
+++ guide/makeindex.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+`latex bmad_manual`;
+`makeindex bmad_manual`;
+
+open (FC, "bmad_manual.ind") || die ("Cannot open File: bmad_manual.idx\n");
+open (F_OUT, ">temp.out") || die ("Cannot open temparary file\n");
+
+while (<FC>) {
+  s/_/\\_/g;
+  print (F_OUT);
+}
+
+close (FC);
+close (F_OUT);
+`mv temp.out bmad_manual.ind`;
+
+
+`latex bmad_manual`;
