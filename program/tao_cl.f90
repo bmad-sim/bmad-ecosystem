83e7e9904d5a0651222f4bcbe8f3912a937c2d9b
John Smith <js344@cornell.edu>
2004-06-24 05:35:26 +0000

    changed single mode execution location

--- program/tao_cl.f90
+++ program/tao_cl.f90
@@ -25,8 +25,16 @@ program tao_cl
   do
     err = .false.
     call tao_get_user_input (cmd_line)
-    call tao_hook_command (cmd_line, found)
-    if (.not. found) call tao_command (cmd_line, err)
+    if (s%global%single_mode) then
+      ! single mode
+      call tao_single_mode (cmd_line(1:1))
+      ! Do the standard calculations and plotting after command execution.
+      call tao_cmd_end_calc
+    else
+      ! command line mode
+      call tao_hook_command (cmd_line, found)
+      if (.not. found) call tao_command (cmd_line, err)
+    endif
     if (.not. err) call tao_cmd_history_record (cmd_line)
   enddo
 
