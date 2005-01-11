d7ccbfbc625ff7d7c629bee3854c7c9f34bb7c0f
David Sagan <dcs16@cornell.edu>
2005-01-11 22:41:35 +0000

    Fixed bug for non super-lord lords.

--- code/find_element_ends.f90
+++ code/find_element_ends.f90
@@ -33,8 +33,8 @@ subroutine find_element_ends (ring, ix_ele, ix_start, ix_end)
                                                          
   type (ring_struct) ring
 
-  integer ix_ele, ix_start, ix_end
-  integer ix1, ix2, n, ix_(100), indx(100)
+  integer ix_ele, ix_start, ix_end, ix_slave
+  integer ix1, ix2, n, ixs(100), indx(100), n_end, n_slave
 
 !
 
@@ -44,17 +44,34 @@ subroutine find_element_ends (ring, ix_ele, ix_start, ix_end)
   if (ring%ele_(ix_ele)%n_slave == 0) then
     ix_start = ix_ele - 1
     ix_end = ix_ele
+
   elseif (ring%ele_(ix_ele)%control_type == super_lord$) then
     ix_start = ring%control_(ix1)%ix_slave - 1
     ix_end = ring%control_(ix2)%ix_slave
+
   else  ! overlay_lord$ or group_lord$
-    n = ring%ele_(ix_ele)%n_slave
-    ix_(1:n) = ring%control_(ix1:ix2)%ix_slave
-    ix_(2:n) = ix_(2:n) + ring%n_ele_use * &
-                             nint(float(ix_(1) - ix_(2:n)) / ring%n_ele_use)
-    call indexx (ix_(1:n), indx(1:n))
-    ix_start = ix_(indx(1)) - 1
-    ix_end = ix_(indx(n))
+    ix_start = ring%n_ele_use + 1
+    ix_end   = 0
+    n = 0
+    n_slave = ring%ele_(ix_ele)%n_slave
+    ixs(1:n_slave) = ring%control_(ix1:ix2)%ix_slave
+    n_end = n_slave
+    do 
+      n = n + 1
+      if (n > n_end) return
+      ix_slave = ixs(n)
+      if (ix_slave > ring%n_ele_use) then
+        n_slave = ring%ele_(ix_slave)%n_slave
+        ix1 = ring%ele_(ix_slave)%ix1_slave
+        ix2 = ring%ele_(ix_slave)%ix2_slave
+        ixs(n_end+1:n_end+n_slave) = ring%control_(ix1:ix2)%ix_slave
+        n_end = n_end + n_slave
+      else
+        ix_start = min(ix_start, ix_slave-1)
+        ix_end   = max(ix_end, ix_slave)
+      endif
+    enddo
+
   endif
 
 end subroutine
