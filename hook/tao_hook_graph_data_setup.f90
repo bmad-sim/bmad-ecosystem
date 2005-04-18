c195b3d7500d7fd4c65da3ce4d0992683881aa7b
David Sagan <dcs16@cornell.edu>
2005-04-18 19:40:25 +0000

    new.

---
+++ hook/tao_hook_graph_data_setup.f90
@@ -0,0 +1,30 @@
+!+
+! Subroutine tao_hook_graph_data_setup (plot, graph, found)
+!
+! Subroutine to customize the calculation of data to be plotted.
+!
+! Input:
+!   plot  -- Tao_plot_struct: Plot structure containing the graph.
+!   graph -- Tao_graph_struct: The graph to calculate the data for.
+!
+! Output:
+!   graph -- Tao_graph_struct: Structure with plot data calulated.
+!   found -- Logical: Set true if this routine calculates data.
+!               Set false if data calc is to be left to tao_plot_data_setup.
+!-
+
+subroutine tao_hook_graph_data_setup (plot, graph, found)
+
+use tao_mod
+
+implicit none
+
+type (tao_plot_struct) plot
+type (tao_graph_struct) graph
+logical found
+
+! This dummy routine just sets found and returns.
+
+found = .false.
+
+end subroutine
