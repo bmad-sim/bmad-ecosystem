e2c4ba8e5efa8d4f8cda9a4b2fad10cf955938f1
cesrulib <cesrulib@cornell.edu>
2005-07-20 19:01:39 +0000

    bug fixed

--- create_fdb.pl
+++ create_fdb.pl
@@ -7,23 +7,28 @@
 # ussage:  ./create_fdb.pl
 # output: cesr_file.fdb
 #############################################################
-@tps_list=qw(h inc c cc cpp cxx f90 F90); #file extensions in use
+@tps_list=qw(h inc c cc cpp cxx f F f90 F90); #file extensions in use
 open FILE1, ">".$ENV{"CESR_CONFIG"}."/cesr_file.fdb" || die "can not open file cesr_file.fdb";
 #take all of the directories except CVS and make loop on them
-foreach $dir (`ls -R \$CESR_CVSSRC| grep \':\'|grep -v CVS`) {
+foreach $dir (`ls -R \$CESR_CVSSRC \$CESR_PKG | grep \':\'|grep -v CVS`) {
     chomp $dir;
     chop $dir;
+    $ff=3;
     $ls_arg="";
     # create command line arg to see all files in the directory
     foreach $x (@tps_list){
         $ls_arg=$ls_arg."$dir/*\.$x ";
     }
+    
     # take all of the files in the directory and loop them
     foreach $x  (`ls $ls_arg`){
         chomp $x;
-        if ($x=~/\/(\w+\.\w+)/){$x=$1};#notdir filename
-        if ($x=~/(?:\w+)\.(?:h|c|cc|cpp|cxx)/i){deal_c();}
-        if ($x=~/(?:\w+)\.(?:inc|f90)/i){deal_f();}
+        if ($x=~/\/(\w+\-*\w*\.\w+)/){$x=$1};#notdir filename
+        if ($x=~/(?:\w+\-*\w*)\.(?:h|c|cc|cpp|cxx)/i){$ff=deal_c();}
+        if ($x=~/(?:\w+\-*\w*)\.(?:inc|f|f90)/i){$ff=deal_f();}
+        if ($x=~/(?:\w+\-*\w*)\.(?:h|c|cc|cpp|cxx|inc|f|f90)/i){
+            if ($ff==0) {print FILE1 "$x : $dir : \n";}
+        }
     }
 } 
 close FILE1 || die "couldn't close file: cesr_file.fdb";
@@ -31,7 +36,8 @@ close FILE1 || die "couldn't close file: cesr_file.fdb";
 #------------------------------------------------------------
 # sub deal_c takes function definition from c-file and prints function name
 sub deal_c{
-    my ($file);
+    my ($file,$found_function);
+    $found_function=0;
     $file="$dir/$x";
     open (FILE2,"<$file") || die "can not open file $file";
     $in_comment=0; # not in a comments field
@@ -44,19 +50,26 @@ sub deal_c{
       next LINE if $in_comment == 1;
       #search for function statement and printing function name
 #      if ( $_ =~ /^(?:\s*\t*)(?:\w+\s+\t*){1,3}(\w+)\W/){
-      if ( $_ =~ /^(?:\s*\t*)(?:(?:void|int|char|float|double|bool|wchar_t|signed|unsigned|long|short)\s+\t*){1,3}(\**\w+)(?:\s*\t*\(.*)/i){
-              print FILE1 "$x : $dir : $1\n";
+      if ( $_ =~ /^(?:\s*\t*)(?:(?:void|int|char|float|double|bool|wchar_t|signed|unsigned|long|short)\*?\s+\t*){1,3}\*?(\w+)(?:\[.*\])?(?:\s*\t*\(.*)/i){
+              print FILE1 "$x : $dir : $1\n"; #basic definition
+              $found_function=1;
+      }
+      if ( $_ =~ /^(?:\s*\t*)(?:(?:void|int|char|float|double|bool|wchar_t|signed|unsigned|long|short)\*?\s+\t*){1,3}\(\*?(\w+)(?:\[.*\])?\)(?:\s*\t*\(.*)/i){
+              print FILE1 "$x : $dir : $1\n";#definition type int* (*func_name)(...)
+              $found_function=1;
       }
       if ( $_=~ /\/\*/) {
           $in_comment=1; #in a comments field
       }
   }
     close (FILE2) || die "can not close file $file";
+    return $found_function;
 }
 
 # sub deal_f takes subroutine and function f90 definition from file and prints their name
 sub deal_f{
-    my ($file);
+    my ($file,$found_function);
+    $found_function=0;
     $file="$dir/$x";
     open (FILE2,"<$file") || die "can not open file $file";
   LINE:while (<FILE2>){
@@ -64,7 +77,9 @@ sub deal_f{
       #search for function and subroutine statement and print their name
       if ( $_ =~ /^(?:\s*\t*)(?:(?:subroutine|function)\s+\t*)(\w+)\W/i){
               print FILE1 "$x : $dir : $1\n";
+              $found_function=1;
       }
   }
     close (FILE2) || die "can not close file $file";
+    return $found_function;
 }
