77a5149ab14b55a150e1bc2853917c55d741f38c
David Sagan <dcs16@cornell.edu>
2006-03-26 06:49:32 +0000

    mirror change in attribute_free routine.

--- code/set_ele_attribute.f90
+++ code/set_ele_attribute.f90
@@ -70,7 +70,7 @@ subroutine set_ele_attribute (ring, i_ele, attrib_name, &
   call pointer_to_attribute (ele, attrib_name, .true., &
                                              ptr_attrib, ix_attrib, err_flag)
   if (err_flag) return
-  if (.not. attribute_free (ele, ix_attrib, ring)) return
+  if (.not. attribute_free (i_ele, ix_attrib, ring)) return
 
 ! Setting the attribute value is trivial
 
