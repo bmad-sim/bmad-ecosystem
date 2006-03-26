29d44b1b8dbc35285fc55f38506ab34574d0bb97
David Sagan <dcs16@cornell.edu>
2006-03-26 06:45:22 +0000

    added optional exit_on_error argument.

--- code/chrom_calc.f90
+++ code/chrom_calc.f90
@@ -1,5 +1,5 @@
 !+
-! Subroutine chrom_calc (ring, delta_e, chrom_x, chrom_y)
+! Subroutine chrom_calc (ring, delta_e, chrom_x, chrom_y, exit_on_error)
 !
 ! Subroutine to calculate the chromaticities by computing the tune change
 ! when then energy is changed.
@@ -8,23 +8,29 @@
 !   use bmad
 !
 ! Input:
-!   ring      -- Ring_struct: Ring
-!   delta_e   -- Real(rp): Delta energy used for the calculation.
-!                    If 0 then default of 1.0e-4 is used.
+!   ring          -- Ring_struct: Ring
+!   delta_e       -- Real(rp): Delta energy used for the calculation.
+!                      If 0 then default of 1.0e-4 is used.
+!   exit_on_error -- Logical, optional: If True then subroutine will terminate 
+!                         programif the orbit does not converge. Default is
+!                         determined by bmad_status%exit_on_error
+!   bmad_status   -- Bmad status common block
+!     %exit_on_error -- Default for exit_on_error argument.
 !
 ! Output:
-!     delta_e     -- Real(rp): Set to 1.0e-4 if on input DELTA_E =< 0.
-!     chrom_x     -- Real(rp): Horizontal chromaticity.
-!     chrom_y     -- Real(rp): Vertical chromaticity.
-!     bmad_status -- Status structure in common block
-!                    See MAT_SYMP_DECOUPLE for for more info.
-!           %type_out  -- Logical: If .true. then will type a message for
-!                         non ok$ bmad_status
+!   delta_e     -- Real(rp): Set to 1.0e-4 if on input DELTA_E =< 0.
+!   chrom_x     -- Real(rp): Horizontal chromaticity.
+!   chrom_y     -- Real(rp): Vertical chromaticity.
+!   bmad_status -- Status structure in common block
+!                  See MAT_SYMP_DECOUPLE for for more info.
+!       %ok          -- Set False if orbit does not converge, True otherwise.
+!       %type_out    -- Logical: If .true. then will type a message for
+!                       non ok$ bmad_status
 !-
 
 #include "CESR_platform.inc"
 
-subroutine chrom_calc (ring, delta_e, chrom_x, chrom_y)
+subroutine chrom_calc (ring, delta_e, chrom_x, chrom_y, exit_on_error)
 
   use bmad_struct
   use bmad_interface, except => chrom_calc
@@ -39,6 +45,7 @@ subroutine chrom_calc (ring, delta_e, chrom_x, chrom_y)
   real(rp) high_tune_x, high_tune_y, low_tune_x, low_tune_y
   real(rp) delta_e, chrom_x, chrom_y
   integer nt
+  logical, optional :: exit_on_error
 
 !
 
@@ -54,10 +61,12 @@ subroutine chrom_calc (ring, delta_e, chrom_x, chrom_y)
 ! lower energy tune
 
   orb(0)%vec(6) = -delta_e
-  call closed_orbit_calc (ring2, orb, 4)
+  call closed_orbit_calc (ring2, orb, 4, exit_on_error = exit_on_error)
+  if (.not. bmad_status%ok) return
   call ring_make_mat6 (ring2, -1, orb)
 
   call twiss_at_start (ring2)
+  if (.not. bmad_status%ok) return
   call twiss_propagate_all (ring2)
   low_tune_x = ring2%ele_(nt)%x%phi / twopi
   low_tune_y = ring2%ele_(nt)%y%phi / twopi
@@ -65,10 +74,12 @@ subroutine chrom_calc (ring, delta_e, chrom_x, chrom_y)
 ! higher energy tune
 
   orb(0)%vec(6) = delta_e
-  call closed_orbit_calc (ring2, orb, 4)
+  call closed_orbit_calc (ring2, orb, 4, exit_on_error = exit_on_error)
+  if (.not. bmad_status%ok) return
   call ring_make_mat6 (ring2, -1, orb)
 
   call twiss_at_start (ring2)
+  if (.not. bmad_status%ok) return
   call twiss_propagate_all (ring2)
   high_tune_x = ring2%ele_(nt)%x%phi / twopi
   high_tune_y = ring2%ele_(nt)%y%phi / twopi
