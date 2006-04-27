0776be748d192d12f84f5d2da5055469288c6555
David Sagan <dcs16@cornell.edu>
2006-04-27 07:07:48 +0000

    updated documentation.

--- code/save_bunch_track.f90
+++ code/save_bunch_track.f90
@@ -1,3 +1,19 @@
+!+
+! Subroutine save_bunch_track (bunch, ele, s_travel)
+!
+! Routine is called by track1_bunch_csr to allow recoding of bunch statistics.
+! This file contains a dummy routine that does nothing. That is, to be useful,
+! a program must be linked with a custom save_bunch_track routine.
+! 
+! Modules needed:
+!   use beam_def_struct
+!
+! Input:
+!   bunch    -- bunch_struct: A bunch of particles.
+!   ele      -- ele_struct: The lattice element the bunch is going through.
+!   s_travel -- Real(rp): Longitudinal distance from beginning of element.
+!-
+
 subroutine save_bunch_track (bunch, ele, s_travel)
 
   use beam_def_struct, only: bunch_struct, ele_struct, rp
