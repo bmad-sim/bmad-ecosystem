ebf41aed34ec405bb1519a8ddbca7370de9b3441
David Sagan <dcs16@cornell.edu>
2006-06-10 18:59:16 +0000

    split calculation into two routines.

--- code/ring_geometry.f90
+++ code/ring_geometry.f90
@@ -1,17 +1,18 @@
 !+
-! Subroutine ring_geometry (ring)
+! Subroutine ring_geometry (lat)
 !
-! Subroutine to calculate the physical placement of all the elements in a ring.
+! Subroutine to calculate the physical placement of all the elements in a lattice.
 ! That is, the layout on the floor. This is the same as the MAD convention.
 !
 ! Modules Needed:
 !   use bmad
 !
 ! Input:
-!   ring -- ring_struct: The ring
+!   lat -- ring_struct: The lattice.
+!     %ele_(0)%floor  -- Floor_position_struct: The starting point for the calculations.
 !
 ! Output:
-!   ring
+!   lat -- Ring_struct: The lattice.
 !     %ele_(i)%floor --  floor_position_struct: Floor position.
 !       %x                -- X position at end of element
 !       %y                -- Y position at end of element
@@ -19,208 +20,257 @@
 !       %theta            -- Orientation angle at end of element in X-Z plane
 !       %phi              -- Elevation angle.
 !       %psi              -- Roll angle.
-!
-! Note: The starting point is taken to be the position in RING%ELE_(0)
 !-
 
 #include "CESR_platform.inc"
 
-subroutine ring_geometry (ring)
+subroutine ring_geometry (lat)
 
-  use bmad_struct
-  use bmad_interface, except => ring_geometry
-  use multipole_mod
+use bmad_struct
+use bmad_interface, except => ring_geometry
 
-  implicit none
+implicit none
 
-  type (ring_struct), target :: ring
-  type (ele_struct), pointer :: ele
+type (ring_struct), target :: lat
+type (ele_struct), pointer :: ele
 
-  integer i, key
+integer i, ix2, ie
 
-  real(rp) knl(0:n_pole_maxx), tilt(0:n_pole_maxx)
-  real(dp) chord_len, angle, leng, old_theta, rho
-  real(dp) s_ang, c_ang, s_the, c_the, s_phi, c_phi, s_psi, c_psi
-  real(dp) pos(3), theta, phi, psi, tlt
-  real(dp) w_mat(3,3), s_mat(3,3), r_mat(3), t_mat(3,3)
-  real(dp) :: twopi_dp = 2 * 3.14159265358979
+!
+
+do i = 1, lat%n_ele_use
+  call ele_geometry (lat%ele_(i-1), lat%ele_(i), lat%param)
+enddo
+
+! put info in superimpose lords
+
+do i = lat%n_ele_use+1, lat%n_ele_max  
+  ele => lat%ele_(i)
+  if (ele%key == super_lord$) then
+    ix2 = ele%ix2_slave
+    ie = lat%control_(ix2)%ix_slave
+    ele%floor = lat%ele_(ie)%floor
+  endif
+enddo
+
+end subroutine
+
+!----------------------------------------------------------------------------
+!----------------------------------------------------------------------------
+!----------------------------------------------------------------------------
+!+
+! Subroutine ele_geometry (ele0, ele, param)
+!
+! Subroutine to calculate the physical (floor) placement of an element given the
+! placement of the preceeding element. This is the same as the MAD convention.
+!
+! Modules Needed:
+!   use bmad
+!
+! Input:
+!   ele0   -- Ele_struct: The preceeding element
+!   param  -- Param_struct: lattice parameters
+!     %particle -- particle type. Needed if there are multipoles.
+!
+! Output:
+!   ele%floor --  floor_position_struct: Floor position.
+!        %x                -- X position at end of element
+!        %y                -- Y position at end of element
+!        %z                -- Z position at end of element
+!        %theta            -- Orientation angle at end of element in X-Z plane
+!        %phi              -- Elevation angle.
+!        %psi              -- Roll angle.
+!-
+
+#include "CESR_platform.inc"
+
+subroutine ele_geometry (ele0, ele, param)
+
+use bmad_struct
+use bmad_interface
+use multipole_mod
+
+implicit none
+
+type (ele_struct) ele0, ele
+type (param_struct) param
+
+integer i, key
+
+real(rp) knl(0:n_pole_maxx), tilt(0:n_pole_maxx)
+real(dp) chord_len, angle, leng, rho
+real(dp) pos(3), theta, phi, psi, tlt
+real(dp), save :: old_theta = 100  ! garbage number
+real(dp), save :: s_ang, c_ang, s_the, c_the, s_phi, c_phi, s_psi, c_psi
+real(dp), save :: w_mat(3,3), s_mat(3,3), r_mat(3), t_mat(3,3)
+real(dp), parameter :: twopi_dp = 2 * 3.14159265358979
 
 ! init
 ! old_theta is used to tell if we have to reconstruct the w_mat
 
-  pos(1) = ring%ele_(0)%floor%x
-  pos(2) = ring%ele_(0)%floor%y
-  pos(3) = ring%ele_(0)%floor%z
-  theta = ring%ele_(0)%floor%theta
-  phi   = ring%ele_(0)%floor%phi
-  psi   = ring%ele_(0)%floor%psi
+pos(1) = ele0%floor%x
+pos(2) = ele0%floor%y
+pos(3) = ele0%floor%z
+theta = ele0%floor%theta
+phi   = ele0%floor%phi
+psi   = ele0%floor%psi
 
-  old_theta = 100  ! garbage number
+knl  = 0   ! initialize
+tilt = 0  
 
-!
+leng = ele%value(l$)
+key = ele%key
+if (key == sbend$ .and. (leng == 0 .or. ele%value(g$) == 0)) key = drift$
 
-  knl  = 0   ! initialize
-  tilt = 0  
-
-  do i = 1, ring%n_ele_use
-
-    ele => ring%ele_(i)
-    leng = ele%value(l$)
-    key = ele%key
-    if (key == sbend$ .and. (leng == 0 .or. ele%value(g$) == 0)) key = drift$
-
-    if (key == ab_multipole$ .or. key == multipole$) then
-      call multipole_ele_to_kt (ele, ring%param%particle, knl, tilt, .true.)
-      key = multipole$
-    endif
+if (key == ab_multipole$ .or. key == multipole$) then
+  call multipole_ele_to_kt (ele, param%particle, knl, tilt, .true.)
+  key = multipole$
+endif
 
 ! General case where layout is not in the horizontal plane
 
-    if (phi /= 0 .or. psi /= 0 .or. key == patch$ .or. &
-               (key == multipole$ .and. knl(0) /= 0 .and. tilt(0) /= 0) .or. &
-               (key == sbend$ .and. ele%value(tilt_tot$) /= 0)) then
+if (phi /= 0 .or. psi /= 0 .or. key == patch$ .or. &
+             (key == multipole$ .and. knl(0) /= 0 .and. tilt(0) /= 0) .or. &
+             (key == sbend$ .and. ele%value(tilt_tot$) /= 0)) then
 
-      if (old_theta /= theta) then
-        s_the = sin(theta); c_the = cos(theta)
-        s_phi = sin(phi);   c_phi = cos(phi)
-        s_psi = sin(psi);   c_psi = cos(psi)
-        w_mat(1,1) =  c_the * c_psi - s_the * s_phi * s_psi
-        w_mat(1,2) = -c_the * s_psi - s_the * s_phi * c_psi
-        w_mat(1,3) =  s_the * c_phi
-        w_mat(2,1) =  c_phi * s_psi
-        w_mat(2,2) =  c_phi * c_psi
-        w_mat(2,3) =  s_phi 
-        w_mat(3,1) = -s_the * c_psi - c_the * s_phi * s_psi
-        w_mat(3,2) =  s_the * s_psi - c_the * s_phi * c_psi 
-        w_mat(3,3) =  c_the * c_phi
-      endif
+  if (old_theta /= theta) then
+    s_the = sin(theta); c_the = cos(theta)
+    s_phi = sin(phi);   c_phi = cos(phi)
+    s_psi = sin(psi);   c_psi = cos(psi)
+    w_mat(1,1) =  c_the * c_psi - s_the * s_phi * s_psi
+    w_mat(1,2) = -c_the * s_psi - s_the * s_phi * c_psi
+    w_mat(1,3) =  s_the * c_phi
+    w_mat(2,1) =  c_phi * s_psi
+    w_mat(2,2) =  c_phi * c_psi
+    w_mat(2,3) =  s_phi 
+    w_mat(3,1) = -s_the * c_psi - c_the * s_phi * s_psi
+    w_mat(3,2) =  s_the * s_psi - c_the * s_phi * c_psi 
+    w_mat(3,3) =  c_the * c_phi
+  endif
 
 !
 
-      select case (key)
+  select case (key)
 
 ! sbend and multipole
 
-      case (sbend$, multipole$)
-        if (key == sbend$) then
-          angle = leng * dble(ele%value(g$))
-          tlt = ele%value(tilt_tot$)
-          rho = 1.0_dp / ele%value(g$)
-          s_ang = sin(angle); c_ang = cos(angle)
-          r_mat = (/ rho * (c_ang - 1), 0.0_dp, rho * s_ang /)
-        else
-          angle = knl(0)
-          tlt = tilt(0)
-          s_ang = sin(angle); c_ang = cos(angle)
-          r_mat = 0
-        endif
+  case (sbend$, multipole$)
+    if (key == sbend$) then
+      angle = leng * dble(ele%value(g$))
+      tlt = ele%value(tilt_tot$)
+      rho = 1.0_dp / ele%value(g$)
+      s_ang = sin(angle); c_ang = cos(angle)
+      r_mat = (/ rho * (c_ang - 1), 0.0_dp, rho * s_ang /)
+    else
+      angle = knl(0)
+      tlt = tilt(0)
+      s_ang = sin(angle); c_ang = cos(angle)
+      r_mat = 0
+    endif
 
-        s_mat(1,:) = (/ c_ang,  0.0_dp, -s_ang /)
-        s_mat(2,:) = (/ 0.0_dp, 1.0_dp,  0.0_dp /)
-        s_mat(3,:) = (/ s_ang,  0.0_dp,  c_ang /) 
+    s_mat(1,:) = (/ c_ang,  0.0_dp, -s_ang /)
+    s_mat(2,:) = (/ 0.0_dp, 1.0_dp,  0.0_dp /)
+    s_mat(3,:) = (/ s_ang,  0.0_dp,  c_ang /) 
 
-        if (tlt /= 0) then
-          s_ang = sin(tlt); c_ang = cos(tlt)
-          t_mat(1,:) = (/ c_ang,  -s_ang,  0.0_dp /)
-          t_mat(2,:) = (/ s_ang,   c_ang,  0.0_dp /)
-          t_mat(3,:) = (/ 0.0_dp,  0.0_dp, 1.0_dp /)
+    if (tlt /= 0) then
+      s_ang = sin(tlt); c_ang = cos(tlt)
+      t_mat(1,:) = (/ c_ang,  -s_ang,  0.0_dp /)
+      t_mat(2,:) = (/ s_ang,   c_ang,  0.0_dp /)
+      t_mat(3,:) = (/ 0.0_dp,  0.0_dp, 1.0_dp /)
 
-          r_mat = matmul (t_mat, r_mat)
+      r_mat = matmul (t_mat, r_mat)
 
-          s_mat = matmul (t_mat, s_mat)
-          t_mat(1,2) = -t_mat(1,2); t_mat(2,1) = -t_mat(2,1) ! form inverse
-          s_mat = matmul (s_mat, t_mat)
-        endif
+      s_mat = matmul (t_mat, s_mat)
+      t_mat(1,2) = -t_mat(1,2); t_mat(2,1) = -t_mat(2,1) ! form inverse
+      s_mat = matmul (s_mat, t_mat)
+    endif
 
-        pos = pos + matmul(w_mat, r_mat)
-        w_mat = matmul (w_mat, s_mat)
+    pos = pos + matmul(w_mat, r_mat)
+    w_mat = matmul (w_mat, s_mat)
 
 ! patch
 
-      case (patch$)
+  case (patch$)
 
-        angle = ele%value(x_pitch_tot$)            ! x_pitch is negative MAD yrot
-        if (angle /= 0) then
-          s_ang = sin(angle); c_ang = cos(angle)
-          s_mat(1,:) = (/  c_ang,  0.0_dp, s_ang /)
-          s_mat(2,:) = (/  0.0_dp, 1.0_dp, 0.0_dp /)
-          s_mat(3,:) = (/ -s_ang,  0.0_dp, c_ang /) 
-          w_mat = matmul(w_mat, s_mat)
-        endif
+    angle = ele%value(x_pitch_tot$)            ! x_pitch is negative MAD yrot
+    if (angle /= 0) then
+      s_ang = sin(angle); c_ang = cos(angle)
+      s_mat(1,:) = (/  c_ang,  0.0_dp, s_ang /)
+      s_mat(2,:) = (/  0.0_dp, 1.0_dp, 0.0_dp /)
+      s_mat(3,:) = (/ -s_ang,  0.0_dp, c_ang /) 
+      w_mat = matmul(w_mat, s_mat)
+    endif
      
-        angle = ele%value(y_pitch_tot$)           ! 
-        if (angle /= 0) then
-          s_ang = sin(angle); c_ang = cos(angle)
-          s_mat(1,:) = (/ 1.0_dp,  0.0_dp, 0.0_dp /)
-          s_mat(2,:) = (/ 0.0_dp,  c_ang,  s_ang /)
-          s_mat(3,:) = (/ 0.0_dp, -s_ang,  c_ang /) 
-          w_mat = matmul(w_mat, s_mat)
-        endif
+    angle = ele%value(y_pitch_tot$)           ! 
+    if (angle /= 0) then
+      s_ang = sin(angle); c_ang = cos(angle)
+      s_mat(1,:) = (/ 1.0_dp,  0.0_dp, 0.0_dp /)
+      s_mat(2,:) = (/ 0.0_dp,  c_ang,  s_ang /)
+      s_mat(3,:) = (/ 0.0_dp, -s_ang,  c_ang /) 
+      w_mat = matmul(w_mat, s_mat)
+    endif
 
-        angle = ele%value(tilt$)
-        if (angle /= 0) then
-          s_ang = sin(angle); c_ang = cos(angle)
-          s_mat(1,:) = (/ c_ang,  -s_ang,  0.0_dp /)
-          s_mat(2,:) = (/ s_ang,   c_ang,  0.0_dp /)
-          s_mat(3,:) = (/ 0.0_dp,  0.0_dp, 1.0_dp /) 
-          w_mat = matmul(w_mat, s_mat)
-        endif
+    angle = ele%value(tilt$)
+    if (angle /= 0) then
+      s_ang = sin(angle); c_ang = cos(angle)
+      s_mat(1,:) = (/ c_ang,  -s_ang,  0.0_dp /)
+      s_mat(2,:) = (/ s_ang,   c_ang,  0.0_dp /)
+      s_mat(3,:) = (/ 0.0_dp,  0.0_dp, 1.0_dp /) 
+      w_mat = matmul(w_mat, s_mat)
+    endif
 
-        r_mat = (/ ele%value(x_offset$), ele%value(y_offset$), &
+    r_mat = (/ ele%value(x_offset$), ele%value(y_offset$), &
                                                     ele%value(z_offset$) /)
-        pos = pos + matmul(w_mat, r_mat)
+    pos = pos + matmul(w_mat, r_mat)
 
 ! everything else. Just a translation
 
-      case default
-        pos = pos + w_mat(:,3) * leng
+  case default
+    pos = pos + w_mat(:,3) * leng
 
-      end select
+  end select
 
 ! if there has been a rotation calculate new theta, phi, and psi
 
-      if (key == sbend$ .or. key == patch$ .or. key == multipole$) then
-        theta = atan2 (w_mat(1,3), w_mat(3,3))
-        theta = theta - twopi_dp * &
-                       nint((theta - ring%ele_(i-1)%floor%theta) / twopi_dp)
-        phi = atan2 (w_mat(2,3), sqrt(w_mat(1,3)**2 + w_mat(3,3)**2))
-        psi = atan2 (w_mat(2,1), w_mat(2,2))
-      endif
+  if (key == sbend$ .or. key == patch$ .or. key == multipole$) then
+    theta = atan2 (w_mat(1,3), w_mat(3,3))
+    theta = theta - twopi_dp * &
+                       nint((theta - ele0%floor%theta) / twopi_dp)
+    phi = atan2 (w_mat(2,3), sqrt(w_mat(1,3)**2 + w_mat(3,3)**2))
+    psi = atan2 (w_mat(2,1), w_mat(2,2))
+  endif
 
-      old_theta = theta
+  old_theta = theta
 
 ! Simple case where the local reference frame stays in the horizontal plane.
 
-    else
+else
 
-      select case (key)
-      case (sbend$)
-        angle = leng * dble(ele%value(g$))
-        chord_len = 2 * leng * sin(angle/2) / angle
-      case (multipole$)
-        angle = knl(0)
-        chord_len = 0
-      case default
-        angle = 0
-        chord_len = leng
-      end select
+  select case (key)
+  case (sbend$)
+    angle = leng * dble(ele%value(g$))
+    chord_len = 2 * leng * sin(angle/2) / angle
+  case (multipole$)
+    angle = knl(0)
+    chord_len = 0
+  case default
+    angle = 0
+    chord_len = leng
+  end select
 
-      theta = theta - angle / 2
-      pos(1) = pos(1) + chord_len * sin(theta)
-      pos(3) = pos(3) + chord_len * cos(theta)
-      theta = theta - angle / 2
+  theta = theta - angle / 2
+  pos(1) = pos(1) + chord_len * sin(theta)
+  pos(3) = pos(3) + chord_len * cos(theta)
+  theta = theta - angle / 2
 
-    endif
+endif
 
 !
 
-    ring%ele_(i)%floor%x = pos(1)
-    ring%ele_(i)%floor%y = pos(2)
-    ring%ele_(i)%floor%z = pos(3)
-    ring%ele_(i)%floor%theta = theta
-    ring%ele_(i)%floor%phi   = phi
-    ring%ele_(i)%floor%psi   = psi
-
-  enddo
+ele%floor%x = pos(1)
+ele%floor%y = pos(2)
+ele%floor%z = pos(3)
+ele%floor%theta = theta
+ele%floor%phi   = phi
 
 end subroutine
