e0629a99b8336b7ce9522f60a0498fcc9aec1158
David Sagan <dcs16@cornell.edu>
2007-04-03 17:30:26 +0000

    more...

--- guide/2pdf.pl
+++ guide/2pdf.pl
@@ -15,16 +15,16 @@ if (! $found) {die ("Revision line not found in: cover_page.tex\n");}
 
 
 `latex bmad-manual`; 
-`dvips -o bmad-manual.ps -P generic bmad-manual`; 
+`dvips -o bmad-manual.ps bmad-manual`; 
 `ps2pdf bmad-manual.ps`;
 `rm /home/dcs/public_html/bmad/bmad-manual-*pdf`;
 `rm /home/dcs/public_html/bmad/bmad-manual-*ps`;
-`mv bmad-manual.pdf /home/dcs/public_html/bmad/bmad-manual-$rev.pdf`; 
+`scp bmad-manual.pdf dcs@lnx209:/home/dcs/public_html/bmad/bmad-manual-$rev.pdf`; 
 `dvips -o bmad-manual.ps bmad-manual`;
-`mv bmad-manual.ps /home/dcs/public_html/bmad/bmad-manual-$rev.ps`; 
+`scp bmad-manual.ps dcs@lnx209:/home/dcs/public_html/bmad/bmad-manual-$rev.ps`; 
 
 open (F_OUT, ">temp.out") || die ("Cannot open temparary file\n");
-$file = "/home/dcs/public_html/bmad/manual.html";
+$file = "dcs@lnx209:/home/dcs/public_html/bmad/manual.html";
 open (FM, $file) || die ("Cannot open File: $file\n");
 while (<FM>) {
   if (/bmad-manual-(\S+?)\.p+/) {s/$1/$rev/g;}
