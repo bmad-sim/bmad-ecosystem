9420814de625efa347a358cd39ea05c3cf767d6d
Richard Helms  <helms@cornell.edu>
2007-12-03 19:16:29 +0000

    Added documentation to utility routines.

--- ring_ma/resources/q.sh
+++ ring_ma/resources/q.sh
@@ -1,8 +1,14 @@
+# Script for running jobs on the grid.
+
 #$ -S /bin/csh
 #$ -l arch=lx24-x86
+
+# Specify here where the log files go
 #$ -o /nfs/acc/user/helms/ctf/jobs/qlog
 #$ -e /nfs/acc/user/helms/ctf/jobs/qlog
+
 echo "Input file       : " $inputfile
-#cd /nfs/cesr/acc/helms/ctf/jobs/2006-07-27
 cd $workingdir
+
+# Change this to point to your local executable.
 /nfs/acc/user/helms/ctf/bin/ring_ma $inputfile
