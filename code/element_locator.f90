5cf5db490dae7aca99727d6bdb5f8f9bde050811
David Sagan <dcs16@cornell.edu>
2007-12-21 22:51:13 +0000

    added ability to locate based on s-position.

--- code/element_locator.f90
+++ code/element_locator.f90
@@ -1,14 +1,20 @@
 !+
 ! Subroutine element_locator (ele_name, lat, ix_ele)
 !
-! Subroutine to locate an element in a lattice. 
+! Subroutine to locate an element in a lattice. If multipole elements
+! match then ix_ele will point to the first one. Also see:
+!   elements_locator
+! The element name may be of the form: "S:<number>". This will match
+! to the element whose longitudinal position at the exit end is
+! closest to <number>.
 !
 ! Modules Needed:
 !   use bmad
 !
 ! Input:
 !   ele_name -- Character(40): Name of the element to find.
-!   lat     -- lat_struct: Lattice to search through.
+!                 The name is case insensitive
+!   lat      -- lat_struct: Lattice to search through.
 !
 ! Output:
 !   ix_ele -- Integer: Index of element in lat%ele(:) array. 
@@ -19,21 +25,39 @@
 
 subroutine element_locator (ele_name, lat, ix_ele)
 
-  use bmad_struct
-  use bmad_interface, except_dummy => element_locator
+use bmad_struct
+use bmad_interface, except_dummy => element_locator
   
-  implicit none
+implicit none
 
-  type (lat_struct) lat
-  integer ix_ele
-  character(*) ele_name
+type (lat_struct) lat
+integer ix_ele, ios
+real(rp) s
+character(*) ele_name
+character(40) e_name
 
-! Very simple.
+! Init
 
-  do ix_ele = 0, lat%n_ele_max
-    if (lat%ele(ix_ele)%name == ele_name) return
-  enddo
+ix_ele = -1
+call str_upcase (e_name, ele_name)
 
-  ix_ele = -1
+! Longitudinal position search
+
+if (e_name(1:2) == 'S:') then
+  read (e_name(3:), *, iostat = ios) s
+  if (ios /= 0) return
+  call ele_at_s (lat, s, ix_ele)
+  ! See if previous element is closer
+  if (ix_ele > 0) then
+    if (s - lat%ele(ix_ele-1)%s < lat%ele(ix_ele)%s - s) ix_ele = ix_ele - 1
+  endif
+  return
+endif
+
+! Name search
+
+do ix_ele = 0, lat%n_ele_max
+  if (lat%ele(ix_ele)%name == e_name) return
+enddo
 
 end subroutine
