bbb258e1c4541c19c356aff96bdc01c2eaa76a60
David Sagan <dcs16@cornell.edu>
2008-01-26 04:07:02 +0000

    corrected file.

--- doc/2pdf.pl
+++ doc/2pdf.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+`scp lnx209:/home/dcs/public_html/bmad/tao.html .`;
+
 $found = 0;
 open (FC, "cover-page.tex") || die ("Cannot open File: cover-page.tex\n");
 while (<FC>) {
@@ -16,20 +18,24 @@ if (! $found) {die ("Revision line not found in: cover_page.tex\n");}
 `latex tao`;
 `dvips -P pdf -o tao.ps -z tao`; 
 `ps2pdf tao.ps`;
-`mv tao.pdf tao_manual-$rev.pdf`; 
+`mv tao.pdf tao-manual-$rev.pdf`; 
 `dvips -o tao.ps tao`;
-`mv tao.ps tao_manual-$rev.ps`; 
-`cp tao_manual-*.p*  /home/dcs/public_html/bmad`;
+`mv tao.ps tao-manual-$rev.ps`; 
+`cp tao-manual-*.p*  /home/dcs/public_html/bmad`;
 
 open (F_OUT, ">temp.out") || die ("Cannot open temparary file\n");
-$file = "/home/dcs/public_html/bmad/tao.html";
+$file = "tao.html";
 open (FM, $file) || die ("Cannot open File: $file\n");
 while (<FM>) {
-  if (/tao_manual-(\S+?)\.p+/) {s/$1/$rev/g;}
+  if (/tao-manual-(\S+?)\.p+/) {s/$1/$rev/g;}
   print (F_OUT);
 }
 
 close (FM);
 close (F_OUT);
 `mv temp.out $file`;
+
+`scp tao.html       lnx209:/home/dcs/public_html/bmad`;
+`scp tao-manual-*.pdf lnx209:/home/dcs/public_html/bmad`;
+`scp tao-manual-*.ps  lnx209:/home/dcs/public_html/bmad`;
 
