5080c007574bc16bb9a57a14d0c63919c93a2001
David Sagan <dcs16@cornell.edu>
2008-03-10 17:54:29 +0000

    0 now chooses current lattice.

--- cesr/cesr_basic_mod.f90
+++ cesr/cesr_basic_mod.f90
@@ -702,7 +702,7 @@ subroutine choose_cesr_lattice (lattice, lat_file, current_lat, lat, choice)
       enddo
   
       print *, ' [Note: To be in this list a lattice file must have a name   ]'
-      print *, ' [      of the form: U:[CESR.BMAD.LAT]bmad_<lattice_name>.lat]'
+      print *, ' [      of the form: BMAD_LAT:bmad_<lattice_name>.lat]'
 
       print *
       print *, 'You can enter a Lattice number or a full file name.'
@@ -717,7 +717,7 @@ subroutine choose_cesr_lattice (lattice, lat_file, current_lat, lat, choice)
     call string_trim (line, line, ix)
     line = line(:ix)
 
-    if (ix == 0 .or. (ix == 1 .and. line == '*')) then
+    if (ix == 0 .or. (ix == 1 .and. (line == '*' .or. line == '0')) then
       default_flag = .true.
       do i_lat = 1, num_lats
         if (lat_list(i_lat) == current_lat) exit
