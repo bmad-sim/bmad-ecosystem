60cb7e2eba4bae44cf9e9c143fcb323b9d6bb451
David Sagan <dcs16@cornell.edu>
2008-10-02 01:46:38 +0000

    Replaced n_seg guess with an exact calculation.

--- synrad/break_wall_into_segments.f90
+++ synrad/break_wall_into_segments.f90
@@ -11,13 +11,24 @@ integer i_seg, ip, n_seg, i, n
 
 real(rp) seg_len_max, wall_len, rr
 
-! if the distance between two wall points is too large then break the interval
+! If the distance between two wall points is too large then break the interval
 ! into more than 1 segment.
 ! x_seg and s_seg are the (x,s) coords of the center of the segment
 
-n = wall%pt(wall%n_pt_tot)%s / seg_len_max + wall%n_pt_tot
+! First count the number of segments needed and allocate
+
+n = 0
+do ip = 1, wall%n_pt_tot
+  wall_len = sqrt((wall%pt(ip)%x - wall%pt(ip-1)%x)**2 + &
+                             (wall%pt(ip)%s - wall%pt(ip-1)%s)**2)
+  n_seg = 1 + wall_len / seg_len_max
+  n = n + n_seg
+enddo
+
 if (allocated(wall%seg)) deallocate(wall%seg)
 allocate (wall%seg(n))
+
+! Now fill in the information.
 
 i_seg = 0
 do ip = 1, wall%n_pt_tot
