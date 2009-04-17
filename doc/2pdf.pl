ada477b0c825fcfb0d74c95199b4ad2b0537be4b
David Sagan <dcs16@cornell.edu>
2009-04-17 19:21:48 +0000

    Added date to html file.

--- doc/2pdf.pl
+++ doc/2pdf.pl
@@ -9,12 +9,16 @@ while (<FC>) {
     $rev = $1;
     print "Revision: $rev\n";
     $found = 1;
+    $date_line = <FC>;
+    $date_line =~ /^ *(\S.+\S) +\\\\/;
+    $date = $1;
+    print "Date: $date\n";
+    last;
   }
 }
 
 close (FC);
 if (! $found) {die ("Revision line not found in: cover_page.tex\n");}
-
 
 `latex bmad`; 
 `dvips -o bmad.ps -z bmad`; 
@@ -25,17 +29,17 @@ if (! $found) {die ("Revision line not found in: cover_page.tex\n");}
 `dvips -o bmad.ps bmad`;
 `mv bmad.ps bmad-manual-$rev.ps`; 
 
-open (F_OUT, ">temp.out") || die ("Cannot open temparary file\n");
-$file = "manual.html";
-open (FM, $file) || die ("Cannot open File: $file\n");
-while (<FM>) {
-  if (/bmad-manual-(\S+?)\.p+/) {s/$1/$rev/g;}
+open (F_OUT, ">manual.html") || die ("Cannot open manual.html file\n");
+$file = "manual_template.html";
+open (F_IN, $file) || die ("Cannot open File: $file\n");
+while (<F_IN>) {
+  s/RRR/$rev/g;
+  s/DDD/$date/g;
   print (F_OUT);
 }
 
-close (FM);
+close (F_IN);
 close (F_OUT);
-`mv temp.out $file`;
 
 `scp manual.html       dcs\@lnx209.lns.cornell.edu:/home/dcs/public_html/bmad`;
 `scp bmad-manual-*.pdf dcs\@lnx209.lns.cornell.edu:/home/dcs/public_html/bmad`;
