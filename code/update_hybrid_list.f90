94f69aa92d12ebca63eb0a4bb6e42a5b687fd14d
David Sagan <dcs16@cornell.edu>
2009-05-23 13:32:40 +0000

    fix bug with keep_overlays_and_groups logic.

--- code/update_hybrid_list.f90
+++ code/update_hybrid_list.f90
@@ -59,7 +59,7 @@ do i = lat%ele(n_in)%ic1_lord, lat%ele(n_in)%ic2_lord
   if (keep_ele(ix)) cycle  ! If already set then don't need to do it again
   if (.not. logic_option(.true., keep_overlays_and_groups) .and. &
                     (lat%ele(ix)%lord_status == overlay_lord$ .or. &
-                    lat%ele(ix)%lord_status == overlay_lord$)) cycle
+                    lat%ele(ix)%lord_status == group_lord$)) cycle
 
   call update_hybrid_list (lat, ix, keep_ele)
 enddo
