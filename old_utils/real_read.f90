c862bc98f966d9549f86b8c3d87c68f60c8dcffa
David Sagan <dcs16@cornell.edu>
2009-07-28 12:42:53 +0000

    fix for gfortran compiler complaint.

--- old_utils/real_read.f90
+++ old_utils/real_read.f90
@@ -10,25 +10,7 @@
 !     real(rp) = real_read('ERROR: ')
 !-
 
-!$Id$
-!$Log$
-!Revision 1.5  2003/07/09 01:29:31  dcs
-!new bmad
-!
-!Revision 1.4  2003/04/30 16:26:42  dcs
-!F90 Standard conforming update.
-!
-!Revision 1.3  2002/02/23 20:34:51  dcs
-!Modified for Single/Double Real Toggle
-!
-!Revision 1.2  2001/09/27 17:47:11  rwh24
-!UNIX compatibility updates
-!
-
-#include "CESR_platform.inc"
-
-
-  real(rp) function real_read(error_message)
+function real_read(error_message) result (r_read)
 
 
   use precision_def
@@ -37,17 +19,17 @@
 
   integer idelim
 
-  real(rp) reltyp
+  real(rp) reltyp, r_read
 
   logical lwait
 
-  character*(*) error_message
+  character(*) error_message
 
 !
 
   lwait = .false.
 
-  real_read = reltyp(lwait, idelim)
+  r_read = reltyp(lwait, idelim)
 
   if (idelim > 0) then
     print *
@@ -58,4 +40,4 @@
 
   return
 
-  end
+end function
