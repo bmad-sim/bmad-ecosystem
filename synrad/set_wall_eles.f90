c1e74f1647ddac98e2eae5a22459df5c51b930d7
David Sagan <dcs16@cornell.edu>
2009-08-14 13:30:27 +0000

    Modified set_wall_eles to take a wall_struct.

--- synrad/set_wall_eles.f90
+++ synrad/set_wall_eles.f90
@@ -1,35 +1,35 @@
-subroutine set_wall_eles (walls, lat)
+!+
+! Subroutine set_wall_eles (wall, lat)
+!
+! Routine to find the lattice element and twiss parameters at each wall segment.
+!-
 
-  use synrad_struct
-  use synrad_interface
+subroutine set_wall_eles (wall, lat)
 
-  implicit none
+use synrad_struct
+use synrad_interface
 
-  type (walls_struct), target :: walls
-  type (wall_struct), pointer :: wall
-  type (lat_struct) :: lat
-  type (ele_struct) :: ele
+implicit none
 
-  integer i, is, ix_ele
+type (wall_struct) :: wall
+type (lat_struct) :: lat
+type (ele_struct) :: ele
 
-  !
-  do i = 1,2
-    if (i == 1) wall => walls%positive_x_wall
-    if (i == 2) wall => walls%negative_x_wall
-    do is = 1, wall%n_seg_tot
+integer i, is, ix_ele
 
-      ! find the element at the s midpoint of the wall segment
-      call ele_at_s (lat, wall%seg(is)%s_mid, ix_ele)
-      wall%seg(is)%ix_ele = ix_ele
+!
 
-      ! Get the betas at the s midpoint
-      call twiss_and_track_at_s (lat, wall%seg(is)%s_mid, ele)
-      wall%seg(is)%a = ele%a
-      wall%seg(is)%b = ele%b
-      
-    end do
+do is = 1, wall%n_seg_tot
 
-  end do
+  ! find the element at the s midpoint of the wall segment
+  call ele_at_s (lat, wall%seg(is)%s_mid, ix_ele)
+  wall%seg(is)%ix_ele = ix_ele
 
+  ! Get the betas at the s midpoint
+  call twiss_and_track_at_s (lat, wall%seg(is)%s_mid, ele)
+  wall%seg(is)%a = ele%a
+  wall%seg(is)%b = ele%b
+  
+end do
 
 end subroutine set_wall_eles
