6a460940d9b416cc918aab765c4dcadba1de470a
David Sagan <dcs16@cornell.edu>
2009-12-02 02:40:20 +0000

    Fixed branch problem.

--- code/s_calc.f90
+++ code/s_calc.f90
@@ -23,7 +23,7 @@ use lat_ele_loc_mod
 implicit none
 
 type (lat_struct), target :: lat
-type (ele_struct), pointer :: ele, lord, ele0, ele1
+type (ele_struct), pointer :: ele, lord, slave
 type (branch_struct), pointer :: branch
 
 integer i, j, n, ic, icon, ix2, nt
@@ -39,7 +39,7 @@ do i = 0, ubound(lat%branch, 1)
   do n = 1, nt
     ele => branch%ele(n)
     ss = ss + ele%value(l$)
-    lat%ele(n)%s = ss
+    branch%ele(n)%s = ss
   enddo
 enddo
 
@@ -50,12 +50,13 @@ lat%param%total_length = ss - lat%ele(0)%s
 ! We need to preserve the s value of this element.
 
 do n = lat%n_ele_track+1, lat%n_ele_max
-  if (lat%ele(n)%key == null_ele$) cycle
-  if (lat%ele(n)%lord_status == super_lord$) then
-    ix2 = lat%control(lat%ele(n)%ix2_slave)%ix_slave
-    lat%ele(n)%s = lat%ele(ix2)%s
+  lord => lat%ele(n)
+  if (lord%key == null_ele$) cycle
+  if (lord%lord_status == super_lord$) then
+    slave => pointer_to_slave (lat, lord, lord%n_slave)
+    lord%s = slave%s
   else
-    lat%ele(n)%s = 0
+    lord%s = 0
   endif
 enddo
 
