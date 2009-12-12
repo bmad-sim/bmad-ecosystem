d9a1e29e7e0ce287ba9704936aa8f47b36dce79f
David Sagan <dcs16@cornell.edu>
2009-12-12 00:19:12 +0000

    better doc.

--- synrad3d/synrad3d_struct.f90
+++ synrad3d/synrad3d_struct.f90
@@ -5,20 +5,21 @@ use bmad_interface
 
 type photon3d_coord_struct
   real(rp) vec(6)             ! Position: (x, vx/c, y, vy/c, z, vz/c)
-  real(rp) energy             ! eV
+  real(rp) energy             ! In eV
   real(rp) track_len          ! length of the track from the start
   integer ix_ele              ! index of element we are in.
   integer ix_wall             ! Index to wall segment
 end type
 
 type photon3d_track_struct
-  type (photon3d_coord_struct) start, old, now  ! coords
-  type (photon3d_coord_struct), allocatable :: reflect(:)
+  type (photon3d_coord_struct) start, old, now  ! coords:
+  type (photon3d_coord_struct), allocatable :: reflect(:) ! Records coords at points where 
+                                                          !   the photon reflects off of the wall.
   real(rp) intensity          ! Intensity of this macro-photon in Photons/(beam_particle*turn)
-  logical :: crossed_lat_end = .false.     ! photon crossed through the lattice beginning or end?
-  logical :: hit_antechamber = .false.
-  integer ix_photon           ! Photon index.
-  integer n_reflect
+  logical :: crossed_lat_end = .false.     ! Photon crossed through the lattice beginning or end?
+  logical :: hit_antechamber = .false.     
+  integer ix_photon                        ! Photon index.
+  integer n_reflect                        ! Number of reflections
 end type
 
 !--------------
@@ -26,26 +27,27 @@ end type
 ! The wall between point i-1 and i is associated with 
 
 type wall3d_pt_struct
-  real(rp) s           ! Longitudinal position.
-  character(16) type   ! elliptical or rectangular
-  real(rp) width2      ! half width
-  real(rp) height2     ! half height
-  real(rp) antechamber_plus_x_height2
-  real(rp) antechamber_minus_x_height2
+  real(rp) s                            ! Longitudinal position.
+  character(16) type                    ! Elliptical or rectangular.
+  real(rp) width2                       ! Half width.
+  real(rp) height2                      ! Half height.
+  real(rp) antechamber_plus_x_height2   ! Anechamber half height on +x side of the wall
+  real(rp) antechamber_minus_x_height2  ! Anechamber half height on -x side of the wall
 end type
+
 
 type wall3d_struct
   type (wall3d_pt_struct), allocatable :: pt(:)
   integer n_pt_max
 end type
 
-!
+! Some parameters that can be set. 
 
 type synrad3d_params_struct
-  real(rp) :: ds_track_step_max = 3
-  real(rp) :: dr_track_step_max = 0.1
-  logical :: allow_reflections = .true.
-  logical :: debug = .false.
+  real(rp) :: ds_track_step_max = 3     ! Maximum longitudinal distance in one photon "step".
+  real(rp) :: dr_track_step_max = 0.1   ! Maximum tranverse distance in one photon "step".
+  logical :: allow_reflections = .true. ! If False, terminate tracking when photon hits the wall.
+  logical :: debug = .false.            
 end type
 
 type (synrad3d_params_struct), save :: synrad3d_params
