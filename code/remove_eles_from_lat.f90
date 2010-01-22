4500e3b13f92335b53b702db20388096cca24381
David Sagan <dcs16@cornell.edu>
2010-01-22 21:21:22 +0000

    Can now handle branches.

--- code/remove_eles_from_lat.f90
+++ code/remove_eles_from_lat.f90
@@ -26,15 +26,26 @@ implicit none
                          
 type (lat_struct), target :: lat
 type (ele_struct), pointer :: ele
+type (branch_struct), pointer :: branch
+type (control_struct), pointer :: ctl
 
-integer i, j, ix, i1, i2
-integer, allocatable :: ixa(:), ic(:), control(:)
+type ele_index_temp
+  integer, allocatable :: ix_new(:)  ! ix_new(old_ele_index) = new_ele_index
+end type
+type (ele_index_temp), allocatable :: ibr(:)
+
+integer i, j, ib, ix, i1, i2
+integer, allocatable :: ic(:), control(:)
 
 logical, optional :: check_controls
 
 ! Allocate
 
-allocate (ixa(lat%n_ele_max))           ! ixa(old_ele_index) = new_ele_index
+allocate (ibr(0:ubound(lat%branch, 1)) )
+do i = 0, ubound(lat%branch, 1)
+  allocate (ibr(i)%ix_new(lat%branch(i)%n_ele_max))
+enddo
+
 allocate (control(lat%n_control_max))
 allocate (ic(lat%n_ic_max))
 
@@ -44,41 +55,43 @@ ic = 0
 ! Mark entries in control and ic arrays for deletion.
 
 do i = 1, lat%n_control_max
-  if (lat%ele(lat%control(i)%ix_slave)%key == -1) control(i) = -1
-  if (lat%ele(lat%control(i)%ix_lord)%key == -1) control(i) = -1
+  ctl => lat%control(i)
+  if (lat%branch(ctl%ix_branch)%ele(ctl%ix_slave)%key == -1) control(i) = -1
+  if (lat%ele(ctl%ix_lord)%key == -1) control(i) = -1
 enddo
 
 do i = 1, lat%n_ic_max
   if (control(lat%ic(i)) == -1) ic(i) = -1
 enddo
 
-! Compress lat%ele(:) array and fill in ixa(:) array.
+! Compress lat%ele(:) array and fill in ibr(:) array.
 
-i2 = 0
-do i = 1, lat%n_ele_max
-  ele => lat%ele(i)
-  if (ele%key == -1) then
-    ixa(i) = -1
-  else
-    i2 = i2 + 1
-    ixa(i) = i2
-    if (i2 /= i) lat%ele(i2) = ele
-  endif
-  if (i == lat%n_ele_track) then
-     lat%n_ele_track = i2
-  endif
+do ib = 0, ubound(lat%branch, 1)
+  branch => lat%branch(ib)
+  if (ib > 0) branch%ix_from_ele = ibr(branch%ix_from_branch)%ix_new(branch%ix_from_ele)
+
+  i2 = 0
+  do i = 1, branch%n_ele_max
+    ele => branch%ele(i)
+    if (ele%key == -1) then
+      ibr(ib)%ix_new(i) = -1
+    else
+      i2 = i2 + 1
+      ibr(ib)%ix_new(i) = i2
+      if (i2 /= i) branch%ele(i2) = ele
+    endif
+    if (i == branch%n_ele_track) then
+       branch%n_ele_track = i2
+    endif
+  enddo
+
+  do i = i2+1, branch%n_ele_max
+    call init_ele(branch%ele(i), ix_ele = i)
+  enddo
+
+  branch%n_ele_max = i2
+
 enddo
-
-if (i2 == lat%n_ele_max) then
-  deallocate (ixa, control, ic)
-  return
-endif
-
-do i = i2+1, lat%n_ele_max
-  call init_ele(lat%ele(i), ix_ele = i)
-enddo
-
-lat%n_ele_max = i2
 
 ! Compress lat%control() array and correct %ix_lord and %ix_slave pointers.
 
@@ -87,28 +100,13 @@ do i = 1, lat%n_control_max
   if (control(i) == -1) cycle
   i2 = i2 + 1
   control(i) = i2
-  lat%control(i2) = lat%control(i)
-  lat%control(i2)%ix_lord = ixa(lat%control(i)%ix_lord)
-  lat%control(i2)%ix_slave = ixa(lat%control(i)%ix_slave)
+  ctl => lat%control(i)
+  lat%control(i2) = ctl
+  lat%control(i2)%ix_lord  = ibr(ctl%ix_branch)%ix_new(ctl%ix_lord)
+  lat%control(i2)%ix_slave = ibr(ctl%ix_branch)%ix_new(ctl%ix_slave)
 enddo
 
 lat%n_control_max = i2
-
-! Correct ele%ix1_slave, etc.
-
-do i = 1, lat%n_ele_max
-  ele => lat%ele(i)
-  if (ele%ix1_slave < 1) cycle
-  i1 = ele%ix1_slave; i2 = ele%ix2_slave
-  ele%ix1_slave = 0  ! Assume no slaves
-  ele%ix2_slave = -1
-  ele%n_slave = 0
-  do j = i1, i2
-    if (control(j) /= -1 .and. ele%ix1_slave == 0) ele%ix1_slave = control(j)
-    if (control(j) /= -1) ele%ix2_slave = control(j)
-  enddo
-  ele%n_slave = ele%ix2_slave - ele%ix1_slave + 1
-enddo
 
 ! Compress lat%ic() array
 
@@ -122,27 +120,57 @@ enddo
 
 lat%n_ic_max = i2
 
-! Correct ele%ic1_lord, etc.
+! Correct slave info.
 
-do i = 1, lat%n_ele_max
-  ele => lat%ele(i)
-  if (ele%ic1_lord < 1) cycle
+do ib = 0, ubound(lat%branch, 1)
+  branch => lat%branch(ib)
 
-  i1 = ele%ic1_lord; i2 = ele%ic2_lord
-  ele%ic1_lord = 0  ! Assume no lords
-  ele%ic2_lord = -1
-  ele%n_lord = 0
-  do j = i1, i2
-    if (ic(j) /= -1 .and. ele%ic1_lord == 0) ele%ic1_lord = ic(j)
-    if (ic(j) /= -1) ele%ic2_lord = ic(j)
+  do i = 1, branch%n_ele_max
+    ele => branch%ele(i)
+    i1 = ele%ix1_slave; i2 = ele%ix2_slave
+    if (i1 < 1) cycle
+    if (control(i1) == i1 .and. control(i2) == i2) cycle
+
+    ele%ix1_slave = 0  ! Start with no slaves
+    ele%ix2_slave = -1
+    ele%n_slave = 0
+    do j = i1, i2
+      if (control(j) /= -1 .and. ele%ix1_slave == 0) ele%ix1_slave = control(j)
+      if (control(j) /= -1) ele%ix2_slave = control(j)
+    enddo
+    ele%n_slave = ele%ix2_slave - ele%ix1_slave + 1
   enddo
-  ele%n_lord = ele%ic2_lord - ele%ic1_lord + 1
-  if (ele%slave_status == super_slave$ .and. ele%n_lord == 0) ele%slave_status = free$
+
+enddo
+
+! Correct lord info.
+
+do ib = 0, ubound(lat%branch, 1)
+  branch => lat%branch(ib)
+
+  do i = 1, branch%n_ele_max
+    ele => branch%ele(i)
+
+    i1 = ele%ic1_lord; i2 = ele%ic2_lord
+    if (i1 < 1) cycle
+    if (ic(i1) == i1 .and. ic(i2) == i2) cycle
+
+    ele%ic1_lord = 0  ! Start with no lords
+    ele%ic2_lord = -1
+    ele%n_lord = 0
+    do j = i1, i2
+      if (ic(j) /= -1 .and. ele%ic1_lord == 0) ele%ic1_lord = ic(j)
+      if (ic(j) /= -1) ele%ic2_lord = ic(j)
+    enddo
+    ele%n_lord = ele%ic2_lord - ele%ic1_lord + 1
+    if (ele%slave_status == super_slave$ .and. ele%n_lord == 0) ele%slave_status = free$
+  enddo
+
 enddo
 
 ! deallocate and do a check
 
-deallocate (ixa, control, ic)
+deallocate (ibr, control, ic)
 
 if (logic_option(.true., check_controls)) call check_lat_controls (lat, .true.)
 
