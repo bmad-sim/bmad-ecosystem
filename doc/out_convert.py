c816e20d936fb0f3b626e94ec10237a3ebf421a5
David Sagan <dcs16@cornell.edu>
2010-01-26 22:29:51 +0000

    more doc.

--- doc/out_convert.py
+++ doc/out_convert.py
@@ -2,21 +2,19 @@
 
 import sys, os
 
-file_in = open('subroutines.tex')
-file_out = open('out.tex', mode = 'w')
-
 for arg in sys.argv[1:]:
   print arg
   file_in = open(arg)
-  file_out = open(arg + 'x', mode = 'w')
+##  file_out = open(arg + 'x', mode = 'w')
 
   for line in file_in.readlines():
-    ixh = line.find(r']{routine!')
-    if ixh != -1:
-      line = line[0:ixh+2] + line[ixh+10:]
+    ix1 = line.find('hyperref[')
+    if ix1 != -1:
+      ix2 = line.find(']', ix1)
+      line = line[0:ix1] + 'Hyperref{' + line[ix1+9:ix2] + '}' + line[ix2+1:]
       print line
-    file_out.write(line)
+##    file_out.write(line)
     
   file_in.close()
-  file_out.close()
-  os.rename(arg + 'x', arg)
+##  file_out.close()
+##  os.rename(arg + 'x', arg)
