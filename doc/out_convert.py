47afbaf09af0218079315d4d1e9d2d951b1b9a7a
David Sagan <dcs16@cornell.edu>
2010-01-27 00:24:39 +0000

    New Hyperref command.

--- doc/out_convert.py
+++ doc/out_convert.py
@@ -5,16 +5,19 @@ import sys, os
 for arg in sys.argv[1:]:
   print arg
   file_in = open(arg)
-##  file_out = open(arg + 'x', mode = 'w')
+  file_out = open(arg + 'x', mode = 'w')
 
   for line in file_in.readlines():
-    ix1 = line.find('hyperref[')
-    if ix1 != -1:
+
+    while True:
+      ix1 = line.find('hyperref[')
+      if ix1 == -1: break
       ix2 = line.find(']', ix1)
       line = line[0:ix1] + 'Hyperref{' + line[ix1+9:ix2] + '}' + line[ix2+1:]
-      print line
-##    file_out.write(line)
+      ## print line
+
+    file_out.write(line)
     
   file_in.close()
-##  file_out.close()
-##  os.rename(arg + 'x', arg)
+  file_out.close()
+  os.rename(arg + 'x', arg)
