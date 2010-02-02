ec80a750dd0d31f5f054f415ca826f4763931073
James Crittenden <jac243@cornell.edu>
2010-02-02 22:06:30 +0000

    Reformat additional output files

--- synrad/synrad_write_power_mod.f90
+++ synrad/synrad_write_power_mod.f90
@@ -152,15 +152,23 @@ subroutine write_header (iu, file, gen_params, synrad_mode)
   write (iu, *) 'I_beam    =', gen_params%i_beam,    ' ! Amps/beam'
   write (iu, *) 'Input eps_y =', 1.e6*gen_params%epsilon_y, ' ! mm-mrad'
 
+  write (iu, *) 'Positron eps_x =', 1.e6*synrad_mode%pos_mode%a%emittance, ' ! mm-mrad'
+  write (iu, *) 'Positron eps_y =', 1.e6*synrad_mode%pos_mode%b%emittance, ' ! mm-mrad'
+  write (iu, *) 'Positron sig_z =', synrad_mode%pos_mode%sig_z, ' ! m'
+  write (iu, *) 'Positron sigE/E =',1.e2*synrad_mode%pos_mode%sigE_E, ' ! %'
+
   write (iu, *) 'Electron eps_x =', 1.e6*synrad_mode%ele_mode%a%emittance, ' ! mm-mrad'
   write (iu, *) 'Electron eps_y =', 1.e6*synrad_mode%ele_mode%b%emittance, ' ! mm-mrad'
   write (iu, *) 'Electron sig_z =', synrad_mode%ele_mode%sig_z, ' ! m'
   write (iu, *) 'Electron sigE/E =',1.e2*synrad_mode%ele_mode%sigE_E, ' ! %'
 
+  write (iu, *)
+  write (iu, *) 'Attribute values are G (bends), k1 (quads), k2 (sextupoles), ks (solenoids), B_max (wigglers)'
+
   write (iu, '(3(/,2x,a))') &
-'          Segment                                  ', &
-'  Ix  Name          S_seg      X_seg     P/len      P/Area     P_tot     Phot/sec      A Beta    B Beta    A Eta     Ele Type       Relevant               Ele',&
-'                     (m)        (m)      (W/m)     (W/mm^2)      (W)      (1/s)         (m)        (m)      (m)      at s_mid       Attribute              Name'
+'Segment                                  ', &
+'  Nr      S_seg     L_seg      X_seg     P/len      P/Area     P_tot     Phot/sec      A Beta    B Beta    A Eta     Ele Type     Attribute       Element       Source          Primary        S_source    Nr ',&
+'           (m)       (m)        (m)      (W/m)     (W/mm^2)      (W)      (1/s)         (m)        (m)      (m)      at s_mid       Value          Name        Ele Type         Source           (m)     Sources'
 
 end subroutine
 
@@ -180,11 +188,11 @@ subroutine write_results (wall, lat, gen_params, use_ele_ix, synrad_mode)
   type (lat_struct) lat
 
   integer use_ele_ix, i
-  character*16 seg_name, ep_source_name, e_source_name, p_source_name
-  character fmt*100, ep_name*2
+  character*16 seg_name, ep_source_name, ep_source_key_name
+  character fmt*130, ep_name*2
   character file1*50, ele_num*6
   character ele_at_seg*16, attrib*10
-  integer key
+  integer key, ep_source_key
   real(rp) value
 
 !
@@ -196,7 +204,7 @@ subroutine write_results (wall, lat, gen_params, use_ele_ix, synrad_mode)
     write (ele_num, '(i6.6)') use_ele_ix
     file1 = trim(file1) // '_' // trim(ele_num)
   endif
-  file1 = trim(file1) // '.dat'
+  file1 = trim(file1) // '.txt'
   call downcase_string (file1)
   call write_header (1, file1, gen_params, synrad_mode)
 
@@ -234,23 +242,28 @@ subroutine write_results (wall, lat, gen_params, use_ele_ix, synrad_mode)
     ep => seg%power
 
     if (ep%ix_ele_source == 0) then
-      ep_source_name = '--------'
+      ep_source_name = '------------------'
       ep_name = '--'
+      ep_source_key = 0
+      ep_source_key_name = '----------'
     else
       ep_source_name = lat%ele(ep%ix_ele_source)%name
+      ep_source_key = lat%ele(ep%ix_ele_source)%key
+      ep_source_key_name = key_name(ep_source_key)
     endif
 
     seg_name = wall%pt(seg%ix_pt)%name
     call str_substitute (seg_name, ' ', '_', .true.)
 
-    fmt = '(i6, 1x, a10, f10.4, 2es11.3, 3es12.4, 3f10.3, 2x, a16, 1x, a10, es12.4, 1x, a)'
+    fmt = '(i6, 1x, f10.4, f10.4, 2es11.3, 3es12.4, 3f10.3, 2x, a10, 1x, es12.4, 1x, a18, 1x, a10, 1x, a18, f11.4, i6)'
     write (1, fmt) &
-              i, seg_name, seg%s, seg%x, &
+              i, seg%s, seg%len, seg%x, &
               ep%power_per_len, &
               1.e-6 * (ep%power_per_area), &
               ep%power_tot, ep%photons_per_sec, &
               seg%a%beta, seg%b%beta,seg%a%eta, key_name(key), &
-              attrib, value, trim(lat%ele(seg%ix_ele)%name)
+              value, trim(lat%ele(seg%ix_ele)%name), &
+              ep_source_key_name, trim(ep_source_name), ep%s_source, ep%n_source
 
 !    fmt = '(f10.4, 2es11.3)'
 !    write (1, fmt) &
