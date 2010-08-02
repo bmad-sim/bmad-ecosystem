5a6f1d11eacf4b51b1ce1ad991d53d9600837c9b
David Sagan <dcs16@cornell.edu>
2010-08-02 18:52:00 +0000

    more crystal code development.

--- code/crystal_attribute_bookkeeper.f90
+++ code/crystal_attribute_bookkeeper.f90
@@ -9,6 +9,7 @@ type (ele_struct) ele
 real(rp) lambda, gamma, delta1, lambda_in, d, alpha, psi, theta0
 real(rp) cos_theta0, sin_theta0, graze_angle_in, ang_tot
 real(rp) h_x, h_y, h_z, nx_out, ny_out, nz_out, nxx_out, nyy_out, nzz_out
+real(rp) cos_graze_in, sin_graze_in
 
 ! If the photon energy or the bragg angle has not been set then cannot do the calc yet.
 
@@ -34,17 +35,19 @@ theta0 = asin(lambda_in / (2 * sqrt(d**2 - h_y**2))) - atan(h_z/h_x)
 cos_theta0 = cos(theta0)
 sin_theta0 = sin(theta0)
 
-graze_angle_in = acos(cos_theta0/delta1)
+cos_graze_in = cos_theta0/delta1
+graze_angle_in = acos(cos_graze_in)
 ele%value(graze_angle_in$) = graze_angle_in
+sin_graze_in = sin(graze_angle_in)
 
 ny_out =  lambda * h_y
 nz_out = lambda * h_z + cos_theta0 / delta1
 nx_out = -sqrt(1 - ny_out**2 - nz_out**2)
 
 
-nxx_out = nz_out * sin(graze_angle_in) - nx_out * cos_theta0/delta1
+nxx_out = nz_out * sin_graze_in - nx_out * cos_graze_in
 nyy_out = ny_out
-nzz_out = nz_out * cos(graze_angle_in) + nx_out * sin_theta0/delta1
+nzz_out = nz_out * cos_graze_in + nx_out * sin_graze_in
 
 ele%value(tilt_corr$) = atan2(nyy_out, nxx_out)
 
