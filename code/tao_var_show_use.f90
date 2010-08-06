c79f05be75f050138ce3e5219038201f90312ce1
David Sagan <dcs16@cornell.edu>
2010-08-06 13:37:26 +0000

    Put in code to prevent string overflow.

--- code/tao_var_show_use.f90
+++ code/tao_var_show_use.f90
@@ -17,11 +17,12 @@ implicit none
 type (tao_v1_var_struct), intent(in) :: v1_var
 
 character(17) :: r_name = "tao_var_show_use"
-character(200) line
+character(400) line
 
 ! find which variables to use
 
 call location_encode (line, v1_var%v%useit_opt, v1_var%v%exists, lbound(v1_var%v,1))
+if (len_trim(line) > len(line) - 60) line = line(1:len(line)-70) // ' ...etc...'
 write (line, '(2x, 2a, i0, a, i0, a, t50, 2a)') trim(v1_var%name), &
                       '[', lbound(v1_var%v, 1), ':', ubound(v1_var%v, 1), ']', &
                       'Using: ' // trim(line)
