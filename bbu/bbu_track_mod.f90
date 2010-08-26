96b4486d6cbefc18b4579baee97492c140ec8bc7
James Crittenden <jac243@cornell.edu>
2010-08-26 13:56:34 +0000

    Clarify log output. Add NCOUNT to SORBIT.DATA

--- bbu/bbu_track_mod.f90
+++ bbu/bbu_track_mod.f90
@@ -254,14 +254,15 @@ do
         if (hom_power_gain < 1/bbu_param%limit_factor) exit
         if (hom_power_gain > bbu_param%limit_factor) exit      
       else
-        write(57,'(2i10,e13.6,1x,e15.6)')irep,n_period,hom_power_sum,hom_power_gain
+        write(57,'(2i10,e13.6,x,i8,x,e15.6)')irep,n_period,hom_power_sum,n_count,hom_power_gain
       endif
     endif
 
-    write(*,'(a,i3,a,e15.6,5x,a,e15.6/,a,e13.6,5x,a,e13.6)')&
+    write(*,'(a,i3,a,e15.6,5x,a,e15.6/,a,e13.6/,a,i8,a/,a,e13.6)')&
          ' Period ', n_period,'   Time: ',bbu_beam%time_now, &
          ' Beam current(A): ',beam_init%bunch_charge / beam_init%dt_bunch, &
-         ' Sum of max HOM powers: ', hom_power_sum, &
+         ' Sum of max HOM wake amplitudes (V): ', hom_power_sum, &
+         '   over ', n_count, ' bunch passages'&
          ' HOM power gain factor: ', hom_power_gain
 
     call track_all (lat, orbit)
