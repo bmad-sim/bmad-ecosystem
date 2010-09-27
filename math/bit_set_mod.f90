ae81426ed56fe0bb4af1cce18e4ce4741fa420d2
David Sagan <dcs16@cornell.edu>
2010-09-27 17:17:59 +0000

    Clened up code.

--- math/bit_set_mod.f90
+++ math/bit_set_mod.f90
@@ -6,7 +6,7 @@ contains
 !------------------------------------------------------------------------------
 !------------------------------------------------------------------------------
 !+
-! Subroutine bit_set (word, pos, set_to)
+! Subroutine bit_set (word, pos, set_to_1)
 !
 ! Routine to set a bit in a word.
 !
@@ -14,26 +14,26 @@ contains
 !   use bit_mod
 !
 ! Input:
-!   word   -- Integer: Input word
-!   pos    -- Integer: position to set.
-!   set_to -- Logical: If True then bit is set to 1. If False bit is set to 0.
+!   word     -- Integer: Input word
+!   pos      -- Integer: position to set.
+!   set_to_1 -- Logical: If True then bit is set to 1. If False bit is set to 0.
 !
 ! Output:
 !   word   -- Integer: Word with bit set.
 !-
 
-subroutine bit_set (word, pos, set_to)
+subroutine bit_set (word, pos, set_to_1)
 
 implicit none
 
 integer word, pos, result
-logical set_to
+logical set_to_1
 
 !
 
-if (set_to) then
+if (set_to_1) then
   result = ibset(word, pos)
-elseif (set_to == 1) then
+else
   result = ibclr(word, pos)
 endif
 
