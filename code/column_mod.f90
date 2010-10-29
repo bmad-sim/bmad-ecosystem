545f9648f77c7b31b3f4a8823b30da181d0a541d
David Sagan <dcs16@cornell.edu>
2010-10-29 19:40:29 +0000

    change to suppress the ranlib "has no symbols" message

--- code/column_mod.f90
+++ code/column_mod.f90
@@ -1,5 +1,9 @@
 module column_mod
-   type column_struct 
-    real row(15)
-   end type
+
+type column_struct 
+  real row(15)
+end type
+
+integer, private :: private_dummy ! This is to suppress the ranlib "has no symbols" message
+
 end module
