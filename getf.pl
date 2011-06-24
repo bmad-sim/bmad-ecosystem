0f514b1b2069e5c287fb9650fbfd1fabd639028b
David Sagan <dcs16@cornell.edu>
2011-06-24 02:13:50 +0000

    Better getf/listf searching.

--- getf.pl
+++ getf.pl
@@ -5,6 +5,18 @@
 use File::Find;
 use File::Spec::Functions;
 
+
+# this is so perl will find getf_common.pl in the same directory as getf.pl
+
+use FindBin ();
+use lib "$FindBin::Bin";
+
+# Now load getf_common.pl and call setup_dirs routine
+
+require "getf_common.pl";
+
+&setup_dirs;
+
 # Following two lines should be uncommented on VMS to check the master_list:
 
 #my @args = ("type_header", @ARGV);
@@ -12,196 +24,7 @@ use File::Spec::Functions;
 
 $found_one = 0;
 
-# The idea is to look for a local copy of the library to search.
-# We have found a local copy when we find one specific file that we know 
-# is in the library.
-# -r is a file test operator which checks if the file is can be read
-
-my $curdir = curdir();
-my $updir = updir();
-
-if (-r catfile( $curdir, "bmad", "modules", "bmad_struct.f90" )) {
-  $bmad_dir = catfile( $curdir, "bmad" );
-} elsif (-r catfile( $updir, "bmad", "modules", "bmad_struct.f90")) {
-  $bmad_dir = catfile( $updir, "bmad" );
-} elsif (-r catfile( $updir, $updir, "bmad", "modules", "bmad_struct.f90")) {
-  $bmad_dir = catfile( $updir, $updir, "bmad" );
-} elsif (-r catfile( $ENV{"ACC_SRC"}, "bmad", "modules", "bmad_struct.f90")) {
-  $bmad_dir = catfile( $ENV{"ACC_SRC"}, "bmad" );
-} else {
-  $bmad_dir = catfile( $ENV{"DIST_BASE_DIR"}, "bmad" );
-}
-
-if (-r catfile( $curdir, "cesr_utils", "modules", "cesr_utils.f90" )) {
-  $cesr_utils_dir = catfile( $curdir, "cesr_utils" );
-} elsif (-r catfile( $updir, "cesr_utils", "modules", "cesr_utils.f90")) {
-  $cesr_utils_dir = catfile( $updir, "cesr_utils" );
-} elsif (-r catfile( $updir, $updir, "cesr_utils", "modules", "cesr_utils.f90")) {
-  $cesr_utils_dir = catfile( $updir, $updir, "cesr_utils" );
-} elsif (-r catfile( $ENV{"ACC_SRC"}, "cesr_utils")) {
-  $cesr_utils_dir = catfile( $ENV{"ACC_SRC"}, "cesr_utils" );
-} else {
-  $cesr_utils_dir = catfile( $ENV{"DIST_BASE_DIR"}, "cesr_utils" );
-}
-
-
-if (-r catfile( $curdir, "sim_utils", "interfaces", "sim_utils.f90" )) {
-  $sim_utils_dir = catfile( $curdir, "sim_utils" );
-} elsif (-r catfile( $updir, "sim_utils", "interfaces", "sim_utils.f90")) {
-  $sim_utils_dir = catfile( $updir, "sim_utils" );
-} elsif (-r catfile( $updir, $updir, "sim_utils", "interfaces", "sim_utils.f90")) {
-  $sim_utils_dir = catfile( $updir, $updir, "sim_utils" );
-} elsif (-r catfile( $ENV{"ACC_SRC"}, "sim_utils")) {
-  $sim_utils_dir = catfile( $ENV{"ACC_SRC"}, "sim_utils" );
-} else {
-  $sim_utils_dir = catfile( $ENV{"DIST_BASE_DIR"}, "sim_utils" );
-}
-
-if (-r catfile( $curdir, "mpm_utils", "code", "butout.f90" )) {
-  $mpm_utils_dir = catfile( $curdir, "mpm_utils" );
-} elsif (-r catfile( $updir, "mpm_utils", "code", "butout.f90")) {
-  $mpm_utils_dir = catfile( $updir, "mpm_utils" );
-} elsif (-r catfile( $updir, $updir, "mpm_utils", "code", "butout.f90")) {
-  $mpm_utils_dir = catfile( $updir, $updir, "mpm_utils" );
-} elsif (-r catfile( $ENV{"ACC_SRC"}, "mpm_utils")) {
-  $mpm_utils_dir = catfile( $ENV{"ACC_SRC"}, "mpm_utils" );
-} else {
-  $mpm_utils_dir = catfile( $ENV{"DIST_BASE_DIR"}, "mpm_utils" );
-}
-
-
-if (-r catfile( $curdir, "recipes_f-90_LEPP", "lib_src", "nr.f90" )) {
-  $recipes_dir = catfile( $curdir, "recipes_f-90_LEPP" );
-} elsif (-r catfile( $updir, "recipes_f-90_LEPP", "lib_src", "nr.f90")) {
-  $recipes_dir = catfile( $updir, "recipes_f-90_LEPP" );
-} elsif (-r catfile( $updir, $updir, "recipes_f-90_LEPP", "lib_src", "nr.f90")) {
-  $recipes_dir = catfile( $updir, $updir, "recipes_f-90_LEPP" );
-} elsif (-r catfile( $ENV{"ACC_SRC"}, "recipes_f-90_LEPP")) {
-  $recipes_dir = catfile( $ENV{"ACC_SRC"}, "recipes_f-90_LEPP" );
-} else {
-  $recipes_dir = catfile( $ENV{"DIST_BASE_DIR"}, "recipes_f-90_LEPP" );
-}
-
-
-if (-r catfile( $curdir, "forest", "code", "i_tpsa.f90" )) {
-  $forest_dir = catfile( $curdir, "forest" );
-} elsif (-r catfile( $updir, "forest", "code", "i_tpsa.f90")) {
-  $forest_dir = catfile( $updir, "forest" );
-} elsif (-r catfile( $updir, $updir, "forest", "code", "i_tpsa.f90")) {
-  $forest_dir = catfile( $updir, $updir, "forest" );
-} elsif (-r catfile( $ENV{"ACC_SRC"}, "forest")) {
-  $forest_dir = catfile( $ENV{"ACC_SRC"}, "forest" );
-} else {
-  $forest_dir = catfile( $ENV{"DIST_BASE_DIR"}, "forest" );
-}
-
-if (-r catfile( $curdir, "tao", "code", "tao_struct.f90" )) {
-  $tao_dir = catfile( $curdir, "tao" );
-} elsif (-r catfile( $updir, "tao", "code", "tao_struct.f90")) {
-  $tao_dir = catfile( $updir, "tao" );
-} elsif (-r catfile( $updir, $updir, "tao", "code", "tao_struct.f90")) {
-  $tao_dir = catfile( $updir, $updir, "tao" );
-} elsif (-r catfile( $ENV{"ACC_SRC"}, "tao")) {
-  $tao_dir = catfile( $ENV{"ACC_SRC"}, "tao" );
-} else {
-  $tao_dir = catfile( $ENV{"DIST_BASE_DIR"}, "tao" );
-}
-
-if (-r catfile( $curdir, "bmadz", "modules", "bmadz_struct.f90" )) {
-  $bmadz_dir = catfile( $curdir, "bmadz" );
-} elsif (-r catfile( $updir, "bmadz", "modules", "bmadz_struct.f90")) {
-  $bmadz_dir = catfile( $updir, "bmadz" );
-} elsif (-r catfile( $updir, $updir, "bmadz", "modules", "bmadz_struct.f90")) {
-  $bmadz_dir = catfile( $updir, $updir, "bmadz" );
-} elsif (-r catfile( $ENV{"ACC_SRC"}, "bmadz")) {
-  $bmadz_dir = catfile( $ENV{"ACC_SRC"}, "bmadz" );
-} else {
-  $bmadz_dir = catfile( $ENV{"DIST_BASE_DIR"}, "bmadz" );
-}
-
-if (-r catfile( $curdir, "nonlin_bpm", "code", "nonlin_bpm_init.f90" )) {
-  $nonlin_bpm_dir = catfile( $curdir, "nonlin_bpm" );
-} elsif (-r catfile( $updir, "nonlin_bpm", "code", "nonlin_bpm_init.f90")) {
-  $nonlin_bpm_dir = catfile( $updir, "nonlin_bpm" );
-} elsif (-r catfile( $updir, $updir, "nonlin_bpm", "code", "nonlin_bpm_init.f90")) {
-  $nonlin_bpm_dir = catfile( $updir, $updir, "nonlin_bpm" );
-} elsif (-r catfile( $ENV{"ACC_SRC"}, "nonlin_bpm")) {
-  $nonlin_bpm_dir = catfile( $ENV{"ACC_SRC"}, "nonlin_bpm" );
-} else {
-  $nonlin_bpm_dir = catfile( $ENV{"DIST_BASE_DIR"}, "nonlin_bpm" );
-}
-
-# Look for arguments
-
-$extra = 0;
-$also_match_params = 1; 
-
-$_ = "@ARGV";  
-@field = split;
-
-for ($i = 0; $i <= @field; $i++) {
-
-  $_ = $field[$i];
-
-  if (! /^\-/) {last;}
-
-  if ($_ eq '-d') {
-    $extra = 1; 
-    $extra_dir = $field[$i+1];
-    $i = $i + 1;
-    next;
-  }
-
-  if ($_ eq '-p') {
-    $also_match_params = 1; 
-    next;
-  }
-
-  last;
-
-}
-
-#
- 
-$match_str = $field[$i];
-$match_str =~ s/\*/\\w\*/g;   # replace "*" by "\w*" (any word characters)
-
-$match_param_str = $match_str;
-if ($match_param_str =~ /\\w\*$/) {$match_param_str = $match_param_str . '\\$*';}
-if ($match_param_str =~ /\$$/) {$match_param_str =~ s/\$$/\\\$/;}
-
-# Help if needed
-
-if ($match_str eq "") {
-  print "Usage:\n";
-  print "  getf {options} <search_string>\n\n";
-  print "Options:\n";
-  print "   -d <dir>    # Search files in <dir> and sub-directories for matches.\n";
-  ## print "   -p          # Match to parameters as well.\n";
-  exit;
-}
-
-# Search for a match
-
-find(\&searchit, $bmad_dir);
-find(\&searchit, $sim_utils_dir);
-find(\&searchit, $cesr_utils_dir);
-find(\&searchit, $tao_dir);
-find(\&searchit, $mpm_utils_dir);
-find(\&searchit, $bmadz_dir);
-find(\&searchit, $nonlin_bpm_dir);
-if ($extra == 1) {find(\&searchit, $extra_dir);}
-## find(\&searchit, $recipes_dir);
-## find(\&searchit, $forest_dir);
-
-if ($found_one == 0) {
-  print "Cannot match string! $match_str\n";
-  ## if ($also_match_params == 1) {print "Also cannot match parameter string $match_param_str\n";}
-  print "Note: getf does not search numerical recipes, forest, nor any program directories.\n";
-  if ($also_match_params == 0) {print "Note: Use '-p' option to search for parameters as well as routines and structures.\n";}
-} else {
-  print "\n";
-}
+&search_it ("getf");
 
 #---------------------------------------------------------
 
