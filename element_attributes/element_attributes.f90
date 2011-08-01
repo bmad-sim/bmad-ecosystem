f68aac6df1100f9fd7a32b11921ff0bdc3be70e4
David Sagan <dcs16@cornell.edu>
2011-08-01 19:18:07 +0000

    Output to file.

--- element_attributes/element_attributes.f90
+++ element_attributes/element_attributes.f90
@@ -18,44 +18,48 @@ character(40) a_name
 
 ! indexes used for a given element key
 
+open (1, file = 'element_attributes.dat')
+
 n_used = 0
 
 do i = 1, n_key
   if (i == overlay_lord$) cycle
-  print *, '!---------------------------------'
-  print *, key_name(i)
+  write (1, *) '!---------------------------------'
+  write (1, *) key_name(i)
   ele%key = i
   do j = 1, n_attrib_special_maxx
     a_name = attribute_name (ele, j) 
     if (a_name == null_name$) cycle
-    print '(i10, 2x, a)', j, a_name
+    write (1, '(i10, 2x, a)') j, a_name
     n_used(j) = n_used(j) + 1
   enddo
-  print *
+  write (1, *)
 enddo
 
 ! number of elements using an index
 
-print *, '!---------------------------------'
-print *, 'Index usage:'
-print *, '   Ix Count'
+write (1, *) '!---------------------------------'
+write (1, *) 'Index usage:'
+write (1, *) '   Ix Count'
 do i = 1, size(n_used)
-  print '(2i6)', i, n_used(i)
+  write (1, '(2i6)') i, n_used(i)
 enddo
 
 ! List of elements using an index
 
 do i = 1, n_attrib_special_maxx
-  print *
-  print *, '!---------------------------------'
-  print '(a, i0)', 'Index: ', i
+  write (1, *)
+  write (1, *) '!---------------------------------'
+  write (1, '(a, i0)') 'Index: ', i
   do j = 1, n_key
     if (j == overlay_lord$) cycle
     ele%key = j
     a_name = attribute_name (ele, i) 
     if (a_name == null_name$) cycle
-    print *, '   ', key_name(ele%key)
+    write (1, *) '   ', key_name(ele%key)
   enddo
 enddo
+
+print *, 'Writen: element_attributes.dat'
 
 end program
