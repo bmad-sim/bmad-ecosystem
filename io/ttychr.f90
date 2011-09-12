6624318d90b1fbfc53cf904d341ee979e69d53a8
Stu Peck <sbp8@cornell.edu>
2011-09-12 13:27:37 +0000

    wrapper to get 1 char, non-blocking

---
+++ io/ttychr.f90
@@ -0,0 +1,11 @@
+         integer function ttychr()
+         use sim_utils    !for get_a_char
+         use input_mod
+         !wrapper to get 1 char, non-blocking  
+         implicit none
+         character*1 ic,ignore_this(2)
+         ignore_this(1)=' '
+         call get_a_char(ic,.false.,ignore_this)  !dont wait, ignore blank
+         ttychr=ichar(ic)
+         return
+         end
