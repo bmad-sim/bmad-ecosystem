e4bde5804f17fc909fb4d59fbb3ba26404eebbcf
Christopher Mayes <cem52@cornell.edu>
2011-09-20 21:08:15 +0000

    work on write_opal_field_grid_file

--- modules/opal_interface_mod.f90
+++ modules/opal_interface_mod.f90
@@ -53,11 +53,20 @@ character(40)	:: r_name = 'write_opal_lattice_file', name
 character(2) 	:: continue_char, eol_char, comment_char
 character(24)	:: rfmt
 character(4000)	:: line
-integer			:: iu,  ios, ix_match, ie, ix_start, ix_end
+integer			:: iu,  ios, ix_match, ie, ix_start, ix_end, iu_fieldgrid
 integer			:: n_names, n
 integer 		:: q_sign
 character(40), allocatable :: names(:)
 integer, allocatable :: an_indexx(:), name_occurrences(:)
+
+
+real(rp)        :: absmax_Ez
+character(40)   :: fieldgrid_output_name
+character(200), allocatable :: fieldgrid_names(:)
+integer			:: fieldgrid_n_names
+integer, allocatable :: fieldgrid_an_indexx(:), fieldgrid_name_occurrences(:)
+
+
 logical, optional :: err
 
 if (present(err)) err = .true.
@@ -108,6 +117,13 @@ n = ix_end - ix_start + 1
 allocate ( names(n), an_indexx(n), name_occurrences(n) )
 name_occurrences = 0
 n_names = 0
+
+!Initialize fieldgrid filename list
+n = ix_end - ix_start + 1
+allocate ( fieldgrid_names(n), fieldgrid_an_indexx(n), fieldgrid_name_occurrences(n) )
+fieldgrid_name_occurrences = 0
+fieldgrid_n_names = 0
+
 
 !-------------------------------------------
 ! Write info to the output file...
@@ -187,16 +203,57 @@ ele_loop: do ie = ix_start, ix_end
 	!----------------------------------------------------------
     case (lcavity$)
       !Check that there is a map or grid associated to make a decent field grid for OPAL
-	  if (.not. associated(ele%rf%field) ) then
-	    call out_io (s_error$, r_name, 'MISSING OPAL FIELD GRID CODE FOR: ' // key_name(ele%key), &
+	  if (.not. associated(ele%rf%field)  )then
+	    call out_io (s_error$, r_name, 'No rf_field_struct for: ' // key_name(ele%key), &
                                      '----')
+        call err_exit
+      endif
+	  if (.not. associated(ele%rf%field%mode(1)%grid)  )then
+	    call out_io (s_error$, r_name, 'No grid for: ' // key_name(ele%key), &
+                                     '----')
+        call err_exit
       endif
       
       write (line, '(a, es13.5)') trim(ele%name) // ': rfcavity, type = "standing", l =', val(l$)
-      call value_to_line (line, ele%s - val(L$), 'elemedge', rfmt, 'R', .false.)
-        
 
+      !Check field map file. If file has not been written, create a new file. 
+      call find_indexx (ele%rf%field%mode(1)%grid%file, fieldgrid_names, fieldgrid_an_indexx, fieldgrid_n_names, ix_match)
+      !Check for match with existing grid
+      if (ix_match > 0) then
+        !File exists. 
+          fieldgrid_name_occurrences(ix_match) = fieldgrid_name_occurrences(ix_match) + 1
+          fieldgrid_output_name = ''
+          write(fieldgrid_output_name, '(a, i0, a)') 'fieldgrid_', ix_match, '.t7'
+          !Get maximum field for "VOLT =" by calling write_opal_field_grid_file with 0 file unit number
+          call write_opal_field_grid_file (0, ele, lat%param, absmax_Ez)
+      else
+        !File does not exist.
         
+        !Add name to list  
+        call find_indexx (ele%rf%field%mode(1)%grid%file, fieldgrid_names, fieldgrid_an_indexx, fieldgrid_n_names, ix_match, add_to_list = .true.)
+        fieldgrid_n_names = fieldgrid_n_names + 1
+        
+        ! Write new fieldgrid file
+        fieldgrid_output_name = ''
+        write(fieldgrid_output_name, '(a, i0, a)') 'fieldgrid_', fieldgrid_n_names, '.t7'
+	    iu_fieldgrid = lunget()
+	    open (iu_fieldgrid, file = fieldgrid_output_name, iostat = ios)
+	    	    call write_opal_field_grid_file (iu_fieldgrid, ele, lat%param, absmax_Ez)
+	    close(iu_fieldgrid)
+	  end if
+	  !Add FMAPFN to line
+      write (line, '(4a)') trim(line),  ', fmapfn = "', trim(fieldgrid_output_name), '"'
+      
+      !Write field scaling in MV/m
+      call value_to_line (line, 1e-6*absmax_ez, 'volt', rfmt, 'R')
+      
+      !Write frequency in MHz
+      call value_to_line (line, 1e-6*ele%rf%field%mode(1)%freq, 'freq', rfmt, 'R')
+      
+      !Write ELEMEDGE
+      call value_to_line (line, ele%s - val(L$), 'elemedge', rfmt, 'R', .false.)
+      
+
 	!----------------------------------------------------------
 	!Default -----------------------------------
 	!----------------------------------------------------------
@@ -249,26 +306,26 @@ end subroutine write_opal_lattice_file
 !------------------------------------------------------------------------
 !------------------------------------------------------------------------
 !+ 
-! Subroutine write_opal_field_grid_file (opal_file_unit, ele, param, err)
+! Subroutine write_opal_field_grid_file (opal_file_unit, ele, param, maxfield, err)
 !
 ! Subroutine to write an OPAL lattice file using the information in
 ! a lat_struct. Optionally only part of the lattice can be generated.
 !
-! Modules needed:
-!   ?? use write_lat_file_mod
 !
 ! Input:
-!   opal_file_unit -- Integer: unit number to write to
+!   opal_file_unit -- Integer: unit number to write to, if > 0
+!                        if < 0, nothing is written, and only maxfield is returned
 !   ele            -- ele_struct: element to make map
 !   param          -- lat_param_struct: Contains lattice information
 !
-! Output:
-!   err    -- Logical, optional: Set True if, say a file could not be opened.
+! Output:          
+!   maxfield       -- Real(rp): absolute maximum found for element field scaling
+!   err            -- Logical, optional: Set True if, say a file could not be opened.
 !-
 
 
 
-subroutine write_opal_field_grid_file (opal_file_unit, ele, param, err)
+subroutine write_opal_field_grid_file (opal_file_unit, ele, param, maxfield, err)
 
 implicit none
 
@@ -277,15 +334,20 @@ integer			:: opal_file_unit
 integer         :: dimensions
 type (ele_struct) :: ele
 type (lat_param_struct) :: param
+real(rp)        :: maxfield
 logical, optional :: err
+
 character(40)	:: r_name = 'write_opal_field_grid_file'
 character(10)   ::  rfmt 
 
-!XXXXX
+
 type (coord_struct) :: orb
 type(em_field_struct) :: field_re, field_im
+type (em_field_point_struct), allocatable :: pt(:,:,:)
+type (em_field_point_struct) :: ref_field
 real(rp) :: x_step, z_step, x_min, x_max, z_min, z_max
-real(rp) :: freq, x, z, Ez, Ex, By
+real(rp) :: freq, x, z, phase_ref
+complex ::  phasor_rotation
 
 integer :: nx, nz, iz, ix
 
@@ -294,7 +356,6 @@ real(rp) :: Ex_factor, Ez_factor, By_factor
 logical loc_ref_frame
 
 !
-
 if (present(err)) err = .true.
 
 
@@ -307,7 +368,7 @@ select case (ele%key)
 
   case (lcavity$) 
                                          
-    freq = ele%value(rf_frequency$)
+    freq = ele%rf%field%mode(1)%freq
 
     !TODO: pass these parameters in somehow
     x_step = 0.001_rp
@@ -336,39 +397,71 @@ select case (ele%key)
   By_factor = -1e6_rp / (fourpi * 1e-7)
 
   !Write header
-  write (opal_file_unit, '(3a)') ' 2DDynamic XZ', '# Created from ele: ', trim(ele%name)
-  write (opal_file_unit, '(2'//rfmt//', i8, a)') 100*z_min, 100*nz*z_step, nz, '# z_min (cm), z_max (cm), n_z_points -1'
-  write (opal_file_unit, '('//rfmt//', a)') 1e-6 * freq, ' # frequency (MHz)'
-  write (opal_file_unit, '(2'//rfmt//', i8, a)') 100*x_min, 100*nx*x_step, nx, '# x_min (cm), x_max (cm), n_x_points -1'
+  write (opal_file_unit, '(3a)') ' 2DDynamic XZ', '  # Created from ele: ', trim(ele%name)
+  write (opal_file_unit, '(2'//rfmt//', i8, a)') 100*z_min, 100*nz*z_step, nz, '  # z_min (cm), z_max (cm), n_z_points -1'
+  write (opal_file_unit, '('//rfmt//', a)') 1e-6 * freq, '  # frequency (MHz)'
+  write (opal_file_unit, '(2'//rfmt//', i8, a)') 100*x_min, 100*nx*x_step, nx, '  # x_min (cm), x_max (cm), n_x_points -1'
 
+  !Allocate temporary pt array
+  allocate ( pt(0:nx, 0:nz, 1:1) )
   !Write data points
+  
+  !initialize maximum found field
+  maxfield = 0
+  
   do ix = 0, nx
     do iz = 0, nz
       x = x_step * ix
       z = z_step * iz 
       orb%vec(1) = x
+      orb%vec(3) = 0.0_rp
       
       !Calculate field at \omegat*t=0 and \omega*t = \pi/2 to get real and imaginary parts
-      call em_field_calc (ele, param, z, 0.0_rp   , orb, loc_ref_frame, field_re)
-      call em_field_calc (ele, param, z, 0.25/freq, orb, loc_ref_frame, field_im)
+      call em_field_calc (ele, param, z, 0.0_rp,        orb, loc_ref_frame, field_re)
+      call em_field_calc (ele, param, z, 0.25/freq , orb, loc_ref_frame, field_im)
+
+      pt(ix, iz, 1)%E(:) = cmplx(field_re%E(:), field_im%E(:))
+      pt(ix, iz, 1)%B(:) = cmplx(field_re%B(:), field_im%B(:))
       
-      !Get maximum field amplitudes
-      Ex =  sqrt(  field_re%E(1)**2 + field_im%E(1)**2 )
-      Ez =  sqrt(  field_re%E(3)**2 + field_im%E(3)**2 )
-      By =  sqrt(  field_re%E(2)**2 + field_im%E(2)**2 )
-      
-      !Output maximum fields
-      write (opal_file_unit, '(4'//rfmt//')') Ez_factor*sign(Ez,field_re%E(3)),  Ex_factor*sign(Ex, field_re%E(1)), &
-                                             0.0, By_factor*sign(By, field_im%B(2))
-    
+      !Update ref_field if larger Ez is found
+      if(abs(pt(ix, iz, 1)%E(3)) > maxfield) then
+         ref_field = pt(ix, iz, 1)
+         maxfield = abs(ref_field%E(3))
+      end if 
     end do
   end do
   
-
+  !Output maximum fields
+  maxfield = maxval(abs(pt(:,:,:)%E(3)))
+  
+  ! Write to file
+  if (opal_file_unit > 0 )  then
+  
+  !Calculate complex rotation number to rotate Ez onto the real axis
+  phase_ref = atan2( aimag(ref_field%E(3) ), real(ref_field%E(3) ) )
+  phasor_rotation = cmplx(cos(phase_ref), -sin(phase_ref))
+  
+  do ix = 0, nx
+    do iz = 0, nz
+      
+      write (opal_file_unit, '(4'//rfmt//')') Ez_factor * real ( pt(ix, iz, 1)%E(3) * phasor_rotation ), &
+                                              Ex_factor * real ( pt(ix, iz, 1)%E(1) * phasor_rotation ), &
+                                              0.0_rp, &
+                                              By_factor * aimag (  pt(ix, iz, 1)%B(2)*phasor_rotation )
+    enddo
+  enddo
+  
+  end if
+   
+   
+   deallocate(pt)
+   
   case default
   call out_io (s_error$, r_name, 'MISSING OPAL FIELD GRID CODE FOR: ' // key_name(ele%key), &
              '----')
   call err_exit
+  
+  
   
 end select 
 
