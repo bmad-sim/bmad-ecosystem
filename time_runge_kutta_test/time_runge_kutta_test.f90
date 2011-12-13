418b2b51cb871fc2a8b709dfeaaebedf31976211
Christopher Mayes <ChristopherMayes@github.com>
2011-12-13 21:47:14 +0000

    Changed REL to ABS in time_runge_kutta_test

--- time_runge_kutta_test/time_runge_kutta_test.f90
+++ time_runge_kutta_test/time_runge_kutta_test.f90
@@ -35,24 +35,24 @@ lat%ele%tracking_method = runge_kutta$
 call track1 (start_orb, lat%ele(ix_test), lat%param, end_orb2)
 
 open (1, file = 'output.now')
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(1)" REL  1E-10', end_orb1%vec(1)
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(2)" REL  1E-10', end_orb1%vec(2)
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(3)" REL  1E-10', end_orb1%vec(3)
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(4)" REL  1E-10', end_orb1%vec(4)
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(5)" REL  1E-10', end_orb1%vec(5)
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(6)" REL  1E-10', end_orb1%vec(6)
-write (1, '(a, es20.10)') '"time_runge_kutta:s     " REL  1E-10', end_orb1%s
-write (1, '(a, es20.10)') '"time_runge_kutta:t     " REL  1E-10', end_orb1%t
+write (1, '(a, es20.10)') '"time_runge_kutta:vec(1)" ABS  1E-10', end_orb1%vec(1)
+write (1, '(a, es20.10)') '"time_runge_kutta:vec(2)" ABS  1E-10', end_orb1%vec(2)
+write (1, '(a, es20.10)') '"time_runge_kutta:vec(3)" ABS  1E-10', end_orb1%vec(3)
+write (1, '(a, es20.10)') '"time_runge_kutta:vec(4)" ABS  1E-10', end_orb1%vec(4)
+write (1, '(a, es20.10)') '"time_runge_kutta:vec(5)" ABS  1E-10', end_orb1%vec(5)
+write (1, '(a, es20.10)') '"time_runge_kutta:vec(6)" ABS  1E-10', end_orb1%vec(6)
+write (1, '(a, es20.10)') '"time_runge_kutta:s     " ABS  1E-10', end_orb1%s
+write (1, '(a, es20.10)') '"time_runge_kutta:t     " ABS  1E-10', end_orb1%t
 
 write(1, *) 
-write (1, '(a, es20.10)') '"runge_kutta:vec(1)" REL  1E-10', end_orb2%vec(1)
-write (1, '(a, es20.10)') '"runge_kutta:vec(2)" REL  1E-10', end_orb2%vec(2)
-write (1, '(a, es20.10)') '"runge_kutta:vec(3)" REL  1E-10', end_orb2%vec(3)
-write (1, '(a, es20.10)') '"runge_kutta:vec(4)" REL  1E-10', end_orb2%vec(4)
-write (1, '(a, es20.10)') '"runge_kutta:vec(5)" REL  1E-10', end_orb2%vec(5)
-write (1, '(a, es20.10)') '"runge_kutta:vec(6)" REL  1E-10', end_orb2%vec(6)
-write (1, '(a, es20.10)') '"runge_kutta:s     " REL  1E-10', end_orb2%s
-write (1, '(a, es20.10)') '"runge_kutta:t     " REL  1E-10', end_orb2%t
+write (1, '(a, es20.10)') '"runge_kutta:vec(1)" ABS  1E-10', end_orb2%vec(1)
+write (1, '(a, es20.10)') '"runge_kutta:vec(2)" ABS  1E-10', end_orb2%vec(2)
+write (1, '(a, es20.10)') '"runge_kutta:vec(3)" ABS  1E-10', end_orb2%vec(3)
+write (1, '(a, es20.10)') '"runge_kutta:vec(4)" ABS  1E-10', end_orb2%vec(4)
+write (1, '(a, es20.10)') '"runge_kutta:vec(5)" ABS  1E-10', end_orb2%vec(5)
+write (1, '(a, es20.10)') '"runge_kutta:vec(6)" ABS  1E-10', end_orb2%vec(6)
+write (1, '(a, es20.10)') '"runge_kutta:s     " ABS  1E-10', end_orb2%s
+write (1, '(a, es20.10)') '"runge_kutta:t     " ABS  1E-10', end_orb2%t
 
 
 
