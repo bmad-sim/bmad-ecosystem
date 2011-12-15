6fbbd3c94d5793e90203ed4b85df4c7937395f81
Stu Peck <sbp8@cornell.edu>
2011-12-15 18:50:00 +0000

    Make stub of separate VMS ttychr version to allow  VMS links to work - reduced functionality for now.  mod to MAKEFILE.VMS , adding vms_only dir to build.

--- io/ttychr.f90
+++ io/ttychr.f90
@@ -4,8 +4,12 @@
          !wrapper to get 1 char, non-blocking  
          implicit none
          character*1 ic,ignore_this(2)
+#if defined (CESR_VMS) 
+          ttychr=vms_ttychr()
+#else
          ignore_this(1)=' '
          call get_a_char(ic,.false.,ignore_this)  !dont wait, ignore blank
          ttychr=ichar(ic)
+#endif
          return
          end
