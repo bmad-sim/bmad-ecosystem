7cc9dc4a50be58f1fb9cb32bef3a556d1c2b37f1
Christopher Mayes <ChristopherMayes@github.com>
2011-12-16 15:19:56 +0000

    new test

--- time_runge_kutta_test/time_runge_kutta_test.f90
+++ time_runge_kutta_test/time_runge_kutta_test.f90
@@ -20,8 +20,8 @@ integer :: ix_test
 call bmad_parser ('lat.bmad', lat)
 
 !element index to test
-ix_test = 10
-start_orb%vec = [0.000, 0.00, 0.00, 0.00, 0.00, 0.00]
+ix_test = 1
+start_orb%vec = [0.001, 0.00, 0.001, 0.00, 0.00, 0.00]
 start_orb%s = lat%ele(ix_test-1)%s
 start_orb%t = lat%ele(ix_test-1)%ref_time
 
@@ -35,14 +35,14 @@ lat%ele%tracking_method = runge_kutta$
 call track1 (start_orb, lat%ele(ix_test), lat%param, end_orb2)
 
 open (1, file = 'output.now')
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(1)" ABS  1E-10', end_orb1%vec(1)
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(2)" ABS  1E-10', end_orb1%vec(2)
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(3)" ABS  1E-10', end_orb1%vec(3)
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(4)" ABS  1E-10', end_orb1%vec(4)
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(5)" ABS  1E-10', end_orb1%vec(5)
-write (1, '(a, es20.10)') '"time_runge_kutta:vec(6)" ABS  1E-10', end_orb1%vec(6)
-write (1, '(a, es20.10)') '"time_runge_kutta:s     " ABS  1E-10', end_orb1%s
-write (1, '(a, es20.10)') '"time_runge_kutta:t     " ABS  1E-10', end_orb1%t
+write (1, '(a, es20.10)') '"time_runge_kutta:diff:vec(1)" ABS  1E-10', end_orb1%vec(1) - end_orb2%vec(1)
+write (1, '(a, es20.10)') '"time_runge_kutta:diff:vec(2)" ABS  1E-10', end_orb1%vec(2) - end_orb2%vec(2)
+write (1, '(a, es20.10)') '"time_runge_kutta:diff:vec(3)" ABS  1E-10', end_orb1%vec(3) - end_orb2%vec(3)
+write (1, '(a, es20.10)') '"time_runge_kutta:diff:vec(4)" ABS  1E-10', end_orb1%vec(4) - end_orb2%vec(4)
+write (1, '(a, es20.10)') '"time_runge_kutta:diff:vec(5)" ABS  1E-10', end_orb1%vec(5) - end_orb2%vec(5)
+write (1, '(a, es20.10)') '"time_runge_kutta:diff:vec(6)" ABS  1E-10', end_orb1%vec(6) - end_orb2%vec(6)
+write (1, '(a, es20.10)') '"time_runge_kutta:diff:s     " ABS  1E-10', end_orb1%s - end_orb2%s
+write (1, '(a, es20.10)') '"time_runge_kutta:diff:t     " ABS  1E-10', end_orb1%t - end_orb2%t
 
 write(1, *) 
 write (1, '(a, es20.10)') '"runge_kutta:vec(1)" ABS  1E-10', end_orb2%vec(1)
