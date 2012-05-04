84c80858399f32c6a57d74c7c674a13da327d82e
David Sagan <dcs16@cornell.edu>
2012-05-04 02:19:58 +0000

    Added info to output.

--- element_attributes/element_attributes.f90
+++ element_attributes/element_attributes.f90
@@ -13,21 +13,39 @@ use basic_attribute_mod
 implicit none
 
 type (ele_struct) ele
-integer i, j, n_used(n_attrib_special_maxx)
+integer i, ik, j, n_used(n_attrib_special_maxx), key_indx(n_key)
 character(40) a_name
+character(20) date
 
-! indexes used for a given element key
+! Element keys
 
 open (1, file = 'element_attributes.dat')
 
-n_used = 0
+call date_and_time_stamp (date, .true.)
+
+write (1, *) date
+write (1, *)
+
+call indexx(key_name, key_indx)
 
 do i = 1, n_key
+  j = key_indx(i)
+  write (1, '(i4, 3x, a, i4, 3x, a)') i, key_name(i), j, trim(key_name(j))
+enddo
+
+write (1, *) 
+
+! indexes used for a given element key
+
+n_used = 0
+
+do ik = 1, n_key
+  i = key_indx(ik)
   if (i == overlay_lord$) cycle
   write (1, *) '!---------------------------------'
   write (1, '(i3, 2x, a)') i, key_name(i)
   ele%key = i
-  do j = 1, n_attrib_special_maxx
+  do j = 1, a0$
     a_name = attribute_name (ele, j) 
     if (a_name == null_name$) cycle
     write (1, '(i10, 2x, a)') j, a_name
@@ -47,7 +65,7 @@ enddo
 
 ! List of elements using an index
 
-do i = 1, n_attrib_special_maxx
+do i = 1, a0$
   write (1, *)
   write (1, *) '!---------------------------------'
   write (1, '(a, i0)') 'Index: ', i
@@ -61,5 +79,27 @@ do i = 1, n_attrib_special_maxx
 enddo
 
 print *, 'Writen: element_attributes.dat'
+
+close (1)
+
+!-----------------------------------------------------
+
+open (1, file = 'element-attributes-list.tex')
+
+do i = 1, n_key
+  if (i == overlay_lord$) cycle
+  write (1, *) '!---------------------------------'
+  write (1, '(i3, 2x, a)') i, key_name(i)
+  ele%key = i
+  do j = 1, a0$
+    a_name = attribute_name (ele, j) 
+    if (a_name == null_name$) cycle
+    write (1, '(i10, 2x, a)') j, a_name
+    n_used(j) = n_used(j) + 1
+  enddo
+  write (1, *)
+enddo
+
+
 
 end program
