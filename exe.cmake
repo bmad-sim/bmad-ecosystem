428a60ee5f84e403026d00a6cbc2cc3f6e52a6e2
cesrulib <cesrulib@cornell.edu>
2012-07-13 18:54:54 +0000

    Adding file for multiple exe building support

---
+++ exe.cmake
@@ -0,0 +1,24 @@
+
+#----------------------------------------------
+# Honor propagated control variable to build
+# any EXEs provided in a list from the main
+# CMakeLists.txt file.
+#----------------------------------------------
+IF (BUILD_EXES)
+  set(BUILD_EXE_TOGGLE "")
+ELSE()
+  set(BUILD_EXE_TOGGLE "EXCLUDE_FROM_ALL")
+ENDIF ()
+
+
+add_executable(${EXENAME}-exe
+    ${BUILD_EXE_TOGGLE}
+    ${MAIN_FILE})
+
+SET_TARGET_PROPERTIES(${EXENAME}-exe
+	PROPERTIES
+	OUTPUT_NAME
+	${EXENAME})
+
+TARGET_LINK_LIBRARIES(${EXENAME}-exe
+	${LINK_LIBS})
