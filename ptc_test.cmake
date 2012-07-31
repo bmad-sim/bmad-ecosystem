cb159d26cea474e847281362086a91e149d0cdc4
Matthew C. Reid <mcr239@cornell.edu>
2012-07-31 16:30:36 +0000

    Adding new build system files

---
+++ ptc_test.cmake
@@ -0,0 +1,22 @@
+set (EXENAME ptc_test)
+set (SRC_FILES
+  
+)
+
+set (INC_DIRS
+  ../include
+  include
+)
+
+set (LINK_LIBS
+  tao 
+  bmad 
+  sim_utils
+  recipes_f-90_LEPP 
+  xsif 
+  pgplot 
+  forest 
+  readline
+  curses
+  termcap
+)
