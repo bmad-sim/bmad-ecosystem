c09aa180b1c9cbe5d5ddd8938bc9ef270e1b5ab8
David Sagan <dcs16@cornell.edu>
2012-08-31 16:48:48 +0000

    Removed VMS preprocessor commands.

--- file/file_directorizer.f90
+++ file/file_directorizer.f90
@@ -38,58 +38,50 @@
 !     outfile = '[abc.def]thisfile.foo'
 !-
 
-#include "CESR_platform.inc"
-
 subroutine file_directorizer (in_file, out_file, directory, add_switch)
 
-  use precision_def
+use precision_def
 
-  implicit none
+implicit none
 
-  integer ilen, i, ix, dix
+integer ilen, i, ix, dix
 
-  logical add_switch
+logical add_switch
 
-  character(*) in_file, out_file, directory
+character(*) in_file, out_file, directory
 
-#if defined (CESR_VMS)
-  character(2) :: dir_str = ']:'
-#endif
-
-#if defined(CESR_UNIX) || defined(CESR_WINCVF)
-  character(1) :: dir_str = '/'
-#endif
+character(1) :: dir_str = '/'
 
 ! 
 
-  out_file = in_file
-  call string_trim (out_file, out_file, ilen) ! trim leading blanks
-  out_file = out_file(1:ilen)      ! trim trailing words
+out_file = in_file
+call string_trim (out_file, out_file, ilen) ! trim leading blanks
+out_file = out_file(1:ilen)      ! trim trailing words
 
-  dix = 0
-  do i = ilen, 1, -1
-    if (index(dir_str, out_file(i:i)) /= 0) then
-      dix = i
-      exit
-    endif
-  enddo
+dix = 0
+do i = ilen, 1, -1
+  if (index(dir_str, out_file(i:i)) /= 0) then
+    dix = i
+    exit
+  endif
+enddo
 
 ! if IN_FILE doesn't already contain a directory then add DIRECTORY independent
 ! of the value of ADD_SWITCH. If IN_FILE contains a directory then only add a
 ! directory if ADD_SWITCH is .true.
 
-  if (.not. add_switch .and. dix /= 0) return
+if (.not. add_switch .and. dix /= 0) return
 
-  if (len_trim(directory) == 0) then
-    out_file = out_file(dix+1:)
+if (len_trim(directory) == 0) then
+  out_file = out_file(dix+1:)
+else
+  ix = len_trim(directory)
+  if (dir_str == '/' .and. directory(ix:ix) /= '/') then
+    out_file = trim(directory) // '/' // out_file(dix+1:)  ! add directory
   else
-    ix = len_trim(directory)
-    if (dir_str == '/' .and. directory(ix:ix) /= '/') then
-      out_file = trim(directory) // '/' // out_file(dix+1:)  ! add directory
-    else
-      out_file = trim(directory) // out_file(dix+1:)  ! add directory
-    endif
+    out_file = trim(directory) // out_file(dix+1:)  ! add directory
   endif
+endif
 
 end subroutine
 
