e0ea5a77fef5b491c93fa8dabdeef4ce0e3c80a9
David Sagan <dcs16@cornell.edu>
2012-09-10 19:52:56 +0000

    New beam_init%full_6D_coupling_calc component.

--- modules/matrix_mod.f90
+++ modules/matrix_mod.f90
@@ -2,20 +2,20 @@
 
 module matrix_mod
 
-  use precision_def
-  use sim_utils
-  use basic_bmad_mod
+use precision_def
+use sim_utils
+use basic_bmad_mod
 
-  integer, parameter :: ok$              = 1
-  integer, parameter :: in_stop_band$    = 2
-  integer, parameter :: non_symplectic$  = 3
-  integer, parameter :: unstable$        = 4
+integer, parameter :: ok$              = 1
+integer, parameter :: in_stop_band$    = 2
+integer, parameter :: non_symplectic$  = 3
+integer, parameter :: unstable$        = 4
 
-  character(16) :: status_name(5) = [    'OK            ', &
-      'IN_STOP_BAND  ', 'NON_SYMPLECTIC', 'UNSTABLE      ', '              ' ]
+character(16) :: status_name(5) = [    'OK            ', &
+    'IN_STOP_BAND  ', 'NON_SYMPLECTIC', 'UNSTABLE      ', '              ' ]
 
 contains
-      
+    
 !--------------------------------------------------------------------
 !--------------------------------------------------------------------
 !--------------------------------------------------------------------
@@ -39,43 +39,43 @@ contains
 
 subroutine mat_inverse (mat, mat_inv, ok, print_err)
 
-  use nr
+use nr
 
-  implicit none
+implicit none
 
-  real(rp) :: mat(:,:)
-  real(rp) :: mat_inv(:,:)
+real(rp) :: mat(:,:)
+real(rp) :: mat_inv(:,:)
 
-  real(rp) :: mat2(size(mat, 1), size(mat, 1)), vec(size(mat, 1))
-  integer :: indx(size(mat, 1))
-  real(rp) d
+real(rp) :: mat2(size(mat, 1), size(mat, 1)), vec(size(mat, 1))
+integer :: indx(size(mat, 1))
+real(rp) d
 
-  integer n, i
-  logical, optional :: ok, print_err
-  character(16) :: r_name = 'mat_inverse'
+integer n, i
+logical, optional :: ok, print_err
+character(16) :: r_name = 'mat_inverse'
 
 !
 
-  n = size (mat, 1)
+n = size (mat, 1)
 
-  mat2 = mat  ! use temp mat so as to not change mat
+mat2 = mat  ! use temp mat so as to not change mat
 
-  vec(1:n) = maxval(abs(mat), dim = 2)
-  if (any(vec(1:n) == 0)) then
-    if (logic_option(.false., print_err)) call out_io (s_error$, r_name, 'SINGULAR MATRIX.')
-    if (present(ok)) ok = .false.
-    return
-  endif
-  if (present(ok)) ok = .true.
+vec(1:n) = maxval(abs(mat), dim = 2)
+if (any(vec(1:n) == 0)) then
+  if (logic_option(.false., print_err)) call out_io (s_error$, r_name, 'SINGULAR MATRIX.')
+  if (present(ok)) ok = .false.
+  return
+endif
+if (present(ok)) ok = .true.
 
-  call ludcmp (mat2, indx, d)
+call ludcmp (mat2, indx, d)
 
-  mat_inv(1:n,1:n) = 0
-  forall (i = 1:n) mat_inv(i,i) = 1
+mat_inv(1:n,1:n) = 0
+forall (i = 1:n) mat_inv(i,i) = 1
 
-  do i = 1, n
-    call lubksb (mat2, indx, mat_inv(1:n,i))
-  enddo
+do i = 1, n
+  call lubksb (mat2, indx, mat_inv(1:n,i))
+enddo
 
 end subroutine
 
@@ -103,48 +103,48 @@ end subroutine
 
 function mat_symp_error (mat) result (error)
 
-  implicit none
+implicit none
 
-  integer i, j, n
+integer i, j, n
 
-  real(rp), intent(in) :: mat(:,:)
-  real(rp) :: m2(size(mat, 1), size(mat, 1))
-  real(rp) error
+real(rp), intent(in) :: mat(:,:)
+real(rp) :: m2(size(mat, 1), size(mat, 1))
+real(rp) error
 
-  logical :: debug = .false.
-
-!
-
-  n = ubound(mat, 1)
-
-  if (mod(n, 2) /= 0) then
-    print *, 'ERROR IN MAT_SYMP_ERROR: MATRIX DOES NOT HAVE EVEN SIZE'
-    if (bmad_status%exit_on_error) call err_exit
-  endif
+logical :: debug = .false.
 
 !
 
-  do j = 2, n, 2                  ! m2 = mat_transpose * S
-    m2(1:n,j)   = -mat(j-1,1:n)
-    m2(1:n,j-1) =  mat(j,  1:n)
+n = ubound(mat, 1)
+
+if (mod(n, 2) /= 0) then
+  print *, 'ERROR IN MAT_SYMP_ERROR: MATRIX DOES NOT HAVE EVEN SIZE'
+  if (bmad_status%exit_on_error) call err_exit
+endif
+
+!
+
+do j = 2, n, 2                  ! m2 = mat_transpose * S
+  m2(1:n,j)   = -mat(j-1,1:n)
+  m2(1:n,j-1) =  mat(j,  1:n)
+enddo
+
+m2 = matmul (m2, mat)
+
+do j = 2, n, 2
+  m2(j, j-1) = m2(j, j-1) - 1
+  m2(j-1, j) = m2(j-1, j) + 1
+enddo
+
+error = maxval(abs(m2))
+
+if (debug) then
+  print *, 'MAT_SYMP_ERROR:', error
+  do i = 1, n
+    print '(5x, (6f11.5))', &
+                      (max(min(m2(i, j), 999.0_rp), -999.0_rp), j = 1, n)
   enddo
-
-  m2 = matmul (m2, mat)
-
-  do j = 2, n, 2
-    m2(j, j-1) = m2(j, j-1) - 1
-    m2(j-1, j) = m2(j-1, j) + 1
-  enddo
-
-  error = maxval(abs(m2))
-
-  if (debug) then
-    print *, 'MAT_SYMP_ERROR:', error
-    do i = 1, n
-      print '(5x, (6f11.5))', &
-                        (max(min(m2(i, j), 999.0_rp), -999.0_rp), j = 1, n)
-    enddo
-  endif
+endif
 
 end function mat_symp_error
 
@@ -152,75 +152,89 @@ end function mat_symp_error
 !--------------------------------------------------------------------
 !--------------------------------------------------------------------
 !+
-! Subroutine mat_symplectify (mat_in, mat_symp)
+! Subroutine mat_symplectify (mat_in, mat_symp, f_scale)
 !
-! Subroutine to form a symplectic matrix that is  "close" to the input matrix.
-!
+! Subroutine to form the symplectic matrix that is as "close" as possible to the input matrix.
+! If mat_in is symplectic, and f_length is 1, then mat_sym will be equal to mat_in.
+! 
+! The f_scale argument is used to "scale" mat_symp such that if mat_in is symplectic the
+! following is true:
+!   f_scale = 0   --> mat_symp = I (unit matrix, always true independent of mat_in)
+!   f_scale = 1/N --> mat_symp^N = mat_in
+!   f_scale = 1   --> mat_symp = mat_in
+!   
 ! Modules needed:
+!   matrix_mod
 !
 ! Input:
 !   mat_in(:,:) -- Real(rp): Input matrix to symplectify.
+!   f_scale     -- Real(rp), optional: Scaling factor. Default is 1.
 !
 ! Output:
 !   mat_symp(:,:) -- Real(rp): Symplectic output matrix
 !-
 
-subroutine mat_symplectify (mat_in, mat_symp)
+subroutine mat_symplectify (mat_in, mat_symp, f_scale)
 
-  implicit none
+implicit none
 
-  real(rp), intent(in)  :: mat_in(:,:)
-  real(rp), intent(out) :: mat_symp(:,:)
+real(rp), intent(in)  :: mat_in(:,:)
+real(rp), intent(out) :: mat_symp(:,:)
+real(rp), intent(in), optional :: f_scale
 
-  real(rp), dimension(size(mat_in, 1), size(mat_in, 2)) :: m1, m2, m3, m_symetric
+real(rp), dimension(size(mat_in, 1), size(mat_in, 2)) :: m1, m2, m3, m_symmetric
 
-  integer n,i,j
+integer n,i,j
 
 !
 
-  n = ubound(mat_in, 1)
+n = ubound(mat_in, 1)
 
-  if (ubound(mat_symp, 1) /= n) then
-    print *, 'ERROR IN MAT_SYMPLECTIFY: UNEQUAL MATRIX SIZES.'
-    if (bmad_status%exit_on_error) call err_exit
-  endif
+if (ubound(mat_symp, 1) /= n) then
+  print *, 'ERROR IN MAT_SYMPLECTIFY: UNEQUAL MATRIX SIZES.'
+  if (bmad_status%exit_on_error) call err_exit
+endif
 
-! form the symmetrix matrix:
+! Form the symmetrix matrix:
 !         m1 = S * (1 - mat_in) * (1 + mat_in)^-1
-!         m_symetric = (m1 + m1_transpose) / 2
+!         m_symmetric = (m1 + m1_transpose) / 2
 
-  m1 =-mat_in
-  forall (i = 1:n) m1(i,i) = m1(i,i) + 1
+m1 =-mat_in
+forall (i = 1:n) m1(i,i) = m1(i,i) + 1
 
-  m2 = mat_in
-  forall (i = 1:n) m2(i,i) = m2(i,i) + 1
-  call mat_inverse (m2, m2)
+m2 = mat_in
+forall (i = 1:n) m2(i,i) = m2(i,i) + 1
+call mat_inverse (m2, m2)
 
-  m3 = matmul(m1, m2)
+m3 = matmul(m1, m2)
 
-  do i = 2, n, 2
-    m1(i-1,1:n) =  m3(i,  1:n)
-    m1(i,  1:n) = -m3(i-1,1:n)
-  enddo
+do i = 2, n, 2
+  m1(i-1,1:n) =  m3(i,  1:n)
+  m1(i,  1:n) = -m3(i-1,1:n)
+enddo
 
-  forall (i = 1:n, j = 1:n) m_symetric(i,j) = (m1(i,j) + m1(j,i)) / 2
+forall (i = 1:n, j = 1:n) m_symmetric(i,j) = (m1(i,j) + m1(j,i)) / 2
 
-! form the symplectic matrix
-!         m_symp = (1 + S * m_symetric) * (1 - S * m_symetric)^-1
+! Scale by f_scale
 
-  do i = 2, n, 2
-    m1(i-1,1:n) =  m_symetric(i,  1:n)
-    m1(i,  1:n) = -m_symetric(i-1,1:n)
-  enddo
+if (present(f_scale)) m_symmetric = f_scale * m_symmetric
 
-  m2 = m1
-  forall (i = 1:n) m2(i,i) = m2(i,i) + 1
+! Form the symplectic matrix
+!         m_symp = (1 + S * m_symmetric) * (1 - S * m_symmetric)^-1
 
-  m3 = -m1
-  forall (i = 1:n) m3(i,i) = m3(i,i) + 1
-  call mat_inverse (m3, m3)
+do i = 2, n, 2
+  m1(i-1,1:n) =  m_symmetric(i,  1:n)
+  m1(i,  1:n) = -m_symmetric(i-1,1:n)
+enddo
 
-  mat_symp = matmul (m2, m3)
+m2 = m1
+forall (i = 1:n) m2(i,i) = m2(i,i) + 1
+
+m3 = -m1
+forall (i = 1:n) m3(i,i) = m3(i,i) + 1
+call mat_inverse (m3, m3)
+
+mat_symp = matmul (m2, m3)
 
 end subroutine
 
@@ -243,23 +257,23 @@ end subroutine
 
 subroutine mat6_dispersion (m_i6, mat6)
 
-  implicit none
+implicit none
 
-  real(rp), intent(inout) :: mat6(:,:)
-  real(rp), intent(in) :: m_i6(:)
+real(rp), intent(inout) :: mat6(:,:)
+real(rp), intent(in) :: m_i6(:)
 
-  real(rp) vec4(4)
+real(rp) vec4(4)
 
 !
 
-  mat6(1:4, 6) = m_i6(1:4)
+mat6(1:4, 6) = m_i6(1:4)
 
-  vec4(1) = -m_i6(2)
-  vec4(2) =  m_i6(1)
-  vec4(3) = -m_i6(4)
-  vec4(4) =  m_i6(3)
+vec4(1) = -m_i6(2)
+vec4(2) =  m_i6(1)
+vec4(3) = -m_i6(4)
+vec4(4) =  m_i6(3)
 
-  mat6(5,1:4) = matmul (vec4, mat6(1:4,1:4))
+mat6(5,1:4) = matmul (vec4, mat6(1:4,1:4))
 
 end subroutine
 
