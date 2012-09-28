fdbba535ed8812424ae19bc34dacc2b98fe1ad29
Michael Ehrlichman <mpe5@cornell.edu>
2012-09-28 04:32:16 +0000

    New cplx_mat_inverse routine is more robust.  Old version was based on a trick that failed if the real part of the matrix was singular.

--- matrix/cplx_mat_inverse.f90
+++ matrix/cplx_mat_inverse.f90
@@ -1,29 +1,26 @@
-#include "CESR_platform.inc"
-
-!--------------------------------------------------------------------
-!--------------------------------------------------------------------
-!--------------------------------------------------------------------
 !+
-! Subroutine cplx_mat_inverse (mat_r, mat_i, inv_r, inv_i, ok, print_error)
+! Subroutine cplx_mat_inverse (mat, mat_inv, ok, print_error)
 !
-! Computes the inverse of a complex matrix by computing inverse using mat_inverse, which
-! computes the inverse of real matrixes.
-! Based on : "Method to Calculate the Inverse of a Complex Matrix using Real Matrix Inversion"
-!            by Andreas Falkenberg
+! Takes the inverse of a square matrix using LU Decomposition from
+! Numerical Recipes.
+!
+! Module needed:
+!   sim_utils_interface
 !
 ! Input:
-!   mat_r(:,:)     -- Real(rp): Real part of input matrix
-!   mat_i(:,:)     -- Real(rp): Imaginary part of input matrix
+!   mat(:,:)     -- complex(rp): Input matrix array
 !   print_err    -- Logical, optional: If True then the subroutine will type out
 !                         a warning message. Default is False.
 ! Output:
-!   inv_r(:,:)     -- Real(rp): Real part of inverted matrix
-!   inv_i(:,:)     -- Real(rp): Imaginary part of inverted matrix
+!   mat_inv(:,:) -- complex(rp): inverse of mat1
 !   ok           -- Logical, optional: Set False for a singular input matrix.
 !-
-subroutine cplx_mat_inverse(mat_r, mat_i, inv_r, inv_i, ok, print_err)
+
+subroutine cplx_mat_inverse (mat_r, mat_i, inv_r, inv_i, ok, print_err)
 
 use precision_def
+use output_mod
+use nr
 use sim_utils_interface
 
 implicit none
@@ -33,18 +30,45 @@ real(rp) :: mat_i(:,:)
 real(rp) :: inv_r(:,:)
 real(rp) :: inv_i(:,:)
 
-real(rp) :: r0(1:size(mat_r,1),1:size(mat_r,1))
-real(rp) :: a_inv(1:size(mat_r,1),1:size(mat_r,1))
-real(rp) :: y00(1:size(mat_r,1),1:size(mat_r,1))
+complex(rp) :: mat(size(mat_r,1),size(mat_r,1))
+complex(rp) :: mat_inv(size(mat_r,1),size(mat_r,1))
 
+complex(rp) :: mat2(size(mat_r, 1), size(mat_r, 1))
+real(rp) :: vec(size(mat_r, 1))
+integer :: indx(size(mat_r, 1))
+real(rp) d
+
+integer n, i
 logical, optional :: ok, print_err
-character(16) :: r_name = 'cplx_mat_inverse'
+character(16) :: r_name = 'mat_inverse'
 
-call mat_inverse(mat_r, a_inv, ok, print_err)
-r0 = matmul(a_inv,mat_i)
-call mat_inverse (matmul(mat_i,r0)+mat_r, inv_r, ok, print_err)
-inv_i = -1.0 * matmul(r0,inv_r)
+!
 
-end subroutine cplx_mat_inverse
+mat = CMPLX(mat_r,mat_i)
 
+n = size (mat, 1)
+
+mat2 = mat  ! use temp mat so as to not change mat
+
+vec(1:n) = maxval(abs(mat), dim = 2)
+if (any(vec(1:n) == 0)) then
+  if (logic_option(.false., print_err)) call out_io (s_error$, r_name, 'SINGULAR MATRIX.')
+  if (present(ok)) ok = .false.
+  return
+endif
+if (present(ok)) ok = .true.
+
+call cplx_ludcmp (mat2, indx, d)
+
+mat_inv(1:n,1:n) = (0,0) 
+forall (i = 1:n) mat_inv(i,i) = (1,0)
+
+do i = 1, n
+  call cplx_lubksb (mat2, indx, mat_inv(1:n,i))
+enddo
+
+inv_r = REAL(mat_inv)
+inv_i = AIMAG(mat_inv)
+
+end subroutine
 
