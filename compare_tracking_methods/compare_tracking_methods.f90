30c049de07c87b349303e4b54b970e1a8e35637c
Avishek Chatterjee <ac448@cornell.edu>
2012-10-03 16:45:30 +0000

    Create ps file.

--- compare_tracking_methods/compare_tracking_methods.f90
+++ compare_tracking_methods/compare_tracking_methods.f90
@@ -6,8 +6,9 @@ use quick_plot
 implicit none
 
 type (lat_struct), target :: lat
+
 character(40) :: input_file  = 'compare_tracking_methods.bmad'
-character(40) :: output_file = 'compare_tracking_methods.out'
+character(40) :: output_file = 'compare_tracking_methods'
 character(20) :: base_method, element_to_vary, attrib_to_vary
 character(20) :: veto_methods(10)
 real(rp) :: scan_start, scan_end, step_size
@@ -96,16 +97,21 @@ do i = 1, nmethods
    if(.not. (i == nmethods)) allocate (dmethod(i)%step(nsteps+1))
 end do
 
-open (1, file = output_file)
+open (1, file = trim(adjustl(output_file)) // '.abs')
 write (1,'(a)',advance='no') "Compare tracking methods as "; write (1,'(a)',advance='no') trim(adjustl(element_to_vary)); write (1,'(a)',advance='no') "%"; write (1,'(a)',advance='no') trim(adjustl(attrib_to_vary))
 write (1,'(a)',advance='no') " is varied. Look at phase space coordinates at exit for lattice with only a "; write (1,'(a)',advance='no') trim(adjustl(key_name(lat%ele(1)%key))); write (1,'(a)',advance='no') "."
 write (1,*); write (1,*)
+
+open (2, file = trim(adjustl(output_file)) // '.diff')
+write (2,'(a)',advance='no') "Compare tracking methods as "; write (2,'(a)',advance='no') trim(adjustl(element_to_vary)); write (2,'(a)',advance='no') "%"; write (2,'(a)',advance='no') trim(adjustl(attrib_to_vary))
+write (2,'(a)',advance='no') " is varied. Look at difference in phase space coordinates at exit for lattice with only a "; write (2,'(a)',advance='no') trim(adjustl(key_name(lat%ele(1)%key))); write (2,'(a)',advance='no') "."
+write (2,*); write (2,*)
 
 step_size = (scan_end - scan_start) / nsteps
 
 ! For each method, perform scan and store phase space coordinates at exit
 k = 0
-DO i = 1, size(calc_method_name)-1
+DO i = 1, ubound(calc_method_name, 1)
    if(.not. valid_tracking_method(lat%ele(1)%key,i)) cycle
    lat%ele(1)%tracking_method = i
    k = k+1
@@ -140,14 +146,20 @@ k = 0
 DO i = 1, nmethods
    if (i == m) cycle
    k = k+1
+   write (2,*) "Tracking Method = ", method(i)%method_name
    dmethod(k)%method_name = method(i)%method_name
    dmethod(k)%short_method_name = method(i)%short_method_name
    DO j = 1, nsteps+1
       DO l = 1, 6
          dmethod(k)%step(j)%vec(l)  = method(i)%step(j)%vec(l) - method(m)%step(j)%vec(l)
       END DO
+      write (2,'(es24.15,es24.15,es24.15,es24.15,es24.15,es24.15,es24.15)',advance='no') scan_start + (j-1) * step_size, dmethod(k)%step(j)%vec(1), dmethod(k)%step(j)%vec(2), dmethod(k)%step(j)%vec(3), dmethod(k)%step(j)%vec(4), dmethod(k)%step(j)%vec(5), dmethod(k)%step(j)%vec(6)
+      write (2,*)
    END DO
+   write (2,*)
 END DO
+
+close(2)
 
 ymin = 0.; ymax = 0.
 
@@ -159,49 +171,12 @@ DO i = 1, nmethods -1
    END DO
 END DO
 
+call qp_open_page ("PS-L", id, 1000.0_rp, 770.0_rp, "POINTS")
+call create_plot
+call qp_close_page
+
 call qp_open_page ("X", id, 1000.0_rp, 770.0_rp, "POINTS")
-
-call qp_set_page_border (0.02_rp, 0.01_rp, 0.01_rp, 0.1_rp, "%PAGE")
-call qp_set_margin (0.07_rp, 0.05_rp, 0.02_rp, 0.01_rp, "%PAGE")
-call qp_calc_and_set_axis ("X", scan_start, scan_end, 4, 8, "ZERO_AT_END")
-
-! Draw axes
-DO i = 1, 6
-   call which_box(i, xbox, ybox)
-   call my_draw_axes (xbox, ybox, ymin(i), ymax(i), y_axis_label(i))
-END DO  
-
-! Draw curves
-DO i = 1, nmethods-1 
-   call qp_set_line_attrib ("PLOT", color = i, pattern = 1)
-   lines(i)%color = i
-   DO j = 1, 6
-      call which_box(j, xbox, ybox)
-      call plot_it (xbox, ybox, ymin(j), ymax(j), scan_var, dmethod(i)%step(:)%vec(j))
-      call qp_draw_text (dmethod(i)%short_method_name, scan_var(int(nsteps*(0.1+i*0.09))), dmethod(i)%step(int(nsteps*(0.1+i*0.09)))%vec(j), height = 10.0_rp, color = i) 
-   END DO
-END DO
-
-! Draw legend
-if (nmethods > 6) then
-   if (mod(nmethods+1,2)) then
-      call qp_draw_curve_legend (0.09_rp, 0.99_rp, "%PAGE", line = lines(1:(nmethods+1)/2), text = dmethod(1:(nmethods+1)/2)%method_name, draw_symbol = .false.)
-      call qp_draw_curve_legend (0.3_rp, 0.99_rp, "%PAGE", line = lines((nmethods+3)/2:(nmethods-1)), text = dmethod((nmethods+3)/2:(nmethods-1))%method_name, draw_symbol = .false.)
-   else
-      call qp_draw_curve_legend (0.09_rp, 0.99_rp, "%PAGE", line = lines(1:nmethods/2), text = dmethod(1:nmethods/2)%method_name, draw_symbol = .false.)
-      call qp_draw_curve_legend (0.3_rp, 0.99_rp, "%PAGE", line = lines(nmethods/2+1:(nmethods-1)), text = dmethod(nmethods/2+1:(nmethods-1))%method_name, draw_symbol = .false.)
-   end if
-else
-   call qp_draw_curve_legend (0.09_rp, 0.99_rp, "%PAGE", line = lines, text = dmethod(:)%method_name, draw_symbol = .false.)   
-end if
-
-! Write description on plot
-call qp_draw_text (start_orb_desc, 0.53_rp, 0.98_rp, "%PAGE", height = 15.0_rp) 
-x_axis_desc = "X axis: parameter being varied (" // trim(adjustl(element_to_vary)) // "%" // trim(adjustl(attrib_to_vary)) // ")."
-call qp_draw_text (x_axis_desc, 0.53_rp, 0.95_rp, "%PAGE", height = 15.0_rp)
-y_axis_desc = "Y axis: absolute difference w.r.t. " // trim(adjustl(method(m)%method_name)) // " tracking."
-call qp_draw_text (y_axis_desc, 0.53_rp, 0.92_rp, "%PAGE", height = 15.0_rp)
-
+call create_plot
 write (*, "(a)", advance = "NO") " Hit any class to end program: "
 accept "(a)", ans
 
@@ -243,10 +218,54 @@ end subroutine my_draw_axes
 subroutine plot_it(xbox,ybox,y_min,y_max,xvec,yvec)
   integer :: xbox, ybox
   real(rp) :: y_min,y_max
-  real(rp), dimension(100) :: xvec, yvec
+  real(rp), dimension(:) :: xvec, yvec
   call qp_set_box (xbox, ybox, 2, 3)
   call qp_calc_and_set_axis ("Y", y_min, y_max, 4, 8, "GENERAL")
   call qp_draw_data (xvec, yvec, symbol_every = 0)
+end subroutine
+
+subroutine create_plot
+  call qp_set_page_border (0.02_rp, 0.01_rp, 0.01_rp, 0.1_rp, "%PAGE")
+  call qp_set_margin (0.07_rp, 0.05_rp, 0.02_rp, 0.01_rp, "%PAGE")
+  call qp_calc_and_set_axis ("X", scan_start, scan_end, 4, 8, "GENERAL")
+
+  ! Draw axes
+  DO i = 1, 6
+     call which_box(i, xbox, ybox)
+     call my_draw_axes (xbox, ybox, ymin(i), ymax(i), y_axis_label(i))
+  END DO
+
+  ! Draw curves
+  DO i = 1, nmethods-1 
+     call qp_set_line_attrib ("PLOT", color = i, pattern = 1)
+     lines(i)%color = i
+     DO j = 1, 6
+        call which_box(j, xbox, ybox)
+        call plot_it (xbox, ybox, ymin(j), ymax(j), scan_var, dmethod(i)%step(:)%vec(j))
+        k = max(1,int(nsteps*(0.1+i*0.09)))
+        call qp_draw_text (dmethod(i)%short_method_name, scan_var(k), dmethod(i)%step(k)%vec(j), height = 10.0_rp, color = i) 
+     END DO
+  END DO
+
+  ! Draw legend
+  if (nmethods > 6) then
+     if (mod(nmethods+1,2)) then
+        call qp_draw_curve_legend (0.09_rp, 0.99_rp, "%PAGE", line = lines(1:(nmethods+1)/2), text = dmethod(1:(nmethods+1)/2)%method_name, draw_symbol = .false.)
+        call qp_draw_curve_legend (0.3_rp, 0.99_rp, "%PAGE", line = lines((nmethods+3)/2:(nmethods-1)), text = dmethod((nmethods+3)/2:(nmethods-1))%method_name, draw_symbol = .false.)
+     else
+        call qp_draw_curve_legend (0.09_rp, 0.99_rp, "%PAGE", line = lines(1:nmethods/2), text = dmethod(1:nmethods/2)%method_name, draw_symbol = .false.)
+        call qp_draw_curve_legend (0.3_rp, 0.99_rp, "%PAGE", line = lines(nmethods/2+1:(nmethods-1)), text = dmethod(nmethods/2+1:(nmethods-1))%method_name, draw_symbol = .false.)
+     end if
+  else
+     call qp_draw_curve_legend (0.09_rp, 0.99_rp, "%PAGE", line = lines, text = dmethod(:)%method_name, draw_symbol = .false.)   
+  end if
+
+  ! Write description on plot
+  call qp_draw_text (start_orb_desc, 0.53_rp, 0.98_rp, "%PAGE", height = 15.0_rp) 
+  x_axis_desc = "X axis: parameter being varied (" // trim(adjustl(element_to_vary)) // "%" // trim(adjustl(attrib_to_vary)) // ")."
+  call qp_draw_text (x_axis_desc, 0.53_rp, 0.95_rp, "%PAGE", height = 15.0_rp)
+  y_axis_desc = "Y axis: absolute difference w.r.t. " // trim(adjustl(method(m)%method_name)) // " tracking."
+  call qp_draw_text (y_axis_desc, 0.53_rp, 0.92_rp, "%PAGE", height = 15.0_rp)
 end subroutine
 
 end program
