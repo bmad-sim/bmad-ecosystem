4bfae9f29b37ce8bccdb50007db99f9f2de6daca
Avishek Chatterjee <ac448@cornell.edu>
2012-10-08 20:49:56 +0000

    Several small changes.

--- tracking_method_test/tracking_method_test.f90
+++ tracking_method_test/tracking_method_test.f90
@@ -7,7 +7,7 @@ implicit none
 type (lat_struct), target :: lat
 
 character(40) :: input_file  = 'tracking_method_test.bmad'
-character(32) :: final_str
+character(36) :: final_str
 integer :: i, j
  
 type (coord_struct) end_orb
@@ -19,15 +19,18 @@ open (1, file = 'output.now')
 DO i = 1, lat%n_ele_max
    DO j = 1, n_methods$
       if(.not. valid_tracking_method(lat%ele(i),j) .or. j == symp_map$ .or. j == custom$) cycle
+      if(lat%ele(i)%key == elseparator$ .and. (j == runge_kutta$ .or. j == boris$ .or. j == time_runge_kutta$)) cycle     
       lat%ele(i)%tracking_method = j
       if (j == linear$) then
-        lat%ele(i)%tracking_method = bmad_standard$
+        lat%ele(i)%tracking_method = symp_lie_ptc$
+        if(lat%ele(i)%key == beambeam$) lat%ele(i)%tracking_method = bmad_standard$
         call make_mat6 (lat%ele(i), lat%param, lat%beam_start)
         lat%ele(i)%tracking_method = j
       endif
       call init_coord (lat%beam_start, lat%beam_start, ele = lat%ele(i), at_exit_end = .false.)
       call track1 (lat%beam_start, lat%ele(i), lat%param, end_orb)
-      final_str = '"' // trim(key_name(lat%ele(i)%key)) // ':' // trim(calc_method_name(j)) // '"'  
+      final_str = '"' // trim(key_name(lat%ele(i)%key)) // ':' // trim(calc_method_name(j)) // '"' 
+      if(lat%ele(i)%key == wiggler$) final_str = '"' // trim(key_name(lat%ele(i)%key)) // '(' // trim(sub_key_name(lat%ele(i)%sub_key)) // ')' // ':' // trim(calc_method_name(j)) // '"'
       write (1,'(a,a,es24.15,es24.15,es24.15,es24.15,es24.15,es24.15,es24.15)',advance='no') final_str, 'REL  1E-10', end_orb%vec(1), end_orb%vec(2), end_orb%vec(3), end_orb%vec(4), end_orb%vec(5), end_orb%vec(6)
       write (1,*)
    END DO
