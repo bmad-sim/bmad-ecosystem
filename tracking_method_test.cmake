ef03b06d914a924c0485013484929848e22a26d9
Avishek Chatterjee <ac448@cornell.edu>
2012-10-08 14:56:17 +0000

    First commit of code.

---
+++ tracking_method_test.cmake
@@ -0,0 +1,17 @@
+set (EXENAME tracking_method_test)
+set (SRC_FILES
+  tracking_method_test/tracking_method_test.f90
+)
+
+set (INC_DIRS
+  ../include
+  include
+)
+
+set (LINK_LIBS
+  bmad 
+  sim_utils
+  recipes_f-90_LEPP 
+  forest
+  xsif 
+)
