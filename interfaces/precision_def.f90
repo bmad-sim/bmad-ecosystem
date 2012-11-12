3bbb00c6625070d3b0ee62fc9ae4ea708cdae97d
David Sagan <dcs16@cornell.edu>
2012-11-12 22:01:43 +0000

    sobseq replaced with super_sobseq.

--- interfaces/precision_def.f90
+++ interfaces/precision_def.f90
@@ -7,6 +7,8 @@ integer, parameter :: rp = rdef
 integer, parameter :: sp = kind(1e0)
 integer, parameter :: dp = selected_real_kind(2*precision(1e0_sp))
 
+integer, parameter :: i4_b = selected_int_kind(9)  ! Equiv to NR I4B
+
 type global_common_struct
   logical :: be_thread_safe = .false.    ! Avoid thread unsafe practices?
   logical :: type_out       = .true.     ! Print error messages?
