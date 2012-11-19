405abd219f9b10969a3dce3f2c618aac128ef9f1
David Sagan <dcs16@cornell.edu>
2012-11-19 17:49:52 +0000

    Moved cpp_bmad interface stuff in include dir to erl_doocs_bmad_interface.

--- scripts/convert.py
+++ scripts/convert.py
@@ -8,13 +8,24 @@ for arg in sys.argv[1:]:
   in_file = open(arg)
   out_file = open('temp.temp', 'w')
   found = False
+  skip_blank = False
 
   for line in in_file.readlines(): 
 
-    if 'scaler' in line:
-      line = line.replace('scaler', 'scalar')
-      found = True
+#    if '#include "CESR_platform.inc' in line:
+#      line = line.replace('scaler', 'scalar')
+#      found = True
 
+    if '#include "CESR_platform.inc' in line:
+      found = True
+      skip_blank = True
+      continue
+
+    if skip_blank and line == '\n': 
+      skip_blank = False
+      continue
+
+    skip_blank = False
     out_file.write(line)
 
   out_file.close()
