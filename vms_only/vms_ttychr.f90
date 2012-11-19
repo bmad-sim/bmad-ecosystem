cc91e701ae37510b8a988ba0c41dcbacd27705ba
David Sagan <dcs16@cornell.edu>
2012-11-19 17:26:02 +0000

    Removed vms_only directory.

--- vms_only/vms_ttychr.f90
+++
@@ -1,9 +0,0 @@
-! stub to allow ttychr to link on VMS
-        integer function vms_ttychr
-        character*1 ic
-        print *,' non-blocking version later: now enter 1 char and <cr> '
-        read(*,1) ic
-1       format(a)
-        vms_ttychr=ichar(ic)
-        return
-        end
