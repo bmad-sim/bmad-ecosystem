9e0280944ca37b2453dee880ed553deea4449884
David Sagan <dcs16@cornell.edu>
2012-12-18 15:52:05 +0000

    Fixed tune.* datum.

--- code/tao_init_data_mod.f90
+++ code/tao_init_data_mod.f90
@@ -575,7 +575,7 @@ do j = n1, n2
 
   if (u%design%lat%param%lattice_type == circular_lattice$ .and. &
               (data_type(1:6)  == 'chrom.' .or. data_type(1:5)  == 'damp.' .or. &
-               data_type(1:17) == 'multi_turn_orbit.' .or. &
+               data_type(1:17) == 'multi_turn_orbit.' .or. data_type(1:5) == 'tune.' .or. &
                data_type(1:13) == 'unstable.ring' .or. index(data_type, 'emit.') /= 0)) then
     dat%exists = .true.
     if (dat%ele_name /= '') then
