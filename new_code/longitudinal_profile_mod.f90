edd3e229257213069623eeb64775bd02943fce9a
Michael Ehrlichman <mpe5@cornell.edu>
2013-01-24 20:30:07 +0000

    Added documentation to the code.

--- new_code/longitudinal_profile_mod.f90
+++ new_code/longitudinal_profile_mod.f90
@@ -1,3 +1,9 @@
+!+
+! Module longitudinal_profile_mod
+!
+! This module is for finding the bunch length taking RF and potential well distortion into account.
+! It is a solver for equation 11 from "Bunch Lengthening Via Vlassov Theory" by M. G. Billing, CBN 79-32
+!-
 MODULE longitudinal_profile_mod
 
 USE bmad
@@ -11,6 +17,29 @@ INTEGER(fgsl_size_t), PARAMETER :: limit = 1000_fgsl_size_t
 CONTAINS
 
 !+
+! Function psi_prime(t, p, dpdt, params) RETURN (fgsl_success)
+!
+! This is equation 11 from CBN 79-32.  It is a dpsi/dt, where psi is the height of the 
+! longitudinal profile and t is time.
+!
+! This function returns fgsl_success.
+!
+! Parameters (parama):
+!  A = args(1)      ! Energy/(sigma_E^2)/(alpha_compaction/(T_ring_period)
+!  Vrf = args(2)    !RF Voltage
+!  Q = args(3)      !Total bunch charge in Coulombs
+!  omega = args(4)  !RF Frequency
+!  phi = args(5)    !Phase of bunch center relative to RF
+!  R = args(6)      !Resistive part of impedance
+!  L = args(7)      !Inductive part of impedance
+!  U0 = args(8)     !Energy lost per turn per particle
+!
+! Input:
+!   t            -- REAL(rp): time relative to RF bucket
+!   p            -- REAL(rp): psi(t)
+!   params       -- TYPE(c_ptr), VALUE: pointer to parameters and constants of DEQ
+! Output:
+!   dpdt         -- REAL(c_double), DIMENSION(*): dpsi_dt
 !-
 FUNCTION psi_prime(t, p, dpdt, params) BIND(c)
   IMPLICIT none
@@ -35,16 +64,29 @@ FUNCTION psi_prime(t, p, dpdt, params) BIND(c)
   U0 = args(8)
   dpdt(1) = -A*p(1)*((Vrf*COS(omega*t+phi) + Q*R*p(1) - U0)/(1 + A*Q*L*p(1)))
 
-  psi_prime = fgsl_success
+  psi_prime = fgsl_success  !always return success
 END FUNCTION psi_prime
 
 !+
+! Subroutine psi_prime_sca(t, p, dpdt, args)
+!
+! This wraps the array-valued psi_prime function as a scalar.
+!
+! See psi_prime comments for details.
+!
+! Input:
+!   t         -- REAL(rp): time relative to RF bucket
+!   p         -- REAL(rp): psi(t)
+!   args(1:8) -- REAL(rp): parameters and constants of DEQ
+! Output:
+!   dpdt      -- REAL(rp): dpsi_dt
+!   
 !-
 SUBROUTINE psi_prime_sca(t, p, dpdt, args)
   IMPLICIT NONE
 
   REAL(rp) t, p, dpdt
-  REAL(rp) args(1:8)
+  REAL(rp), TARGET :: args(1:8)
   REAL(c_double) pa(1), dpdta(1)
   TYPE(c_ptr) ptr
 
@@ -58,6 +100,17 @@ SUBROUTINE psi_prime_sca(t, p, dpdt, args)
 END SUBROUTINE psi_prime_sca
 
 !+
+! Function jac(t, p, dfdp, dfdt, params)
+!
+! Where f = dpsi/dt, this returns df/dp and df/dt.
+!
+! Input:
+!   t       -- REAL(c_double), VALUE: time relative to RF
+!   p       -- REAL(c_double), DIMENSION(*): psi(t)
+!   params  -- TYPE(c_ptr), VALUE: parameters.  See psi_prime comments for details.
+! Output:
+!   dfdp    -- REAL(c_double), DIMENSION(*): d(dpsi/dt)/dp
+!   dfdt    -- REAL(c_double), DIMENSION(*): d(dpsi/dt)/dt
 !-
 FUNCTION jac(t, p, dfdp, dfdt, params) BIND(c)
   IMPLICIT none
@@ -89,22 +142,28 @@ FUNCTION jac(t, p, dfdp, dfdt, params) BIND(c)
 END FUNCTION jac
 
 !+
+! Subroutine solve_psi_adaptive(t0,t1,p0,args,p1)
+!
+! Solve dpsi/dt for psi(t1) using adaptive steps and method:
+!   "Implicit Bulirsch-Stoer method of Bader and Deuflhard."
+!
+! The boundary condition p0 is psi(t0)
+!
+! Input:
+!   t0        -- REAL(fgsl_double), VALUE: initial time
+!   t1        -- REAL(fgsl_double), VALUE: final time
+!   p0        -- REAL(rp): Boundary condition psi(t0)
+!   args(1:8) -- REAL(rp): Parameters.  See psi_prime comments for details.
+! Output:
+!   p1        -- REAL(rp): psi(t1)
 !-
 SUBROUTINE solve_psi_adaptive(t0,t1,p0,args,p1)
-  ! t0    initial time
-  ! t1    final time
-  ! p0    boundary condition psi(t0)
-  ! args  arguments for DEQ
-  ! p1    psi(t1)
-  ! 
-  ! Step size is adaptive.
-  !
   IMPLICIT none
   
   REAL(fgsl_double), VALUE :: t0  ! fgsl_odeiv2_driver_* fails without VALUE
   REAL(fgsl_double) t1
   REAL(rp) p0
-  REAL(rp) args(1:8)
+  REAL(rp), TARGET :: args(1:8)
   REAL(rp) p1
   type(fgsl_odeiv2_system) :: ode_system
   TYPE(fgsl_odeiv2_driver) :: ode_drv
@@ -128,7 +187,7 @@ SUBROUTINE solve_psi_adaptive(t0,t1,p0,args,p1)
 
   IF(status .ne. fgsl_success) THEN
     WRITE(*,'(A)') "ERROR: fgsl_odeiv2_driver_apply failed during bunch length calculation."
-    WRITE(*,'(A,2I)') "fgsl_odeiv2_driver_apply returned (success is zero): ", status
+    WRITE(*,'(A,2I6)') "fgsl_odeiv2_driver_apply returned (success is zero): ", status
     STOP
   ENDIF
 
@@ -139,23 +198,31 @@ SUBROUTINE solve_psi_adaptive(t0,t1,p0,args,p1)
 END SUBROUTINE solve_psi_adaptive
 
 !+
+! Subroutine solve_psi_fixed_steps(t0,t1,p0,args,t,p)
+!
+! Solve dpsi/dt for psi(t1) using fixed steps and method:
+!   "Implicit Bulirsch-Stoer method of Bader and Deuflhard."
+!
+! The boundary condition p0 is psi(t0).
+!
+! Number of steps is determined by SIZE(p).
+!
+! Input:
+!   t0        -- REAL(fgsl_double), VALUE: initial time
+!   t1        -- REAL(fgsl_double), VALUE: final time
+!   p0        -- REAL(rp): Boundary condition psi(t0)
+!   args(1:8) -- REAL(rp): Parameters.  See psi_prime comments for details.
+! Output:
+!   t(:)      -- REAL(rp): Array of times from t0 to t1
+!   p(:)      -- REAL(rp): Array of psi evaluated at t(:)
 !-
 SUBROUTINE solve_psi_fixed_steps(t0,t1,p0,args,t,p)
-  ! t0    initial time
-  ! t1    final time
-  ! p0    boundary condition psi(t0)
-  ! args  arguments for DEQ
-  ! t(:)  array of times
-  ! p(:)  array of psi
-  ! 
-  ! Step size is fixed and determined by (t1-t0)/size(p)
-  !
   IMPLICIT none
   
   REAL(rp) t0
   REAL(rp) t1
   REAL(rp) p0
-  REAL(rp) args(1:8)
+  REAL(rp), TARGET :: args(1:8)
   REAL(rp) t(:)
   REAL(rp) p(:)
   type(fgsl_odeiv2_system) :: ode_system
@@ -183,11 +250,11 @@ SUBROUTINE solve_psi_fixed_steps(t0,t1,p0,args,t,p)
   p(1) = p0
 
   DO i=2,n
-    status = fgsl_odeiv2_driver_apply_fixed_step(ode_drv, tcur, step_size, 1, y)
+    status = fgsl_odeiv2_driver_apply_fixed_step(ode_drv, tcur, step_size, 1_8, y)
 
     IF(status .ne. fgsl_success) THEN
       WRITE(*,'(A)') "ERROR: fgsl_odeiv2_driver_apply_fixed_step failed during bunch length calculation."
-      WRITE(*,'(A,2I)') "fgsl_odeiv2_driver_apply_fixed_step returned ", status, fgsl_success
+      WRITE(*,'(A,2I6)') "fgsl_odeiv2_driver_apply_fixed_step returned ", status, fgsl_success
       STOP
     ENDIF
 
@@ -200,6 +267,17 @@ SUBROUTINE solve_psi_fixed_steps(t0,t1,p0,args,t,p)
 END SUBROUTINE solve_psi_fixed_steps
 
 !+
+! Subroutine integrate_psi(bound,p0,args,result)
+!
+! Integrate psi(t) from -bound to +bound.  The integration is done in two parts.  First from 0 to -bound, then from
+! 0 to +bound.
+!
+! Input:
+!   bound      -- REAL(rp): integration bound
+!   p0         -- REAL(rp): psi(0).  Boundary condition.
+!   args(1:8)  -- REAL(rp): Parameters and constants of DEQ.  See psi_prime comments for details.
+! Output:
+!   result     -- REAL(rp): Integral of psi from -bound to +bound.
 !-
 SUBROUTINE integrate_psi(bound,p0,args,result)
   REAL(rp) bound
@@ -253,6 +331,18 @@ SUBROUTINE integrate_psi(bound,p0,args,result)
 END SUBROUTINE integrate_psi
 
 !+
+! Subroutine find_normalization(bound,p0,args,pnrml)
+!
+! Finds value for boundary condition psi(0) that results in integral
+! of psi(t) from -bound to +bound to be 1.0.  This is done with the secant method.
+! Repeadedly calls integrate_psi with different values for psi(0).
+!
+! Input:
+!   bound     -- REAL(rp): -bound and +bound are integration boundaries
+!   p0        -- REAL(rp): Boundary condition psi(0)
+!   args(1:8) -- REAL(rp): Parameters and constants of DEQ.  See psi_prime comments for details.
+! Output:
+!   pnrml     -- REAL(rp): Value for psi(0) that results in integral of psi(t) from -bound to +bound being equal to 1.0
 !-
 SUBROUTINE find_normalization(bound,p0,args,pnrml)
   !Secant Method
@@ -283,6 +373,22 @@ SUBROUTINE find_normalization(bound,p0,args,pnrml)
 END SUBROUTINE find_normalization
 
 !+
+! Subroutine find_fwhm(bound,args,fwhm)
+!
+! Finds the full width at half max of psi(t).  fwhm * c_light / TwoRtTwoLnTwo is taken as the bunch length.
+!
+! Steps followed:
+!   Find value for p(0) that normalizes the solution to dpsi/dt.
+!   Find max value of p(t) for the value of p(0) found in the previous step.
+!   Find find tlower, tlower < 0, such that p(tlower) = pmax/2.
+!   Find find tupper, tupper > 0, such that p(tupper) = pmax/2.
+!   fwhm is tupper-tlower
+!
+! Input:
+!   bound      -- REAL(rp): -bound and +bound is integration bound.
+!   args(1:8)  -- REAL(rp): Parameters and constants of dpsi/dt.  See comments of psi_prime for details.
+! Output:
+!   fwhm       -- REAL(rp): Full width at half max of psi(t)
 !-
 SUBROUTINE find_fwhm(bound,args,fwhm)
   IMPLICIT NONE
@@ -390,6 +496,16 @@ SUBROUTINE find_fwhm(bound,args,fwhm)
 END SUBROUTINE find_fwhm
 
 !+
+! Subroutine get_bl_from_fwhm(bound,args,sigma)
+!
+! Calculate bunch length as fwhm * c_light / TwoRtTwoLnTwo.
+! Where fwhm is full width at half max of solution to dpsi/dt.
+!
+! Input:
+!   bound     -- REAL(rp): -bound and +bound are lower and upper integration bound.
+!   args(1:8) -- REAL(rp): Parameters and constants of dpsi/dt.  See comments of psi_prime for details.
+! Output:
+!   sigma     -- REAL(rp): Bunch length
 !-
 SUBROUTINE get_bl_from_fwhm(bound,args,sigma)
   IMPLICIT none
