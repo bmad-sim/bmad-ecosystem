a03c32281f1101c02ff1c3f225f00eb4f34ac692
David Sagan <dcs16@cornell.edu>
2013-04-19 03:07:47 +0000

    Added mat_scale_p0 routine.

--- matrix/mat_symp_error.f90
+++ matrix/mat_symp_error.f90
@@ -1,15 +1,24 @@
 !+
-! Function mat_symp_error (mat) result (error)
+! Function mat_symp_error (mat, p0_ratio) result (error)
 !
 ! Routine to check the symplecticity of a square matrix. The error is
 ! defined to via:
 !     error = maxval (abs (Mat_transpose * S * Mat - S))
 !
+! When using coordinates like (x, Px/P0, y, Py/P0, z, dP/P0), where P0 is
+! the reference momentum, then:
+!   p0_ratio = P0(exit coords) / P0(entrance coords). 
+! Normally this is 1 except when dealing with something like an accelerating RF cavity.
+! When the p0_ratio is not 1, mat is scalled to coordinates p0_ratio is 1 before the
+! symplectic error is computed.
+!
 ! Module Needed:
 !   use sim_utils_interface
 !
 ! Input:
-!   mat(:,:) -- Real(rp): Matrix to check
+!   mat(:,:)    -- Real(rp): Matrix to check
+!   p0_ratio    -- Real(rp): optional: Ratio of p0_exit / p0_entrance. Default is 1.
+!                            If present, the size of mat_in must be 6.
 !
 ! Output:
 !   error -- Real(rp): difference from symplecticity:
@@ -18,7 +27,7 @@
 !             = 1    --> Terrible.
 !-
 
-function mat_symp_error (mat) result (error)
+function mat_symp_error (mat, p0_ratio) result (error)
 
 use output_mod, except => mat_symp_error
 
@@ -27,7 +36,8 @@ implicit none
 integer i, j, n
 
 real(rp), intent(in) :: mat(:,:)
-real(rp) :: m2(size(mat, 1), size(mat, 1))
+real(rp), optional :: p0_ratio
+real(rp) :: m1(size(mat, 1), size(mat, 1)), m2(size(mat, 1), size(mat, 1))
 real(rp) error
 
 logical :: debug = .false.
@@ -43,12 +53,14 @@ endif
 
 !
 
+m1 = mat_scale_p0 (mat, p0_ratio, .true.)
+
 do j = 2, n, 2                  ! m2 = mat_transpose * S
-  m2(1:n,j)   = -mat(j-1,1:n)
-  m2(1:n,j-1) =  mat(j,  1:n)
+  m2(1:n,j)   = -m1(j-1,1:n)
+  m2(1:n,j-1) =  m1(j,  1:n)
 enddo
 
-m2 = matmul (m2, mat)
+m2 = matmul (m2, m1)
 
 do j = 2, n, 2
   m2(j, j-1) = m2(j, j-1) - 1
--- matrix/mat_symplectify.f90
+++ matrix/mat_symplectify.f90
@@ -1,27 +1,39 @@
 !+
-! Subroutine mat_symplectify (mat_in, mat_symp, f_scale)
+! Subroutine mat_symplectify (mat_in, mat_symp, p0_ratio, r_root)
 !
 ! Subroutine to form the symplectic matrix that is as "close" as possible to the input matrix.
-! If mat_in is symplectic, and f_length is 1, then mat_sym will be equal to mat_in.
+! If mat_in is symplectic, and r_root is 1, then mat_sym = mat_in.
 ! 
-! The f_scale argument is used to "scale" mat_symp such that if mat_in is symplectic the
+! When using coordinates like (x, Px/P0, y, Py/P0, z, dP/P0), where P0 is 
+! the reference momentum, then:
+!   p0_ratio = P0(exit coords) / P0(entrance coords). 
+! Normally this is 1 except when dealing with something like an accelerating RF cavity.
+! In the case where p0_ratio is not 1, the procedure is:
+!   1) scale mat_in to a coordinate system where p0_ratio is 1.
+!   2) symplecterize matrix.
+!   3) scale back to a coordinate system with the given p0_ratio.
+! Note: This also is usful with (-t, E) longitudinal coordinates
+!
+! The r_root argument is used to "scale" mat_symp such that if mat_in is symplectic the
 ! following is true:
-!   f_scale = 0   --> mat_symp = I (unit matrix, always true independent of mat_in)
-!   f_scale = 1/N --> mat_symp^N = mat_in
-!   f_scale = 1   --> mat_symp = mat_in
+!   r_root = 0   --> mat_symp = I (unit matrix, always true independent of mat_in)
+!   r_root = 1/N --> mat_symp^N ~ mat_in
+!   r_root = 1   --> mat_symp ~ mat_in
 !   
 ! Module needed:
 !   use sim_utils_interface
 !
 ! Input:
 !   mat_in(:,:) -- Real(rp): Input matrix to symplectify.
-!   f_scale     -- Real(rp), optional: Scaling factor. Default is 1.
+!   p0_ratio    -- Real(rp): optional: Ratio of p0_exit / p0_entrance. Default is 1.
+!                            If present, the size of mat_in must be 6.
+!   r_root      -- Real(rp), optional: Scaling factor. Default is 1.
 !
 ! Output:
 !   mat_symp(:,:) -- Real(rp): Symplectic output matrix
 !-
 
-subroutine mat_symplectify (mat_in, mat_symp, f_scale)
+subroutine mat_symplectify (mat_in, mat_symp, p0_ratio, r_root)
 
 use output_mod, except => mat_symplectify
 
@@ -29,7 +41,7 @@ implicit none
 
 real(rp), intent(in)  :: mat_in(:,:)
 real(rp), intent(out) :: mat_symp(:,:)
-real(rp), intent(in), optional :: f_scale
+real(rp), intent(in), optional :: p0_ratio, r_root
 
 real(rp), dimension(size(mat_in, 1), size(mat_in, 2)) :: m1, m2, m3, m_symmetric
 
@@ -39,7 +51,7 @@ integer n,i,j
 
 n = ubound(mat_in, 1)
 
-if (ubound(mat_symp, 1) /= n) then
+if (ubound(mat_symp, 2) /= n) then
   print *, 'ERROR IN MAT_SYMPLECTIFY: UNEQUAL MATRIX SIZES.'
   if (global_com%exit_on_error) call err_exit
 endif
@@ -48,10 +60,12 @@ endif
 !         m1 = S * (1 - mat_in) * (1 + mat_in)^-1
 !         m_symmetric = (m1 + m1_transpose) / 2
 
-m1 =-mat_in
+m3 = mat_scale_p0 (mat_in, p0_ratio, .true.)
+
+m1 =-m3
 forall (i = 1:n) m1(i,i) = m1(i,i) + 1
 
-m2 = mat_in
+m2 = m3
 forall (i = 1:n) m2(i,i) = m2(i,i) + 1
 call mat_inverse (m2, m2)
 
@@ -64,9 +78,9 @@ enddo
 
 forall (i = 1:n, j = 1:n) m_symmetric(i,j) = (m1(i,j) + m1(j,i)) / 2
 
-! Scale by f_scale
+! Scale by r_root
 
-if (present(f_scale)) m_symmetric = f_scale * m_symmetric
+if (present(r_root)) m_symmetric = r_root * m_symmetric
 
 ! Form the symplectic matrix
 !         m_symp = (1 + S * m_symmetric) * (1 - S * m_symmetric)^-1
@@ -83,6 +97,6 @@ m3 = -m1
 forall (i = 1:n) m3(i,i) = m3(i,i) + 1
 call mat_inverse (m3, m3)
 
-mat_symp = matmul (m2, m3)
+mat_symp = mat_scale_p0 (matmul(m2, m3), p0_ratio)
 
 end subroutine
