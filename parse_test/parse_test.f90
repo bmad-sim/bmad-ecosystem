21d100708bed0967b06457699f736a0efa7e6742
David Sagan <dcs16@cornell.edu>
2013-06-18 15:35:05 +0000

    Added parse_test to list of tests.

--- parse_test/parse_test.f90
+++ parse_test/parse_test.f90
@@ -20,13 +20,13 @@ call bmad_parser ('parse_test.bmad', lat)
 
 open (1, file = 'output.now')
 
-write (1, '(a, f10.4)') '"1 REL"  1E-10', lat%ele(1)%value(k1$)
-write (1, '(a, f10.4)') '"2 REL"  1E-10', lat%ele(2)%value(k1$)
-write (1, '(a, f10.4)') '"3 REL"  1E-10', lat%ele(3)%value(k1$)
-write (1, '(a, f10.4)') '"4 REL"  1E-10', lat%ele(4)%value(k1$)
-write (1, '(a, f10.4)') '"5 REL"  1E-10', lat%ele(5)%value(k2$)
-write (1, '(2a)')       '"TM1"     STR ', tracking_method_name(lat%ele(1)%tracking_method)
-write (1, '(2a)')       '"TM5"     STR ', tracking_method_name(lat%ele(5)%tracking_method)
+write (1, '(a, f10.4)') '"1 REL"  ABS 0', lat%ele(1)%value(k1$)
+write (1, '(a, f10.4)') '"2 REL"  ABS 0', lat%ele(2)%value(k1$)
+write (1, '(a, f10.4)') '"3 REL"  ABS 0', lat%ele(3)%value(k1$)
+write (1, '(a, f10.4)') '"4 REL"  ABS 0', lat%ele(4)%value(k1$)
+write (1, '(a, f10.4)') '"5 REL"  ABS 0', lat%ele(5)%value(k2$)
+write (1, '(4a)')       '"TM1"     STR ', '"', trim(tracking_method_name(lat%ele(1)%tracking_method)), '"'
+write (1, '(4a)')       '"TM5"     STR ', '"', trim(tracking_method_name(lat%ele(5)%tracking_method)), '"'
 
 close(1)
 
