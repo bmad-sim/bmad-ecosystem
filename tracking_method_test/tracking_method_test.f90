33d58d8f18c3affdd3e31195fe3e03b87a62f14f
David Sagan <dcs16@cornell.edu>
2013-07-11 20:07:05 +0000

    Fixed tolerance.

--- tracking_method_test/tracking_method_test.f90
+++ tracking_method_test/tracking_method_test.f90
@@ -7,7 +7,7 @@ use mad_mod
 implicit none
 
 type (lat_struct), target :: lat
-type (coord_struct) end_orb
+type (coord_struct) start_orb, end_orb
 type (branch_struct), pointer :: branch
 type (ele_struct), pointer :: ele
 
@@ -44,7 +44,7 @@ do ib = 0, ubound(lat%branch, 1)
   DO i = 1, branch%n_ele_max - 1
      ele => branch%ele(i)
      DO j = 1, n_methods$
-        if(.not. valid_tracking_method(ele,j) .or. j == symp_map$ .or. j == custom$) cycle
+        if(.not. valid_tracking_method(ele, branch%param%particle, j) .or. j == symp_map$ .or. j == custom$) cycle
         if(ele%key == elseparator$ .and. (j == runge_kutta$ .or. j == boris$ .or. j == time_runge_kutta$)) cycle
         call kill_taylor(ele%taylor)
         ele%tracking_method = j
@@ -54,10 +54,14 @@ do ib = 0, ubound(lat%branch, 1)
           call make_mat6 (ele, branch%param, lat%beam_start)
           ele%tracking_method = j
         endif
-        call init_coord (lat%beam_start, lat%beam_start, ele = ele, at_downstream_end = .false.)
-        call track1 (lat%beam_start, ele, branch%param, end_orb)
+        start_orb = lat%beam_start
+        call init_coord (start_orb, start_orb, ele, .false., E_photon = ele%value(p0c$) * (1 + start_orb%vec(6)))
+        call track1 (start_orb, ele, branch%param, end_orb)
         final_str = '"' // trim(ele%name) // ':' // trim(tracking_method_name(j)) // '"' 
-        write (1,'(2a,7es18.10)') final_str, tolerance(final_str), end_orb%vec, c_light * (end_orb%t - lat%beam_start%t)
+        write (1,'(2a,7es18.10)') final_str, tolerance(final_str), end_orb%vec, c_light * (end_orb%t - start_orb%t)
+        if (branch%param%particle == photon$) then
+          write (1, '(4a, 2es18.10)') '"', trim(ele%name), ':E_Field', '"        ABS 1E-10', end_orb%field
+        endif
      END DO
      write (1,*)
   END DO
@@ -72,16 +76,16 @@ character(10) function tolerance(instr)
   character(38) :: instr
 
   select case (instr)
-     case('"RFCAVITY1:Time_Runge_Kutta"          ') ; tolerance = 'REL  1E-08'
-     case('"RFCAVITY2:Linear"                    ') ; tolerance = 'REL  1E-07'
-     case('"RFCAVITY2:Time_Runge_Kutta"          ') ; tolerance = 'REL  1E-08'
-     case('"SBEND4:Symp_Lie_PTC"                 ') ; tolerance = 'REL  1E-03'
-     case('"SBEND4:Linear"                       ') ; tolerance = 'REL  1E-03'
-     case('"SBEND6:Symp_Lie_PTC"                 ') ; tolerance = 'REL  1E-03'
-     case('"SBEND6:Linear"                       ') ; tolerance = 'REL  1E-03'
-     case('"LCAVITY1:Time_Runge_Kutta"           ') ; tolerance = 'REL  1E-08'
-     case('"LCAVITY3:Time_Runge_Kutta"           ') ; tolerance = 'REL  1E-08'
-     case default ; tolerance = 'REL  1E-10'
+     case('"RFCAVITY1:Time_Runge_Kutta"') ; tolerance = 'REL  1E-08'
+     case('"RFCAVITY2:Linear"')           ; tolerance = 'REL  1E-07'
+     case('"RFCAVITY2:Time_Runge_Kutta"') ; tolerance = 'REL  1E-08'
+     case('"SBEND4:Symp_Lie_PTC"')        ; tolerance = 'REL  1E-03'
+     case('"SBEND4:Linear"')              ; tolerance = 'REL  1E-03'
+     case('"SBEND6:Symp_Lie_PTC"')        ; tolerance = 'REL  1E-03'
+     case('"SBEND6:Linear"')              ; tolerance = 'REL  1E-03'
+     case('"LCAVITY1:Time_Runge_Kutta"')  ; tolerance = 'REL  1E-08'
+     case('"LCAVITY3:Time_Runge_Kutta"')  ; tolerance = 'REL  1E-08'
+     case default                         ; tolerance = 'REL  1E-10'
   end select
 
 end function tolerance
