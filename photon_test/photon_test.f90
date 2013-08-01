9883fc390a44c510b82a85aee9e12ebfc6f0d019
David Sagan <dcs16@cornell.edu>
2013-08-01 19:28:44 +0000

    Added photon_test.

---
+++ photon_test/photon_test.f90
@@ -0,0 +1,33 @@
+program photon_test
+
+use photon_init_mod
+
+implicit none
+
+type (coord_struct) orbit
+real(rp) E_rel, prob
+integer i
+
+!
+
+open (1, file = 'output.now', recl = 200)
+
+do i = 0, 5
+  call photon_energy_init(E_rel, i / 5.0_rp)
+  write (1, '(a, i0, a, f16.10)') '"E_rel_', i, '" ABS 0', E_rel 
+enddo
+
+call ran_engine ('quasi')
+call photon_init (0.10_rp, 0.02_rp, 1d4, 1.0d3, 1.1d3, .true., orbit, prob)
+write (1, '(a, 6f14.8)') '"Photon_vec"   ABS 0', orbit%vec
+write (1, '(a, 6f14.4)') '"Photon_p0c"   ABS 0', orbit%p0c
+write (1, '(a, 6f14.8)') '"Photon_field" ABS 0', orbit%field
+write (1, '(a, 6f14.8)') '"Photon_prob"  ABS 0', prob
+
+call photon_init (0.10_rp, 0.02_rp, 1d4, 0d0, 0d0, .true., orbit, prob)
+write (1, '(a, 6f14.8)') '"Photon2_vec"   ABS 0', orbit%vec
+write (1, '(a, 6f14.4)') '"Photon2_p0c"   ABS 0', orbit%p0c
+write (1, '(a, 6f14.8)') '"Photon2_field" ABS 0', orbit%field
+write (1, '(a, 6f14.8)') '"Photon2_prob"  ABS 0', prob
+
+end program
