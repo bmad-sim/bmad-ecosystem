ce887b2e77a09c5ae1aaa18f09a4f8ca82ae1929
David Sagan <dcs16@cornell.edu>
2013-08-06 15:09:00 +0000

    Stub for ptc call.

---
+++ code/tao_ptc_cmd.f90
@@ -0,0 +1,20 @@
+!+
+! Subroutine tao_ptc_cmd (input_str)
+!
+! Interface to ptc layout.
+!
+! Input:
+!   input_str  -- Character(*): What to show.
+!-
+
+subroutine tao_ptc_cmd (input_str)
+
+use tao_mod
+
+implicit none
+
+character(*) input_str
+
+!
+
+end subroutine
