91dcd697d7174b2fd70ce04c862cfe64392c68d0
David Sagan <dcs16@cornell.edu>
2013-08-07 14:07:55 +0000

    kill_ptc_layouts now in stand alone file to solve module dependency problem.

---
+++ code/kill_ptc_layouts.f90
@@ -0,0 +1,39 @@
+!+
+! Subroutine kill_ptc_layouts (lat)
+!
+! Routine to kill the layouts associated with a Bmad lattice.
+!
+! Module Needed:
+!   use ptc_layout_mod
+!
+! Input: 
+!   lat  -- lat_struct: Bmad lattice with associated layouts.
+!-
+
+subroutine kill_ptc_layouts (lat)
+
+use ptc_layout_mod
+use madx_ptc_module
+
+implicit none
+
+type (lat_struct), target :: lat
+type (branch_struct), pointer :: branch
+
+integer ib, il
+
+!
+
+if (.not. allocated(lat%branch)) return
+
+do ib = 0, ubound(lat%branch, 1)
+  branch => lat%branch(ib)
+  if (.not. allocated(branch%ptc%layout)) cycle
+  do il = 1, size(branch%ptc%layout)
+    call kill_layout_in_universe(branch%ptc%layout(il)%ptr)
+  enddo
+  deallocate(branch%ptc%layout)
+enddo
+
+end subroutine kill_ptc_layouts
+
