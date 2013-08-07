82e5761cd25695b6bd01e376d3ea6933032fb9e0
David Sagan <dcs16@cornell.edu>
2013-08-07 13:57:43 +0000

    more ptc devel.

--- code/tao_ptc_cmd.f90
+++ code/tao_ptc_cmd.f90
@@ -1,5 +1,5 @@
 !+
-! Subroutine tao_ptc_cmd (input_str)
+! Subroutine tao_ptc_cmd (what, input_str)
 !
 ! Interface to ptc layout.
 !
@@ -7,14 +7,40 @@
 !   input_str  -- Character(*): What to show.
 !-
 
-subroutine tao_ptc_cmd (input_str)
+subroutine tao_ptc_cmd (what, input_str)
 
 use tao_mod
+use ptc_layout_mod
 
 implicit none
 
-character(*) input_str
+integer ix_cmd
+
+character(*) what, input_str
+character(16) :: command(2) = ['init  ', 'script']
+character(16) cmd_name
+character(*), parameter :: r_name = 'tao_ptc_cmd'
 
 !
+
+call match_word (what, command, ix_cmd, .true., matched_name = cmd_name)
+if (ix_cmd == 0) then
+  call out_io (s_error$, r_name, 'BAD COMMAND (SHOULD BE "init", or "script")')
+  return
+endif
+
+select case (cmd_name)
+
+! init
+
+case ('init')
+  call lat_to_ptc_layout (s%u(1)%model%lat)
+
+! script
+
+case ('script')
+  call read_ptc_command77 (input_str)
+
+end select
 
 end subroutine
