a3b7b5cc5c6a5b442c34907df89ccdee070ab26d
David Sagan <dcs16@cornell.edu>
2013-08-08 20:46:05 +0000

    change for change in set_flags_for_changed_attribute call.

--- compare_tracking_methods_text/compare_tracking_methods_text.f90
+++ compare_tracking_methods_text/compare_tracking_methods_text.f90
@@ -22,16 +22,16 @@ DO i = 1, lat%n_ele_max - 1
       if(.not. valid_tracking_method(lat%ele(i), lat%param%particle, j) .or. j == symp_map$ .or. j == custom$) cycle
       if(lat%ele(i)%key == elseparator$ .and. (j == runge_kutta$ .or. j == boris$ .or. j == time_runge_kutta$)) cycle
       lat%ele(i)%tracking_method = j
-      call set_flags_for_changed_attribute(lat,lat%ele(i),lat%ele(i)%tracking_method)
+      call set_flags_for_changed_attribute(lat%ele(i),lat%ele(i)%tracking_method)
       call lattice_bookkeeper(lat)
       if (j == linear$) then
         lat%ele(i)%tracking_method = symp_lie_ptc$
         if(lat%ele(i)%key == beambeam$) lat%ele(i)%tracking_method = bmad_standard$
-        call set_flags_for_changed_attribute(lat,lat%ele(i),lat%ele(i)%tracking_method)
+        call set_flags_for_changed_attribute(lat%ele(i),lat%ele(i)%tracking_method)
         call lattice_bookkeeper(lat)
         call make_mat6 (lat%ele(i), lat%param, lat%beam_start)
         lat%ele(i)%tracking_method = j
-        call set_flags_for_changed_attribute(lat,lat%ele(i),lat%ele(i)%tracking_method)
+        call set_flags_for_changed_attribute(lat%ele(i),lat%ele(i)%tracking_method)
         call lattice_bookkeeper(lat)
       endif
       call init_coord (lat%beam_start, lat%beam_start, ele = lat%ele(i), at_downstream_end = .false.)
