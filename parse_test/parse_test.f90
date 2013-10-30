4cc9b19898ddb9c514842cd856f45ad119519379
David Sagan <dcs16@cornell.edu>
2013-10-30 20:17:32 +0000

    more devel.

--- parse_test/parse_test.f90
+++ parse_test/parse_test.f90
@@ -27,6 +27,7 @@ write (1, '(a, f10.4)') '"4 REL"  ABS 0', lat%ele(4)%value(k1$)
 write (1, '(a, f10.4)') '"5 REL"  ABS 0', lat%ele(5)%value(k2$)
 write (1, '(4a)')       '"TM1"     STR ', '"', trim(tracking_method_name(lat%ele(1)%tracking_method)), '"'
 write (1, '(4a)')       '"TM5"     STR ', '"', trim(tracking_method_name(lat%ele(5)%tracking_method)), '"'
+write (1, '(4a)')       '"Custom"  STR ', '"', trim(attribute_name(lat%ele(1), custom_attribute1$)), '"'
 
 close(1)
 
