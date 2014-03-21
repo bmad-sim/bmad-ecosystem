c29587f11583b83de29418ebadac866d5e7b747d
David Sagan <dcs16@cornell.edu>
2014-03-21 11:09:15 +0000

    change for change in bmad structures.

--- xraylib_test/xraylib_test.f90
+++ xraylib_test/xraylib_test.f90
@@ -98,8 +98,8 @@ ele => lat%ele(1)    ! crystal
 write (1, '(a, f16.10)') '"Bragg_In"   REL 1E-8', ele%value(bragg_angle_in$)
 write (1, '(a, f16.10)') '"Bragg_Out"  REL 1E-8', ele%value(bragg_angle_out$)
 write (1, '(a, f16.10)') '"Alpha_Ang"  REL 1E-8', ele%value(alpha_angle$)
-write (1, '(a, f16.10)') '"F0_Re"      REL 1E-8', ele%value(f0_re$)
-write (1, '(a, f16.10)') '"F0_Im"      REL 1E-8', ele%value(f0_im$)
+write (1, '(a, f16.10)') '"F0_Re"      REL 1E-8', real(ele%photon%material%f_0)
+write (1, '(a, f16.10)') '"F0_Im"      REL 1E-8', aimag(ele%photon%material%f_0)
 write (1, '(a, es20.8)') '"FH_Re"      REL 1E-8', real(ele%photon%material%f_h)
 write (1, '(a, es20.8)') '"FH_Im"      REL 1E-8', aimag(ele%photon%material%f_h)
 write (1, '(a, es20.8)') '"Darwin_Sig" REL 1E-8', ele%value(darwin_width_sigma$)
@@ -109,8 +109,8 @@ write (1, '(a, es20.8)') '"D_Spacing"  REL 1E-8', ele%value(d_spacing$)
 ele => lat%ele(2)    ! multilayer_mirror
 write (1, '(a, es20.8)') '"Graze_Ang"  REL 1E-8', ele%value(graze_angle$)
 write (1, '(a, es20.8)') '"V1_Cell"    REL 1E-8', ele%value(v1_unitcell$)
-write (1, '(a, es20.8)') '"F0_Re1"     REL 1E-8', ele%value(f0_re1$)
-write (1, '(a, es20.8)') '"F0_Im2"     REL 1E-8', ele%value(f0_im2$)
+write (1, '(a, es20.8)') '"F0_Re1"     REL 1E-8', real(ele%photon%material%f0_m1)
+write (1, '(a, es20.8)') '"F0_Im2"     REL 1E-8', aimag(ele%photon%material%f0_m2)
 
 !
 
