d4eb66f5b60c4eef2d0bf4af5d7d6f4b194cccc3
David Sagan <dcs16@cornell.edu>
2014-04-23 21:11:21 +0000

    More SAD devel.

--- modules/sad_mod.f90
+++ modules/sad_mod.f90
@@ -39,7 +39,7 @@ type (lat_param_struct) :: param
 
 real(rp) rel_pc, dz4_coef(4,4), mass, e_tot
 real(rp) ks, k1, length, z_start, charge_dir, kx, ky
-real(rp) xp_start, yp_start, mat4(4,4), mat1(6,6), f1, f2, ll
+real(rp) xp_start, yp_start, mat4(4,4), mat1(6,6), f1, f2, ll, k0
 real(rp) knl(0:n_pole_maxx), tilt(0:n_pole_maxx)
 real(rp), pointer :: mat6(:,:)
 real(rp) :: vec0(6), kmat(6,6)
@@ -68,9 +68,9 @@ n_div = nint(ele%value(num_steps$))
 rel_pc = 1 + orbit%vec(6)
 orientation = ele%orientation * orbit%direction
 charge_dir = param%rel_tracking_charge * orientation
+mat6 => ele%mat6
 
 if (make_matrix) then
-  mat6 => ele%mat6
   call mat_make_unit(mat6)
 endif
 
@@ -126,13 +126,18 @@ tilt = tilt - tilt(1)
 
 call offset_particle (ele2, param, set$, orbit, set_multipoles = .false., set_hvkicks = .false.)
 
-! Edge kick
+! Quadrupole edge kick
 
 if (make_matrix) then
   call quadrupole_edge_mat6 (ele, first_track_edge$, orbit, kmat, fringe_here)
   if (fringe_here) mat6 = kmat
 endif
 call quadrupole_edge_kick (ele, first_track_edge$, orbit)
+
+! Dipole edge kick
+
+k0 = knl(0)/length
+call sad_linear_dipole_edge (ele, k0, tilt(0), first_track_edge$, orbit, mat6, make_matrix)
 
 ! Body
 
@@ -157,10 +162,10 @@ do nd = 0, n_div
   ! track step
 
   if (abs(k1) < 1d-40) then
-    xp_start = orbit%vec(2) + ks * orbit%vec(3) / (2 * rel_pc)
-    yp_start = orbit%vec(4) - ks * orbit%vec(1) / (2 * rel_pc)
+    xp_start = orbit%vec(2) + ks * orbit%vec(3) / 2 
+    yp_start = orbit%vec(4) - ks * orbit%vec(1) / 2
     call solenoid_mat4_calc (ks, ll, rel_pc, mat4)
-    orbit%vec(5) = orbit%vec(5) - ll * (xp_start**2 + yp_start**2 ) / 2
+    orbit%vec(5) = orbit%vec(5) - ll * (xp_start**2 + yp_start**2 ) / (2 * rel_pc**2)
     orbit%vec(1:4) = matmul (mat4, orbit%vec(1:4))
   else
     vec0 = 0
@@ -200,11 +205,19 @@ if (ele%value(x_offset_mult$) /= 0 .or. ele%value(y_offset_mult$) /= 0) then
   orbit%vec(4) = orbit%vec(4) + ele%value(x_offset_mult$) * ks / 2
 endif
 
+! Dipole edge kick
+
+call sad_linear_dipole_edge (ele, k0, tilt(0), second_track_edge$, orbit, mat6, make_matrix)
+
+! Quadrupole edge kick
+
 if (make_matrix) then
   call quadrupole_edge_mat6 (ele, second_track_edge$, orbit, kmat, fringe_here)
   if (fringe_here) mat6 = matmul(kmat, mat6)
 endif
 call quadrupole_edge_kick (ele, second_track_edge$, orbit)
+
+!
 
 call offset_particle (ele2, param, unset$, orbit, set_multipoles = .false., set_hvkicks = .false.)
 
@@ -232,5 +245,91 @@ orbit%s = ele%s
 if (.not. end_in) end_orb = orbit
 
 end subroutine sad_mult_track_and_mat 
+
+!----------------------------------------------------------------------------------------------
+!----------------------------------------------------------------------------------------------
+!----------------------------------------------------------------------------------------------
+!+
+! Subroutine sad_linear_dipole_edge (ele, g_bend, tilt, particle_at, orbit, mat6, make_matrix)
+!
+! Routine to track through the "linear" dipole fringe field.
+!
+! Input:
+!   ele         -- ele_struct: Element with fringe.
+!   g_bend      -- real(rp): Dipole bend strength.
+!   tilt        -- real(rp): field rotation.
+!   particle_at -- integer: Either first_track_edge$ or second_track_edge$.
+!   orbit       -- coord_struct: Starting coordinates.
+!   mat6(6,6)   -- real(rp): Transfer matrix up to the fringe.
+!   make_matrix -- logical: Make the transfer matrix?
+!
+! Output:
+!   orbit       -- coord_struct: Ending coordinates.
+!   mat6(6,6)   -- real(rp), optional: Transfer matrix including the fringe.
+!-
+
+subroutine sad_linear_dipole_edge (ele, g_bend, tilt, particle_at, orbit, mat6, make_matrix)
+
+implicit none
+
+type (ele_struct) ele
+type (coord_struct) orbit
+
+real(rp) :: mat6(6,6)
+real(rp) g_bend, tilt
+real(rp) g, px, y, y2, rel_p, p_zy, yg, kmat(6,6)
+
+integer fringe_type, fringe_at, physical_end, particle_at
+logical make_matrix
+
+! Fringe here?
+
+fringe_type = nint(ele%value(fringe_type$))
+if (fringe_type /= sad_linear$ .and. fringe_type /= sad_full$) return
+
+fringe_at = nint(ele%value(fringe_at$))
+physical_end = physical_ele_end (particle_at, orbit%direction, ele%orientation)
+if (.not. at_this_ele_end(physical_end, fringe_at)) return
+
+! Rotate
+
+if (tilt /= 0) call tilt_coords (tilt, orbit%vec)
+
+! edge kick
+
+g = g_bend
+if (particle_at == second_track_edge$) g = -g
+
+px = orbit%vec(2)
+y = orbit%vec(3)
+y2 = y**2
+rel_p = 1 + orbit%vec(6)
+p_zy = sqrt(rel_p**2 - px**2)
+yg = y2 * g**2 / 12
+
+orbit%vec(1) = orbit%vec(1) + g * y2 * (1 - yg) * rel_p**2 / (2 * p_zy**3)
+orbit%vec(4) = orbit%vec(4) - g * px * y * (1 - 2 * yg) / p_zy
+orbit%vec(5) = orbit%vec(5) - g * y2 * px * (1 - yg) * rel_p / (2 * p_zy**3)
+
+if (make_matrix) then
+  call mat_make_unit(kmat)
+  kmat(1,2) = 3 * g * px * y2 * (1 - yg) * rel_p**2 / (2 * p_zy**5)
+  kmat(1,3) =  g * (y - 2 * y * yg) * rel_p**2 / p_zy**3
+  kmat(1,6) = -g * y2 * (1 - yg) * rel_p * (rel_p**2 + 2 * px**2) / (2 * p_zy**5)
+  kmat(4,2) = -g * y * (1 - 2 * yg) * rel_p**2 / p_zy**3
+  kmat(4,3) = -g * px * (1 - 6 * yg) / p_zy
+  kmat(4,6) =  g * px * y * (1 - 2 * yg) * rel_p / p_zy**3
+  kmat(5,2) = -g * y2 * (1 - yg) * (rel_p**2 + 2 * px**2) * rel_p / (2 * p_zy**5)
+  kmat(5,3) = -g * px * (y - 2 * y * yg) * rel_p / p_zy**3
+  kmat(5,6) =  g * px * y2 * (1 - 2 * yg) * (2 * rel_p**2 + px**2) / (2 * p_zy**5)
+  call tilt_mat6(kmat, tilt)
+  mat6 = matmul(kmat, mat6)
+endif
+
+! Rotate
+
+if (tilt /= 0) call tilt_coords (-tilt, orbit%vec)
+
+end subroutine sad_linear_dipole_edge
 
 end module
