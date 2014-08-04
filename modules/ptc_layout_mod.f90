1da0fef545b22edf7d3248a796c54cad26e5a6d8
David Sagan <dcs16@cornell.edu>
2014-08-04 18:06:07 +0000

    no fringe with zero length sad_mult.

--- modules/ptc_layout_mod.f90
+++ modules/ptc_layout_mod.f90
@@ -1126,6 +1126,8 @@ if (attribute_index(ele, 'FRINGE_TYPE') > 0) then  ! If fringe_type is a valid a
   case (sad_full$)
     call set_integer (p%permfringe, pp%permfringe, 3)
   end select
+
+  if (ele%key == sad_mult$ .and. ele%value(l$) == 0) call set_integer (p%permfringe, pp%permfringe, 0)
 endif
 
 if (attribute_index(ele, 'FRINGE_AT') > 0) then  ! If fringe_at is a valid attribute
