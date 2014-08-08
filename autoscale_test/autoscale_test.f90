29453293a752532d78cd2615e751b3843a46967e
David Sagan <dcs16@cornell.edu>
2014-08-08 17:17:43 +0000

    Looser tol for gfortran.

--- autoscale_test/autoscale_test.f90
+++ autoscale_test/autoscale_test.f90
@@ -14,6 +14,6 @@ open (1, file = 'output.now')
 
 ele => lat%ele(1)
 write (1, '(a, f16.12)') '"Auto_Phase" ABS 4e-5', ele%em_field%mode(1)%phi0_ref
-write (1, '(a, es18.10)') '"Auto_Amp"   REL 1e-7', ele%em_field%mode(1)%field_scale
+write (1, '(a, es18.10)') '"Auto_Amp"   REL 1e-6', ele%em_field%mode(1)%field_scale
 
 end program
