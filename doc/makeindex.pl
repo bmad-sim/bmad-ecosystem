57d16f5aef66f652254b03e0eb820ad47f44cbc3
David Sagan <dcs16@cornell.edu>
2014-10-31 18:39:27 +0000

    Added N_slice = 0 default for beambeam element.

--- doc/makeindex.pl
+++ doc/makeindex.pl
@@ -3,16 +3,4 @@
 `pdflatex bmad`;
 `makeindex bmad`;
 `makeindex bmad.rdx -o bmad.rnd`;
-
-#open (FC, "bmad.ind") || die ("Cannot open File: bmad.idx\n");
-#open (F_OUT, ">temp.out") || die ("Cannot open temparary file\n");
-#while (<FC>) {
-#  s/_/\\_/g;
-#  s/\%/\\\%/g;
-#  print (F_OUT);
-#}
-#close (FC);
-#close (F_OUT);
-#`mv temp.out bmad.ind`;
-
 `pdflatex bmad`;
