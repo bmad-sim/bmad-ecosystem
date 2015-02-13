aff1e2e25ff94b2c6cc9ec2603430e3e74f58ac8
David Sagan <dcs16@cornell.edu>
2015-02-13 20:30:13 +0000

    more...

--- twiss_track_test/twiss_track_test.f90
+++ twiss_track_test/twiss_track_test.f90
@@ -37,6 +37,7 @@ allocate (orb(0:lat%n_ele_max))
 
 bmad_com%rel_tol_tracking = 1e-7
 bmad_com%abs_tol_tracking = 1e-10
+bmad_com%radiation_damping_on = .true.
 
 orb(0)%vec = 0
 call twiss_at_start (lat)
