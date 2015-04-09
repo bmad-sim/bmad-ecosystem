fe1f0eb922030bcba10d2b4ffe9a146527a1ff0f
David Sagan <dcs16@cornell.edu>
2015-04-09 14:46:50 +0000

    Moved dynamic_aperture and analyzer programs to bsim_cesr.

--- code/bsim_interface.f90
+++ code/bsim_interface.f90
@@ -32,36 +32,6 @@ module bsim_interface
      end subroutine histogram
   end interface
   
-  interface
-     subroutine lum_tracker(ring,n_part, start, end)
-       use bmad_struct, only: lat_struct, coord_struct
-       implicit none
-       type(lat_struct) ring
-       type(coord_struct) start(:),end(:)
-       type(coord_struct), allocatable, save :: co(:)
-       integer n_part, i, j
-     end subroutine lum_tracker
-  end interface
-  
-  interface
-     subroutine save_last_pturns(pturns, end, n_part, turn)
-       use bmad_struct, only: coord_struct
-       type (coord_struct)  end(1:)
-       integer pturns, n_part, turn
-     end subroutine save_last_pturns
-  end interface
-  
- interface
-  subroutine implement_pathlength_patch(path_length_patch,ring, delta_frf, frf) 
-   use bmad_struct
-   use bmad_interface
-   implicit none
-   type (lat_struct) ring
-   real(rp), optional :: delta_frf, frf
-   logical path_length_patch
-  end subroutine implement_pathlength_patch
- end interface
-
 integer, private :: private_dummy ! This is to suppress the ranlib "has no symbols" message
 
 end module bsim_interface
