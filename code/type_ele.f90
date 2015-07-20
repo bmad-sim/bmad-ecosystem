42735efc66a168ea5fd44b18a6c2a02b5424ce31
David Sagan <dcs16@cornell.edu>
2015-07-20 19:57:36 +0000

    New field DC map using bessel functions.

--- code/type_ele.f90
+++ code/type_ele.f90
@@ -299,59 +299,6 @@ if (associated(ele%a_pole_elec)) then
 
 endif
 
-! RF field coefs
-
-if (associated(ele%em_field)) then
-  if (logic_option(.false., type_field)) then
-    do i = 1, size(ele%em_field%mode)
-      rfm => ele%em_field%mode(i)
-      if (rfm%master_scale == 0) then
-        name = 'None'
-      else
-        name = attribute_name(ele, rfm%master_scale)
-      endif
-      nl=nl+1; li(nl) = ''
-      nl=nl+1; write (li(nl), '(a, i0)')        'Mode #:', i
-      nl=nl+1; write (li(nl), '(a, i0)')        '    m:             ', rfm%m
-      nl=nl+1; write (li(nl), '(a, i0)')        '    harmonic:      ', rfm%harmonic
-      nl=nl+1; write (li(nl), '(2a)')           '    master_scale:  ', trim(name)
-      nl=nl+1; write (li(nl), '(a, es16.8)')    '    f_damp:        ', rfm%f_damp
-      nl=nl+1; write (li(nl), '(a, es16.8)')    '    phi0_ref:     ', rfm%phi0_ref
-      nl=nl+1; write (li(nl), '(a, es16.8)')    '    phi0_azimuth:  ', rfm%phi0_azimuth
-      nl=nl+1; write (li(nl), '(a, es16.8)')    '    field_scale:   ', rfm%field_scale
-
-      if (associated(rfm%map)) then
-        nl=nl+1; write (li(nl), '(2a)')         '    File:          ', trim(rfm%map%file)
-        nl=nl+1; write (li(nl), '(2a)')         '    ele_anchor_pt: ', anchor_pt_name(rfm%map%ele_anchor_pt)
-        nl=nl+1; write (li(nl), '(a, i0)')      '    n_link:        ', rfm%map%n_link
-        nl=nl+1; write (li(nl), '(a, es16.8)')  '    dz:            ', rfm%map%dz
-        nl=nl+1; write (li(nl), '(a)')          '  Term                e                           b'
-        do j = 1, min(10, size(rfm%map%term))
-          if (nl+1 > size(li)) call re_associate(li, 2 * nl, .false.)
-          nl=nl+1; write (li(nl), '(i5, 3x, 2(a, 2es12.4), a)') j, &
-                                             '(', rfm%map%term(j)%e_coef, ')  (', rfm%map%term(j)%b_coef, ')'
-        enddo
-        if (size(rfm%map%term) > 10) then
-          nl=nl+1; li(nl) = '     .... etc ...'
-        endif
-      endif
-
-      if (associated(rfm%grid)) then
-        nl=nl+1; write (li(nl), '(2a)')         '    File:          ', trim(rfm%grid%file)
-        nl=nl+1; write (li(nl), '(2a)')         '    Type:          ', em_grid_type_name(rfm%grid%type)
-        nl=nl+1; write (li(nl), '(2a)')         '    ele_anchor_pt: ', anchor_pt_name(rfm%grid%ele_anchor_pt)
-        nl=nl+1; write (li(nl), '(a, i0)')      '    n_link:        ', rfm%grid%n_link
-        nl=nl+1; write (li(nl), '(a, 3f14.6)')  '    dr:            ', rfm%grid%dr
-        nl=nl+1; write (li(nl), '(a, 3f14.6)')  '    r0:            ', rfm%grid%r0
-        nl=nl+1; write (li(nl), '(a, 3f14.6)')  '    r_max - r0:    ', ubound(rfm%grid%pt)*rfm%grid%dr - rfm%grid%r0
-      endif
-
-    enddo
-  else
-    nl=nl+1; write (li(nl), '(a, i5)') 'Number of Field modes:', size(ele%em_field%mode)
-  endif
-endif
-
 ! Encode on/off status etc.
 
 if (.not. ele%is_on) then
@@ -455,6 +402,59 @@ endif
 
 if (attribute_index(ele, 'CSR_CALC_ON') /= 0) then
   nl=nl+1; write (li(nl), fmt_l) 'CSR_CALC_ON', '=', ele%csr_calc_on
+endif
+
+! RF field coefs
+
+if (associated(ele%em_field)) then
+  if (logic_option(.false., type_field)) then
+    do i = 1, size(ele%em_field%mode)
+      rfm => ele%em_field%mode(i)
+      if (rfm%master_scale == 0) then
+        name = 'None'
+      else
+        name = attribute_name(ele, rfm%master_scale)
+      endif
+      nl=nl+1; li(nl) = ''
+      nl=nl+1; write (li(nl), '(a, i0)')        'Mode #:', i
+      nl=nl+1; write (li(nl), '(a, i0)')        '    m:             ', rfm%m
+      nl=nl+1; write (li(nl), '(a, i0)')        '    harmonic:      ', rfm%harmonic
+      nl=nl+1; write (li(nl), '(2a)')           '    master_scale:  ', trim(name)
+      nl=nl+1; write (li(nl), '(a, es16.8)')    '    f_damp:        ', rfm%f_damp
+      nl=nl+1; write (li(nl), '(a, es16.8)')    '    phi0_ref:     ', rfm%phi0_ref
+      nl=nl+1; write (li(nl), '(a, es16.8)')    '    phi0_azimuth:  ', rfm%phi0_azimuth
+      nl=nl+1; write (li(nl), '(a, es16.8)')    '    field_scale:   ', rfm%field_scale
+
+      if (associated(rfm%map)) then
+        nl=nl+1; write (li(nl), '(2a)')         '    File:          ', trim(rfm%map%file)
+        nl=nl+1; write (li(nl), '(2a)')         '    ele_anchor_pt: ', anchor_pt_name(rfm%map%ele_anchor_pt)
+        nl=nl+1; write (li(nl), '(a, i0)')      '    n_link:        ', rfm%map%n_link
+        nl=nl+1; write (li(nl), '(a, es16.8)')  '    dz:            ', rfm%map%dz
+        nl=nl+1; write (li(nl), '(a)')          '  Term                e                           b'
+        do j = 1, min(10, size(rfm%map%term))
+          if (nl+1 > size(li)) call re_associate(li, 2 * nl, .false.)
+          nl=nl+1; write (li(nl), '(i5, 3x, 2(a, 2es12.4), a)') j, &
+                                             '(', rfm%map%term(j)%e_coef, ')  (', rfm%map%term(j)%b_coef, ')'
+        enddo
+        if (size(rfm%map%term) > 10) then
+          nl=nl+1; li(nl) = '     .... etc ...'
+        endif
+      endif
+
+      if (associated(rfm%grid)) then
+        nl=nl+1; write (li(nl), '(2a)')         '    File:          ', trim(rfm%grid%file)
+        nl=nl+1; write (li(nl), '(2a)')         '    Type:          ', em_grid_type_name(rfm%grid%type)
+        nl=nl+1; write (li(nl), '(2a)')         '    ele_anchor_pt: ', anchor_pt_name(rfm%grid%ele_anchor_pt)
+        nl=nl+1; write (li(nl), '(a, i0)')      '    n_link:        ', rfm%grid%n_link
+        nl=nl+1; write (li(nl), '(a, 3f14.6)')  '    dr:            ', rfm%grid%dr
+        nl=nl+1; write (li(nl), '(a, 3f14.6)')  '    r0:            ', rfm%grid%r0
+        nl=nl+1; write (li(nl), '(a, 3f14.6)')  '    r_max - r0:    ', ubound(rfm%grid%pt)*rfm%grid%dr - rfm%grid%r0
+      endif
+
+    enddo
+  else
+    nl=nl+1; write (li(nl), '(a, i5)') 'Number of Field modes:', size(ele%em_field%mode)
+  endif
 endif
 
 ! wall3d cross-sections.
