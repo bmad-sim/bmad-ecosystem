1bac1d55413c2e5583a40ffae2d58bd5365f8758
David Sagan <dcs16@cornell.edu>
2015-07-20 19:57:02 +0000

    Added i_bessel_extended and switched to gsl for bessel evaluation.

--- math/i_bessel.f90
+++ math/i_bessel.f90
@@ -1,9 +1,8 @@
 !+
 ! Function I_bessel(m, arg) result (i_bes)
 !
-! Function to evaluate the modified bessel function I:
-!   I_bes = I_m(arg)                         For arg > 0
-!   I_bes = I_m(-i*arg) = i^{-m} * J(arg)    For arg < 0
+! Function to evaluate the modified bessel function of the 
+! first kind I.
 !
 ! Modules needed:
 !   use sim_utils
@@ -13,43 +12,25 @@
 !   arg  -- Real(rp): Bessel argument.
 !
 ! Output:
-!   i_bes -- Complex(rp): Bessel value.
+!   i_bes -- Real: Bessel value.
 !-
 
 function I_bessel(m, arg) result (i_bes)
 
 use physical_constants
-use nr
+use fgsl
 
 implicit none
 
 integer m
-real(rp) arg
-complex(rp) i_bes
+real(rp) arg, i_bes
 
 !
 
 select case(m)
-case (0)
-  if (arg > 0) then
-    i_bes = bessi0(arg)
-  else
-    i_bes = bessj0(-arg)
-  endif
-
-case (1)
-  if (arg > 0) then
-    i_bes = bessi1(arg)
-  else
-    i_bes = -i_imaginary * bessj1(-arg)
-  endif
-
-case default
-  if (arg > 0) then
-    i_bes = bessi(m, arg)
-  else
-    i_bes = (-i_imaginary)**m * bessj(m, -arg)
-  endif
+case (0);     i_bes = fgsl_sf_bessel_ic0(arg)
+case (1);     i_bes = fgsl_sf_bessel_ic1(arg)
+case default; i_bes = fgsl_sf_bessel_icn(m, arg)
 end select
 
 end function I_bessel
