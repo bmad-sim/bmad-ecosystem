c43877568f75ab24075473c277b06718630681ff
David Sagan <dcs16@cornell.edu>
2015-10-24 03:49:12 +0000

    Modifications for implementing chess x-ray beam lines in cesrv.

--- abs_time_test/abs_time_test.f90
+++ abs_time_test/abs_time_test.f90
@@ -20,6 +20,7 @@ call track_all (lat, orb1)
 
 lat%absolute_time_tracking = .true.
 call autoscale_phase_and_amp (lat%ele(2), lat%param, err_flag)
+call lattice_bookkeeper (lat)
 
 call reallocate_coord (orb2, lat%n_ele_max)
 call init_coord (orb2(0), lat%beam_start, lat%ele(0), downstream_end$)
