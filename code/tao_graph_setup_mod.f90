d3689ed76ab590fc0698d4cc47c8481bbd8cca22
David Sagan <dcs16@cornell.edu>
2016-01-04 15:10:51 +0000

    Added "show lat -energy" option.

--- code/tao_graph_setup_mod.f90
+++ code/tao_graph_setup_mod.f90
@@ -1628,8 +1628,7 @@ case ('s')
       s1 = ele%s
       ele%logic = (s0 >= graph%x%min-eps) .and. (s1 <= graph%x%max+eps)
       if (branch%param%geometry == closed$) then
-        ele%logic = ele%logic .or. &
-                   ((s0-l_tot >= graph%x%min-eps) .and. (s1-l_tot <= graph%x%max+eps))
+        ele%logic = ele%logic .or. ((s0-l_tot >= graph%x%min-eps) .and. (s1-l_tot <= graph%x%max+eps))
       endif
     enddo
     n_dat = count (branch%ele(:)%logic)
