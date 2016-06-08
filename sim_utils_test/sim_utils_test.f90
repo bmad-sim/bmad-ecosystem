1dc8ced5c3e4ead6a4591cccfa695c3beb28cb7d
David Sagan <dcs16@cornell.edu>
2016-06-08 14:42:28 +0000

    more...

--- sim_utils_test/sim_utils_test.f90
+++ sim_utils_test/sim_utils_test.f90
@@ -15,10 +15,15 @@ implicit none
 type (coord_struct) orbit
 
 real(rp) array(4), dE, freq(3)
-complex(rp) c_data(64),  amp(3)
-integer i, which, where
+complex(rp) cdata(32)
+complex(rp) amp(3)
+integer i, which, where, n_freq
 logical match
 character(40) str, sub1, sub2, sub3
+
+real(rp) sig1, sig2, sig3
+real(rp) phi1, phi2, phi3
+complex(rp) amp1, amp2, amp3
 
 !
 
@@ -26,15 +31,24 @@ open (1, file = 'output.now')
 
 ! naff test
 
-do i = 1, size(c_data)
-  c_data(i) = cmplx(cos(i / 3.0_rp), 0 * cos(1 + i / 3.0_rp))
+amp1 = cmplx(1.8000,0.0000)
+sig1 = 0.753262
+amp2 = cmplx(0.3000,0.3000)
+sig2 = 0.423594
+amp3 = cmplx(0.01230,0.1545)
+sig3 = 0.173
+
+do i = 1, size(cdata)
+  phi1 = twopi*(sig1*(i-1))
+  phi2 = twopi*(sig2*(i-1))
+  phi3 = twopi*(sig3*(i-1))
+  cdata(i) = amp1*exp(cmplx(0.0d0,-phi1)) + amp2*exp(cmplx(0.0d0,-phi2)) + amp3*exp(cmplx(0.0d0,-phi3))
 enddo
 
-call naff (c_data, freq, amp)
-write (1, '(a, i0)') '"naff-n_freq" ABS 0   ', 3
-write (1, '(a, 3es16.8)') '"naff-freq1" REL 1E-6   ', freq(1), amp(1)
-write (1, '(a, 3es16.8)') '"naff-freq2" REL 1E-6   ', freq(2), amp(2)
-write (1, '(a, 3es16.8)') '"naff-freq3" REL 4E-6   ', freq(3), amp(3)
+call naff (cdata, freq, amp)
+write (1, '(a, 3es16.8)') '"naff-freq1" REL 1E-6   ', freq(1), real(amp(1)), aimag(amp(1))
+write (1, '(a, 3es16.8)') '"naff-freq2" REL 1E-6   ', freq(2), real(amp(2)), aimag(amp(2))
+write (1, '(a, 3es16.8)') '"naff-freq3" REL 1E-6   ', freq(3), real(amp(3)), aimag(amp(3))
 
 ! Random test
 
