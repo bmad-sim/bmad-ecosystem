abf191694c041e71210f01a432039e8283706e29
David Sagan <dcs16@cornell.edu>
2016-06-20 15:56:15 +0000

    Expanded closed_orbit_test.

--- closed_orbit_test/closed_orbit_test.f90
+++ closed_orbit_test/closed_orbit_test.f90
@@ -4,9 +4,39 @@ use bmad
 
 implicit none
 
-bmad_com%radiation_damping_on = .true.
+type (lat_struct) lat
+type (coord_struct), allocatable :: orb_fwd(:), orb_back(:)
+
+real(rp) :: vec0(6) = 0
+integer i, n, n_dim
+
+!
 
 open (1, file = 'output.now')
+
+call bmad_parser ('bmad_L9A18A000-_MOVEREC.lat', lat)
+
+n = lat%n_ele_track
+call reallocate_coord(orb_fwd, lat)
+call reallocate_coord(orb_back, lat)
+call init_coord (orb_back(n), vec0, lat%ele(n), downstream_end$, antiparticle(lat%param%particle), -1)
+
+call set_on_off (rfcavity$, lat, off$)
+
+do n_dim = 4, 6
+  if (n_dim == 6) then
+    bmad_com%radiation_damping_on = .true.
+    call set_on_off (rfcavity$, lat, on$)
+  endif
+
+  call closed_orbit_calc(lat, orb_fwd, n_dim)
+  call closed_orbit_calc(lat, orb_back, n_dim, -1)
+
+  write (1, '(a, i0, a, 6es14.6)') '"Fwd', n_dim, '"  ABS 1E-10', orb_fwd(0)%vec
+  write (1, '(a, i0, a, 6es14.6)') '"Rev', n_dim, '"  ABS 1E-10', orb_back(0)%vec
+enddo
+
+!
 
 call run1 ('ad05f_pos_RF.lat', 1)
 call run1 ('ad05f_pos_RF.lat', 2)
--- sim_utils_test/sim_utils_test.f90
+++ sim_utils_test/sim_utils_test.f90
@@ -46,9 +46,9 @@ do i = 1, size(cdata)
 enddo
 
 call naff (cdata, freq, amp)
-write (1, '(a, 3es16.8)') '"naff-freq1" REL 1E-6   ', freq(1), real(amp(1)), aimag(amp(1))
-write (1, '(a, 3es16.8)') '"naff-freq2" REL 1E-6   ', freq(2), real(amp(2)), aimag(amp(2))
-write (1, '(a, 3es16.8)') '"naff-freq3" REL 1E-6   ', freq(3), real(amp(3)), aimag(amp(3))
+write (1, '(a, 3es16.8)') '"naff-freq1" REL 2E-6   ', freq(1), real(amp(1)), aimag(amp(1))
+write (1, '(a, 3es16.8)') '"naff-freq2" REL 2E-6   ', freq(2), real(amp(2)), aimag(amp(2))
+write (1, '(a, 3es16.8)') '"naff-freq3" REL 2E-6   ', freq(3), real(amp(3)), aimag(amp(3))
 
 ! Random test
 
