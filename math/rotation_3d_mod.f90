8a7d96c970833de74d053cc9ba44add6604aebe3
David Sagan <dcs16@cornell.edu>
2016-07-06 03:31:33 +0000

    Added quaternion 3D rotation routines.

--- math/rotation_3d_mod.f90
+++ math/rotation_3d_mod.f90
@@ -2,6 +2,8 @@ module rotation_3d_mod
 
 use sim_utils
 
+implicit none
+
 contains
 
 !------------------------------------------------------------------------------
@@ -30,8 +32,6 @@ contains
 !-
 
 subroutine w_mat_to_axis_angle (w_mat, axis, angle)
-
-implicit none
 
 real(rp) w_mat(3,3), axis(3), angle
 real(rp) sin_ang, cos_ang
@@ -87,8 +87,6 @@ end subroutine w_mat_to_axis_angle
 
 subroutine axis_angle_to_w_mat (axis, angle, w_mat)
 
-implicit none
-
 real(rp) w_mat(3,3), axis(3), angle
 real(rp) sin_a, cos_a, norm, x, y, z
 character(*), parameter :: r_name = 'axis_angle_to_w_mat'
@@ -123,6 +121,219 @@ end subroutine axis_angle_to_w_mat
 !------------------------------------------------------------------------------
 !------------------------------------------------------------------------------
 !+
+! Function quat_to_omega (quat) result (omega)
+! 
+! Routine to convert from omega (axis + angle) representation to a quaternion.
+!
+! Input:
+!   quat(0:3)   -- real(rp): Rotation quaternion. Assumed normalized.
+!
+! Output:
+!   omega(3)    -- real(rp): Axis of rotation + magnitude = rotation angle.
+!-
+
+function quat_to_omega (quat) result (omega)
+
+real(rp) omega(3), quat(0:3)
+real(rp) anorm, angle
+
+!
+
+anorm = norm2(quat(1:3))
+
+if (anorm == 0) then
+  omega = 0
+else
+  angle = 2 * atan2(anorm, quat(0))
+  omega = angle * quat(1:3) / anorm
+endif
+
+end function quat_to_omega 
+
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!+
+! Function omega_to_quat (omega) result (quat)
+! 
+! Routine to convert from omega + angle representation to a quaternion.
+!
+! Input:
+!   omega(3)    -- real(rp): Axis of rotation + magnitude = rotation angle.
+!
+! Output:
+!   quat(0:3)   -- real(rp): Rotation quaternion.
+!-
+
+function omega_to_quat (omega) result (quat)
+
+real(rp) omega(3), angle, quat(0:3)
+
+!
+
+angle = norm2(omega)
+
+if (angle == 0) then
+  quat = [1, 0, 0, 0]
+else
+  quat = [cos(angle/2), omega * (sin(angle/2) / angle)]
+endif
+
+end function omega_to_quat 
+
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!+
+! Subroutine quat_to_axis_angle (quat, axis, angle)
+! 
+! Routine to convert from axis + angle representation to a quaternion.
+!
+! Input:
+!   quat(0:3)   -- real(rp): Rotation quaternion. Assumed normalized.
+!
+! Output:
+!   axis(3)     -- real(rp): Axis of rotation.
+!   angle       -- real(rp): angle of rotation.
+!-
+
+subroutine quat_to_axis_angle (quat, axis, angle)
+
+real(rp) axis(3), angle, quat(0:3)
+real(rp) anorm
+
+!
+
+anorm = norm2(quat(1:3))
+
+if (anorm == 0) then
+  angle = 0
+  axis = [0, 0, 1]  ! Arbitrary.
+else
+  angle = 2 * atan2(anorm, quat(0))
+  axis = quat(1:3) / anorm
+endif
+
+end subroutine quat_to_axis_angle 
+
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!+
+! Function axis_angle_to_quat (axis, angle) result (quat)
+! 
+! Routine to convert from axis + angle representation to a quaternion.
+!
+! Input:
+!   axis(3)     -- real(rp): Axis of rotation.
+!   angle       -- real(rp): angle of rotation.
+!
+! Output:
+!   quat(0:3)   -- real(rp): Rotation quaternion.
+!-
+
+function axis_angle_to_quat (axis, angle) result (quat)
+
+real(rp) axis(3), angle, quat(0:3)
+
+!
+
+quat = [cos(angle/2), axis * sin(angle/2)]
+
+end function axis_angle_to_quat 
+
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!+
+! Function quat_inverse (q_in) result (q_out)
+!
+! Routine to create the inverse of a quaternian. 
+!
+! Input:
+!   q_in(0:3)     -- real(rp): Quaternion input.
+!
+! Output:
+!   q_out(0:3)    -- real(rp): Inverse quaternion.
+!-
+
+function quat_inverse (q_in) result (q_out)
+
+real(rp) q_in(0:3), q_out(0:3)
+
+q_out = [q_in(0), -q_in(1:3)] / dot_product(q_in, q_in)
+
+end function quat_inverse
+
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!+
+! Function quat_mul (q1, q2) result (q_out)
+!
+! Routine to multiply two quaternions q_out = q1 * q2.
+! Note: q_out = q1 * q2 represents a rotation of q2 first followed by q1.
+!
+! Input:
+!   q1(0:3), q2(0:3)  -- real(rp): Quaternions.
+!
+! Output:
+!   q_out(0:3)        -- real(rp): Resultant q1 * q2
+!-
+
+function quat_mul (q1, q2) result (q_out)
+
+real(rp) q1(0:3), q2(0:3), q_out(0:3)
+
+!
+
+q_out = [q1(0)*q2(0) - q1(1)*q2(1) - q1(2)*q2(2) - q1(3)*q2(3), & 
+         q1(0)*q2(1) + q1(1)*q2(0) + q1(2)*q2(3) - q1(3)*q2(2), & 
+         q1(0)*q2(2) - q1(1)*q2(3) + q1(2)*q2(0) + q1(3)*q2(1), & 
+         q1(0)*q2(3) + q1(1)*q2(2) - q1(2)*q2(1) + q1(3)*q2(0)]
+
+end function quat_mul
+
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!+
+! Subroutine rotate_vec_given_quat (vec, quat)
+!
+! Routine to rotate a vector using a quaternion..
+!
+! Input:
+!   vec(3)      -- real(rp): Initial vector.
+!   q_out(0:3)  -- real(rp): Quaternion to rotate with.
+!
+! Output:
+!   vec(3)    -- real(rp): Final vector.
+!-
+
+subroutine rotate_vec_given_quat (vec, quat)
+
+real(rp) :: vec(3), quat(0:3)
+real(rp) :: q0_inv
+
+! Use quaternion rotation formula: vec -> q * vec * q^-1
+
+q0_inv = -(quat(1)*vec(1) + quat(2)*vec(2) + quat(3)*vec(3))
+
+vec = [quat(0)*vec(1) + quat(2)*vec(3) - quat(3)*vec(2), &
+       quat(0)*vec(2) + quat(3)*vec(1) - quat(1)*vec(3), &
+       quat(0)*vec(3) + quat(1)*vec(2) - quat(2)*vec(1)]
+
+
+vec = [quat(0)*vec(1) + quat(2)*vec(3) - quat(3)*vec(2) - q0_inv*quat(1), &
+       quat(0)*vec(2) + quat(3)*vec(1) - quat(1)*vec(3) - q0_inv*quat(2), &
+       quat(0)*vec(3) + quat(1)*vec(2) - quat(2)*vec(1) - q0_inv*quat(3)]
+
+end subroutine rotate_vec_given_quat
+
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!+
 ! Subroutine rotate_vec_given_axis_angle (vec, axis, angle)
 !
 ! Routine to rotate a vector.
@@ -138,9 +349,7 @@ end subroutine axis_angle_to_w_mat
 
 subroutine rotate_vec_given_axis_angle (vec, axis, angle)
 
-implicit none
-
-real(rp) :: vec(:), angle, ca, sa, q0
+real(rp) :: vec(:), angle, ca, sa, q0_inv
 real(rp) :: axis(:)
 
 ! Use quaternion rotation formula: vec -> q * vec * q^-1
@@ -151,15 +360,16 @@ if (angle == 0) return ! Simple case
 ca = cos(angle/2)
 sa = sin(angle/2)
 
-q0 = -sa * (axis(1)*vec(1) + axis(2)*vec(2) + axis(3)*vec(3))
+q0_inv = -sa * (axis(1)*vec(1) + axis(2)*vec(2) + axis(3)*vec(3))
+
 vec = [ca*vec(1) + sa * (axis(2)*vec(3) - axis(3)*vec(2)), &
        ca*vec(2) + sa * (axis(3)*vec(1) - axis(1)*vec(3)), &
        ca*vec(3) + sa * (axis(1)*vec(2) - axis(2)*vec(1))]
 
 
-vec = [ca*vec(1) + sa * (axis(2)*vec(3) - axis(3)*vec(2) - q0*axis(1)), &
-       ca*vec(2) + sa * (axis(3)*vec(1) - axis(1)*vec(3) - q0*axis(2)), &
-       ca*vec(3) + sa * (axis(1)*vec(2) - axis(2)*vec(1) - q0*axis(3))]
+vec = [ca*vec(1) + sa * (axis(2)*vec(3) - axis(3)*vec(2) - q0_inv*axis(1)), &
+       ca*vec(2) + sa * (axis(3)*vec(1) - axis(1)*vec(3) - q0_inv*axis(2)), &
+       ca*vec(3) + sa * (axis(1)*vec(2) - axis(2)*vec(1) - q0_inv*axis(3))]
 
 end subroutine rotate_vec_given_axis_angle
 
@@ -184,8 +394,6 @@ end subroutine rotate_vec_given_axis_angle
 !-
 
 subroutine rotate_vec (vec, axis, angle)
-
-implicit none
 
 real(rp) :: vec(:), angle, ca, sa
 integer :: axis
@@ -231,8 +439,6 @@ end subroutine
 !-
 
 subroutine rotate_mat(mat, axis, angle)
-
-implicit none
 
 real(rp) :: mat(:,:),  angle, temp, ca, sa
 integer :: axis
