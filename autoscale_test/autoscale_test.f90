bc10e7b2b10245a5fce9efe1908118b71956a763
David Sagan <dcs16@cornell.edu>
2016-07-26 17:34:36 +0000

    Fixed fringe electric field dpz calc from last checkin.

--- autoscale_test/autoscale_test.f90
+++ autoscale_test/autoscale_test.f90
@@ -13,7 +13,7 @@ call bmad_parser ('autoscale.bmad', lat)
 open (1, file = 'output.now')
 
 ele => lat%ele(1)
-write (1, '(a, f16.12)') '"Auto_Phase" ABS 4e-5', ele%value(phi0_autoscale$)
+write (1, '(a, f16.12)') '"Auto_Phase" ABS 2e-4', ele%value(phi0_autoscale$)
 write (1, '(a, es18.10)') '"Auto_Amp"   REL 1e-6', ele%value(field_autoscale$)
 
 end program
