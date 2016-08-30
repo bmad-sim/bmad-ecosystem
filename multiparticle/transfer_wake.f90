20b4d6b6327bf02af1c00be90d65422a31b3e5f2
David Sagan <dcs16@cornell.edu>
2016-08-30 19:24:35 +0000

    Fixed %lr_spline logic.

--- multiparticle/transfer_wake.f90
+++ multiparticle/transfer_wake.f90
@@ -36,13 +36,27 @@ if (associated (wake_in)) then
   do i = 1, n_lr_pa
     lrp_in  => wake_in%lr_spline(i)
     lrp_out => wake_out%lr_spline(i)
-    if (size(lrp_out%bunch) /= size(lrp_in%bunch)) then
-      deallocate (lrp_out%bunch)
-      allocate (lrp_in%bunch(size(lrp_out%bunch)))
+
+    if (allocated(lrp_in%bunch) .and. .not. allocated(lrp_out%bunch)) then
+      allocate (lrp_out%bunch(size(lrp_in%bunch)))
+    elseif (.not. allocated(lrp_in%bunch) .and. allocated(lrp_out%bunch)) then
+      deallocate(lrp_out%bunch)
+    elseif (allocated(lrp_in%bunch)) then
+      if (size(lrp_out%bunch) /= size(lrp_in%bunch)) then
+        deallocate (lrp_out%bunch)
+        allocate (lrp_out%bunch(size(lrp_in%bunch)))
+      endif
     endif
-    if (size(lrp_out%spline) /= size(lrp_in%spline)) then
-      deallocate (lrp_out%spline)
-      allocate (lrp_in%spline(size(lrp_out%spline)))
+
+    if (allocated(lrp_in%spline) .and. .not. allocated(lrp_out%spline)) then
+      allocate (lrp_out%spline(size(lrp_in%spline)))
+    elseif (.not. allocated(lrp_in%spline) .and. allocated(lrp_out%spline)) then
+      deallocate(lrp_out%spline)
+    elseif (allocated(lrp_in%spline)) then
+      if (size(lrp_out%spline) /= size(lrp_in%spline)) then
+        deallocate (lrp_out%spline)
+        allocate (lrp_out%spline(size(lrp_in%spline)))
+      endif
     endif
   enddo
 
