99161825f4828da182b841bca6580eb2ee69eac2
David Sagan <dcs16@cornell.edu>
2016-09-02 18:14:47 +0000

    Monor physical constants update.

--- interfaces/physical_constants.f90
+++ interfaces/physical_constants.f90
@@ -13,12 +13,12 @@ real(rp), parameter :: fourpi = 4 * pi
 real(rp), parameter :: sqrt_2 = 1.414213562373095048801688724209698d0
 real(rp), parameter :: sqrt_3 = 1.732050807568877293527446341505872d0 
 
-real(rp), parameter :: e_mass = 0.5109989461d-3           ! [GeV] FOR MAD COMPATIBILITY USE ONLY. USE M_ELECTRON INSTEAD.
-real(rp), parameter :: p_mass   = 0.9382720813d0          ! [GeV] FOR MAD COMPATIBILITY USE ONLY. USE M_PROTON INSTEAD.
-
 real(rp), parameter :: m_electron = 0.5109989461d6        ! Mass [eV]
 real(rp), parameter :: m_proton   = 0.9382720813d9        ! Mass [eV]
-real(rp), parameter :: m_muon     = 105.6583745d6        ! Mass [eV]
+real(rp), parameter :: m_muon     = 105.6583715d6        ! Mass [eV]
+
+real(rp), parameter :: e_mass = 1d-9 * m_electron         ! [GeV] FOR MAD COMPATIBILITY USE ONLY. USE M_ELECTRON INSTEAD.
+real(rp), parameter :: p_mass   = 1d-9 * m_proton         ! [GeV] FOR MAD COMPATIBILITY USE ONLY. USE M_PROTON INSTEAD.
 
 real(rp), parameter :: m_pion_0 = 134.9766d6             ! Mass [eV]
 real(rp), parameter :: m_pion_charged = 139.57018d6      ! Mass [eV]
@@ -28,9 +28,9 @@ real(rp), parameter :: m_deuteron   = 1.875612928d9      ! Mass [eV]
 real(rp), parameter :: atomic_mass_unit = 931.494095d6  ! unified atomic mass unit u (or dalton) in [eV]
  
 real(rp), parameter :: c_light = 2.99792458d8            ! speed of light
-real(rp), parameter :: r_e = 2.8179402894d-15            ! classical electron radius
+real(rp), parameter :: r_e = 2.8179403227d-15            ! classical electron radius
 real(rp), parameter :: r_p = r_e * m_electron / m_proton ! proton radius
-real(rp), parameter :: e_charge = 1.6021892d-19          ! electron charge [Coul]
+real(rp), parameter :: e_charge = 1.6021766208d-19          ! electron charge [Coul]
 real(rp), parameter :: h_planck = 4.13566733d-15         ! Planck's constant [eV*sec]
 real(rp), parameter :: h_bar_planck = 6.58211899d-16     ! h_planck/twopi [eV*sec]
 
