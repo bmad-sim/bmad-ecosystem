46ac0f9ce4c7e5a24e1b6ae36078f4fe5f6610d2
David Sagan <dcs16@cornell.edu>
2016-09-04 05:06:07 +0000

    More fibre to ele devel.

--- interface/fibre_to_ele.f90
+++ interface/fibre_to_ele.f90
@@ -1,5 +1,5 @@
 !+
-! Subroutine fibre_to_ele (ptc_fibre, ele_array, ix_ele, err_flag)
+! Subroutine fibre_to_ele (ptc_fibre, branch, ix_ele, err_flag, ignore_ptc_specific_parameters)
 !
 ! Routine to transfer parameters from a PTC fibre to one, or more if needed, bmad lattice elements.
 ! For example, a fibre with a patch needs multiple elements.
@@ -9,12 +9,16 @@
 !
 ! Input:
 !   ptc_fibre     -- fibre: PTC fiber.
-!   ix_ele        -- integer: Index in ele(:) array of element to use
-!   ele_array(0:) -- ele_struct: Array of Bmad elements. Indexed from 0.
+!   ix_ele        -- integer: Index in ele(:) array of element last used.
+!   branch        -- branch_struct: branch containing elements.
+!   ignore_ptc_specific_parameters
+!                 -- logical, optional: If True, ignore PTC specific parameters like integrator_order.
+!                      Default is False. True is used when the fibre has been created via MAD. In this
+!                      case, the PTC specific parameters will not have good values.
 !
 ! Output:
-!   ele_array(0:) -- ele_struct: Array of Bmad elements. Indexed from 0.
-!   ix_ele        -- integer: Index to next unused element.
+!   branch        -- branch_struct: branch containing elements.
+!   ix_ele        -- integer: Index to element created (upper index if more than one created).
 !   err_flag      -- logical: Set true if there is an error. False otherwise.
 !-
 
@@ -23,14 +27,14 @@
 ! Energy patch
 
 
-subroutine fibre_to_ele (ptc_fibre, ele_array, ix_ele, err_flag)
+subroutine fibre_to_ele (ptc_fibre, branch, ix_ele, err_flag, ignore_ptc_specific_parameters)
 
 use madx_ptc_module, ptc_pi => pi, ptc_twopi => twopi
 use bmad, except_dummy => fibre_to_ele
 
 type (fibre), target :: ptc_fibre
-type (ele_struct), target :: ele_array(0:)
 type (ele_struct), pointer :: ele
+type (branch_struct) branch
 type (fibre), pointer :: fib
 type (element), pointer :: mag
 type (work) wk
@@ -41,6 +45,7 @@ real(rp) knl(0:n_pole_maxx), tn(0:n_pole_maxx), ele_tilt, this_kick
 integer ix_ele, nmul, i, ix
 
 logical err_flag
+logical, optional :: ignore_ptc_specific_parameters
 
 character(*), parameter :: r_name = 'fibre_to_ele'
 character(40) name
@@ -94,12 +99,13 @@ character(*) name
 
 !
 
-ele => ele_array(ix_ele)
+ix_ele = ix_ele + 1
+ele => branch%ele(ix_ele)
+call init_ele(ele, patch$, -1, ix_ele, branch)
 
 ele%name = name
-ix_ele = ix_ele + 1
 
-w_mat = matmul(matmul(w_mat_for_y_pitch(-angles(1)), w_mat_for_x_pitch(-angles(2))), w_mat_for_tilt(angles(3)))
+w_mat = matmul(matmul(w_mat_for_y_pitch(-angles(2)), w_mat_for_x_pitch(-angles(1))), w_mat_for_tilt(angles(3)))
 call floor_w_mat_to_angles (w_mat, ele%value(x_pitch$), ele%value(y_pitch$), ele%value(tilt$))
 
 off = matmul(transpose(w_mat), offset)
@@ -117,53 +123,107 @@ end subroutine patch_out
 
 subroutine ele_out (name, err_flag)
 
-character(*) name
+type (all_pointer_struct) a_ptr
 
 real(rp) ab_max, ab
 real(rp), parameter :: cm1 = 0.01  ! 1 cm radius
 real(rp) a_pole(0:n_pole_maxx), b_pole(0:n_pole_maxx)
+
 integer i, n, ix_max
-logical err_flag
+
+logical err_flag, err
+
+character(*) name
 
 !
 
-ele => ele_array(ix_ele)
-ele%name = name
 ix_ele = ix_ele + 1
+ele => branch%ele(ix_ele)
 
-call set_energy (ele)
-
-!
+! Sbend
 
 if (mag%p%b0 /= 0) then
-  ele%key = sbend$
+  call init_ele(ele, sbend$, -1, ix_ele, branch)
+
+  ele%value(g$) = fib%mag%p%b0
+  ele%value(angle$) = ele%value(g$) * ele%value(l$)
+  ix = nint(ele%value(ptc_field_geometry$))
+  if (ix == straight$ .or. ix == true_rbend$) then
+    ele%value(e1$) = fib%mag%p%edge(1) + ele%value(angle$)/2
+    ele%value(e2$) = fib%mag%p%edge(2) + ele%value(angle$)/2
+  else
+    ele%value(e1$) = fib%mag%p%edge(1)
+    ele%value(e2$) = fib%mag%p%edge(2)
+  endif
+  ele%value(hgap$) = fib%mag%hgap
+  ele%value(fint$) = fib%mag%fint
+  ele%value(ref_tilt$) = fib%mag%p%tiltd
+
+! Marker
+
+elseif (fib%mag%kind == kind0) then
+  call init_ele(ele, marker$, -1, ix_ele, branch)
+
+! Elseparator
 
 elseif (fib%mag%kind == kind15) then
-  ele%key = elseparator$
+  call init_ele(ele, elseparator$, -1, ix_ele, branch)
+
+  this_kick = fib%mag%volt * 1d6 / ele%value(e_tot$)
+  if (fib%charge < 0) this_kick = -this_kick
+  ele_tilt = fib%mag%p%tiltd - ele%value(tilt_tot$)
+  ele%value(hkick$) = -this_kick * sin(ele_tilt)
+  ele%value(vkick$) = -this_kick * cos(ele_tilt)
+
+! Multipole
 
 elseif (associated(mag%k3)) then
-  ele%key = multipole$
+  call init_ele(ele, multipole$, -1, ix_ele, branch)
+  call multipole_ab_to_kt (a_pole, b_pole, knl, tn)
+  ele%a_pole = knl
+  ele%b_pole = tn
+
+! Solenoid
 
 elseif (associated(mag%s5)) then
-  ele%key = solenoid$
+  call init_ele(ele, solenoid$, -1, ix_ele, branch)
+  ele%value(ks$) = fib%mag%b_sol
+
+! RFcavity
 
 elseif (associated(mag%c4)) then
-  ele%key = rfcavity$
+  call init_ele(ele, rfcavity$, -1, ix_ele, branch)
+
+  ele%value(rf_frequency$) = fib%mag%freq
+
+  select case (nint(ele%value(cavity_type$)))
+  case (traveling_wave$);     ele%value(voltage$) = fib%mag%volt*1d6
+  case (standing_wave$);      ele%value(voltage$) = fib%mag%volt*0.5d6
+  case (ptc_standard$);       ele%value(voltage$) = fib%mag%volt*1d6
+  end select
+
+  if (ele%key == lcavity$) then
+    ele%value(phi0$) = pi/2 - fib%mag%lag/twopi
+  else
+    ele%value(phi0$) = fib%mag%lag/twopi
+  endif
+
+! Collimator
 
 elseif (associated(mag%d0)) then
   if (associated (mag%p%aperture)) then
     if (mag%p%aperture%kind == 1) then
-      ele%key = ecollimator$
+      call init_ele(ele, ecollimator$, -1, ix_ele, branch)
     elseif (mag%p%aperture%kind == 2) then
-      ele%key = rcollimator$
+      call init_ele(ele, rcollimator$, -1, ix_ele, branch)
     else
-      ele%key = drift$
+      call init_ele(ele, drift$, -1, ix_ele, branch)
       call out_io (s_error$, r_name, 'UNKNOWN PTC APERTURE TYPE: \i0\ ', &
                                      'FOR ELEMENT: ' // name, i_array = [fib%mag%p%aperture%kind])
       err_flag = .true.
     endif
   else
-    ele%key = drift$
+    call init_ele(ele, drift$, -1, ix_ele, branch)
   endif
 
 elseif (associated(mag%k2)) then
@@ -181,24 +241,24 @@ elseif (associated(mag%k2)) then
   a_pole(0:n-1) = mag%an
   b_pole(0:n-1) = mag%bn
 
-  select case (i)
+  select case (ix_max)
   case(2)
-    ele%key = quadrupole$
+    call init_ele(ele, quadrupole$, -1, ix_ele, branch)
     ele%value(k1$) = mag%bn(2)
     b_pole(1) = 0
 
   case(3)
-    ele%key = sextupole$
-    ele%value(k1$) = mag%bn(3)
+    call init_ele(ele, sextupole$, -1, ix_ele, branch)
+    ele%value(k2$) = 2 * mag%bn(3)
     b_pole(2) = 0
 
   case(4)
-    ele%key = sextupole$
-    ele%value(k1$) = mag%bn(4)
+    call init_ele(ele, octupole$, -1, ix_ele, branch)
+    ele%value(k3$) = 6 * mag%bn(4)
     b_pole(3) = 0
 
   case default
-    ele%key = kicker$
+    call init_ele(ele, kicker$, -1, ix_ele, branch)
     ele%value(hkick$) = mag%bn(1)
     ele%value(vkick$) = mag%an(1)
     a_pole(0) = 0
@@ -213,144 +273,33 @@ elseif (associated(mag%k2)) then
 
 endif
 
+call set_energy (ele)
+ele%name = name
 ele%value(l$) = fib%mag%p%ld
 
-if (attribute_name(ele, num_steps$) == 'NUM_STEPS') then
-  ele%value(num_steps$) = real(fib%mag%p%nst, rp)
-  if (ele%value(num_steps$) == 0) then
-    ele%value(ds_step$) = 0
-  else
-    ele%value(ds_step$) = ele%value(l$) / ele%value(num_steps$)
+
+if (.not. logic_option(.false., ignore_ptc_specific_parameters)) then
+  call pointer_to_attribute(ele, 'NUM_STEPS', .false., a_ptr, err, .false.)
+  if (associated(a_ptr%r)) then
+    a_ptr%r = real(fib%mag%p%nst, rp)
+    if (a_ptr%r == 0) then
+      ele%value(ds_step$) = 0
+    else
+      ele%value(ds_step$) = ele%value(l$) / a_ptr%r
+    endif
   endif
 endif
 
 ! If integrator_order is defined for this element then update
 
-name = attribute_name(ele, integrator_order$)
-if (name(1:1) /= '!') ele%value(integrator_order$) = fib%mag%p%method
-
-! Multipole
-
-a_pole = 0
-b_pole = 0
-nmul = min(fib%mag%p%nmul, n_pole_maxx+1)
-a_pole(0:nmul-1) = fib%mag%an(1:nmul)
-b_pole(0:nmul-1) = fib%mag%bn(1:nmul)
-
-call multipole_ab_to_kt (a_pole, b_pole, knl, tn)
-
-! Electric Multipole
-
-if (associated(fib%mag%tp10)) then
-  if (associated(fib%mag%tp10%ae)) then
-    if (.not. associated(ele%a_pole_elec)) call elec_multipole_init(ele)
-    nmul = min(size(fib%mag%tp10%ae), n_pole_maxx+1)
-    ele%a_pole_elec(0:nmul-1) = 1d9 * VOLT_C * fib%mag%tp10%ae(1:nmul)
-    ele%b_pole_elec(0:nmul-1) = 1d9 * VOLT_C * fib%mag%tp10%be(1:nmul)
-  endif
+if (.not. logic_option(.false., ignore_ptc_specific_parameters)) then
+  call pointer_to_attribute(ele, 'INTEGRATOR_ORDER', .false., a_ptr, err, .false.)
+  if (associated(a_ptr%r)) a_ptr%r = fib%mag%p%method
 endif
 
 !
 
-if (ele%key == sbend$) then
-  ele%value(ref_tilt_tot$) = fib%mag%p%tiltd
-else
-  ele%value(tilt_tot$) = fib%mag%p%tiltd
-endif
-
-!
-
-select case (ele%key)
-case (ab_multipole$)
-  ele%a_pole = a_pole
-  ele%b_pole = b_pole
-
-case (drift$)
-
-! 
-
-case (elseparator$)
-  this_kick = fib%mag%volt * 1d6 / ele%value(e_tot$)
-  if (fib%charge < 0) this_kick = -this_kick
-  ele_tilt = fib%mag%p%tiltd - ele%value(tilt_tot$)
-  ele%value(hkick$) = -this_kick * sin(ele_tilt)
-  ele%value(vkick$) = -this_kick * cos(ele_tilt)
-
-case (hkicker$)
-  ele%value(kick$) = knl(1)
-
-case (lcavity$, rfcavity$)
-  ele%value(rf_frequency$) = fib%mag%freq
-
-  select case (nint(ele%value(cavity_type$)))
-  case (traveling_wave$);     ele%value(voltage$) = fib%mag%volt*1d6
-  case (standing_wave$);      ele%value(voltage$) = fib%mag%volt*0.5d6
-  case (ptc_standard$);       ele%value(voltage$) = fib%mag%volt*1d6
-  end select
-
-  if (ele%key == lcavity$) then
-    ele%value(phi0$) = pi/2 - fib%mag%lag/twopi
-  else
-    ele%value(phi0$) = fib%mag%lag/twopi
-  endif
-
-case (multipole$)
-  ele%a_pole = knl
-  ele%b_pole = tn
-
-case (octupole$)
-  ele%value(k3$) = knl(3)
-  ele%value(tilt$) = tn(3)
-  knl(3) = 0
-  tn(3) = 0
-
-case (quadrupole$)
-  ele%value(k1$) = knl(1)
-  ele%value(tilt$) = tn(1)
-  knl(1) = 0
-  tn(1) = 0
-
-case (sbend$)
-  ele%value(g$) = fib%mag%p%b0
-  ele%value(angle$) = ele%value(g$) * ele%value(l$)
-  ix = nint(ele%value(ptc_field_geometry$))
-  if (ix == straight$ .or. ix == true_rbend$) then
-    ele%value(e1$) = fib%mag%p%edge(1) + ele%value(angle$)/2
-    ele%value(e2$) = fib%mag%p%edge(2) + ele%value(angle$)/2
-  else
-    ele%value(e1$) = fib%mag%p%edge(1)
-    ele%value(e2$) = fib%mag%p%edge(2)
-  endif
-  ele%value(hgap$) = fib%mag%hgap
-  ele%value(fint$) = fib%mag%fint
-
-case (sextupole$)
-  ele%value(k2$) = knl(2)
-  ele%value(tilt$) = tn(2)
-  knl(2) = 0
-  tn(2) = 0
-
-case (solenoid$)
-  ele%value(ks$) = fib%mag%b_sol
-
-case (sol_quad$)
-  ele%value(ks$) = fib%mag%b_sol
-  ele%value(k1$) = knl(1)
-  ele%value(tilt$) = tn(1)
-  knl(1) = 0
-  tn(1) = 0
-
-case (wiggler$, undulator$)
-
-
-case default
-end select
-
-! Multipoles
-
-if (any(knl /= 0)) then
-endif
-
+if (ele%key /= sbend$) ele%value(tilt$) = fib%mag%p%tiltd
 
 ! kicks
 
@@ -385,10 +334,10 @@ subroutine set_energy (ele)
 type (ele_struct) ele
 
 wk = fib
-ele%value(p0c_start$) = wk%p0c
-ele%value(E_tot_start$) = wk%energy
-ele%value(p0c$) = wk%p0c
-ele%value(E_tot$) = wk%energy
+ele%value(E_tot_start$) = 1d9 * wk%energy
+ele%value(E_tot$)       = 1d9 * wk%energy
+ele%value(p0c_start$)   = 1d9 * wk%p0c
+ele%value(p0c$)         = 1d9 * wk%p0c
 
 end subroutine set_energy
 
--- interface/ptc_read_flat_file.f90
+++ interface/ptc_read_flat_file.f90
@@ -1,5 +1,5 @@
 !+
-! Subroutine ptc_read_flat_file (flat_file, err_flag, lat)
+! Subroutine ptc_read_flat_file (flat_file, err_flag, lat, create_end_marker)
 !
 ! Routine to read a PTC "flat file" (or two linked flat files) and optionally create 
 ! a Bmad lattice based upon the flat file(s).
@@ -7,27 +7,31 @@
 ! The information from the flat file is stored in the PTC universes M_u (and possibly M_t).
 !
 ! Input:
-!   flat_file(:)  -- character(*): Name(s) of PTC flat file(s).
+!   flat_file(:)      -- character(*): Name(s) of PTC flat file(s).
+!   create_end_marker -- logical, optional: Put a marker element named END at the end of the lattice brances?
+!                          Default is True.
 !
 ! Output:
 !   err_flag      -- logical: Set True if there is a problem.
 !   lat           -- lat_struct, optional: If present then setup a Bmad lattice.
 !-
 
-subroutine ptc_read_flat_file (flat_file, err_flag, lat)
+subroutine ptc_read_flat_file (flat_file, err_flag, lat, create_end_marker)
 
 use ptc_interface_mod, except_dummy => ptc_read_flat_file
 use madx_ptc_module
 
 implicit none
 
-type (lat_struct), optional :: lat
+type (lat_struct), optional, target :: lat
 type (ele_struct), pointer :: ele
+type (branch_struct), pointer :: branch
 type (fibre), pointer :: fib
 
 integer iu, ios, i, ix_ele
 
 logical err_flag
+logical, optional :: create_end_marker
 logical old_style
 
 character(*) flat_file(:)
@@ -75,30 +79,56 @@ if (.not. present(lat)) return
 ! Fill in the lattice
 
 if (size(flat_file) == 1) then
-  call init_lat(lat, M_u%start%n)
+  call init_lat(lat, M_u%start%n + 1)
+  branch => lat%branch(0)
+  branch%param%geometry = open$
 
   fib => M_u%start%start
-  lat%param%particle = species_of(fib%mass, nint(fib%charge))
+  branch%param%particle = species_of(1d9*fib%mass, nint(fib%charge))
+  branch%param%default_tracking_species = ref_particle$
 
-  ix_ele = 1
+  ix_ele = 0
   do i = 1, M_u%start%n
     if (i /= 1) fib => fib%next
     ! $START and $END are marker elements inserted by mad-x
     if (index(fib%mag%name, '$START') /= 0) cycle
     if (index(fib%mag%name, '$END') /= 0) cycle
 
-    call fibre_to_ele(fib, lat%ele, ix_ele, err_flag)
+    call fibre_to_ele(fib, branch, ix_ele, err_flag, .true.)
     if (err_flag) return
 
-    print *
-    print *, '!----------------------------------------------------------'
-    print *, i, '  ', fib%mag%name
-    call type_ptc_fibre (fib, .true.)
-
+!    print *
+!    print *, '!----------------------------------------------------------'
+!    print *, i, '  ', fib%mag%name
+!    call type_ptc_fibre (fib, .true.)
   enddo
 
-else
+  if (logic_option(.true., create_end_marker)) then
+    ix_ele = ix_ele + 1
+    ele => lat%ele(ix_ele)
+    call init_ele (ele, marker$, -1, ix_ele, branch)
+    ele%name = 'END'
+  endif
 
+  branch%ele(0)%value(E_tot_start$) = branch%ele(1)%value(E_tot_start$)
+  branch%ele(0)%value(E_tot$)       = branch%ele(1)%value(E_tot$)      
+  branch%ele(0)%value(p0c_start$)   = branch%ele(1)%value(p0c_start$)  
+  branch%ele(0)%value(p0c$)         = branch%ele(1)%value(p0c$)        
+
+  branch%n_ele_track = ix_ele
+  branch%n_ele_max = ix_ele
+
+  lat%input_file_name = flat_file(1)
+  lat%use_name = 'from_flat_file'
+
+  call lattice_bookkeeper(lat)
+  call lat_sanity_check(lat, err_flag)
+
+  branch%param%total_length = branch%ele(ix_ele)%value(l$)
+
+else
+  print *, 'Multiple flat file parsing not yet implemented!'
+  stop
 endif
 
 end subroutine ptc_read_flat_file
