3877cabead59a84b15e53e2a42cf8ec40cd48fe0
David Sagan <dcs16@cornell.edu>
2016-09-10 00:08:24 +0000

    Increased precision.

--- math/sqrt1.f90
+++ math/sqrt1.f90
@@ -17,12 +17,12 @@ use precision_def
 real(rp) eps, ds1
 real(rp), parameter :: c1 = 1.0_rp / 2.0_rp, c2 = -1.0_rp / 8.0_rp 
 real(rp), parameter :: c3 = 1.0_rp / 16.0_rp, c4 = -5.0_rp / 128.0_rp
-real(rp), parameter :: c5 = 7.0_rp / 256.0_rp
+real(rp), parameter :: c5 = 7.0_rp / 256.0_rp, c6 = -21.0_rp / 1024.0_rp
 
 !
 
 if (abs(eps) < 1d-2) then
-  ds1 = (c1 + (c2 + (c3 + (c4 + c5 * eps) * eps) * eps) * eps) * eps
+  ds1 = (c1 + (c2 + (c3 + (c4 + (c5 + c6 * eps) * eps) * eps) * eps) * eps) * eps
 else
   ds1 = sqrt(1 + eps) - 1
 endif
