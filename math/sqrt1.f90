52a4f52bd60c61809e5964f3ad360251a50324dc
David Sagan <dcs16@cornell.edu>
2016-09-10 00:02:22 +0000

    Corrected coefs.

--- math/sqrt1.f90
+++ math/sqrt1.f90
@@ -1,7 +1,7 @@
 !+
 ! function sqrt1(eps) result (ds1)
 !
-! Routine to calculate Sqrt[1+eps] - 1 with a precision of about 1e-12.
+! Routine to calculate Sqrt[1+eps] - 1 with a precision of about 1e-14.
 !
 ! Input:
 !   eps -- real(rp):
@@ -15,13 +15,14 @@ function sqrt1(eps) result (ds1)
 use precision_def
 
 real(rp) eps, ds1
-real(rp), parameter :: c1 = 1.0_rp / 2.0_rp, c2 = 1.0_rp / 8.0_rp 
-real(rp), parameter :: c3 = 1.0_rp / 16.0_rp, c4 = 5.0_rp / 128.0_rp
+real(rp), parameter :: c1 = 1.0_rp / 2.0_rp, c2 = -1.0_rp / 8.0_rp 
+real(rp), parameter :: c3 = 1.0_rp / 16.0_rp, c4 = -5.0_rp / 128.0_rp
+real(rp), parameter :: c5 = 7.0_rp / 256.0_rp
 
 !
 
 if (abs(eps) < 1d-2) then
-  ds1 = (c1 + (c2 + (c3 + c4 * eps) * eps) * eps) * eps
+  ds1 = (c1 + (c2 + (c3 + (c4 + c5 * eps) * eps) * eps) * eps) * eps
 else
   ds1 = sqrt(1 + eps) - 1
 endif
