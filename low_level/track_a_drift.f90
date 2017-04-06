17c805b8240d32bfbcc22536e18c686c7799f80f
David Sagan <dcs16@cornell.edu>
2017-04-06 05:11:01 +0000

    Added include_ref_motion optional arg.

--- low_level/track_a_drift.f90
+++ low_level/track_a_drift.f90
@@ -1,20 +1,22 @@
 !+
-! Subroutine track_a_drift (orb, length, mat6, make_matrix)
+! Subroutine track_a_drift (orb, length, mat6, make_matrix, include_ref_motion)
 !
 ! Subroutine to track a particle as through a drift.
 !
 ! Input:
-!   orb         -- coord_struct: Orbit at start of the drift.
-!   length      -- Real(rp): Length to drift through.
-!   mat6(6,6)   -- Real(rp), optional: Transfer matrix before fringe.
-!   make_matrix -- logical, optional: Propagate the transfer matrix? Default is false.
+!   orb                 -- coord_struct: Orbit at start of the drift.
+!   length              -- Real(rp): Length to drift through.
+!   mat6(6,6)           -- Real(rp), optional: Transfer matrix up to the drift.
+!   make_matrix         -- logical, optional: Propagate the transfer matrix? Default is false.
+!   include_ref_motion  -- logical, optional: Include effect of the motion of the reference particle?
+!                           Default is True. False is basically only used by offset_particle.
 !
 ! Output:
 !   orb        -- coord_struct: Orbit at end of the drift.
-!   mat6(6,6)  -- Real(rp), optional: Transfer matrix transfer matrix including fringe.
+!   mat6(6,6)  -- Real(rp), optional: Transfer matrix including the drift.
 !-
 
-subroutine track_a_drift (orb, length, mat6, make_matrix)
+subroutine track_a_drift (orb, length, mat6, make_matrix, include_ref_motion)
 
 use bmad_struct
 
@@ -25,10 +27,10 @@ type (ele_struct) ele
 type (lat_param_struct) param
 
 real(rp), optional :: mat6(6,6)
-real(rp) matd(6,6), e_tot, e_particle, rel_len
+real(rp) matd(6,6), e_tot_ref, e_particle, rel_len
 real(rp) length, rel_pc, dz, px, py, pz, pxy2, beta_ref
 
-logical, optional :: make_matrix
+logical, optional :: make_matrix, include_ref_motion
 
 ! Everything but photons
 
@@ -41,16 +43,25 @@ if (pxy2 >= 1) then
   return
 endif
 pz = sqrt(1 - pxy2)
-beta_ref = orb%p0c / sqrt(mass_of(orb%species)**2 + orb%p0c**2)
 
 orb%vec(1) = orb%vec(1) + length * px / pz
 orb%vec(3) = orb%vec(3) + length * py / pz
 
 if (orb%beta > 0) then
-  dz = length * (orb%beta /beta_ref - 1/pz)
+  if (logic_option(.true., include_ref_motion)) then
+    beta_ref = orb%p0c / sqrt(mass_of(orb%species)**2 + orb%p0c**2)
+    dz = length * (orb%beta /beta_ref - 1/pz)
+  else
+    dz = -length /pz
+  endif
   orb%t = orb%t + length / (orb%beta * pz * c_light)
+
 else
-  dz = length * (1 - 1/pz)
+  if (logic_option(.true., include_ref_motion)) then
+    dz = length * (1 - 1/pz)
+  else
+    dz = -length /pz
+  endif
 endif
 
 orb%vec(5) = orb%vec(5) + dz
@@ -58,8 +69,6 @@ orb%s = orb%s + length
 
 if (logic_option(.false., make_matrix)) then
   call mat_make_unit(matd)
-  e_tot = sqrt(orb%p0c**2 + mass_of(orb%species)**2)
-  e_particle = orb%p0c * (1 + orb%vec(6)) / orb%beta
   rel_len = length / (rel_pc * pz)
   matd(1,2) =  rel_len * (px**2 / pz**2 + 1)
   matd(3,4) =  rel_len * (py**2 / pz**2 + 1)
@@ -69,8 +78,15 @@ if (logic_option(.false., make_matrix)) then
   matd(3,6) = -rel_len * py / pz**2
   matd(5,2) = -rel_len * px / pz**2 
   matd(5,4) = -rel_len * py / pz**2
-  matd(5,6) =  rel_len * (px**2 + py**2) / pz**2 + &
-                    length * mass_of(orb%species)**2 * e_tot / e_particle**3
+  if (logic_option(.true., include_ref_motion)) then
+    e_tot_ref = sqrt(orb%p0c**2 + mass_of(orb%species)**2)
+    e_particle = orb%p0c * (1 + orb%vec(6)) / orb%beta
+    matd(5,6) =  rel_len * (px**2 + py**2) / pz**2 + &
+                    length * mass_of(orb%species)**2 * e_tot_ref / e_particle**3
+  else
+    matd(5,6) =  rel_len * (px**2 + py**2) / pz**2
+  endif
+
   mat6 = matmul(matd, mat6)
 endif
 
