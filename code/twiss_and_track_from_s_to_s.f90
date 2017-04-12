2bca1e25e77a587efdb1cd17d095fb96229d6cc7
David Sagan <dcs16@cornell.edu>
2017-04-12 21:05:32 +0000

    Fix bug when S at start of lattice not zerol.

--- code/twiss_and_track_from_s_to_s.f90
+++ code/twiss_and_track_from_s_to_s.f90
@@ -55,7 +55,7 @@ type (ele_struct) ele_here
 type (ele_struct), pointer :: ele0, ele_track
 
 real(rp) s_start, s_end
-real(rp) s0, ds
+real(rp) s0, ds, s_true_end
 
 integer ix_start, ix_end
 integer ix_ele
@@ -69,15 +69,16 @@ character(40), parameter :: r_name = 'twiss_and_track_from_s_to_s'
 ! Easy case & error check
 
 s_start = orbit_start%s
+ix_end = element_at_s (branch, s_end, .true., err_flag, s_true_end)
 
-if (s_start == s_end .and. branch%param%geometry == open$) then
+if (s_start == s_true_end .and. branch%param%geometry == open$) then
   orbit_end = orbit_start
   if (present(ele_end)) ele_end = ele_start
   if (present(err)) err = .false.
   return
 endif
 
-if (s_end < s_start .and. branch%param%geometry == open$) then
+if (s_true_end < s_start .and. branch%param%geometry == open$) then
   call out_io (s_abort$, r_name, 'S_END < S_START WITH A LINEAR LATTICE.')
   if (global_com%exit_on_error) call err_exit
   if (present(err)) err = .true.
@@ -102,7 +103,6 @@ if (orbit%location == downstream_end$) then
   ix_start = ix_start + 1
 endif
 
-ix_end = element_at_s (branch, s_end, .true., err_flag = err_flag)
 if (present(err)) err = err_flag
 if (err_flag) return
 
@@ -113,8 +113,8 @@ track_upstream_end = (orbit%location == upstream_end$)
 
 ! Track within a single element case
 
-if (s_end > s_start .and. ix_start == ix_end) then
-  call twiss_and_track_intra_ele (ele0, branch%param, s_start-s0, s_end-s0, track_upstream_end, &
+if (s_true_end > s_start .and. ix_start == ix_end) then
+  call twiss_and_track_intra_ele (ele0, branch%param, s_start-s0, s_true_end-s0, track_upstream_end, &
                       .true., orbit_start, orbit_end, ele_start, ele_end, err, compute_floor_coords)
   return
 endif
@@ -163,14 +163,14 @@ do
   ix_ele = modulo(ix_ele, branch%n_ele_track) + 1
 enddo
 
-! Track to s_end
+! Track to s_true_end
 
 if (present(ele_end)) then
   ele_here%mat6 = ele_end%mat6
   ele_here%vec0 = ele_end%vec0
 endif
 
-ds = s_end-branch%ele(ix_end)%s_start
+ds = s_true_end-branch%ele(ix_end)%s_start
 if (ds /= 0) then
   call twiss_and_track_intra_ele (branch%ele(ix_end), branch%param, 0.0_rp, ds, .true., .true., &
                                   orbit_end, orbit_end, ele_end, ele_end, err, compute_floor_coords)
