7bf23936aa3fa59c3bb30282856f3eaf7214795e
David Sagan <dcs16@cornell.edu>
2017-06-11 04:08:56 +0000

    Coded spin tracking for bmad_standard tracking.

--- mat6_calc_method_test/mat6_calc_method_test.f90
+++ mat6_calc_method_test/mat6_calc_method_test.f90
@@ -68,7 +68,7 @@ do ib = 0, ubound(lat%branch, 1)
     ns = len_trim(ele%name) + 28
 
     do j = 1, n_methods$
-      if (.not. valid_mat6_calc_method(ele, branch%param%particle, j) .or. j == static$ .or. j == custom$) cycle
+      if (.not. valid_mat6_calc_method(ele, branch%param%particle, j) .or. j == static$ .or. j == custom$ .or. j == mad$) cycle
       if (ele%key /= taylor$) call kill_taylor(ele%taylor)
       ele%mat6_calc_method = j
       call init_coord (start_orb, lat%beam_start, ele, upstream_end$, branch%param%particle)
@@ -84,7 +84,7 @@ do ib = 0, ubound(lat%branch, 1)
     do k = 1, 8
       do j = 1, n_methods$
         ! if (j == mad$ .and. custom_test) cycle
-        if(.not. valid_mat6_calc_method(ele, branch%param%particle, j) .or. j == static$ .or. j == custom$) cycle
+        if(.not. valid_mat6_calc_method(ele, branch%param%particle, j) .or. j == static$ .or. j == custom$ .or. j == mad$) cycle
         ele2 => eles(j)
         if (k < 7) then
           if (custom_test) then
