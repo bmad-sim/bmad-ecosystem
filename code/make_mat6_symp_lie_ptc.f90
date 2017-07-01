dc5ad7d639897acdb733535dd78e1ba137d5ef1b
David Sagan <dcs16@cornell.edu>
2017-07-01 01:19:22 +0000

    Minor fixes for backwards tracking.

--- code/make_mat6_symp_lie_ptc.f90
+++ code/make_mat6_symp_lie_ptc.f90
@@ -1,5 +1,5 @@
 !+
-! Subroutine make_mat6_symp_lie_ptc (ele, param, c0, c1)
+! Subroutine make_mat6_symp_lie_ptc (ele, param, start_orb, end_orb)
 !
 ! Subroutine to make the 6x6 transfer matrix for an element. 
 !
@@ -7,41 +7,65 @@
 !   use bmad
 !
 ! Input:
-!   ele    -- Ele_struct: Element with transfer matrix
-!   param  -- lat_param_struct: Parameters are needed for some elements.
-!   c0     -- Coord_struct: Coordinates at the beginning of element. 
+!   ele        -- Ele_struct: Element with transfer matrix
+!   param      -- lat_param_struct: Parameters are needed for some elements.
+!   start_orb  -- Coord_struct: Coordinates at the beginning of element. 
 !
 ! Output:
-!   ele    -- Ele_struct: Element with transfer matrix.
-!     %vec0  -- 0th order map component
-!     %mat6  -- 6x6 transfer matrix.
-!   c1     -- Coord_struct: Coordinates at end of element.
+!   ele        -- Ele_struct: Element with transfer matrix.
+!     %vec0      -- 0th order map component
+!     %mat6      -- 6x6 transfer matrix.
+!   end_orb    -- Coord_struct: Coordinates at end of element.
 !-
 
-subroutine make_mat6_symp_lie_ptc (ele, param, c0, c1)
+subroutine make_mat6_symp_lie_ptc (ele, param, start_orb, end_orb)
 
 use ptc_interface_mod, except_dummy => make_mat6_symp_lie_ptc
 
 implicit none
 
 type (ele_struct), target :: ele
-type (coord_struct) :: c0, c1
+type (coord_struct) :: start_orb, end_orb, s_orb
 type (lat_param_struct)  param
 type (taylor_struct) bmad_taylor(6)
 
+real(rp) dtime_ref
 logical st
 
 !
 
+s_orb = start_orb
+
 st = bmad_com%spin_tracking_on
 bmad_com%spin_tracking_on = .false.
 
-call ele_to_taylor(ele, param, bmad_taylor, c0, .true.)
-call taylor_to_mat6 (bmad_taylor, c0%vec, ele%vec0, ele%mat6)
-call track1_taylor (c0, ele, param, c1, bmad_taylor)
+call ele_to_taylor(ele, param, bmad_taylor, start_orb, .true.)
+call taylor_to_mat6 (bmad_taylor, start_orb%vec, ele%vec0, ele%mat6)
+
+end_orb = start_orb
+call track_taylor (end_orb%vec, bmad_taylor, end_orb%vec)
 call kill_taylor (bmad_taylor)
 
 bmad_com%spin_tracking_on = st
+
+! Time change of particle
+
+dtime_ref = ele%value(delta_ref_time$)
+
+if (s_orb%vec(6) == end_orb%vec(6) .and. ele%value(p0c$) == ele%value(p0c_start$)) then
+  end_orb%t = s_orb%t + dtime_ref + (s_orb%vec(5) - end_orb%vec(5)) / (end_orb%beta * c_light)
+else
+  call convert_pc_to (ele%value(p0c$) * (1 + end_orb%vec(6)), end_orb%species, beta = end_orb%beta)
+  end_orb%t = s_orb%t + dtime_ref + s_orb%vec(5) / (s_orb%beta * c_light) - end_orb%vec(5) / (end_orb%beta * c_light)
+endif
+
+! Set s-position
+
+if (end_orb%direction == 1) then
+  end_orb%s = ele%s
+else
+  end_orb%s = ele%s_start
+endif
 
 end subroutine
 
