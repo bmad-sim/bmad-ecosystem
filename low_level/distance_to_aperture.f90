e665c419627a782dcf15a685220cc57192fb2b48
David Sagan <dcs16@cornell.edu>
2017-07-10 23:41:53 +0000

    Fix bug: no_aperture_here never set.

--- low_level/distance_to_aperture.f90
+++ low_level/distance_to_aperture.f90
@@ -13,7 +13,7 @@
 !   ele             -- ele_struct: Element containing aperture.
 !
 ! Output:
-!   no_aperture_here  -- logical, optional: True if aperture does not exist at the
+!   no_aperture_here  -- logical: True if aperture does not exist at the
 !                          longitudinal location of the particle.
 !   dist              -- real(rp): Percentage distance to the aperture.
 !-
@@ -31,50 +31,56 @@ real(rp) dist, x_particle, y_particle, r_wall, x_lim, y_lim, x0, y0, r, position
 integer particle_at, physical_end
 logical no_aperture_here
 
-!
+! Init
 
 dist = real_garbage$
 no_aperture_here = .true.
+if (.not. bmad_com%aperture_limit_on) return
+
+! Custom calc
 
 if (ele%aperture_type == custom_aperture$) then
   dist = distance_to_aperture_custom (orbit, particle_at, ele, no_aperture_here)
   return
 endif
 
-!
+! Aperture here?
 
 physical_end = physical_ele_end (particle_at, orbit%direction, ele%orientation)
 if (.not. at_this_ele_end (physical_end, ele%aperture_at)) return
 
-x_lim = (ele%value(x1_limit$) + ele%value(x2_limit$)) / 2
-y_lim = (ele%value(y1_limit$) + ele%value(y2_limit$)) / 2
-x0 = (ele%value(x2_limit$) - ele%value(x1_limit$)) / 2
-y0 = (ele%value(y2_limit$) - ele%value(y1_limit$)) / 2
-x_particle = orbit%vec(1) - x0
-y_particle = orbit%vec(3) - y0
+! Calc dist
 
-if (.not. bmad_com%aperture_limit_on .or. x_lim == 0) x_lim = bmad_com%max_aperture_limit
-if (.not. bmad_com%aperture_limit_on .or. y_lim == 0) y_lim = bmad_com%max_aperture_limit
+no_aperture_here = .false.
 
 select case (ele%aperture_type)
 
-case (elliptical$)
-  r = (x_particle / x_lim)**2 + (y_particle / y_lim)**2
-  dist = sqrt(r) - 1
+case (elliptical$, rectangular$, auto_aperture$)
+  x_lim = (ele%value(x1_limit$) + ele%value(x2_limit$)) / 2
+  y_lim = (ele%value(y1_limit$) + ele%value(y2_limit$)) / 2
+  x0 = (ele%value(x2_limit$) - ele%value(x1_limit$)) / 2
+  y0 = (ele%value(y2_limit$) - ele%value(y1_limit$)) / 2
+  x_particle = orbit%vec(1) - x0
+  y_particle = orbit%vec(3) - y0
 
-case (rectangular$, auto_aperture$)
-  if (abs(x_particle/x_lim) > abs(y_particle/y_lim)) then
-    dist = abs(x_particle/x_lim) - 1
-  else
-    dist = abs(y_particle/y_lim) - 1
+  if (x_lim == 0) x_lim = bmad_com%max_aperture_limit
+  if (y_lim == 0) y_lim = bmad_com%max_aperture_limit
+
+  if (ele%aperture_type == elliptical$) then
+    r = (x_particle / x_lim)**2 + (y_particle / y_lim)**2
+    dist = sqrt(r) - 1
+  else      ! rectangular$, auto_aperture$
+    if (abs(x_particle/x_lim) > abs(y_particle/y_lim)) then
+      dist = abs(x_particle/x_lim) - 1
+    else
+      dist = abs(y_particle/y_lim) - 1
+    endif
   endif
 
 case (wall3d$)
   position = [orbit%vec(1:4), orbit%s-ele%s_start, 1.0_rp]
   d_radius = wall3d_d_radius (position, ele, no_wall_here = no_aperture_here, radius_wall = r_wall)
-
   dist = d_radius / r_wall
-
 end select
 
 end function distance_to_aperture
