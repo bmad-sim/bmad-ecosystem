811b9d568b0b08f04d8a5bd057dd1810b976bf31
David Sagan <dcs16@cornell.edu>
2017-07-29 22:15:06 +0000

    Better lat_compute_ref_energy_and_time bookkeeping.

--- tao_test/run.py
+++ tao_test/run.py
@@ -1,11 +1,28 @@
 import subprocess
+import os
 
 out_file = open('output.now', 'w')
 
-results = subprocess.run(['../../production/bin/tao', '-noplot', '-lat', 'lat.bmad'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+#-----------
+
+os.chdir('test1')
+results = subprocess.run(['../../../production/bin/tao', '-noplot', '-lat', 'lat.bmad'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+os.chdir('..')
 
 if 'contact DCS' in results or 'FATAL' in results:
-  out_file.write ('"Bookkeeper" STR  "BAD"')
+  out_file.write ('"Bookkeeper1" STR  "BAD"\n')
   print(results)
 else:
-  out_file.write ('"Bookkeeper" STR  "GOOD"')
+  out_file.write ('"Bookkeeper1" STR  "GOOD"\n')
+
+#-----------
+
+os.chdir('test2')
+results = subprocess.run(['../../../production/bin/tao', '-noplot', '-lat', 'lat.bmad'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+os.chdir('..')
+
+if 'contact DCS' in results or 'FATAL' in results:
+  out_file.write ('"Bookkeeper2" STR  "BAD"\n')
+  print(results)
+else:
+  out_file.write ('"Bookkeeper2" STR  "GOOD"\n')
