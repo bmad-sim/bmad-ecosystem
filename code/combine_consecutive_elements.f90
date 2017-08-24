f4904e569d9f0ff012a714cbe569d04dfa49f502
David Sagan <dcs16@cornell.edu>
2017-08-24 23:47:01 +0000

    More update of combine_consecutive_elements.

--- code/combine_consecutive_elements.f90
+++ code/combine_consecutive_elements.f90
@@ -5,8 +5,7 @@
 ! This allows simplification, for example, of lattices where elements have been 
 ! split to compute the beta function at the center.
 !
-! Modules needed:
-!   use bmad
+! Note: Lattice_bookkeeper is not called by this routine.
 !
 ! Input:
 !   lat -- Lat_struct: Lattice.
@@ -35,24 +34,41 @@ ele_loop: do i = 1, lat%n_ele_track
   ele1 => lat%ele(i)
   ele2 => lat%ele(i+1)
 
-  if (ele1%name /= ele2%name) cycle ! ignore if not a matching pair
+  if (ele1%name /= ele2%name .or. ele1%key /= ele2%key) cycle ! ignore if not a matching pair
+
+  ! Error checks
+
+  select case (ele1%key)
+  case (patch$, match$)
+    call out_io (s_error$, r_name, 'ELEMENTS OF TYPE ' // trim(key_name(ele1%key)) // ' CANNOT BE COMBINED: ' // ele1%name)
+  end select
 
   if (lat%ele(i-1)%name == ele1%name .and. ele2%key /= marker$) then
     call out_io (s_error$, r_name, 'TRIPLE CONSECUTIVE ELEMENTS HAVE SAME NAME! ' // ele1%name)
-    if (global_com%exit_on_error) call err_exit
+    cycle
   endif
 
   if (ele1%key == sbend$) then
     if (ele1%value(e2$) /= 0 .or. ele2%value(e1$) /= 0) then
-      call out_io (s_error$, r_name, 'CONSECUTIVE BENDS HAVE INTERNAL FACE ANGLES: ' // ele1%name)
+      call out_io (s_error$, r_name, 'CONSECUTIVE BENDS HAVE "INTERNAL" FACE ANGLES: ' // ele1%name)
       cycle
     endif
-    ele1%value(e2$) = ele2%value(e2$)
-    ele2%value(e1$) = ele2%value(e1$)
+
+    if (nint(ele1%value(fringe_at$)) == exit_end$ .or. nint(ele1%value(fringe_at$)) == both_ends$ .or. &
+        nint(ele2%value(fringe_at$)) == entrance_end$ .or. nint(ele2%value(fringe_at$)) == both_ends$) then
+      call out_io (s_error$, r_name, 'CONSECUTIVE BENDS HAVE "INTERNAL" FRINGE FIELDS: ' // ele1%name)
+      cycle
+    endif
   endif
 
   do jv = 1, size(ele1%value)
     if (attribute_name(ele1, jv) == 'REF_TIME_START' .or. attribute_name(ele1, jv) == null_name$) cycle
+    if (ele1%key == sbend$) then
+      select case (jv)
+      case (e1$, e2$, hgap$, hgapx$, fint$, fintx$, h1$, h2$, fringe_at$); cycle
+      end select
+    end if
+
     if (abs(ele1%value(jv) - ele2%value(jv)) > 1d-14 * (abs(ele1%value(jv)) + abs(ele2%value(jv)))) then
       call out_io (s_error$, r_name, 'ELEMENT PARAMETERS DO NOT MATCH FOR: ' // ele1%name)
       cycle ele_loop
@@ -64,13 +80,53 @@ ele_loop: do i = 1, lat%n_ele_track
     cycle
   endif
 
+  ! Now combine
+
   ele2%key = -1   ! mark for deletion
+  call set_flags_for_changed_attribute (ele1)
 
   ele1%value(l$) = 2 * ele1%value(l$)
-  ele1%value(hkick$) = 2 * ele1%value(hkick$)
-  ele1%value(vkick$) = 2 * ele1%value(vkick$)
-  ele1%value(BL_hkick$) = 2 * ele1%value(BL_hkick$)
-  ele1%value(BL_vkick$) = 2 * ele1%value(BL_vkick$)
+
+  if (has_attribute(ele1, 'HKICK')) then
+    ele1%value(hkick$) = 2 * ele1%value(hkick$)
+    ele1%value(vkick$) = 2 * ele1%value(vkick$)
+    ele1%value(BL_hkick$) = 2 * ele1%value(BL_hkick$)
+    ele1%value(BL_vkick$) = 2 * ele1%value(BL_vkick$)
+  endif
+
+  if (has_attribute(ele1, 'KICK')) then
+    ele1%value(kick$) = 2 * ele1%value(kick$)
+    ele1%value(BL_kick$) = 2 * ele1%value(BL_kick$)
+  endif
+
+  if (associated(ele1%a_pole)) then
+    ele1%a_pole = 2 * ele1%a_pole
+    if (ele1%key /= multipole$) ele1%b_pole = 2 * ele1%b_pole
+  endif
+
+  if (associated(ele1%a_pole_elec)) then
+    ele1%a_pole_elec = 2 * ele1%a_pole_elec
+    ele1%b_pole_elec = 2 * ele1%b_pole_elec
+  endif
+
+  select case (ele1%key)
+  case (sbend$)
+    ele1%value(e2$) = ele2%value(e2$)
+    ele1%value(h2$) = ele2%value(h2$)
+    ele1%value(hgapx$) = ele2%value(hgapx$)
+    ele1%value(fintx$) = ele2%value(fintx$)
+    ele1%value(angle$) = 2 * ele1%value(angle$)
+    if (nint(ele2%value(fringe_at$)) == exit_end$) then
+      if (nint(ele1%value(fringe_at$)) == no_end$) then
+        ele1%value(fringe_at$) = exit_end$
+      else
+        ele1%value(fringe_at$) = both_ends$
+      endif
+    endif
+
+  case (rfcavity$, lcavity$)
+    ele1%value(voltage$) = 2 * ele1%value(voltage$)
+  end select
 
 enddo ele_loop
 
