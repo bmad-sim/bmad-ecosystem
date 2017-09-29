5f41025f8e05148bde603331d5a72121c9504e68
David Sagan <dcs16@cornell.edu>
2017-09-29 04:47:54 +0000

    Binary field lattice files now implemented.

--- code/tao_c_interface_mod.f90
+++ code/tao_c_interface_mod.f90
@@ -18,7 +18,8 @@ contains
 !
 ! Output:
 !   n_lines -- integer(c_int): C access to scratch%n_lines
-!
+!-
+
 function tao_c_scratch_n_lines() bind(c) result(n_lines)
 integer(c_int) ::  n_lines
 n_lines = scratch%n_lines
@@ -38,7 +39,8 @@ end function
 !
 ! Output:
 !   c_string_ptr -- type(c_ptr): C pointer to access scratch%lines(n)
-!
+!-
+
 function tao_c_scratch_line(n) bind(c) result(c_string_ptr)
 type(c_ptr) :: c_string_ptr
 integer(c_int), value :: n
@@ -47,8 +49,6 @@ i = n
 scratch%c_line = c_string(scratch%lines(i)) 
 c_string_ptr = c_loc(scratch%c_line(1)) ! must point to (1) to avoid gfortran compiler error
 end function
-
-
 
 !------------------------------------------------------------------------
 !+ 
@@ -64,7 +64,8 @@ end function
 !
 ! Output:
 !   err      -- integer(c_int): Error code from tao_parse_command_args (0 => no errors)
-!
+!-
+
 function tao_c_set_init_args(c_str) bind(c) result(error)
 use tao_command_mod
 character(len=1,kind=c_char), dimension(*), intent(in) ::  c_str
@@ -86,7 +87,7 @@ end function
 
 !------------------------------------------------------------------------
 !+ 
-! function tao_c_scratch_line(n) bind(c) result(c_string_ptr)
+! function tao_c_command (c_str) bind(c) result(err)
 !
 ! Function to send a command to Tao from C. 
 !
@@ -98,8 +99,9 @@ end function
 !
 ! Output:
 !   err      -- integer(c_int): Error code from tao_top_level (0 => no errors)
-!
-function tao_c_command(c_str) bind(c) result(err)
+!-
+
+function tao_c_command (c_str) bind(c) result(err)
 character(len=1,kind=c_char), dimension(*), intent(in) ::  c_str
 character(200) :: f_str
 integer :: errcode
