883a358adcad0f4809cf313ebe082a2c0cc28d14
David Sagan <dcs16@cornell.edu>
2017-11-12 16:38:15 +0000

    Added place arg to tao_hook_plot_setup.

--- hook/tao_hook_plot_setup.f90
+++ hook/tao_hook_plot_setup.f90
@@ -1,12 +1,23 @@
 !+
-! Subroutine tao_hook_plot_setup ()
+! Subroutine tao_hook_plot_setup (place)
 !
 ! Subroutine for custimizing setting up the plot data.
+!
+! Input:
+!   place(:)    -- tao_place_input: Placement of plots
+!
+! Output:
+!   place(:)    -- tao_place_input: Possibly modified placement of plots.
 !-
 
-subroutine tao_hook_plot_setup ()
+subroutine tao_hook_plot_setup (place)
 
+use tao_input_struct
 use tao_mod, dummy => tao_hook_plot_setup
+
+implicit none
+
+type (tao_place_input) place(:)
 
 ! This is just a dummy subroutine used as the default.
 
