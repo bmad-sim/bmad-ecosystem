cc7c2d79e348c85702f865b7b946894cd2b5e6ac
David Sagan <dcs16@cornell.edu>
2017-11-14 15:23:36 +0000

    Try another hook_init_plotting organization.

--- hook/tao_hook_init_plotting.f90
+++ hook/tao_hook_init_plotting.f90
@@ -1,23 +1,14 @@
 !+
-! Subroutine tao_hook_init_plotting (place) 
+! Subroutine tao_hook_init_plotting () 
 !
 ! Hook routine to initialize plotting.
-!
-! Input:
-!   place(:)    -- tao_place_input: Placement of plots
-!
-! Output:
-!   place(:)    -- tao_place_input: Possibly modified placement of plots.
 !-
 
-subroutine tao_hook_init_plotting (place)
+subroutine tao_hook_init_plotting ()
 
-use tao_input_struct
 use tao_mod, dummy => tao_hook_init_plotting
 
 implicit none
-
-type (tao_place_input) place(:)
 
 !
 
