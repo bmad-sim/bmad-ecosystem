c4496156a3279f492f4838c9024eec861eae972a
David Sagan <dcs16@cornell.edu>
2017-12-05 05:36:05 +0000

    First pass at a long term tracking program.

---
+++ long_term_tracking/long_term_tracking.f90
@@ -0,0 +1,89 @@
+program long_term_tracking
+
+use beam_mod
+use twiss_and_track_mod
+
+implicit none
+
+type (lat_struct) lat
+type (beam_init_struct) beam_init
+type (bunch_struct) bunch, bunch_init
+type (coord_struct), allocatable :: closed_orb(:)
+
+integer ix_ele_start, n_turns
+integer i, n, ie
+logical err_flag, rfcavity_on
+
+character(200) lat_file, init_file, dat_file
+
+namelist / long_term_tracking_params / lat_file, n_turns, ix_ele_start, bmad_com, dat_file, beam_init, rfcavity_on 
+
+! Parse command line
+
+init_file = 'long_term_tracking.init'
+
+if (cesr_iargc() == 1) then
+  call cesr_getarg(1, init_file)
+elseif (cesr_iargc() > 1) then
+  print *, 'Extra stuff on the command line!? Stopping here.'
+  stop
+endif
+
+! Read parameters
+
+ix_ele_start = 0
+dat_file = 'long_term_tracking.dat'
+rfcavity_on = .true.
+
+print '(2a)', 'Initialization file: ', trim(init_file)
+
+open (1, file = init_file, status = 'old')
+read (1, nml = long_term_tracking_params)
+close (1)
+
+! Lattice init
+
+call bmad_parser (lat_file, lat)
+if (.not. rfcavity_on) call set_on_off (rfcavity$, lat, off$)
+call twiss_and_track (lat, closed_orb)
+
+! Bunch init
+
+call init_bunch_distribution (lat%ele(ix_ele_start), lat%param, beam_init, 0, bunch, err_flag)
+if (err_flag) stop
+
+do n = 1, size(bunch%particle)
+  bunch%particle(n)%vec = bunch%particle(n)%vec + closed_orb(ix_ele_start)%vec
+enddo
+
+allocate(bunch_init%particle(size(bunch%particle)))
+bunch_init%particle = bunch%particle
+
+! Track
+
+do n = 1, n_turns
+  do ie = ix_ele_start+1, lat%n_ele_track
+    call track1_bunch(bunch, lat, lat%ele(ie), bunch, err_flag)
+  enddo
+  do ie = 1, ix_ele_start
+    call track1_bunch(bunch, lat, lat%ele(ie), bunch, err_flag)
+  enddo
+enddo
+
+! Output results
+
+open (1, file = dat_file, recl = 300)
+write (1, '(a)') '                                                     Start                                                        !', &
+                 '                                                     End                                                          '
+write (1, '(a)') '#  Ix    x           px            y           py           z          pz       spin_x       spin_y        spin_z |', &
+                      '    x           px            y           py           z          pz       spin_x       spin_y        spin_z'
+
+do i = 1, size(bunch%particle)
+  write (1, '(i6, 6f13.8, 4x, f13.8, 6x, 6f13.8, 4x, f13.8)'), i, &
+                    bunch_init%particle(i)%vec, bunch_init%particle(i)%spin, &
+                    bunch%particle(i)%vec, bunch%particle(i)%spin
+enddo
+
+close (1)
+
+end program
