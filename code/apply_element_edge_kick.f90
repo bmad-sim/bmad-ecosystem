b98aee4ddd247e659d2e35917498af202b3bfd33
David Sagan <dcs16@cornell.edu>
2017-12-08 21:14:52 +0000

    Fix track1_bmad_spin to not use antiparticle.

--- code/apply_element_edge_kick.f90
+++ code/apply_element_edge_kick.f90
@@ -1,5 +1,6 @@
 !+
-! Subroutine apply_element_edge_kick (orb, fringe_info, track_ele, param, track_spin, mat6, make_matrix, rf_time, apply_sol_fringe)
+! Subroutine apply_element_edge_kick (orb, fringe_info, track_ele, param, track_spin, mat6, make_matrix, rf_time, 
+!                                                                                apply_sol_fringe, reverse_the_charge)
 !
 ! Subroutine, used with runge_kutta and boris tracking, to track through the edge fringe field of an element.
 ! This routine is used and with the bmad_standard field_calc where the field can have an abrubt, 
@@ -17,16 +18,18 @@
 ! Additionally, Any element that has an electric multipole has an edge kick.
 !
 ! Input:
-!   orb               -- Coord_struct: Starting coords in element reference frame.
-!   fringe_info       -- fringe_edge_info_struct: Fringe information.
-!   track_ele         -- ele_struct: Element being tracked through. Is different from fringe_info%hard_ele
-!                          when there are superpositions and track_ele can be a super_slave of fringe_info%hard_ele.
-!   param             -- lat_param_struct: lattice parameters.
-!   track_spin        -- logical: Track the spin?
-!   mat6(6,6)         -- Real(rp), optional: Transfer matrix before fringe.
-!   make_matrix       -- logical, optional: Propagate the transfer matrix? Default is false.
-!   rf_time           -- real(rp), optional: RF clock time. If not present then the time will be calculated using the standard algorithm.
-!   apply_sol_fringe  -- logical, optional: Apply the solenoid fringe kick? Default is True.
+!   orb                -- Coord_struct: Starting coords in element reference frame.
+!   fringe_info        -- fringe_edge_info_struct: Fringe information.
+!   track_ele          -- ele_struct: Element being tracked through. Is different from fringe_info%hard_ele
+!                           when there are superpositions and track_ele can be a super_slave of fringe_info%hard_ele.
+!   param              -- lat_param_struct: lattice parameters.
+!   track_spin         -- logical: Track the spin?
+!   mat6(6,6)          -- Real(rp), optional: Transfer matrix before fringe.
+!   make_matrix        -- logical, optional: Propagate the transfer matrix? Default is false.
+!   rf_time            -- real(rp), optional: RF clock time. If not present then the time will be calculated using the standard algorithm.
+!   apply_sol_fringe   -- logical, optional: Apply the solenoid fringe kick? Default is True.
+!   reverse_the_charge -- logical, optional: If present and True then reverse the particle's charge. This reverses
+!                           the applied kick and is useful for going backwards through the edge.
 !
 ! Output:
 !   fringe_info   -- fringe_edge_info_struct: Fringe information.
@@ -34,7 +37,8 @@
 !   mat6(6,6)     -- Real(rp), optional: Transfer matrix transfer matrix including fringe.
 !-
 
-subroutine apply_element_edge_kick (orb, fringe_info, track_ele, param, track_spin, mat6, make_matrix, rf_time, apply_sol_fringe)
+subroutine apply_element_edge_kick (orb, fringe_info, track_ele, param, track_spin, mat6, make_matrix, rf_time, &
+                                                                                apply_sol_fringe, reverse_the_charge)
 
 use track1_mod, except_dummy => apply_element_edge_kick
 
@@ -49,17 +53,19 @@ type (ele_struct), pointer :: hard_ele
 
 real(rp), optional :: mat6(6,6), rf_time
 real(rp) f, l_drift, ks4, s_edge, s, phi, omega(3), pc, z_saved, beta_ref, ds
-real(rp) a_pole_elec(0:n_pole_maxx), b_pole_elec(0:n_pole_maxx)
+real(rp) a_pole_elec(0:n_pole_maxx), b_pole_elec(0:n_pole_maxx), charge
 complex(rp) xiy, c_vec
 
 integer physical_end, dir, i, fringe_at, at_sign, sign_z_vel, particle_at, ix_elec_max
 
-logical, optional :: make_matrix, apply_sol_fringe
+logical, optional :: make_matrix, apply_sol_fringe, reverse_the_charge
 logical finished, track_spin, track_spn
 
 ! The setting of fringe_info%hard_location is used by calc_next_fringe_edge to calculate the next fringe location.
 
 particle_at = fringe_info%particle_at
+charge = charge_of(orb%species)
+if (logic_option(.false., reverse_the_charge)) charge = -charge
 
 if (associated(fringe_info%hard_ele)) then
   hard_ele => fringe_info%hard_ele
@@ -150,7 +156,7 @@ case (sbend$)
 case (solenoid$, sol_quad$, bend_sol_quad$)
   if (logic_option(.true., apply_sol_fringe)) then
     ! To make reverse tracking the same as forward tracking, use a symmetrical orbital-spin-orbital kick scheme.
-    ks4 = at_sign * charge_of(orb%species) * hard_ele%value(bs_field$) * c_light / (4 * orb%p0c)
+    ks4 = at_sign * charge * hard_ele%value(bs_field$) * c_light / (4 * orb%p0c)
     orb%vec(2) = orb%vec(2) + ks4 * orb%vec(3)
     orb%vec(4) = orb%vec(4) - ks4 * orb%vec(1)
     if (track_spn) then
@@ -164,7 +170,7 @@ case (solenoid$, sol_quad$, bend_sol_quad$)
 case (lcavity$, rfcavity$, e_gun$)
 
   ! Add on bmad_com%significant_length to make sure we are just inside the cavity.
-  f = at_sign * charge_of(orb%species) / (2 * orb%p0c)
+  f = at_sign * charge / (2 * orb%p0c)
   s = s_edge
 
   if (at_this_ele_end(physical_end, nint(hard_ele%value(fringe_at$)))) then
@@ -186,7 +192,7 @@ case (lcavity$, rfcavity$, e_gun$)
     orb%vec(4) = orb%vec(4) - field%e(3) * orb%vec(3) * f - c_light * field%b(3) * orb%vec(1) * f
 
     if (track_spn) then
-      f = at_sign * charge_of(orb%species) / 2.0_rp
+      f = at_sign * charge / 2.0_rp
       call rotate_spin_given_field (orb, sign_z_vel, &
                                            -[orb%vec(1), orb%vec(3), 0.0_rp] * (f * field%b(3)), &
                                            -[orb%vec(1), orb%vec(3), 0.0_rp] * (f * field%e(3)))
@@ -200,7 +206,7 @@ case (lcavity$, rfcavity$, e_gun$)
 case (elseparator$)
   ! Longitudinal fringe field
   if (hard_ele%value(l$) /= 0) then
-    f = at_sign * charge_of(orb%species) * (hard_ele%value(p0c$) / hard_ele%value(l$))
+    f = at_sign * charge * (hard_ele%value(p0c$) / hard_ele%value(l$))
     phi = f * (hard_ele%value(hkick$) * orb%vec(1) + hard_ele%value(vkick$) * orb%vec(3))
     call apply_energy_kick (phi, orb, [f * hard_ele%value(hkick$), f * hard_ele%value(vkick$)], mat6, make_matrix)
     if (track_spn) then
@@ -232,7 +238,7 @@ logical err_flag
 
 if (hard_ele%field_calc == bmad_standard$) then
   if (ix_elec_max > -1) then
-    f = at_sign * charge_of(orb%species) 
+    f = at_sign * charge 
 
     if (hard_ele%key == sbend$ .and. nint(hard_ele%value(exact_multipoles$)) /= off$) then
       call bend_exact_multipole_field (hard_ele, param, orb, .true., field, .false., .true.)
@@ -261,7 +267,7 @@ endif
 ! DC fieldmap fringe. 
 
 if (hard_ele%field_calc == fieldmap$ .and. hard_ele%tracking_method /= bmad_standard$) then
-  f = at_sign * charge_of(orb%species) 
+  f = at_sign * charge 
   call em_field_calc(hard_ele, param, s_edge, orb, .true., field, .false., err_flag, .true.)
   call apply_energy_kick (-f * field%phi, orb, f * field%E(1:2), mat6, make_matrix)
   if (track_spn) call rotate_spin_given_field (orb, sign_z_vel, EL = [0.0_rp, 0.0_rp, -f * field%phi])
