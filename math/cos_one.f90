d2c8b3803c9cc7276bfd44594a134646254a3770
David Sagan <dcs16@cornell.edu>
2018-02-27 09:04:35 +0000

    Switch to exact formula.

--- math/cos_one.f90
+++ math/cos_one.f90
@@ -1,8 +1,7 @@
 !+
 ! function cos_one(angle) result (cos1)
 !
-! Routine to calculate Cos[angle] - 1 to near machine precision.
-! Maximum loss of precision is about 1 digit.
+! Routine to calculate Cos[angle] - 1 to machine precision.
 !
 ! This is usful if angle can be near zero where the direct evaluation of
 ! cos[angle] - 1 is inaccurate.
@@ -20,22 +19,10 @@ use precision_def
 implicit none
 
 real(rp), intent(in) :: angle
-real(rp) cos1, a2
-real(rp), parameter :: c2 = -1.0_rp/2.0_rp, c4 = 1.0_rp/24.0_rp, c6 = -1.0_rp/720.0_rp
-real(rp), parameter :: c8 = 1.0_rp/40320.0_rp, c10 = -1.0_rp/3628800.0_rp, c12 = 1.0/479001600.0_rp
+real(rp) cos1
 
 !
 
-if (abs(angle) < 0.01) then
-  a2 = angle**2
-  cos1 = (c2 + (c4 + (c6 * a2)) * a2) * a2
-
-elseif (abs(angle) < 0.3) then
-  a2 = angle**2
-  cos1 = (c2 + (c4 + (c6 + (c8 + (c10 + (c12 * a2)) * a2) * a2) * a2) * a2) * a2
-
-else
-  cos1 = cos(angle) - 1
-endif
+cos1 = -2 * sin(angle/2)**2
 
 end function
