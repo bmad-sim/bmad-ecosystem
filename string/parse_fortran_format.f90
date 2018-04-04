2c9446aeb33ca0d941b7eeb4397b9e358e60888d
David Sagan <dcs16@cornell.edu>
2018-04-04 19:49:10 +0000

    Added checking for the power scale factor in parse_fortran_format.

--- string/parse_fortran_format.f90
+++ string/parse_fortran_format.f90
@@ -1,9 +1,9 @@
 !+
-! Subroutine parse_fortran_format (format_str, multiplyer, code, width, digits)
+! Subroutine parse_fortran_format (format_str, multiplyer, power, code, width, digits)
 !
 ! Routine to parse a Fortran edit descriptor.
 ! This routine assumes that format_str will be a edit descriptor for a single entity like '3f10.6'.
-! Strings multiple entities (any string with a comma) are not allowed.
+! format_str must only have a single edit descriptor. That is, format_str must not have any commas.
 !
 ! Also see: real_num_fortran_format
 !
@@ -13,21 +13,24 @@
 ! Output:
 !   multiplyer  -- integer: Multiplyer prefix before the code string. EG '5I2' -> multiplyer = 5, 
 !                   'es12.6' -> multiplyer = 1 (default).
+!   power       -- integer: Power scale factor. EG: '3pf12.2' -> power = 3. Set to 0 if no power factor present.
 !   code        -- character(*): Format code. EG 'T37' -> code = 'T'. Set to '' if there is a decode error.
+!                   Note: If a power factor is present, code will *not* contain "p". EG: '3pf12.5' -> code = 'f'.
 !   width       -- integer: Field width. EG 'A23' -> width = 23. Set to -1 if not present (EG: '34X')
 !   digits      -- integer: number of digits after the decimal place for real numbers. EG '4f10.3' -> digits = 3.
 !                   Set to -1 if not present.
 !-
 
-subroutine parse_fortran_format (format_str, multiplyer, code, width, digits)
+subroutine parse_fortran_format (format_str, multiplyer, power, code, width, digits)
 
 use sim_utils_interface, dummy => parse_fortran_format
 
 implicit none
 
 integer multiplyer, width, digits
-integer i, i0, nn
+integer i1, i0, nn, power, n_parens
 character(*) format_str, code
+logical found_parens
 
 ! Init
 
@@ -35,74 +38,98 @@ multiplyer = 1
 code = ''
 width = -1
 digits = -1
+power = 0
+n_parens = 0
 nn = len(format_str)
+if (nn == 0) return
 
 ! ignore leading spaces
 
-do i = 1, nn
-  if (format_str(i:i) /= ' ') exit
+do i1 = 1, nn
+  if (format_str(i1:i1) /= ' ') exit
 enddo
 
-if (i == nn + 1) return
+call check_for_open_parens ()
+if (i1 > nn) return
 
 ! Look for multiplyer
 
-i0 = i
-do i = i0, nn
-  if (index('1234567890', format_str(i:i)) == 0) exit
-enddo
+if (index('1234567890', format_str(i1:i1)) /= 0) then
+  i0 = i1
+  do i1 = i0, nn
+    if (index('1234567890', format_str(i1:i1)) == 0) exit
+  enddo
+  read (format_str(i0:i1-1), *) multiplyer
+endif
 
-if (i /= i0) then
-  if (.not. is_integer(format_str(i0:i-1))) return
-  read (format_str(i0:i-1), *) multiplyer
+! Look for power factor
+
+call check_for_open_parens (found_parens)
+if (i1 > nn .or. (found_parens .and. i1 >= nn)) return
+
+if (found_parens .and. index('1234567890', format_str(i1:i1)) /= 0) then ! If something like '3(4pf12.4)' then 
+  i0 = i1
+  do i1 = i0, nn
+    if (index('1234567890', format_str(i1:i1)) == 0) exit
+  enddo
+  read (format_str(i0:i1-1), *) power
+  if (i1 > nn-1) return
+  if (format_str(i1:i1) /= 'p' .and. format_str(i1:i1) /= 'P') return
+  i1 = i1 + 1
+
+elseif (format_str(i1:i1) == 'p' .or. format_str(i1:i1) == 'P') then  ! If something like '4pf12.4'
+  power = multiplyer
+  multiplyer = 1
+  i1 = i1 + 1
 endif
 
 ! Look for code
 
-i0 = i
-do i = i0, nn
-  if (index('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', format_str(i:i)) == 0) exit
+if (i1 > nn .or. (found_parens .and. i1 >= nn)) return
+
+i0 = i1
+do i1 = i0, nn
+  if (index('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', format_str(i1:i1)) == 0) exit
 enddo
 
-if (i == i0) return
-code = format_str(i0:i-1)
-if (no_more(i)) return
+if (i1 == i0) return
+code = format_str(i0:i1-1)
+if (no_more(i1)) return
 
 ! Look for width
 
-i0 = i
-do i = i0, nn
-  if (index('1234567890', format_str(i:i)) == 0) exit
+i0 = i1
+do i1 = i0, nn
+  if (index('1234567890', format_str(i1:i1)) == 0) exit
 enddo
 
-if (i == i0 .or. .not. is_integer(format_str(i0:i-1))) then
+if (i1 == i0 .or. .not. is_integer(format_str(i0:i1-1))) then
   code = ''   ! Decode error
   return
 endif
 
-read (format_str(i0:i-1), *) width
-if (no_more(i)) return
+read (format_str(i0:i1-1), *) width
+if (no_more(i1)) return
 
 ! Look for digits
 
-if (format_str(i:i) /= '.') then
+if (format_str(i1:i1) /= '.') then
   code = ''   ! Decode error
   return
 endif
 
-i0 = i + 1
-do i = i0, nn
-  if (index('1234567890', format_str(i:i)) == 0) exit
+i0 = i1 + 1
+do i1 = i0, nn
+  if (index('1234567890', format_str(i1:i1)) == 0) exit
 enddo
 
-if (i == i0 .or. .not. is_integer(format_str(i0:i-1))) then
+if (i1 == i0 .or. .not. is_integer(format_str(i0:i1-1))) then
   code = ''   ! Decode error
   return
 endif
 
-read (format_str(i0:i-1), *) digits
-if (no_more(i)) return
-
+read (format_str(i0:i1-1), *) digits
+if (no_more(i1)) return
 
 !----------------------------------------
 contains
@@ -114,14 +141,43 @@ integer i1
 
 !
 
-if (i1 == nn + 1) then
-  nothing_else = .true.
-  return
-endif
+do
+  if (i1 == nn + 1) then
+    nothing_else = .true.
+    if (n_parens /= 0) code = ''
+    return
+  endif
+
+  if (format_str(i1:i1) == ')') then
+    n_parens = n_parens - 1
+    i1 = i1 + 1
+  else
+    exit
+  endif
+enddo
 
 nothing_else = (format_str(i1:i1) == '')
 if (nothing_else .and. format_str(i1+1:) /= '') code = ''   ! Decode error
 
-end function
+end function no_more
+
+!----------------------------------------
+! contains
+
+subroutine check_for_open_parens(found_parens)
+
+logical, optional :: found_parens
+
+! 
+
+if (present(found_parens)) found_parens = .false.
+if (i1 > nn) return
+if (format_str(i1:i1) /= '(') return
+
+if (present(found_parens)) found_parens = .true.
+n_parens = n_parens + 1
+i1 = i1 + 1
+
+end subroutine check_for_open_parens
 
 end subroutine
