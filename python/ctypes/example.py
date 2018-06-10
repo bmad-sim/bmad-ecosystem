c5a1f96dbf57de879913b402c092999866ac4377
David Sagan <dcs16@cornell.edu>
2018-06-10 06:27:15 +0000

    Generalized so pytao.py does not have to be in same directory.

--- python/ctypes/example.py
+++ python/ctypes/example.py
@@ -1,8 +1,12 @@
-import pytao
 import os
+import sys
 
-tao = pytao.pytao()
 BASE = os.environ['ACC_ROOT_DIR']
+sys.path.insert (0, BASE + '/tao/python/ctypes')   # directory containing pytao.py
+
+import pytao
+
+tao = pytao.pytao()     # Assumes Tao library is in the standard directory.
 bmadfile = BASE + '/tao/examples/cesr/bmad_L9A18A000-_MOVEREC.lat'
 tao.init('-lat ' + bmadfile)
 tao.cmd('show ele 1')
