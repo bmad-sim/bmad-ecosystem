a5a71d1a17d4e529cb6267da456c97ff08fe2d94
David Sagan <dcs16@cornell.edu>
2018-06-14 18:51:27 +0000

    Fixed problem in tao_var_repoint when "-no_grouping" had been used with a variable.

--- code/tao_var_repoint.f90
+++ code/tao_var_repoint.f90
@@ -2,9 +2,6 @@
 ! Suburoutine tao_var_repoint()
 !
 ! Routine to reset tao variable pointers.
-! This is needed if the lattice is perturbed.
-! For example, this routine needs to be called after a read command (which calls bmad_parser2) since the
-! lattice file called may use superposition.
 !-
 
 subroutine tao_var_repoint ()
@@ -16,29 +13,32 @@ implicit none
 type (tao_var_struct), pointer :: var_ptr
 type (tao_var_slave_struct), pointer :: sl
 type (tao_universe_struct), pointer :: u
-type (all_pointer_struct), allocatable :: a_ptr(:), b_ptr(:)
+type (all_pointer_struct) :: a_ptr
+type (ele_struct), pointer :: ele
 
-integer i, j, num
+integer i, j
 logical err
 
-!
+! Remember: If the "-no_grouping" swithch has been used, there can be multiple elements with the same name
+! but a variable, in this case, will only have one of them as a slave.
+! This makes finding the correct slave element difficult if the slave element has shifted position. 
+! Therefore, it is forbidden for any Tao command (for example the "read lattice" command) to modify element positions.
 
 do i = 1, size(s%var)
   var_ptr => s%var(i)
-  num = 0
   if (size(var_ptr%slave) == 0) cycle
 
-  do 
-    if (num == size(var_ptr%slave)) exit
-    sl => var_ptr%slave(num+1)
+  do j = 1, size(var_ptr%slave)
+    sl => var_ptr%slave(j)
     u => s%u(sl%ix_uni)
-    call pointers_to_attribute (u%model%lat, var_ptr%ele_name, var_ptr%attrib_name, .true., a_ptr, err, .false.)
-    call pointers_to_attribute (u%base%lat, var_ptr%ele_name, var_ptr%attrib_name, .true., b_ptr, err, .false.)
-    do j = 1, size(a_ptr)
-      var_ptr%slave(num+j)%model_value => a_ptr(j)%r
-      var_ptr%slave(num+j)%base_value => b_ptr(j)%r
-    enddo
-    num = num + size(a_ptr)
+
+    ele => pointer_to_ele(u%model%lat, sl%ix_ele, sl%ix_branch)
+    call pointer_to_attribute (ele, var_ptr%attrib_name, .true., a_ptr, err, .false.)
+    sl%model_value => a_ptr%r
+
+    ele => pointer_to_ele(u%base%lat, sl%ix_ele, sl%ix_branch)
+    call pointer_to_attribute (ele, var_ptr%attrib_name, .true., a_ptr, err, .false.)
+    sl%base_value => a_ptr%r
   enddo
 
   var_ptr%model_value => var_ptr%slave(1)%model_value
