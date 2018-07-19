cbc4c025a09665aaadfe9339161acd56d6a5b252
David Sagan <dcs16@cornell.edu>
2018-07-19 06:30:45 +0000

    Fix for gfortran problems.

--- low_level/transfer_lat_parameters.f90
+++ low_level/transfer_lat_parameters.f90
@@ -26,7 +26,6 @@ type (lat_struct) :: lat_out
 
 !
 
-lat_out%constant                  = lat_in%constant
 lat_out%use_name                  = lat_in%use_name
 lat_out%lattice                   = lat_in%lattice
 lat_out%input_file_name           = lat_in%input_file_name
