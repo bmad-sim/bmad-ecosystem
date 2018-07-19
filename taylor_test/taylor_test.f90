765c6733f28d6c78bc247d4ac001634fcbff22b8
David Sagan <dcs16@cornell.edu>
2018-07-19 04:56:51 +0000

    Added Sokolov-Ternov spin flipping.

--- taylor_test/taylor_test.f90
+++ taylor_test/taylor_test.f90
@@ -26,7 +26,7 @@ open (1, file = 'output.now', recl = 200)
 !ele1 => lat%ele(1)
 !ele1%tracking_method = taylor$
 !call attribute_bookkeeper (ele1, lat%param, .true.)  ! So taylor map will not be killed in tracking
-!call ele_to_taylor (ele1, lat%param, ele1%taylor, lat%beam_start)
+!call ele_to_taylor (ele1, lat%param, lat%beam_start)
 !
 !call concat_ele_taylor(ele1%taylor, ele2, ele2%taylor)
 
