217ec5860f5de6031e9bdf9b78e3c1300d1073f7
David Sagan <dcs16@cornell.edu>
2018-09-10 19:55:24 +0000

    Fixed case where var not associated with a lattice element.

--- code/tao_var_repoint.f90
+++ code/tao_var_repoint.f90
@@ -10,10 +10,11 @@ use tao_struct
 
 implicit none
 
-type (tao_var_struct), pointer :: var_ptr
+type (tao_var_struct), pointer :: var
 type (tao_var_slave_struct), pointer :: sl
 type (tao_universe_struct), pointer :: u
 type (all_pointer_struct) :: a_ptr
+type (all_pointer_struct), allocatable :: all_ptr(:)
 type (ele_struct), pointer :: ele
 
 integer i, j
@@ -25,24 +26,35 @@ logical err
 ! Therefore, it is forbidden for any Tao command (for example the "read lattice" command) to modify element positions.
 
 do i = 1, size(s%var)
-  var_ptr => s%var(i)
-  if (size(var_ptr%slave) == 0) cycle
+  var => s%var(i)
+  if (size(var%slave) == 0) cycle
 
-  do j = 1, size(var_ptr%slave)
-    sl => var_ptr%slave(j)
+  do j = 1, size(var%slave)
+    sl => var%slave(j)
     u => s%u(sl%ix_uni)
 
-    ele => pointer_to_ele(u%model%lat, sl%ix_ele, sl%ix_branch)
-    call pointer_to_attribute (ele, var_ptr%attrib_name, .true., a_ptr, err, .false.)
-    sl%model_value => a_ptr%r
+    ! Something like 'BEAM_START' will not have an associated lattice element.
+    if (sl%ix_ele < 0) then
+      call pointers_to_attribute (u%model%lat, var%ele_name, var%attrib_name, .true., all_ptr, err)
+      sl%model_value => all_ptr(1)%r
 
-    ele => pointer_to_ele(u%base%lat, sl%ix_ele, sl%ix_branch)
-    call pointer_to_attribute (ele, var_ptr%attrib_name, .true., a_ptr, err, .false.)
-    sl%base_value => a_ptr%r
+      call pointers_to_attribute (u%base%lat, var%ele_name, var%attrib_name, .true., all_ptr, err)
+      sl%base_value => all_ptr(1)%r
+
+    else
+      ele => pointer_to_ele(u%model%lat, sl%ix_ele, sl%ix_branch)
+      call pointer_to_attribute (ele, var%attrib_name, .true., a_ptr, err, .false.)
+      sl%model_value => a_ptr%r
+
+      ele => pointer_to_ele(u%base%lat, sl%ix_ele, sl%ix_branch)
+      call pointer_to_attribute (ele, var%attrib_name, .true., a_ptr, err, .false.)
+      sl%base_value => a_ptr%r
+    endif
+
   enddo
 
-  var_ptr%model_value => var_ptr%slave(1)%model_value
-  var_ptr%base_value => var_ptr%slave(1)%base_value
+  var%model_value => var%slave(1)%model_value
+  var%base_value => var%slave(1)%base_value
 enddo
 
 end subroutine
