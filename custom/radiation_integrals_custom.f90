ce787ebba378fef25638a0463e7acee6b6987fe7
David Sagan <dcs16@cornell.edu>
2018-09-13 16:03:53 +0000

    Fixed radiaiton_integrals_custom to be more user friendly.

--- custom/radiation_integrals_custom.f90
+++ custom/radiation_integrals_custom.f90
@@ -1,27 +1,27 @@
 !+
-! Subroutine radiation_integrals_custom (lat, ir, orb, err_flag)
+! Subroutine radiation_integrals_custom (lat, ir, orb, rad_int1, err_flag)
 !
 ! Dummy routine for custom elements. Will generate an error if called.
 ! A valid radiation_integrals_custom is needed only if the 
 ! radiation_integrals routine is being used.
 !
 ! General rule: Your code may NOT modify any argument that is not listed as
-! an output agument below."
+! an output agument below.
 !
 ! Modules needed:
 !   use rad_int_common
 !
 ! Input:
-!   lat    -- lat_struct: Lattice with the custom element.
-!   ir     -- Integer: lat%ele(ir) is the custom element.
-!   orb(:) -- Coord_struct: Orbit around which integrals are to be evaluated.
+!   lat         -- lat_struct: Lattice with the custom element.
+!   ir          -- integer: lat%ele(ir) is the custom element.
+!   orb(:)      -- coord_struct: Orbit around which integrals are to be evaluated.
 !
 ! Output:
-!   ric      -- Rad_int_all_ele_struct: Common block for storing the results.
-!   err_flag -- Logical: Set true if there is an error. False otherwise.
+!   rad_int1    -- rad_int1_struct: Structure for storing the results.
+!   err_flag    -- logical: Set true if there is an error. False otherwise.
 !-
 
-subroutine radiation_integrals_custom (lat, ir, orb, err_flag)
+subroutine radiation_integrals_custom (lat, ir, orb, rad_int1, err_flag)
 
 use bmad_interface, dummy => radiation_integrals_custom
 
@@ -29,6 +29,7 @@ implicit none
 
 type (lat_struct) lat
 type (coord_struct) orb(0:)
+type (rad_int1_struct) rad_int1
 integer ir
 logical err_flag
 character(32) :: r_name = 'radiation_integrals_custom'
