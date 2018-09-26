d2557aa2eca3c4235a08909b0a1bfa181757e4f5
David Sagan <dcs16@cornell.edu>
2018-09-26 19:11:35 +0000

    Now track_beam calls track_bunch instead of track1_beam.

---
+++ custom/track1_bunch_hook.f90
@@ -0,0 +1,40 @@
+!+
+! Subroutine track1_bunch_hook (bunch_start, lat, ele, bunch_end, err, centroid, direction)
+!
+! Routine that can be customized for tracking a bunch through a single element.
+!
+! Input:
+!   bunch_start   -- Bunch_struct: Starting bunch position.
+!   lat          -- lat_struct: Lattice containing element to be tracked through.
+!   ele          -- Ele_struct: Element to track through.
+!   centroid(0:) -- coord_struct, optional: Approximate centroid orbit. Only needed if CSR is on.
+!                     Hint: Calculate this before bunch tracking by tracking a single particle.
+!   direction    -- integer, optional: +1 (default) -> Track forward, -1 -> Track backwards.
+!
+! Output:
+!   bunch_end    -- bunch_struct: Ending bunch position.
+!   err         -- Logical: Set true if there is an error. 
+!                    EG: Too many particles lost for a CSR calc.
+!   finished    -- logical: When set True, the standard track1_bunch code will not be called.
+!-
+
+subroutine track1_bunch_hook (bunch_start, lat, ele, bunch_end, err, centroid, direction, finished)
+
+use bmad, dummy => track1_bunch_hook
+
+implicit none
+
+type (bunch_struct) bunch_start
+type (bunch_struct) :: bunch_end
+type (lat_struct) :: lat
+type (ele_struct) ele
+type (coord_struct), optional :: centroid(0:)
+
+integer, optional :: direction
+logical err, finished
+
+!
+
+finished = .false.
+
+end subroutine
