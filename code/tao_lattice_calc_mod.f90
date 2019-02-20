04cb3e4fbc568921889354293e9bde8a7cf00a9f
David Sagan <dcs16@cornell.edu>
2019-02-20 05:50:34 +0000

    Added ix_branch to dynamic aperture tructure.

--- code/tao_lattice_calc_mod.f90
+++ code/tao_lattice_calc_mod.f90
@@ -211,12 +211,12 @@ uni_loop: do iuni = lbound(s%u, 1), ubound(s%u, 1)
 
     ! Dynamic aperture calc. Only for rings
 
-    if (u%calc%dynamic_aperture) then  
+    if (u%calc%dynamic_aperture .and. u%dynamic_aperture%ix_branch == ib) then  
       if (.not. rf_is_on(branch)) call reallocate_coord (orb, branch%n_ele_track)
       do j=1, size(u%dynamic_aperture%pz)
         scan => u%dynamic_aperture%scan(j)
         ! Check for open lattice. Only 1 turn is allowed
-        if (branch%param%geometry == open$ .and. (scan%param%n_turn > 1 ))then
+        if (branch%param%geometry == open$ .and. (scan%param%n_turn > 1))then
           call out_io (s_fatal$, r_name, 'DYNAMIC APERTURE CALC n_turn > 1 FOR OPEN LATTICE')
           call err_exit       
         endif
