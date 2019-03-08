f7255d5f26baea7190a93a2a20a5cdee60bde0b4
David Sagan <dcs16@cornell.edu>
2019-03-08 18:27:17 +0000

    write_beam test program now links OK.

--- hdf5/hdf5_write_bunch.f90
+++ hdf5/hdf5_write_bunch.f90
@@ -1,6 +1,6 @@
 module hdf5_bunch_mod
 
-use hdf5_interface
+use openpmd_interface
 use bmad_interface
 
 contains
@@ -33,16 +33,16 @@ call date_and_time_stamp (date_time, .true.)
 base_path = '/data/bunch%T/'
 particle_path = 'particles/'
 
-call hdf5_write_string_attrib(fid, 'openPMD', '2.0.0')
-call hdf5_write_string_attrib(fid, 'openPMDextension', 'BeamPhysics; SpeciesType')
-call hdf5_write_string_attrib(fid, 'basePath', base_path)
-call hdf5_write_string_attrib(fid, 'particlesPath', particle_path)
-call hdf5_write_string_attrib(fid, 'software', 'Bmad')
-call hdf5_write_string_attrib(fid, 'softwareVersion', '1.0')
-call hdf5_write_string_attrib(fid, 'date', date_time)
-call hdf5_write_string_attrib(fid, 'latticeFile', lat%input_file_name)
+call pmd_write_string_attrib(fid, 'openPMD', '2.0.0')
+call pmd_write_string_attrib(fid, 'openPMDextension', 'BeamPhysics; SpeciesType')
+call pmd_write_string_attrib(fid, 'basePath', base_path)
+call pmd_write_string_attrib(fid, 'particlesPath', particle_path)
+call pmd_write_string_attrib(fid, 'software', 'Bmad')
+call pmd_write_string_attrib(fid, 'softwareVersion', '1.0')
+call pmd_write_string_attrib(fid, 'date', date_time)
+call pmd_write_string_attrib(fid, 'latticeFile', lat%input_file_name)
 if (lat%lattice /= '') then
-  call hdf5_write_string_attrib(fid, 'latticeName', lat%lattice)
+  call pmd_write_string_attrib(fid, 'latticeName', lat%lattice)
 endif
 
 ! Loop over bunches
@@ -54,10 +54,10 @@ do ib = 1, size(bunches)
   write (bunch_path, '(a, i0, 2a)') base_path(1:ix-1), ib, trim(base_path(ix+2:)), trim(particle_path)
   call h5gcreate_f(fid, bunch_path, gid, h5_err)
 
-  call h5gcreate(gid, 'position', zid, h5_err)
-  call hdf5_write_real_vector_to_group(zid, 'x', bunch%particle(:)%vec(1))
-  call hdf5_write_real_vector_to_group(zid, 'y', bunch%particle(:)%vec(3))
-  call hdf5_write_real_to_group(zid, 'z', 0.0_rp, size(bunch%particle))
+  call h5gcreate_f(gid, 'position', zid, h5_err)
+  call pmd_write_real_vector_to_dataset(zid, 'x', unit_m, bunch%particle(:)%vec(1), h5_err)
+  call pmd_write_real_vector_to_dataset(zid, 'y', unit_m, bunch%particle(:)%vec(3), h5_err)
+  call pmd_write_real_to_pseudo_dataset(zid, 'z', unit_m, 0.0_rp, size(bunch%particle), h5_err)
   call h5gclose_f(zid, h5_err)
 enddo
 
