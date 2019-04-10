69fea1d72a874f3134f735e5de14332382b40f2d
David Sagan <dcs16@cornell.edu>
2019-04-10 13:22:08 +0000

    Minor code beautification.

--- program/lux_mpi.f90
+++ program/lux_mpi.f90
@@ -27,8 +27,8 @@ character(MPI_MAX_PROCESSOR_NAME) name
 
 call mpi_init(ierr)
 if (ierr /= MPI_SUCCESS) then
-   print *,'Error starting MPI program. Terminating.'
-   call mpi_abort(MPI_COMM_WORLD, rc, ierr)
+  print *,'Error starting MPI program. Terminating.'
+  call mpi_abort(MPI_COMM_WORLD, rc, ierr)
 end if
 
 ! Get the number of processors this job is using:
@@ -41,8 +41,8 @@ call mpi_comm_rank(MPI_COMM_WORLD, lux_com%mpi_rank, ierr)
 ! Get the name of this processor (usually the hostname)
 call mpi_get_processor_name(name, leng, ierr)
 if (ierr /= MPI_SUCCESS) then
-   print *,'Error getting processor name. Terminating.'
-   call mpi_abort(MPI_COMM_WORLD, rc, ierr)
+  print *,'Error getting processor name. Terminating.'
+  call mpi_abort(MPI_COMM_WORLD, rc, ierr)
 end if
 
 master_rank = 0
@@ -74,7 +74,7 @@ is_done_tag = 1001
 ! Master collects the work of the slaves
 
 if (lux_com%mpi_rank == master_rank) then
-  call print_this ('Master: Starting...')
+  call print_mpi_info ('Master: Starting...')
 
   allocate (slave_is_done(num_slaves))
   slave_is_done = .false.
@@ -87,14 +87,14 @@ if (lux_com%mpi_rank == master_rank) then
 
   do
     ! Get data from a slave
-    call print_this ('Master: Waiting for a Slave...')
+    call print_mpi_info ('Master: Waiting for a Slave...')
     call mpi_recv (slave_grid%pt, data_size, MPI_REAL8, MPI_ANY_SOURCE, results_tag, MPI_COMM_WORLD, stat, ierr)
     call lux_add_in_mpi_slave_data (slave_grid%pt, lux_param, lux_com, lux_data)
     slave_rank = stat(MPI_SOURCE)
-    call print_this ('Master: Gathered data from Slave: ', slave_rank)
+    call print_mpi_info ('Master: Gathered data from Slave: ', slave_rank)
 
     ! Tell slave if more tracking needed
-    call print_this ('Master: Commanding this Slave. Photons left:', num8 = num_photons_left)
+    call print_mpi_info ('Master: Commanding this Slave. Photons left:', num8 = num_photons_left)
     if (num_photons_left < 1) slave_is_done(slave_rank) = .true.
     call mpi_send (slave_is_done(slave_rank), 1, MPI_LOGICAL, slave_rank, is_done_tag, MPI_COMM_WORLD, ierr)
     if (.not. slave_is_done(slave_rank)) num_photons_left = num_photons_left - lux_com%n_photon_stop1
@@ -103,7 +103,7 @@ if (lux_com%mpi_rank == master_rank) then
     if (all(slave_is_done)) exit
   enddo
 
-  call print_this ('Master: All done!')
+  call print_mpi_info ('Master: All done!')
 
   ! write results and quit
 
@@ -117,20 +117,20 @@ endif
 !-------------------------------
 ! A slave process tracks photons
 
-call print_this ('Slave: Starting...')
+call print_mpi_info ('Slave: Starting...')
 
 do
   ! Init the output arrays
-  call print_this ('Slave: Tracking Photons...')
+  call print_mpi_info ('Slave: Tracking Photons...')
   call lux_init_data (lux_param, lux_com, lux_data)
   call lux_track_photons (lux_param, lux_com, lux_data)
 
   ! Send results to the Master
-  call print_this ('Slave: Sending Data...')
+  call print_mpi_info ('Slave: Sending Data...')
   call mpi_send (detec_grid%pt, data_size, MPI_BYTE, master_rank, results_tag, MPI_COMM_WORLD, ierr)
 
   ! Query Master if more tracking needed
-  call print_this ('Slave: Query to master...')
+  call print_mpi_info ('Slave: Query to master...')
   call mpi_recv (am_i_done, 1, MPI_LOGICAL, master_rank, is_done_tag, MPI_COMM_WORLD, stat, ierr)
   if (am_i_done) exit
 
@@ -138,7 +138,7 @@ enddo
 
 if (lux_param%photon1_out_file /= '') close (lux_com%iu_photon1_out)
 
-call print_this ('Slave: All done!')
+call print_mpi_info ('Slave: All done!')
 
 ! And end
 
@@ -148,7 +148,7 @@ call mpi_finalize(ierr)
 !---------------------------------------------------------------------
 contains
 
-subroutine print_this (line, num, num8)
+subroutine print_mpi_info (line, num, num8)
 
 character(*) line
 character(20) dtime
@@ -166,6 +166,6 @@ else
   print '(a, 2x, i0, 2a)', dtime, lux_com%mpi_rank, ': ', trim(line)
 endif
 
-end subroutine print_this
+end subroutine print_mpi_info
 
 end program
