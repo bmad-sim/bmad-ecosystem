57d02650ef5b77852724649a04d39f05038fe743
David Sagan <dcs16@cornell.edu>
2019-04-28 04:44:30 +0000

    more devel.

--- gui/main.py
+++ gui/main.py
@@ -1,66 +1,76 @@
 import tkinter as tk
 from tkinter import messagebox
-from tkinter import Menu
-
 import sys
 
 #----------------------------------------------------------------
-
-def read_cmd():
-  print ('Read called')
-
-def write_cmd():
-  print ('Write called')
-
-def reinit_cmd():
-  print ('Reinit called')
-
-def quit_cmd(event = ''):
-  result = messagebox.askquestion("Quit", "Are You Sure?", icon='warning')
-  if result == 'yes':
-    sys.exit(0)
-  else:
-    return
-
-def optimizer_cmd():
-  print ('Optimizer called')
-  win = tk.Toplevel()
-  win.title('Optimizer')
-
-def plotting_cmd():
-  print ('Plotting called')
-
-def wave_cmd():
-  print ('Wave called')
 
 def xx_cmd():
   print ('Xx called')
 
 #---------------------------------------------------------------
+# Root window 
 
-root = tk.Tk()
-root.title("Tao")
-root.geometry('350x200')
-root.protocol("WM_DELETE_WINDOW", quit_cmd)
+class tao_root_window(tk.Tk):
 
-menubar = Menu(root)
+  def __init__(self, *args, **kwargs):
+    tk.Tk.__init__(self, *args, **kwargs)
 
-file_menu = Menu(menubar)
-file_menu.add_command(label = 'Read...', command = read_cmd)
-file_menu.add_command(label = 'Write...', command = write_cmd)
-file_menu.add_command(label = 'Reinit...', command = reinit_cmd)
-file_menu.add_separator()
-file_menu.add_command(label = 'Quit', command = quit_cmd, accelerator = 'Ctrl+Q')
-root.bind_all("<Control-q>", quit_cmd)
-menubar.add_cascade(label = 'File', menu = file_menu)
+    self.title("Tao")
+    self.geometry('350x200')
+    self.protocol("WM_DELETE_WINDOW", quit_cmd)
 
-window_menu = Menu(menubar)
-window_menu.add_command(label = 'Optimizer...', command = optimizer_cmd)
-window_menu.add_command(label = 'Plotting...', command = plotting_cmd)
-window_menu.add_command(label = 'Wave...', command = wave_cmd)
-menubar.add_cascade(label = 'Window', menu = window_menu)
+    menubar = tk.Menu(self)
+
+    file_menu = tk.Menu(menubar)
+    file_menu.add_command(label = 'Read...', command = read_cmd)
+    file_menu.add_command(label = 'Write...', command = write_cmd)
+    file_menu.add_command(label = 'Reinit...', command = reinit_cmd)
+    file_menu.add_separator()
+    file_menu.add_command(label = 'Quit', command = quit_cmd, accelerator = 'Ctrl+Q')
+    self.bind_all("<Control-q>", quit_cmd)
+    menubar.add_cascade(label = 'File', menu = file_menu)
+
+    window_menu = tk.Menu(menubar)
+    window_menu.add_command(label = 'Optimizer...', command = optimizer_cmd)
+    window_menu.add_command(label = 'Plotting...', command = plotting_cmd)
+    window_menu.add_command(label = 'Wave...', command = wave_cmd)
+    menubar.add_cascade(label = 'Window', menu = window_menu)
+
+    self.config(menu=menubar)
+
+  #-------------------------
+
+  def read_cmd():
+    print ('Read called')
+
+  def write_cmd():
+    print ('Write called')
+
+  def reinit_cmd():
+    print ('Reinit called')
+
+  def quit_cmd(event = ''):
+    result = messagebox.askquestion("Quit", "Are You Sure?", icon='warning')
+    if result == 'yes':
+      sys.exit(0)
+    else:
+      return
+
+  def optimizer_cmd():
+    print ('Optimizer called')
+    win = tk.Toplevel()
+    win.title('Optimizer')
+
+  def plotting_cmd():
+    print ('Plotting called')
+
+  def wave_cmd():
+    print ('Wave called')
 
 
 
-root.config(menu=menubar)
-root.mainloop()
+#---------------------------------------------------------------
+
+if __name__ == "__main__":
+  root = tao_root_window()
+  root.mainloop()
