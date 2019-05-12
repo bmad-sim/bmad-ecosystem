bfd2bf3ab0c64fece16db2559754c063bd413104
David Sagan <dcs16@cornell.edu>
2019-05-12 18:57:24 +0000

    First pass at cubic interpolation module.

--- math/cubic_interpolation_mod.f90
+++ math/cubic_interpolation_mod.f90
@@ -1,26 +1,58 @@
 !+
-! Module tricubic_interpolation
+! Module cubic_interpolation_mod
 !
-! Module implementing tricubic interpolation of a field that has been measured on a grid.
+! Module implementing cubic interpolation of a field that has been measured on a grid.
 !
-! Based upon:
+! See the Wikipedia article on bicubic interpolation on a 2D grid.
+!
+! The tricubic interpolation on a 3D grid is based upon:
 !   "Tricubic Interpolation in Three Dimensions", 
 !   F. Lekien, J. Marsden, Journal of Numerical Methods and Engineering, (2005).
 !
-! The tricubic interpolation is C^1. That is, it is continuous and has a continuous first derivative.
+! Note: Cubic interpolation is C^1. That is, it is continuous and has a continuous first derivative.
 !-
 
-module tricubic_interpolation
+module cubic_interpolation_mod
 
-use precision_def
+use sim_utils_struct
 
 implicit none
+
+!--------------------------------------------
+! Bicubic interpolation structures
+
+! Field and derivatives at a grid point needed for bicubic interpolation.
+! The derivatives are normalized by the distance between grid points dx, dy.
+! For example: %d2f_dxdy (structure component) = d^2f/dxdz * dx * dy
+
+type field1_at_2D_grid_struct
+  real(rp) f                                 ! Field
+  real(rp) df_dx, df_dy                      ! Normalized field 1st derivatives
+  real(rp) d2f_dxdy                          ! Normalized field 2nd derivatives
+end type
+
+! Field and derivatives at the 8 grid points needed for bicubic interpolation.
+
+type field_at_2D_grid_struct
+  type (field1_at_2D_grid_struct) pt(0:1, 0:1)
+end type
+
+! Coefficients needed to evaluate bicubic interpolation.
+
+type bicubic_coef_struct
+  ! (ix, iy) = index of grid element coefs are for.
+  integer :: ix = int_garbage$, iy = int_garbage$
+  real(rp) :: coef(0:3,0:3) = 0 ! Coefs
+end type
+
+!--------------------------------------------
+! Tricubic interpolation structures
 
 ! Field and derivatives at a grid point needed for tricubic interpolation.
 ! The derivatives are normalized by the distance between grid points dx, dy, dz.
 ! For example: %d2f_dxdz (structure component) = d^2f/dxdz * dx * dz
 
-type field1_at_grid_struct
+type field1_at_3D_grid_struct
   real(rp) f                                 ! Field
   real(rp) df_dx, df_dy, df_dz               ! Normalized field 1st derivatives
   real(rp) d2f_dxdy, d2f_dxdz, d2f_dydz      ! Normalized field 2nd derivatives
@@ -29,14 +61,16 @@ end type
 
 ! Field and derivatives at the 8 grid points needed for tricubic interpolation.
 
-type field_at_grid_struct
-  type (field1_at_grid_struct) pt(0:1, 0:1, 0:1)
+type field_at_3D_grid_struct
+  type (field1_at_3D_grid_struct) pt(0:1, 0:1, 0:1)
 end type
 
 ! Coefficients needed to evaluate tricubic interpolation.
 
 type tricubic_coef_struct
-  real(rp) coef(0:3,0:3,0:3)
+  ! (ix, iy, iz) = index of grid element coefs are for.
+  integer :: ix = int_garbage$, iy = int_garbage$, iz = int_garbage$
+  real(rp) :: coef(0:3,0:3,0:3) = 0 ! Coefs
 end type
 
 contains
@@ -44,26 +78,191 @@ contains
 !------------------------------------------------------------------------------------------------
 !------------------------------------------------------------------------------------------------
 !------------------------------------------------------------------------------------------------
+!+
+! Subroutine bicubic_interpolation_coefs (field_at_grid, bi_coef)
+! 
+! Routine to compute the coefficients for bicubic interpolation.
+!
+! Use the routine bicubic_eval to evaluate the interpolation function.
+!
+! Note: The derivatives in field_at_grid are normalized by the distance between grid points dx, dy.
+! For example: %d2f_dxdy (structure component) = d^2f/dxdz * dx * dy
+!
+! Input:
+!   field_at_grid   -- field_at_2D_grid_struct: Field and normalized derivatives at the 4 grid points.
+!
+! Output:
+!   bi_coef         -- bicubic_coef_struct: Coefficients.
+!-
+
+subroutine bicubic_interpolation_coefs (field_at_grid, bi_coef)
+
+type (field_at_2D_grid_struct), target :: field_at_grid
+type (bicubic_coef_struct) bi_coef
+type (field1_at_2D_grid_struct), pointer :: fg
+
+real(rp) field_array(16)
+integer i, j, ij
+
+type m16_row_struct
+  integer pt(16)
+end type
+
+type m16_matrix_struct
+  type (m16_row_struct) row(16)
+end type
+
+type (m16_matrix_struct), parameter :: coef_mat = m16_matrix_struct( [ &
+ m16_row_struct([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), &
+ m16_row_struct([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), &
+ m16_row_struct([-3, 3, 0, 0, -2, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), &
+ m16_row_struct([2, -2, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), &
+ m16_row_struct([0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]), &
+ m16_row_struct([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]), &
+ m16_row_struct([0, 0, 0, 0, 0, 0, 0, 0, -3, 3, 0, 0, -2, -1, 0, 0]), &
+ m16_row_struct([0, 0, 0, 0, 0, 0, 0, 0, 2, -2, 0, 0, 1, 1, 0, 0]), &
+ m16_row_struct([-3, 0, 3, 0, 0, 0, 0, 0, -2, 0, -1, 0, 0, 0, 0, 0]), &
+ m16_row_struct([0, 0, 0, 0, -3, 0, 3, 0, 0, 0, 0, 0, -2, 0, -1, 0]), &
+ m16_row_struct([9, -9, -9, 9, 6, 3, -6, -3, 6, -6, 3, -3, 4, 2, 2, 1]), &
+ m16_row_struct([-6, 6, 6, -6, -3, -3, 3, 3, -4, 4, -2, 2, -2, -2, -1, -1]), &
+ m16_row_struct([2, 0, -2, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0]), &
+ m16_row_struct([0, 0, 0, 0, 2, 0, -2, 0, 0, 0, 0, 0, 1, 0, 1, 0]), &
+ m16_row_struct([-6, 6, 6, -6, -4, -2, 4, 2, -3, 3, -3, 3, -2, -1, -2, -1]), &
+ m16_row_struct([4, -4, -4, 4, 2, 2, -2, -2, 2, -2, 2, -2, 1, 1, 1, 1]) ])
+
+!
+
+do i = 0, 1
+do j = 0, 1
+  ij = i + 2*j + 1
+  fg => field_at_grid%pt(i,j)
+  field_array(ij:ij+12:4) = [fg%f, fg%df_dx, fg%df_dy, fg%d2f_dxdy]
+enddo
+enddo
+
+do i = 0, 3
+do j = 0, 3
+  ij = i + 4*j + 1
+  bi_coef%coef(i,j) = sum(coef_mat%row(ij)%pt(:) * field_array(:))
+enddo
+enddo
+
+end subroutine bicubic_interpolation_coefs
+
+!------------------------------------------------------------------------------------------------
+!------------------------------------------------------------------------------------------------
+!------------------------------------------------------------------------------------------------
+!+
+! Function bicubic_eval (x_norm, y_norm, bi_coef, df_dx, df_dy) result (f_val)
+!
+! Routine to evaluate a bicubic interpolating function.
+!
+! Use the routine bicubic_interpolation_coefs to generate bi_coef.
+!
+! Note: In the equations below, the four points of the grid box being interpolated range 
+! from (x0, y0) to (x0+dx, y0+dy).
+!
+! Input:
+!   x_norm    -- real(rp): x_norm = (x - x0) / dx 
+!   y_norm    -- real(rp): y_norm = (y - y0) / dy
+!   bi_coef   -- bicubic_coef_struct: Coefficients.
+!
+! Output:
+!   f_val     -- real(rp): Value of f.
+!   df_dx     -- real(rp): Normalized first derivative: True df/dx = df_dx * dx 
+!   df_dy     -- real(rp): Normalized first derivative: True df/dy = df_dy * dy
+!- 
+
+function bicubic_eval (x_norm, y_norm, bi_coef, df_dx, df_dy) result (f_val)
+
+type (bicubic_coef_struct) bi_coef
+
+real(rp) x_norm, y_norm, z_norm, f_val
+real(rp), optional :: df_dx, df_dy
+real(rp) x_power(0:3), y_power(0:3), z_power(0:3)
+
+integer i, j
+
+! Init
+
+x_power(0) = 1
+y_power(0) = 1
+
+do i = 1, 3
+  x_power(i) = x_power(i-1) * x_norm
+  y_power(i) = y_power(i-1) * y_norm
+enddo
+
+! Evaluate f_val
+
+f_val = 0
+do i = 0, 3
+do j = 0, 3
+  f_val = f_val + x_power(i) * y_power(j) * bi_coef%coef(i,j)
+enddo
+enddo
+
+! Evaluate derivatives
+
+if (present(df_dx)) then
+  df_dx = 0
+  do i = 1, 3
+  do j = 0, 3
+    df_dx = df_dx + i * x_power(i-1) * y_power(j) * bi_coef%coef(i,j)
+  enddo
+  enddo
+endif
+
+if (present(df_dy)) then
+  df_dy = 0
+  do i = 0, 3
+  do j = 1, 3
+    df_dy = df_dy + j * x_power(i) * y_power(j-1) * bi_coef%coef(i,j)
+  enddo
+  enddo
+endif
+
+end function bicubic_eval
+
+!------------------------------------------------------------------------------------------------
+!------------------------------------------------------------------------------------------------
+!------------------------------------------------------------------------------------------------
+!+
+! Subroutine tricubic_interpolation_coefs (field_at_grid, tri_coef)
+! 
+! Routine to compute the coefficients for tricubic interpolation.
+!
+! Use the routine tricubic_eval to evaluate the interpolation function.
+!
+! Note: The derivatives in field_at_grid are normalized by the distance between grid points dx, dy.
+! For example: %d2f_dxdy (structure component) = d^2f/dxdz * dx * dy
+!
+! Input:
+!   field_at_grid   -- field_at_2D_grid_struct: Field and normalized derivatives at the 4 grid points.
+!
+! Output:
+!   tri_coef        -- tricubic_coef_struct: Coefficients.
+!-
 
 subroutine tricubic_interpolation_coefs (field_at_grid, tri_coef)
 
-type (field_at_grid_struct), target :: field_at_grid
+type (field_at_3D_grid_struct), target :: field_at_grid
 type (tricubic_coef_struct) tri_coef
-type (field1_at_grid_struct), pointer :: fg
+type (field1_at_3D_grid_struct), pointer :: fg
 
 real(rp) field_array(64)
 integer i, j, k, ijk
 
 type m64_row_struct
-  integer col(64)
+  integer pt(64)
 end type
 
 type m64_matrix_struct
   type (m64_row_struct) row(64)
 end type
 
-type (m64_matrix_struct), parameter :: coef_mat = m64_matrix_struct( &
-  [m64_row_struct([ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), &
+type (m64_matrix_struct), parameter :: coef_mat = m64_matrix_struct( [ &
+  m64_row_struct([ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), &
   m64_row_struct([ 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), &
   m64_row_struct([-3, 3, 0, 0, 0, 0, 0, 0,-2,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), &
   m64_row_struct([ 2,-2, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), &
@@ -126,7 +325,7 @@ type (m64_matrix_struct), parameter :: coef_mat = m64_matrix_struct( &
   m64_row_struct([ 4, 0,-4, 0,-4, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0,-2, 0,-2, 0, 2, 0,-2, 0, 2, 0,-2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]), &
   m64_row_struct([ 0, 0, 0, 0, 0, 0, 0, 0, 4, 0,-4, 0,-4, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0,-2, 0,-2, 0, 2, 0,-2, 0, 2, 0,-2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0]), &
   m64_row_struct([-12,12,12,-12,12,-12,-12,12,-8,-4, 8, 4, 8, 4,-8,-4,-6, 6,-6, 6, 6,-6, 6,-6,-6, 6, 6,-6,-6, 6, 6,-6,-4,-2,-4,-2, 4, 2, 4, 2,-4,-2, 4, 2,-4,-2, 4, 2,-3, 3,-3, 3,-3, 3,-3, 3,-2,-1,-2,-1,-2,-1,-2,-1]), &
-  m64_row_struct([ 8,-8,-8, 8,-8, 8, 8,-8, 4, 4,-4,-4,-4,-4, 4, 4, 4,-4, 4,-4,-4, 4,-4, 4, 4,-4,-4, 4, 4,-4,-4, 4, 2, 2, 2, 2,-2,-2,-2,-2, 2, 2,-2,-2, 2, 2,-2,-2, 2,-2, 2,-2, 2,-2, 2,-2, 1, 1, 1, 1, 1, 1, 1, 1])])
+  m64_row_struct([ 8,-8,-8, 8,-8, 8, 8,-8, 4, 4,-4,-4,-4,-4, 4, 4, 4,-4, 4,-4,-4, 4,-4, 4, 4,-4,-4, 4, 4,-4,-4, 4, 2, 2, 2, 2,-2,-2,-2,-2, 2, 2,-2,-2, 2, 2,-2,-2, 2,-2, 2,-2, 2,-2, 2,-2, 1, 1, 1, 1, 1, 1, 1, 1]) ])
 
 !
 
@@ -144,7 +343,7 @@ do i = 0, 3
 do j = 0, 3
 do k = 0, 3
   ijk = i + 4*j + 16*k + 1
-  tri_coef%coef(i,j,k) = sum(coef_mat%row(ijk)%col(:) * field_array(:))
+  tri_coef%coef(i,j,k) = sum(coef_mat%row(ijk)%pt(:) * field_array(:))
 enddo
 enddo
 enddo
@@ -154,17 +353,40 @@ end subroutine tricubic_interpolation_coefs
 !------------------------------------------------------------------------------------------------
 !------------------------------------------------------------------------------------------------
 !------------------------------------------------------------------------------------------------
+!+
+! Function tricubic_eval (x_norm, y_norm, z_norm, tri_coef, df_dx, df_dy, df_dz) result (f_val)
+!
+! Routine to evaluate a tricubic interpolating function.
+!
+! Use the routine tricubic_interpolation_coefs to generate tri_coef.
+!
+! Note: In the equations below, the eight points of the grid box being interpolated range 
+! from (x0, y0, z0) to (x0+dx, y0+dy, z0+dz).
+!
+! Input:
+!   x_norm    -- real(rp): x_norm = (x - x0) / dx 
+!   y_norm    -- real(rp): y_norm = (y - y0) / dy
+!   z_norm    -- real(rp): z_norm = (z - z0) / dz
+!   tri_coef   -- tricubic_coef_struct: Coefficients.
+!
+! Output:
+!   f_val     -- real(rp): Value of f.
+!   df_dx     -- real(rp): Normalized first derivative: True df/dx = df_dx * dx
+!   df_dy     -- real(rp): Normalized first derivative: True df/dy = df_dy * dy
+!   df_dz     -- real(rp): Normalized first derivative: True df/dz = df_dz * dz
+!- 
 
-function tricubic_eval (tri_coef , x_norm, y_norm, z_norm) result (value)
+function tricubic_eval (x_norm, y_norm, z_norm, tri_coef, df_dx, df_dy, df_dz) result (f_val)
 
 type (tricubic_coef_struct) tri_coef
 
-real(rp) x_norm, y_norm, z_norm, value
+real(rp) x_norm, y_norm, z_norm, f_val
+real(rp), optional :: df_dx, df_dy, df_dz
 real(rp) x_power(0:3), y_power(0:3), z_power(0:3)
 
 integer i, j, k
 
-!
+! Init
 
 x_power(0) = 1
 y_power(0) = 1
@@ -176,14 +398,51 @@ do i = 1, 3
   z_power(i) = z_power(i-1) * z_norm
 enddo
 
-value = 0
+! Evaluate f_val
+
+f_val = 0
 do i = 0, 3
 do j = 0, 3
 do k = 0, 3
-  value = value + x_power(i) * y_power(j) * z_power(k) * tri_coef%coef(i,j,k)
+  f_val = f_val + x_power(i) * y_power(j) * z_power(k) * tri_coef%coef(i,j,k)
 enddo
 enddo
 enddo
+
+! Evaluate derivatives
+
+if (present(df_dx)) then
+  df_dx = 0
+  do i = 1, 3
+  do j = 0, 3
+  do k = 0, 3
+    df_dx = df_dx + i * x_power(i-1) * y_power(j) * z_power(k) * tri_coef%coef(i,j,k)
+  enddo
+  enddo
+  enddo
+endif
+
+if (present(df_dy)) then
+  df_dy = 0
+  do i = 0, 3
+  do j = 1, 3
+  do k = 0, 3
+    df_dy = df_dy + j * x_power(i) * y_power(j-1) * z_power(k) * tri_coef%coef(i,j,k)
+  enddo
+  enddo
+  enddo
+endif
+
+if (present(df_dz)) then
+  df_dz = 0
+  do i = 0, 3
+  do j = 0, 3
+  do k = 1, 3
+    df_dz = df_dz + k * x_power(i) * y_power(j) * z_power(k-1) * tri_coef%coef(i,j,k)
+  enddo
+  enddo
+  enddo
+endif
 
 end function tricubic_eval
 
