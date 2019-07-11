b15bee9d5d673bae5f31cb59fd3455fa214e3bda
Kevin J. Kowalski <kjk226@cornell.edu>
2019-07-11 14:59:49 +0000

    clickable graph optimization

--- gui/tao_ele_location.py
+++ gui/tao_ele_location.py
@@ -2,10 +2,10 @@ from taoplot import taoplot
 import matplotlib
 import matplotlib.pyplot as plt
 
-def in_element(x,y,taoplot):
-	'''takes coordinates and taoplot instance, returns list of element indices of elements that contain the specified coordinates'''
+def in_element(x,y,fig_info):
+	'''takes coordinates and taoplot.plot() output, returns list of element indices of elements that contain the specified coordinates'''
 	inIndexList = []
-	returnList = taoplot.plot()[1]
+	returnList = fig_info
 	try:
 		if returnList[0] == 'lat_layout' or returnList[0] == 'data':
 			for i in returnList[4]:
@@ -19,7 +19,7 @@ def in_element(x,y,taoplot):
 	
 		if returnList[0] == 'floor_plan':
 			for i in returnList[7]:
-				if ((x-returnList[8][i][0])**2 + (y-returnList[8][i][1])**2) <= ((returnList[9][i])**2):
+				if ((x-returnList[8][str(i)][0])**2 + (y-returnList[8][str(i)][1])**2) <= ((returnList[9][i])**2):
 					inIndexList.append(i)
 	except TypeError:
 		pass
