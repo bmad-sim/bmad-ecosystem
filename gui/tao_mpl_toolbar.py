f8077988828359f080509f16a04c17c6b79e4b39
Kevin J. Kowalski <kjk226@cornell.edu>
2019-07-18 14:13:04 +0000

    improved toolbar

---
+++ gui/tao_mpl_toolbar.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tkinter as tk
+import matplotlib as mpl
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+
+class taotoolbar(NavigationToolbar2Tk):
+	def __init__(self,canvas_,parent_):
+		self.toolitems = (
+			('Home', 'Reset original view', 'home', 'home'),
+			('Pan', 'Pan axes with left mouse, zoom with right', 'move', 'pan'),
+			('Zoom', 'Zoom to rectangle', 'zoom_to_rect', 'zoom'),
+			(None,None,None,None),
+			('Save', 'Save image of figure', 'filesave', 'save_figure'),
+			)
+
+		NavigationToolbar2Tk.__init__(self,canvas_,parent_)
+
+
