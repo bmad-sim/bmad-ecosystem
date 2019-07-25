63992ccf769cf24168c98028e0d91d80ef458430
David Sagan <dcs16@cornell.edu>
2019-07-25 16:45:57 +0000

    Fix apparent_emit case.

--- code/tao_datum_has_associated_ele.f90
+++ code/tao_datum_has_associated_ele.f90
@@ -8,9 +8,9 @@
 !
 ! Output:
 !   has_associated_ele -- integer:
-!                           no$          -- Does not have an associated lattice element.
-!                           yes$         -- Has an associated lattice element.
-!                           provisional$ -- Does not have an associated lattice element for circular lbranches.
+!                           no$          -- Must not have an associated lattice element.
+!                           yes$         -- Must have an associated lattice element.
+!                           provisional$ -- Does not have an associated lattice element for circular branches.
 !                           maybe$       -- Is possible to have an associated lattice element or not.
 !-
 
@@ -29,10 +29,10 @@ if (data_type == 'unstable.orbit' .or. data_type(1:7) == 'normal.' .or. data_typ
                       data_type(1:18) == 'spin.polarization_' .or. data_type == 'spin.depolarization_rate') then
   has_associated_ele = no$
 
-elseif (data_type == 'chrom.a' .or. data_type == 'chrom.b' .or. &
-             data_type(1:12) == 'chrom.dtune.' .or. data_type(1:5) == 'damp.' .or. &
-             data_type(1:17) == 'multi_turn_orbit.' .or. data_type(1:5) == 'tune.' .or. &
-             data_type(1:13) == 'unstable.ring' .or. index(data_type, 'emit.') /= 0) then
+elseif (data_type == 'chrom.a' .or. data_type == 'chrom.b' .or. data_type(1:12) == 'chrom.dtune.' .or. &
+             data_type(1:5) == 'damp.' .or. data_type(1:17) == 'multi_turn_orbit.' .or. &
+             data_type(1:5) == 'tune.' .or. data_type(1:13) == 'unstable.ring' .or. &
+             data_type(1:5) == 'emit.' .or. data_type(1:10) == 'norm_emit.') then
   has_associated_ele = provisional$
 
 elseif ((data_type(1:11) == 'expression:' .and. index(data_type, 'ele::#[') == 0) .or. data_type(1:8) == 'rad_int.') then
