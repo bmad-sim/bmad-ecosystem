1d7539a923931196c93f95d7247f29af830e6f82
Kevin J. Kowalski <kjk226@cornell.edu>
2019-08-09 15:11:52 +0000

    scroll wheel zoom improvements

--- gui/tao_mpl_toolbar.py
+++ gui/tao_mpl_toolbar.py
@@ -44,10 +44,12 @@ class taotoolbar(NavigationToolbar2Tk):
 				# get the current x and y limits
 				cur_xlim = ax.get_xlim()
 				cur_ylim = ax.get_ylim()
-				cur_xrange = (cur_xlim[1] - cur_xlim[0])*.5
-				cur_yrange = (cur_ylim[1] - cur_ylim[0])*.5
 				xdata = event.xdata # get event x location
 				ydata = event.ydata # get event y location
+				x_left = xdata - cur_xlim[0]
+				x_right = cur_xlim[1] - xdata 
+				y_top = ydata - cur_ylim[0]
+				y_bottom = cur_ylim[1] - ydata
 				if event.button == 'up':
 					# deal with zoom in
 					scale_factor = 1/base_scale
@@ -58,10 +60,10 @@ class taotoolbar(NavigationToolbar2Tk):
 					# deal with something that should never happen
 					scale_factor = 1
 				# set new limits
-				ax.set_xlim([xdata - cur_xrange*scale_factor,
-					xdata + cur_xrange*scale_factor])
-				ax.set_ylim([ydata - cur_yrange*scale_factor,
-					ydata + cur_yrange*scale_factor])
+				ax.set_xlim([xdata - x_left*scale_factor,
+					xdata + x_right*scale_factor])
+				ax.set_ylim([ydata - y_top*scale_factor,
+					ydata + y_bottom*scale_factor])
 				self.canvas.draw_idle()
 
 
