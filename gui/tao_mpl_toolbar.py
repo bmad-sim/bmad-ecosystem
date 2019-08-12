1bac5ee3355be47069a94786249bcf946d2665e3
Kevin J. Kowalski <kjk226@cornell.edu>
2019-08-12 21:02:15 +0000

    help tool fix

--- gui/tao_mpl_toolbar.py
+++ gui/tao_mpl_toolbar.py
@@ -155,8 +155,8 @@ class taotoolbar(NavigationToolbar2Tk):
 		# Title string
 		title = 'Graph Help'
 		# Help items
-		help_items = ['Home    ','', 'Back    ','','Forward    ','','Pan/Zoom    ','','Zoom to Rectangle    ','','Save    ']
-		help_descrips = ["Returns to original view, shortcuts are 'h' or 'r'.",'',"Returns to previous view, shortcuts are 'c' or 'left arrow'.",'',"Undoes the last back command, shortcuts are 'v' or 'right arrow'.",'',"Toggles panning by left clicking and dragging, and zooming by using the scroll wheel or by right clicking and dragging. Holding 'x' restricts panning and zooming to the x axis, holding 'y' restricts panning and zooming to the y axis, and holding control maintains the aspect ratio.",'',"Toggles zooming by left clicking and dragging to select the new window. Holding 'x' restricts zooming to the x axis, holding 'y' restricts zooming to the y axis, and holding control maintains the aspect ratio.",'',"Saves the current figure as an image file, shortcut is 'ctrl + s'"]
+		help_items = ['Home    ','', 'Back    ','','Forward    ','','Pan/Zoom    ','','Zoom Rectangle    ','','Save    ']
+		help_descrips = ["Returns to original view, shortcuts are 'h' or 'r'.",'',"Returns to previous view, shortcuts are 'c' or 'left arrow'.",'',"Undoes the last back command, shortcuts are 'v' or 'right arrow'.",'',"Toggles panning by left clicking and dragging, and zooming by using the scroll wheel or by right clicking and dragging. Holding 'x' restricts panning and zooming to the x axis, holding 'y' restricts panning and zooming to the y axis, and holding control maintains the aspect ratio.",'',"Toggles zooming by left clicking and dragging to select the new window. Holding 'x' restricts zooming to the x axis, holding 'y' restricts zooming to the y axis, and holding control maintains the aspect ratio.",'',"Saves the current figure as an image file, shortcut is 'ctrl + s'."]
 		wl = 600
 
 		tk.Label(win, text=title).grid(row=0, column=0, columnspan=2, sticky='EW')
