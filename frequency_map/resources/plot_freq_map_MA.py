6688d84f4a2ec2be2e6b88c160382be8322a7ac0
James Shanks <js583@cornell.edu>
2019-08-28 00:53:11 +0000

    update make_inputs and plotting scripts --jsh

--- frequency_map/resources/plot_freq_map_MA.py
+++ frequency_map/resources/plot_freq_map_MA.py
@@ -1,8 +1,4 @@
-#! /usr/bin/python
-
-#######################################################################
-### Plot frequency_map results as a momentum aperture (MA) projection
-#######################################################################
+#! /home/shanksj/misc/anaconda/bin/python
 
 import numpy as np
 import numpy.ma as ma
@@ -10,37 +6,19 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pylab, math
 import os
+import matplotlib.colors as colors
 
-#######################################################################
-### Modify these lines according to your job:
+matplotlib.rcParams["savefig.directory"] = os.getcwd()
+matplotlib.rcParams['image.cmap'] = 'jet'
 
-filename   = 'combined.txt' # file containing frequency_map output
-makeinputs = 'make_inputs_MA.py' # script used to generate GRID inputs
+filename = 'combined.txt'
+makeinputs = 'make_inputs_MA.py'
 
-# Set limits for amplitude scale. Recall A = log(sqrt[dQx^2+dQy^2])
+dQxcutoff = 2.5e-5
+dQycutoff = 2.5e-3
 minimum = 1.e-12
 maximum = 0.1
-
-# Tune plane limits for plotting:
-Qxmin = 0.5
-Qxmax = 1.0
-Qymin = 0.5
-Qymax = 1.0
-
-
-
-
-
-
-
-########################################################################
-########################################################################
-##             Begin Plotting Code - Don't Edit Below Here            ##
-########################################################################
-########################################################################
-
-xscale = 1.
-yscale = 1.
+plotTitle = ""
 
 file = open(makeinputs,'r')
 for line in file:
@@ -62,6 +40,20 @@ for line in file:
                 break
 file.close()
 
+xscale = 1.#/np.sqrt(22.74e-9  * 11.187)
+escale = 1.#/7.837E-04
+
+Qxmin = 0.5
+Qxmax = 1.0
+Qymin = 0.5
+Qymax = 1.0
+
+
+
+###########################################
+#######      Primary routine      #########
+###########################################
+
 x = []
 y = []
 e = []
@@ -74,32 +66,30 @@ Qz = []
 dQ = []
 dQmask = []
 
-steps = int((x1-x0)/(dx))
+xsteps = int((x1-x0)/(dx))
+esteps = int((e1-e0)/(de))
+
 
 # populate arrays with float zeroes to start:
-for ix in range(steps+2):
-	x.append(x0+ix*dx)
-	e.append(e0+ix*de)
-	Qx0temp = []
-	Qy0temp = []
-	Qxtemp = []
-	Qytemp = []
-	dQtemp = []
-	dQmasktemp = []
-	for jx in range(steps+2):
-		Qx0temp.append(minimum)
-		Qy0temp.append(minimum)
-		Qxtemp.append(minimum)
-		Qytemp.append(minimum)
-		dQtemp.append(minimum)
-		dQmasktemp.append(True)
-	Qx0.append(Qx0temp)
-	Qy0.append(Qy0temp)
-	Qx.append(Qxtemp)
-	Qy.append(Qytemp)
-	dQ.append(dQtemp)
-	dQmask.append(dQmasktemp)
 
+Qx0    = np.zeros([xsteps,esteps])
+Qy0    = np.zeros([xsteps,esteps])
+Qx     = np.zeros([xsteps,esteps])
+Qy     = np.zeros([xsteps,esteps])
+dQ     = np.zeros([xsteps,esteps])
+dQmask = np.zeros([xsteps,esteps])
+
+Qx0.fill(minimum)
+Qx0.fill(minimum)
+Qx.fill(minimum)
+Qx.fill(minimum)
+dQ.fill(minimum)
+dQmask.fill(True)
+
+x = np.array([x0+ix*dx for ix in range(xsteps)])
+e = np.array([e0+ie*de for ie in range(esteps)])
+
+        
 logmin = np.abs(np.log10(minimum))
 logmax = np.abs(np.log10(maximum))
 
@@ -112,11 +102,8 @@ for line in file:
 	
 	tot = tot+1
 
-	if any(fileLine[ix] == 'NaN' for ix in range(len(fileLine))):
+	if any(fileLine[ix] == '*' for ix in range(len(fileLine))):
 		continue
-	#if ix in range(len(fileline)):
-	#	if fileLine[ix] == 'NaN':
-	#		fileline[ix] = minimum
 
 	thisx = float(fileLine[0])
 	thisy = float(fileLine[1])
@@ -149,6 +136,7 @@ for line in file:
 		dQmask[xidx][eidx] = False
 
 x = [ix*xscale for ix in x]
+e = [ie*escale for ie in e]
 
 xA = np.array(x)
 eA = np.array(e)
@@ -169,33 +157,41 @@ dQAmasked = ma.masked_array(dQA,dQmask)
 # Plotting:
 
 #plt.clf()
-fig = plt.figure(num=1,figsize=(8,6.),dpi=72)
+fig = plt.figure(num=1,figsize=(6.67,4.),dpi=150)
 fig.subplots_adjust(left=0.12,right=0.98,top=0.92,bottom=0.10)
 ax1 = fig.add_subplot(111)
 #ax1.set_aspect('equal')
 mappable = ax1.pcolor(xA,eA,dQAmasked, vmin=-logmin, vmax=-logmax)
 ax1.set_xlim(x0*xscale,x1*xscale)
-ax1.set_ylim(e0,e1)
-ax1.set_xlabel('x (m)')
-ax1.set_ylabel('$\delta$')
+#ax1.set_xlim(-30,30)
+ax1.set_ylim(e0*escale,e1*escale)
+ax1.set_xlabel(r'x (m)')
+ax1.set_ylabel(r'$\delta$')
 fig.colorbar(mappable, ax=ax1)
 ax1.set_title('$\Delta Q$ vs. (x,$\delta$)')
+fig.tight_layout()
 plt.savefig('x_delta_dQ.png')
+plt.savefig('x_delta_dQ.eps')
+
 
 ###########################
 # plotting of \Delta Q vs. (Qx,Qy)
 
-fig2 = plt.figure(num=2,figsize=(10.,6.),dpi=100)
-fig2.subplots_adjust(left=0.1,right=0.94,top=0.93,bottom=0.1)
+fig2 = plt.figure(num=2,figsize=(6.67,4.),dpi=150)
+fig2.subplots_adjust(left=0.06,right=0.97,top=0.94,bottom=0.06)
 ax2 = fig2.add_subplot(111)
-ax2.scatter(Qx0A.reshape(-1),Qy0A.reshape(-1), s=8, c=dQAmasked.reshape(-1), marker='s', edgecolors='none')
+ax2.scatter(Qx0A.reshape(-1),Qy0A.reshape(-1), s=8, c=dQAmasked.reshape(-1), norm=colors.Normalize(vmin=-logmin, vmax=-logmax, clip=True), marker='s', edgecolors='none')
 ax2.set_xlabel('Qx')
 ax2.set_ylabel('Qy')
+ax2.axis('equal')
+
 ax2.set_xlim(Qxmin, Qxmax)
 ax2.set_ylim(Qymin, Qymax)
 ax2.grid()
 ax2.set_title('$\Delta Q$ vs. ($Q_x$,$Q_y$)')
+fig2.tight_layout()
 plt.savefig('Qx_Qy_dQ_MA.png')
-plt.show()
+plt.savefig('Qx_Qy_dQ_MA.eps')
+
 
 plt.show()
