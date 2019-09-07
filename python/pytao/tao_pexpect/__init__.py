5af2bb7b06db1cc6009adbf53978bcdec7d0ade2
John M. Mastroberti <jmm699@cornell.edu>
2019-09-07 14:31:47 +0000

    promoted tao_pexpect to a package

---
+++ python/pytao/tao_pexpect/__init__.py
@@ -0,0 +1,6 @@
+'''
+This package provides the pexpect implementation of the interface between
+tao and python.  The ctypes implementaion is much faster and should be
+used if possible.
+'''
+from .tao_pipe import *
