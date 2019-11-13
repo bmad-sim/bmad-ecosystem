73ee2b8cacdae805a017c35688d72755ce09b0d6
David Sagan <dcs16@cornell.edu>
2019-11-13 22:42:59 +0000

    Corrected radiation tracking.

--- modules/radiation_mod.f90
+++ modules/radiation_mod.f90
@@ -41,10 +41,10 @@ end subroutine release_rad_int_cache
 !---------------------------------------------------------------------------
 !---------------------------------------------------------------------------
 !+
-! Subroutine calc_radiation_tracking_g_factors (ele, orbit, param, edge, len2, g_x, g_y, g, g2, g3)
+! Subroutine calc_radiation_tracking_integrals (ele, orbit, param, edge, int_gx, int_gy, int_g, int_g2, int_g3)
 !
-! Routine to calculate the g bending strength parameters for half the element. 
-! g = 1/rho where rho is the radius of curvature. g points raially outward in the plane of bending.
+! Routine to calculate the integrated g bending strength parameters for half the element. 
+! g = 1/rho where rho is the radius of curvature. g points radially outward in the bending plane.
 ! If the particle is at the starting edge then the calculation is over the first half of the element.
 ! If the particle is at the exit edge then the calculation is over the 2nd half of the element.
 !
@@ -55,14 +55,13 @@ end subroutine release_rad_int_cache
 !     
 !
 ! Output:
-!   len2      -- real(rp): Half length traveled by particle. Set to zero if no bending present.
-!   g_x       -- real(rp): Average of x-component of g.
-!   g_y       -- real(rp): Average of y-component of g.
-!   g2        -- real(rp): Average of g^2.
-!   g3        -- real(rp): Average of g^3.
+!   int_gx    -- real(rp): Integral of x-component of g.
+!   int_gy    -- real(rp): Integral of y-component of g.
+!   int_g2    -- real(rp): Integral of g^2.
+!   int_g3    -- real(rp): Integral of g^3.
 !-
 
-subroutine calc_radiation_tracking_g_factors (ele, orbit, param, edge, len2, g_x, g_y, g2, g3)
+subroutine calc_radiation_tracking_integrals (ele, orbit, param, edge, int_gx, int_gy, int_g2, int_g3)
 
 implicit none
 
@@ -74,8 +73,9 @@ type (coord_struct) start0_orb, start_orb, end_orb
 type (track_struct), save, target :: track_save
 type (track_struct), pointer :: track
 
-real(rp) len2, g_x, g_y, g2, g3, kx, ky, kx_tot, ky_tot, s_here
-real(rp) a_pole_mag(0:n_pole_maxx), b_pole_mag(0:n_pole_maxx), a_pole_elec(0:n_pole_maxx), b_pole_elec(0:n_pole_maxx)
+real(rp) len_half, len2, int_gx, int_gy, int_g2, int_g3, kx, ky, kx_tot, ky_tot, s_here, g2, g3, gx, gy
+real(rp) a_pole_mag(0:n_pole_maxx), b_pole_mag(0:n_pole_maxx)
+real(rp) a_pole_elec(0:n_pole_maxx), b_pole_elec(0:n_pole_maxx)
 real(rp), parameter :: del_orb = 1d-4
 
 integer edge, direc, track_method_saved
@@ -83,18 +83,17 @@ integer i, j, ix_mag_max, ix_elec_max
 
 logical err_flag
 
-character(*), parameter :: r_name = 'calc_radiation_tracking_g_factors'
+character(*), parameter :: r_name = 'calc_radiation_tracking_integrals'
 
 ! Init
 
-g_x = 0; g_y = 0
-g2 = 0;  g3 = 0
+int_gx = 0; int_gy = 0
+int_g2 = 0; int_g3 = 0
 
 select case (ele%key)
 case (quadrupole$, sextupole$, octupole$, sbend$, sol_quad$, wiggler$, undulator$, em_field$)
 ! All other types ignored.
 case default
-  len2 = 0
   return
 end select
 
@@ -117,27 +116,28 @@ endif
 ! The problem with a negative element length is that it is not possible to undo the stochastic part of the radiation kick.
 ! In this case the best thing is to just set everything to zero
 
-len2 = ele%value(l$) / 2
-if (len2 < 0) then
-  len2 = 0
-  return
-endif
+len_half = ele%value(l$) / 2
+if (len_half < 0) return
 
 !---------------------------------
 ! Calculate the radius of curvature for an on-energy particle
 ! Wiggler, undulator, em_field case
 
 if (ele%key == wiggler$ .or. ele%key == undulator$ .or. ele%key == em_field$) then
-  g_x = 0
-  g_y = 0
+  int_gx = 0
+  int_gy = 0
 
   if (ele%field_calc == planar_model$) then
     g2 = abs(ele%value(k1$))
     g3 = 4 * sqrt(2*g2)**3 / (3 * pi)  
+    int_g2 = len_half * g2
+    int_g3 = len_half * g3
 
   elseif (ele%field_calc == helical_model$) then
     g2 = abs(ele%value(k1$))
     g3 = sqrt(g2)**3
+    int_g2 = len_half * g2
+    int_g3 = len_half * g3
 
   else
     if (.not. associated(ele%rad_int_cache) .or. ele%rad_int_cache%stale) then
@@ -174,9 +174,9 @@ if (ele%key == wiggler$ .or. ele%key == undulator$ .or. ele%key == em_field$) th
       endif
     endif
 
-    g2 = ele%rad_int_cache%g2_0 + dot_product(orbit%vec(1:4)-ele%rad_int_cache%orb0(1:4), ele%rad_int_cache%dg2_dorb(1:4))
-    g3 = ele%rad_int_cache%g3_0 + dot_product(orbit%vec(1:4)-ele%rad_int_cache%orb0(1:4), ele%rad_int_cache%dg3_dorb(1:4))
-    if (g3 < 0) g3 = 0
+    int_g2 = len_half * (ele%rad_int_cache%g2_0 + dot_product(orbit%vec(1:4)-ele%rad_int_cache%orb0(1:4), ele%rad_int_cache%dg2_dorb(1:4)))
+    int_g3 = len_half * (ele%rad_int_cache%g3_0 + dot_product(orbit%vec(1:4)-ele%rad_int_cache%orb0(1:4), ele%rad_int_cache%dg3_dorb(1:4)))
+    if (int_g3 < 0) int_g3 = 0
   endif
 
   return
@@ -190,8 +190,8 @@ endif
 orbit2 = orbit
 call offset_particle (ele, param, set$, orbit2, s_pos = s_here)
 call canonical_to_angle_coords (orbit2)
-orbit2%vec(1) = orbit2%vec(1) + direc * orbit2%vec(2) * len2 / 2.0_rp ! Extrapolate to center of region 1/4 of way into element.
-orbit2%vec(3) = orbit2%vec(3) + direc * orbit2%vec(4) * len2 / 2.0_rp
+orbit2%vec(1) = orbit2%vec(1) + direc * orbit2%vec(2) * len_half / 2.0_rp ! Extrapolate to center of region 1/4 of way into element.
+orbit2%vec(3) = orbit2%vec(3) + direc * orbit2%vec(4) * len_half / 2.0_rp
 
 call multipole_ele_to_ab (ele, .false., ix_mag_max, a_pole_mag, b_pole_mag, magnetic$, include_kicks$)
 call multipole_ele_to_ab (ele, .false., ix_elec_max, a_pole_elec, b_pole_elec, electric$, include_kicks$)
@@ -211,22 +211,27 @@ do i = 0, ix_elec_max
   ky_tot = ky_tot + ky
 enddo
 
-!
+! A positive kick means that g is negative.
 
 select case (ele%key)
 case (sbend$)
-  g_x = kx_tot + ele%value(g$) + ele%value(g_err$) 
-  g_y = ky_tot
-  g2 = g_x**2 + g_y**2
+  gx = -kx_tot/ele%value(l$) + ele%value(g$) + ele%value(g_err$) 
+  gy = -ky_tot/ele%value(l$)
+  g2 = gx**2 + gy**2
   g3 = sqrt(g2)**3
-  len2 = len2 * (1.0_rp + ele%value(g$) * orbit2%vec(1))
 
 case default
-  g_x = kx_tot
-  g_y = ky_tot
-  g2 = g_x**2 + g_y**2
+  gx = -kx_tot/ele%value(l$)
+  gy = -ky_tot/ele%value(l$)
+  g2 = gx**2 + gy**2
   g3 = sqrt(g2)**3
 end select
+
+len2 = len_half * (1.0_rp + ele%value(g$) * orbit2%vec(1))
+int_gx = len2 * gx
+int_gy = len2 * gy
+int_g2 = len2 * g2
+int_g3 = len2 * g3
 
 !-------------------------------------------------------
 contains
@@ -268,7 +273,7 @@ g3 = g3 / (n1 + 1)
 
 end subroutine calc_g
 
-end subroutine calc_radiation_tracking_g_factors
+end subroutine calc_radiation_tracking_integrals
 
 !---------------------------------------------------------------------------
 !---------------------------------------------------------------------------
@@ -310,7 +315,7 @@ type (lat_param_struct) :: param
 
 integer :: edge
 
-real(rp) s_len, g_x, g_y, this_ran, mc2, g2, g3
+real(rp) int_gx, int_gy, this_ran, mc2, int_g2, int_g3
 real(rp) gamma_0, dE_p, fact_d, fact_f, q_charge2, p_spin, spin_norm(3), norm
 real(rp), parameter :: rad_fluct_const = 55.0_rp * classical_radius_factor * h_bar_planck * c_light / (24.0_rp * sqrt_3)
 real(rp), parameter :: spin_const = 5.0_rp * sqrt_3 * classical_radius_factor * h_bar_planck * c_light / 16
@@ -323,8 +328,8 @@ character(*), parameter :: r_name = 'track1_radiation'
 
 if (.not. bmad_com%radiation_damping_on .and. .not. bmad_com%radiation_fluctuations_on) return
 
-call calc_radiation_tracking_g_factors (ele, orbit, param, edge, s_len, g_x, g_y, g2, g3)
-if (s_len == 0) return
+call calc_radiation_tracking_integrals (ele, orbit, param, edge, int_gx, int_gy, int_g2, int_g3)
+if (int_g2 == 0) return
 
 ! Apply the radiation kicks
 ! Basic equation is E_radiated = xi * (dE/dt) * sqrt(L) / c_light
@@ -336,14 +341,14 @@ gamma_0 = ele%value(e_tot$) / mc2
 
 fact_d = 0
 if (bmad_com%radiation_damping_on) then
-  fact_d = damp_const * q_charge2 * gamma_0**3 * g2 * s_len / mc2
+  fact_d = damp_const * q_charge2 * gamma_0**3 * int_g2 / mc2
   if (bmad_com%backwards_time_tracking_on) fact_d = -fact_d
 endif
 
 fact_f = 0
 if (bmad_com%radiation_fluctuations_on) then
   call ran_gauss (this_ran)
-  fact_f = sqrt(rad_fluct_const * q_charge2 * s_len * gamma_0**5 * g3) * this_ran / mc2
+  fact_f = sqrt(rad_fluct_const * q_charge2 * gamma_0**5 * int_g3) * this_ran / mc2
 endif
 
 dE_p = (1 + orbit%vec(6)) * (fact_d + fact_f) * synch_rad_com%scale 
@@ -352,18 +357,19 @@ orbit%vec(2) = orbit%vec(2) * (1 - dE_p)
 orbit%vec(4) = orbit%vec(4) * (1 - dE_p)
 orbit%vec(6) = orbit%vec(6)  - dE_p * (1 + orbit%vec(6))
 
-! Sokolov-Ternov Spin flip?
+! Sokolov-Ternov Spin flip
+! The equation is not correct
 
-if (bmad_com%spin_tracking_on .and. bmad_com%spin_sokolov_ternov_flipping_on) then
-  norm = norm2(orbit%spin)
-  if (norm /= 0) then
-    spin_norm = orbit%spin / norm
-    call ran_uniform (this_ran)
-    p_spin = (spin_const * q_charge2 * gamma_0**5 * s_len / (orbit%beta * mc2**2)) * &
-          (g3 - c1_spin * g3 * (spin_norm(3))**2 + c2_spin * g2 * dot_product([g_y, -g_x], spin_norm(1:2))) 
-    if (this_ran < p_spin) orbit%spin = -orbit%spin  ! spin flip
-  endif
-endif
+!if (bmad_com%spin_tracking_on .and. bmad_com%spin_sokolov_ternov_flipping_on) then
+!  norm = norm2(orbit%spin)
+!  if (norm /= 0) then
+!    spin_norm = orbit%spin / norm
+!    call ran_uniform (this_ran)
+!    p_spin = (spin_const * q_charge2 * gamma_0**5 / (orbit%beta * mc2**2)) * &
+!          (int_g3 - c1_spin * int_g3 * (spin_norm(3))**2 + c2_spin * int_g2 * dot_product([g_y, -g_x], spin_norm(1:2))) 
+!    if (this_ran < p_spin) orbit%spin = -orbit%spin  ! spin flip
+!  endif
+!endif
 
 end subroutine track1_radiation 
 
