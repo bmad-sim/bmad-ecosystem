e3b3a7d54109bd37e9ed1005511f9f5b14a390a8
David Sagan <dcs16@cornell.edu>
2019-11-26 00:36:32 +0000

    Updated to 2018 CODATA.

--- interfaces/physical_constants.f90
+++ interfaces/physical_constants.f90
@@ -7,16 +7,18 @@ module physical_constants
 
 use precision_def
 
+! Updated to 2018 CODATA
+
 real(rp), parameter :: pi = 3.141592653589793238462643383279d0
 real(rp), parameter :: twopi = 2 * pi
 real(rp), parameter :: fourpi = 4 * pi
 real(rp), parameter :: sqrt_2 = sqrt(2.0_rp)               ! 1.414213562373095048801688724209698d0
 real(rp), parameter :: sqrt_3 = sqrt(3.0_rp)               ! 1.732050807568877293527446341505872d0 
 
-real(rp), parameter :: m_electron = 0.5109989461d6         ! Mass [eV]
-real(rp), parameter :: m_proton   = 0.9382720813d9         ! Mass [eV]
-real(rp), parameter :: m_neutron  = 0.9395654133d9         ! Mass [eV]
-real(rp), parameter :: m_muon     = 105.6583715d6          ! Mass [eV]
+real(rp), parameter :: m_electron = 0.51099895000d6         ! Mass [eV]
+real(rp), parameter :: m_proton   = 0.93827208816d9         ! Mass [eV]
+real(rp), parameter :: m_neutron  = 0.93956542052d9         ! Mass [eV]
+real(rp), parameter :: m_muon     = 105.6583755d6           ! Mass [eV]
 
 
 real(rp), parameter :: e_mass = 1d-9 * m_electron          ! [GeV] FOR MAD COMPATIBILITY USE ONLY. USE M_ELECTRON INSTEAD.
@@ -25,39 +27,39 @@ real(rp), parameter :: p_mass   = 1d-9 * m_proton          ! [GeV] FOR MAD COMPA
 real(rp), parameter :: m_pion_0 = 134.9766d6               ! Mass [eV]
 real(rp), parameter :: m_pion_charged = 139.57018d6        ! Mass [eV]
 
-real(rp), parameter :: m_deuteron   = 1.875612928d9        ! Mass [eV]
+real(rp), parameter :: m_deuteron = 1.87561294257d9        ! Mass [eV]
 
-real(rp), parameter :: atomic_mass_unit = 931.494095d6     ! unified atomic mass unit u (or dalton) in [eV]
+real(rp), parameter :: atomic_mass_unit = 931.49410242d6   ! unified atomic mass unit u (or dalton) in [eV]
  
 real(rp), parameter :: c_light = 2.99792458d8              ! speed of light
-real(rp), parameter :: r_e = 2.8179403227d-15              ! classical electron radius
+real(rp), parameter :: r_e = 2.8179403262d-15              ! classical electron radius
 real(rp), parameter :: r_p = r_e * m_electron / m_proton   ! proton radius
-real(rp), parameter :: e_charge = 1.6021766208d-19         ! electron charge [Coul]
-real(rp), parameter :: h_planck = 4.13566733d-15           ! Planck's constant [eV*sec]
-real(rp), parameter :: h_bar_planck = 6.58211899d-16       ! h_planck/twopi [eV*sec]
+real(rp), parameter :: e_charge = 1.602176634d-19          ! electron charge [Coul]
+real(rp), parameter :: h_planck = 4.135667696d-15          ! Planck's constant [eV*sec]
+real(rp), parameter :: h_bar_planck = h_planck / twopi     ! h_planck/twopi [eV*sec]
 
 real(rp), parameter :: mu_0_vac = fourpi * 1d-7                     ! Permeability of free space
 real(rp), parameter :: eps_0_vac = 1 / (c_light*c_light * mu_0_vac) ! Permittivity of free space
 
 ! Radiation constants
 
-real(rp), parameter :: classical_radius_factor = 1.439964416d-9  ! e^2 / (4 pi eps_0) [m*eV]
-                                                                 !  = classical_radius * mass * c^2. 
-                                                                 ! Is same for all particles of charge +/- 1.
+real(rp), parameter :: classical_radius_factor = r_e * m_electron ! e^2 / (4 pi eps_0) [m*eV]
+                                                                  !  = classical_radius * mass * c^2. 
+                                                                  ! Is same for all particles of charge +/- 1.
 ! Chemistry
 
-real(rp), parameter :: N_avogadro = 6.02214129d23    ! Number / mole
+real(rp), parameter :: N_avogadro = 6.02214076d23    ! Number / mole  (exact)
 
 ! Anomalous magnetic moment.
 ! Note: Deuteron g-factor
 !   g_deu = (g_p / (mu_p / mu_N)) (mu_deu / mu_N) * (m_deu / m_p) * (q_p / q_deu) * (S_p / S_deu)
 ! The anomlous mag moment a = (g - 2) / 2 as always.
 
-real(rp), parameter :: fine_structure_constant =  7.29735257d-3
-real(rp), parameter :: anomalous_mag_moment_electron = 1.159652193d-3
-real(rp), parameter :: anomalous_mag_moment_proton   = 1.79284735d0
-real(rp), parameter :: anomalous_mag_moment_muon     = 1.1659208d-3  ! ~fine_structure_constant / twopi
-real(rp), parameter :: anomalous_mag_moment_deuteron = -0.1429872724d0
+real(rp), parameter :: fine_structure_constant =  7.2973525693d-3
+real(rp), parameter :: anomalous_mag_moment_electron = 1.15965218128d-3
+real(rp), parameter :: anomalous_mag_moment_proton   = 1.79284734463d0
+real(rp), parameter :: anomalous_mag_moment_muon     = 1.16592089d-3  ! ~fine_structure_constant / twopi
+real(rp), parameter :: anomalous_mag_moment_deuteron = -0.1425617662d0
 real(rp), parameter :: anomalous_mag_moment_neutron  = -1.91304273d0
 real(rp), parameter :: anomalous_mag_moment_He3      = -4.184153686d0
 
