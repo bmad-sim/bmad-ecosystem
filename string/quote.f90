3c9dc12488d326d11deaed0a20d0b62df8579fe2
David Sagan <dcs16@cornell.edu>
2019-12-05 18:35:46 +0000

    Now will not add quote marks if quote marks already present.

--- string/quote.f90
+++ string/quote.f90
@@ -2,7 +2,8 @@
 ! Function quote(str) result (q_str)
 !
 ! Function to put double quote marks (") around a string.
-! The string will be trimmed of trailing blanks.
+! The string will not be modified if there are already quote marks of either kind.
+! The string will always be trimmed of trailing blanks (but not leading blanks).
 !
 ! Input:
 !   str     -- character(*): Input string
@@ -18,8 +19,14 @@ integer n
 
 !
 
-n = len_trim(str) + 2
-allocate(character(n) :: q_str)
+n = len_trim(str)
+if ((str(1:1) == "'" .and. str(n:n) == "'") .or. (str(1:1) == '"' .and. str(n:n) == '"')) then
+  allocate(character(n) :: q_str)
+  q_str = trim(str)
+  return
+endif
+
+allocate(character(n+2) :: q_str)
 q_str = '"' // trim(str) // '"'
 
 end function quote
