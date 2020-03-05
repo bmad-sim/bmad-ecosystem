47d5ba718021e0ef929a2eb7200634b9fd155fa5
David Sagan <dcs16@cornell.edu>
2020-03-05 14:14:17 +0000

    Better chrom_tune search algorithm.

--- code/chrom_tune.f90
+++ code/chrom_tune.f90
@@ -7,7 +7,7 @@
 ! The two groups are then varied by fractional amounts to match the desired chromaticities.
 ! 
 ! Input:
-!   lat     -- lat_struct: Lat to use, 
+!   lat      -- lat_struct: Lat to use, 
 !   delta_e  -- Real(rp): Delta energy used for the calculation.
 !                    If 0 then default of 1.0d-4 is used.
 !   target_x -- Real(rp): Target X Chromaticity
@@ -17,7 +17,7 @@
 !               A good number is: err_tol = 0.05_rp
 !
 ! Output:
-!   lat     -- lat_struct: Lat with sextupole set
+!   lat      -- lat_struct: Lat with sextupole set
 !   delta_e  -- Real(rp): Set to 1.0d-4 if on input DELTA_E =< 0.
 !   err_flag -- Logical: .false. if match successful, .true. if failed
 !                   Fails if takes longer than 100 iterations.
@@ -32,22 +32,23 @@ subroutine chrom_tune(lat, delta_e, target_x, target_y, err_tol, err_flag)
 
 use bmad_interface, except_dummy => chrom_tune
 use nr, only: gaussj
+use super_recipes_mod
 
 implicit none
 
 type (lat_struct) lat
 type (ele_pointer_struct), allocatable :: ele_sex(:)
+type (super_mrqmin_storage_struct) storage
 
-integer i, j, ix, iy, n_sex
+integer i, j, ix, iy, n_sex, status
 integer, allocatable :: ix_x_sex(:), ix_y_sex(:)
 
 real(rp), allocatable :: sex_y_value(:), sex_x_value(:)
-real(rp) target_x, target_y, chrom_x, chrom_y
-real(rp) delta_x, delta_y, d_chrom, chrom_x0
-real(rp) chrom_y0, chrom(2,1), matrix(2,2)
-real(rp) delta_e, err_tol
+real(rp) target_x, target_y, chrom_x, chrom_y, chisq
+real(rp) d_chrom, chrom_x0, chrom_y0, a_lambda
+real(rp) delta_e, err_tol, k2_vec(2), weight(2), chrom_target(2)
  
-logical err_flag
+logical err_flag, maska(2)
 character(*), parameter :: r_name = 'chrom_tune'
 
 !                      
@@ -86,52 +87,67 @@ endif
 call re_allocate (ix_x_sex, ix);  call re_allocate (sex_x_value, ix)
 call re_allocate (ix_y_sex, iy);  call re_allocate (sex_y_value, iy)
 
-delta_x = 0.1
-delta_y = 0.1
-
 !
       
+sex_x_value(:) = lat%ele(ix_x_sex(:))%value(k2$)
+sex_y_value(:) = lat%ele(ix_y_sex(:))%value(k2$)
+k2_vec = 0
+maska = .false.
+weight = 1
+chrom_target = [target_x, target_y]
+a_lambda = -1
+
 do j = 1, 100
+  call super_mrqmin (chrom_target, weight, k2_vec, chisq, chrom_func, storage, a_lambda, status)
 
-  sex_x_value(:) = lat%ele(ix_x_sex(:))%value(k2$)
-  sex_y_value(:) = lat%ele(ix_y_sex(:))%value(k2$)
-
-  call chrom_calc(lat, delta_e, chrom_x0, chrom_y0)
+  if (status < 0) then
+    call out_io (s_error$, r_name, 'SINGULAR MATRIX ENCOUNTERED!')
+    stop
+  endif
 
   d_chrom = abs(target_x - chrom_x0) + abs(target_y - chrom_y0)
-
   if (d_chrom < err_tol) then
     err_flag = .false.
+    lat%ele(ix_x_sex(:))%value(k2$) = sex_x_value(:) * (1 + k2_vec(1))
+    lat%ele(ix_y_sex(:))%value(k2$) = sex_y_value(:) * (1 + k2_vec(2))
     return
-  end if
-
-  lat%ele(ix_x_sex(:))%value(k2$) = sex_x_value(:)*(1 + delta_x)
-  lat%ele(ix_y_sex(:))%value(k2$) = sex_y_value(:)
-  call chrom_calc(lat, delta_e, chrom_x, chrom_y)
-   
-  matrix(1,1) = (chrom_x - chrom_x0) / delta_x
-  matrix(1,2) = (chrom_y - chrom_y0) / delta_x
-   
-  lat%ele(ix_x_sex(:))%value(k2$) = sex_x_value(:)
-  lat%ele(ix_y_sex(:))%value(k2$) = sex_y_value(:)*(1 + delta_y)
-  call chrom_calc(lat, delta_e, chrom_x, chrom_y)
-   
-  matrix(2,1) = (chrom_x - chrom_x0) / delta_y
-  matrix(2,2) = (chrom_y - chrom_y0) / delta_y
-
-  chrom(1,1) = (target_x - chrom_x0)
-  chrom(2,1) = (target_y - chrom_y0)
-
-  call gaussj(matrix, chrom)
-   
-  lat%ele(ix_x_sex(:))%value(k2$) = sex_x_value(:)*(1 + chrom(1,1))
-  lat%ele(ix_y_sex(:))%value(k2$) = sex_y_value(:)*(1 + chrom(2,1))
-      
-end do
-
-!
+  endif
+enddo
 
 call out_io (s_error$, r_name, 'CANNOT ADJUST SEXTUPOLES TO GET DESIRED CHROMATICITIES!')
 err_flag = .true.
+
+!------------------------------------------------------
+contains
+
+subroutine chrom_func (a_try, y_fit, dy_da, status)
+
+real(rp), intent(in) :: a_try(:)
+real(rp), intent(out) :: y_fit(:)
+real(rp), intent(out) :: dy_da(:, :)
+
+real(rp) :: delta = 0.01
+integer status, i
+
+!
+
+lat%ele(ix_x_sex(:))%value(k2$) = sex_x_value(:) * (1 + a_try(1))
+lat%ele(ix_y_sex(:))%value(k2$) = sex_y_value(:) * (1 + a_try(2))
+call chrom_calc(lat, delta_e, chrom_x0, chrom_y0)
+y_fit = [chrom_x0, chrom_y0]
+
+lat%ele(ix_x_sex(:))%value(k2$) = sex_x_value(:) * (1 + a_try(1) + delta)
+lat%ele(ix_y_sex(:))%value(k2$) = sex_y_value(:) * (1 + a_try(2))
+call chrom_calc(lat, delta_e, chrom_x, chrom_y)
+dy_da(1,1) = (chrom_x - chrom_x0) / delta
+dy_da(2,1) = (chrom_y - chrom_y0) / delta
+
+lat%ele(ix_x_sex(:))%value(k2$) = sex_x_value(:) * (1 + a_try(1))
+lat%ele(ix_y_sex(:))%value(k2$) = sex_y_value(:) * (1 + a_try(2) + delta)
+call chrom_calc(lat, delta_e, chrom_x, chrom_y)
+dy_da(1,2) = (chrom_x - chrom_x0) / delta
+dy_da(2,2) = (chrom_y - chrom_y0) / delta
+
+end subroutine
 
 end subroutine
