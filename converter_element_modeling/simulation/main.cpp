d413afeafa9097928717c70bd0db1fac8fabb083
John M. Mastroberti <jmm699@cornell.edu>
2020-04-21 00:48:57 +0000

    Updates to config file parsing

--- converter_element_modeling/simulation/main.cpp
+++ converter_element_modeling/simulation/main.cpp
@@ -30,20 +30,18 @@ void signal_handler(int) {
 
 int main() {
   // First: Read in run settings from config file
-  SimSettings my_settings;
+  const auto settings = parse_config_file("config.txt");
 
-  bool success = parse_config_file("config.txt", my_settings);
-
-  if (success) {
-    std::cout << my_settings;
+  if (settings.valid()) {
+    std::cout << settings;
   } else {
-    std::cout << "Could not read config file\n";
+    std::cout << "Please correct the above issues in config.txt\n";
     return 1;
   }
 
 
   // Now, make sure output directory exists and is empty
-  auto& od = my_settings.output_directory; // short var name
+  auto& od = settings.output_directory; // short var name
   if (!fs::is_directory(od)) {
     fs::create_directory(od);
   } else if (!fs::is_empty(od)) {
@@ -64,8 +62,8 @@ int main() {
     }
   }
   // Set up directory structure for dxds/dyds data
-  for (auto E: my_settings.in_energies) {
-    for (auto T: my_settings.target_thicknesses) {
+  for (auto E: settings.pc_in) {
+    for (auto T: settings.target_thickness) {
       char dirname[100];
       sprintf(dirname, "%s/dir_dat/E%0.0lf_T%0.3lf", od.c_str(), E, T);
       fs::create_directories(dirname);
@@ -79,25 +77,18 @@ int main() {
   eff_file.open(od + "/yields.dat");
   eff_file << "E\tT\tY\n";
   auto runManager = Initialize_Geant();
-  for (auto E: my_settings.in_energies) {
-    for (auto T: my_settings.target_thicknesses) {
+  for (auto E: settings.pc_in) {
+    for (auto T: settings.target_thickness) {
       std::cout << "Simulating target thickness T = "
         << T << "cm, incoming electron energy " << E << " MeV\n";
       std::ofstream outfile;
       sprintf(outfile_name, "%s/E%0.0lf_T%0.3lf_er.dat", od.c_str(), E, T);
 
-#ifdef AUTO_BIN
-      CalibrationBinner calbin(runManager, my_settings, E, T);
-      auto [num_E_bins, num_r_bins] = calbin.new_calibrate();
+      CalibrationBinner calbin(runManager, settings, E, T);
+      auto [num_E_bins, num_r_bins] = calbin.calibrate();
 
       // Construct the real binner using settings obtained during calibration
       Binner my_binner(std::move(calbin));
-#else
-      // Instead of auto binning, use fixed bins
-      int num_E_bins = 25;
-      int num_r_bins = 25;
-      Binner my_binner(num_E_bins, num_r_bins, 50.0, 0.20);
-#endif
 
       // Set up the handling of C-c interupt
       std::signal(SIGINT, signal_handler);
@@ -107,7 +98,7 @@ int main() {
       int num_runs=0;
       constexpr size_t RUN_LENGTH = 10000;
       while (!my_binner.has_enough_data() && !HALT_SIGNAL) {
-        run_simulation(runManager, my_settings.target_material, E, T, &my_binner, 10000);
+        run_simulation(runManager, settings.target_material, E, T, &my_binner, 10000);
         num_runs++;
 #ifndef CONTINUOUS_PRINTOUT
       }
@@ -117,13 +108,13 @@ int main() {
       int pos_tot = my_binner.get_total();
       // set up first line with r values
       outfile << num_r_bins;
-      for (int j=0; j<num_r_bins; j++)
+      for (size_t j=0; j<num_r_bins; j++)
         outfile << '\t' << my_binner.get_r_val(j);
       outfile << '\n';
       // each line should be one E value
-      for (int i=0; i<num_E_bins; i++) {
+      for (size_t i=0; i<num_E_bins; i++) {
         outfile << my_binner.get_E_val(i);
-        for (int j=0; j<num_r_bins; j++) {
+        for (size_t j=0; j<num_r_bins; j++) {
           double bin_E = my_binner.get_E_val(i);
           double bin_r = my_binner.get_r_val(j);
           const auto& bin = my_binner.get_bin(i,j);
