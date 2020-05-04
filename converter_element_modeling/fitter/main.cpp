ba4162a71233398dbc0eb4f8e9251374372e16a8
John M. Mastroberti <jmm699@cornell.edu>
2020-05-04 11:59:40 +0000

    Added gnuplot scripts; minor bug fixes

--- converter_element_modeling/fitter/main.cpp
+++ converter_element_modeling/fitter/main.cpp
@@ -11,6 +11,7 @@
 #include "cauchy.hpp"
 #include "read_data.hpp"
 #include "meta_fit.hpp"
+#include "gnuplot.hpp"
 template<typename T>
 inline constexpr T sqr(T x) { return x*x; }
 template<typename T>
@@ -21,6 +22,13 @@ template<size_t n>
 std::string TAB() { if constexpr (n==0) return ""; else return tab + TAB<n-1>(); }
 
 namespace fs = std::filesystem;
+
+double dp_diff(fitType T, const DataPoint& p1, const DataPoint& p2) {
+  if      (T==fitType::CX) return std::abs(p1.cx - p2.cx);
+  else if (T==fitType::AX) return std::abs(p1.ax - p2.ax);
+  else if (T==fitType::AY) return std::abs(p1.ay - p2.ay);
+  else                     return std::abs(p1.beta - p2.beta);
+}
 
 int main(int argc, char* argv[]) {
   // Given many dxds/dyds bin files of the form "E%lf_r%lf_bin.dat",
@@ -48,25 +56,35 @@ int main(int argc, char* argv[]) {
 
   // Loop over each folder in dir_dat, running the fitting process on each
   std::string folder_name_format = data_dir + "/dir_dat/" + "E%lf_T%lf";
+
+  char coef_file_name[100];
+  std::ofstream coef_file;
   for (auto& E_T_folder : fs::directory_iterator(data_dir + "/dir_dat")) {
     double Ein, T, Eout, r;
     // Parse Ein, T out of the directory name
     const char *E_T_folder_name = (E_T_folder.path()).c_str();
-    sscanf(E_T_folder_name, folder_name_format.c_str(), &Ein, &T);
+    auto ret = sscanf(E_T_folder_name, folder_name_format.c_str(), &Ein, &T);
+    if (ret != 2) continue; // parsing failure
     Ein *= 1e6; // convert to eV
     T *= 1e-2; // convert to meters
     // Vector for holding fitting results
     std::vector<DataPoint> data_points;
     data_points.reserve(200);
 
+    sprintf(coef_file_name, "%s/coef.dat", E_T_folder_name);
+    coef_file.open(coef_file_name);
+    coef_file << "E\tr\tcx\tcy\tax\tay\tbeta\tamp\tchi2\n";
+
     // Loop over each file in the folder
     std::string file_name_format = std::string(E_T_folder_name) + "/E%lf_r%lf_bin.dat";
     for (auto& bin_file : fs::directory_iterator(E_T_folder.path())) {
       // Parse Eout, r from the file name
       const char *bin_file_name = (bin_file.path()).c_str();
-      sscanf(bin_file_name, file_name_format.c_str(), &Eout, &r);
+      ret = sscanf(bin_file_name, file_name_format.c_str(), &Eout, &r);
+      if (ret!=2) continue; // parsing failure
       Eout *= 1e6; // convert to eV
       r *= 1e-2; // convert to meters
+
 
       // Read in the data from the file
       std::vector<BinPoint> bins;
@@ -104,15 +122,74 @@ int main(int argc, char* argv[]) {
           return p1.E < p2.E;
         });
 
+
+    // Remove outliers
+    const double MAX_DIFF = 20.0;
+    bool removed=true;
+    while (removed) {
+      removed = false;
+      for (int fit_i=0; fit_i<4; fit_i++) {
+        size_t data_len = data_points.size();
+        std::vector<bool> good(data_points.size());
+        std::fill(good.begin(), good.end(), true);
+        fitType fitT = (fitType) fit_i;
+        double new_diff, old_diff = dp_diff(fitT, data_points[1], data_points[0]);
+        for (size_t i=2; i<data_len; i++) {
+          new_diff = dp_diff(fitT, data_points[i], data_points[i-1]);
+          if (((new_diff/old_diff > MAX_DIFF) || !good[i-1]) &&
+              (data_points[i].E == data_points[i-1].E) &&
+              (data_points[i].E > crossover_point)) {
+            good[i] = false;
+            removed = true;
+          }
+          old_diff = new_diff;
+          //std::cout << "Checks: "
+          //  << (new_diff/old_diff > MAX_DIFF || !good[i-1]) << ", "
+          //  << (data_points[i].E == data_points[i-1].E) << ", "
+          //  << (data_points[i].E > crossover_point) << '\n';
+        }
+        auto checker = [&good,i=0](const DataPoint& p) mutable { return !good[i++]; };
+        data_points.erase(std::remove_if(data_points.begin(), data_points.end(), checker), data_points.end());
+      }
+    }
+    data_points.erase(data_points.end()-1, data_points.end());
+    //auto& pend1 = data_points[data_points.size()-1];
+    //auto& pend2 = data_points[data_points.size()-2];
+    //auto& pend3 = data_points[data_points.size()-3];
+    //std::cout << "Checks at end: "
+    //  << (dp_diff(fitType::AX, pend1, pend2)/dp_diff(fitType::AX, pend2, pend3) > MAX_DIFF ) << ", "
+    //  << (pend1.E == pend2.E) << ", "
+    //  << (pend1.E > crossover_point) << '\n';
+    //std::cout << "Diff at end: " << dp_diff(fitType::AX, data_points[data_points.size()-1], data_points[data_points.size()-2]) << '\n';
+    //getchar();
+
     // Do the meta fits
     auto cx_fit_results = cx_fit(data_points);
     auto ax_fit_results = ax_fit(data_points, crossover_point);
     auto ay_fit_results = ay_fit(data_points, crossover_point);
     auto beta_fit_results = beta_fit(data_points, crossover_point);
 
+    // Write gnuplot files
+    output_gp_cx(cx_fit_results, data_points, E_T_folder_name, crossover_point);
+    output_gp_ax(ax_fit_results, data_points, E_T_folder_name, crossover_point);
+    output_gp_ay(ay_fit_results, data_points, E_T_folder_name, crossover_point);
+    output_gp_beta(beta_fit_results, data_points, E_T_folder_name, crossover_point);
+
+    for (const auto& p : data_points) {
+      coef_file << p.E << '\t' << p.r << '\t'
+        << p.cx << '\t'       << p.cy << '\t'
+        << p.ax << '\t'       << p.ay << '\t'
+        << p.beta << '\t'      << p.amp << '\n';
+    }
+    coef_file.close();
+
     // Store the results of the fit
     metafits.push_back({Ein, T, cx_fit_results,
         ax_fit_results, ay_fit_results, beta_fit_results});
+  }
+  if (!metafits.size()) {
+    std::cout << "Error: could not find data to fit to\n";
+    return 1;
   }
   // Sort fits by T and then by Ein
   std::sort(metafits.begin(), metafits.end(),
@@ -129,9 +206,12 @@ int main(int argc, char* argv[]) {
 
   double T = metafits[0].T;
   bmad_file << "distribution = {\n";
+  // TODO: read material from config file
+  bmad_file << TAB<1>() << "material = tungsten" << ",\n";
+  bmad_file << TAB<1>() << "species_out = positron" << ",\n";
   bmad_file << TAB<1>() << "thickness = " << T << ",\n";
   // TODO: USE ACTUAL dxy_ds_max
-  bmad_file << TAB<1>() << "dxy_ds_max = " << 10;
+  bmad_file << TAB<1>() << "dxy_ds_max = " << 1.8;
   for (const auto& mf : metafits) {
     if (mf.T != T) { // start a new distribution
       T = mf.T;
@@ -169,7 +249,7 @@ int main(int argc, char* argv[]) {
       for (auto& p : probs) er_file >> p;
       bmad_file << ",\n" << TAB<3>() << "row = {pc_out = " << Eout;
       bmad_file << ", prob = [0.0, "; // r=0 -> prob = 0
-      for (size_t i=0; i<num_cols-1; i++) bmad_file << probs[i] << ", ";
+      for (size_t i=0; i<num_cols-1; i++) bmad_file << 1e-4 * probs[i] << ", "; // convert to prob / (eV * m)
       bmad_file << probs[num_cols-1] << "]}";
     }
     bmad_file << "\n" << TAB<2>() << "},\n";
@@ -182,31 +262,33 @@ int main(int argc, char* argv[]) {
     for (const auto& beta1d : mf.beta.low_e_fits) {
       bmad_file << TAB<4>() << "fit_1d_r = {pc_out = " << beta1d.E
         << ", poly = ["
-        << beta1d.a0 << ", "
-        << beta1d.a1 << ", "
-        << beta1d.a2 << ", "
-        << beta1d.a3 << ", "
-        << beta1d.a4 << "]},\n";
+        << 0.0 << ", "
+        << beta1d.a << ", "
+        << beta1d.b << ", "
+        << beta1d.c << ", "
+        << beta1d.d << "]},\n";
     }
     const auto& fit2b = mf.beta.high_e_fit;
-    bmad_file << TAB<4>() << "poly_pc = ["
-      << fit2b.a0 << ", "
+    bmad_file << TAB<4>() << "poly_pc = [1.0, "
       << fit2b.a1 << ", "
-      << fit2b.a2 << ", 1.0],\n";
+      << fit2b.a2 << ", "
+      << fit2b.a3 << "],\n";
     bmad_file << "        poly_r = ["
-      << fit2b.b0 << ", "
+      //<< fit2b.b0 << ", "
+      << 0.0 << ", "
       << fit2b.b1 << ", "
       << fit2b.b2 << ", "
       << fit2b.b3 << "]},\n";
 
     // cx
     bmad_file << TAB<3>() << "c_x = {\n";
-    bmad_file << TAB<4>() << "poly_pc = ["
-      << mf.cx.a0 << ", "
+    bmad_file << TAB<4>() << "poly_pc = [1.0, "
       << mf.cx.a1 << ", "
-      << mf.cx.a2 << ", 1.0],\n";
+      << mf.cx.a2 << ", "
+      << mf.cx.a3 << "],\n";
     bmad_file << TAB<4>() << "poly_r = ["
-      << mf.cx.b0 << ", "
+      //<< mf.cx.b0 << ", "
+      << 0.0 << ", "
       << mf.cx.b1 << ", "
       << mf.cx.b2 << ", "
       << mf.cx.b3 << "]},\n";
@@ -222,15 +304,15 @@ int main(int argc, char* argv[]) {
         << ax1d.d << "]},\n";
     }
     const auto& fit2ax = mf.ax.high_e_fit;
-    bmad_file << TAB<4>() << "fit_2d_pc = {k = " << fit2ax.ke << ", poly = ["
-      << fit2ax.ae << ", "
-      << fit2ax.be << ", "
-      << fit2ax.ce << ", 1.0]},\n";
+    bmad_file << TAB<4>() << "fit_2d_pc = {k = " << fit2ax.ke << ", poly = [1.0, "
+      << fit2ax.a1 << ", "
+      << fit2ax.a2 << ", "
+      << fit2ax.a3 << "]},\n";
     bmad_file << TAB<4>() << "fit_2d_r = {k = " << fit2ax.kr << ", poly = ["
-      << fit2ax.ar << ", "
-      << fit2ax.br << ", "
-      << fit2ax.cr << ", "
-      << fit2ax.dr << "]}},\n";
+      << fit2ax.b0 << ", "
+      << fit2ax.b1 << ", "
+      << fit2ax.b2 << ", "
+      << fit2ax.b3 << "]}},\n";
 
     // ay
     bmad_file << TAB<3>() << "alpha_y = {\n";
@@ -243,15 +325,15 @@ int main(int argc, char* argv[]) {
         << ax1d.d << "]},\n";
     }
     const auto& fit2ay = mf.ay.high_e_fit;
-    bmad_file << TAB<4>() << "fit_2d_pc = {k = " << fit2ay.ke << ", poly = ["
-      << fit2ay.ae << ", "
-      << fit2ay.be << ", "
-      << fit2ay.ce << ", 1.0]},\n";
+    bmad_file << TAB<4>() << "fit_2d_pc = {k = " << fit2ay.ke << ", poly = [1.0, "
+      << fit2ay.a1 << ", "
+      << fit2ay.a2 << ", "
+      << fit2ay.a3 << "]},\n";
     bmad_file << TAB<4>() << "fit_2d_r = {k = " << fit2ax.kr << ", poly = ["
-      << fit2ay.ar << ", "
-      << fit2ay.br << ", "
-      << fit2ay.cr << ", "
-      << fit2ay.dr << "]}}\n";
+      << fit2ay.b0 << ", "
+      << fit2ay.b1 << ", "
+      << fit2ay.b2 << ", "
+      << fit2ay.b3 << "]}}\n";
 
     bmad_file << TAB<2>() << "}\n" << TAB<1>() << "}";
   }
--- converter_element_modeling/fitter/meta_fit.hpp
+++ converter_element_modeling/fitter/meta_fit.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <variant>
+#include <cmath>
+#include <gsl/gsl_poly.h>
 struct DataPoint {
   double E;
   double r;
@@ -18,33 +20,23 @@ enum class fitType { CX, AX, AY, BETA };
 //////////////////CX////////////////////
 
 struct cFitResults {
-  double a0, a1, a2;
-  double b0, b1, b2, b3;
+  double a1, a2, a3;
+  double b1, b2, b3;
   double chi2;
 };
 
 //////////////////////AX/Y////////////////////
 struct aFitResults2D {
-  double ke;
-  double kr;
-  double ae;
-  double be;
-  double ce;
-  //double de;
-  double ar;
-  double br;
-  double cr;
-  double dr;
+  double ke, kr;
+  double a1, a2, a3;
+  double b0, b1, b2, b3;
   double chi2;
 };
 
 struct aFitResults1D {
   double E;
   double k;
-  double a;
-  double b;
-  double c;
-  double d;
+  double a, b, c, d;
   double chi2;
 };
 
@@ -56,18 +48,14 @@ struct aFitResults {
 ////////////////////////BETA/////////////////////
 
 struct betaFitResults2D {
-  double a0, a1, a2;
-  double b0, b1, b2, b3;
+  double a1, a2, a3;
+  double b1, b2, b3;
   double chi2;
 };
 
 struct betaFitResults1D {
   double E;
-  double a0;
-  double a1;
-  double a2;
-  double a3;
-  double a4;
+  double a, b, c, d;
   double chi2;
 };
 
@@ -75,15 +63,6 @@ struct betaFitResults {
   std::vector<betaFitResults1D> low_e_fits;
   betaFitResults2D high_e_fit;
 };
-
-using FitResults = std::variant<cFitResults, aFitResults, betaFitResults>;
-
-template<fitType T>
-constexpr size_t type_ix() {
-  if constexpr(T==fitType::CX) return 0;
-  else if constexpr(T==fitType::AX || T==fitType::AY) return 1;
-  else return 2;
-}
 
 
 struct MetaFitResults {
@@ -97,8 +76,66 @@ struct MetaFitResults {
 
 
 cFitResults cx_fit(const std::vector<DataPoint>& data_points);
-
 aFitResults ax_fit(const std::vector<DataPoint>& data_points, double crossover_point);
 aFitResults ay_fit(const std::vector<DataPoint>& data_points, double crossover_point);
-
 betaFitResults beta_fit(const std::vector<DataPoint>& data_points, double crossover_point);
+
+template<fitType T, size_t dim>
+struct fit_t_helper {
+  typedef void type;
+};
+
+template<> struct fit_t_helper<fitType::CX,0>   { typedef cFitResults      type; };
+template<> struct fit_t_helper<fitType::CX,2>   { typedef cFitResults      type; };
+template<> struct fit_t_helper<fitType::AX,0>   { typedef aFitResults      type; };
+template<> struct fit_t_helper<fitType::AX,1>   { typedef aFitResults1D    type; };
+template<> struct fit_t_helper<fitType::AX,2>   { typedef aFitResults2D    type; };
+template<> struct fit_t_helper<fitType::AY,0>   { typedef aFitResults      type; };
+template<> struct fit_t_helper<fitType::AY,1>   { typedef aFitResults1D    type; };
+template<> struct fit_t_helper<fitType::AY,2>   { typedef aFitResults2D    type; };
+template<> struct fit_t_helper<fitType::BETA,0> { typedef betaFitResults   type; };
+template<> struct fit_t_helper<fitType::BETA,1> { typedef betaFitResults1D type; };
+template<> struct fit_t_helper<fitType::BETA,2> { typedef betaFitResults2D type; };
+
+template<fitType T, size_t dim>
+using fit_t_part = typename fit_t_helper<T,dim>::type;
+template<fitType T>
+using fit_t = typename fit_t_helper<T,0>::type;
+
+// Helper templates
+template<fitType T, size_t d>
+constexpr bool good_fit_type() {
+  if constexpr (d==2)
+    return (T==fitType::CX || T==fitType::AX || T==fitType::AY || T==fitType::BETA);
+  else if constexpr (d==1)
+    return (T==fitType::AX || T==fitType::AY || T==fitType::BETA);
+  else
+    return false;
+}
+
+template<fitType T, size_t dim>
+double eval(const fit_t_part<T, dim>& fit, const DataPoint& p) {
+  // Evaluates the given fit at the given (E,r) point
+  // and returns the result
+  static_assert(good_fit_type<T,dim>());
+  if constexpr (dim==2) {
+    double pc_coefs[] = {1.0, fit.a1, fit.a2, fit.a3};
+    double r_coefs[] = {0.0, fit.b1, fit.b2, fit.b3};
+    double ke = 0.0, kr = 0.0;
+    if constexpr(T==fitType::AX || T==fitType::AY) {
+      r_coefs[0] = fit.b0;
+      ke = fit.ke;
+      kr = fit.kr;
+    }
+    return gsl_poly_eval(pc_coefs, 4, p.E)
+      * gsl_poly_eval(r_coefs, 4, p.r)
+      * std::exp(-(ke * p.E + kr * p.r));
+  } else {
+    double r_coefs[] = {fit.a, fit.b, fit.c, fit.d};
+    if constexpr(T==fitType::BETA)
+      return gsl_poly_eval(r_coefs, 4, p.r) * p.r;
+    else
+      return gsl_poly_eval(r_coefs, 4, p.r) * std::exp(-fit.k*p.r);
+  }
+}
+
