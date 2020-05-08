3fb1e50ccdea4b2b5904b4d6d5a616b86f24a330
David Sagan <dcs16@cornell.edu>
2020-05-08 15:54:58 +0000

    Combined dsinc into sinc, etc.

--- special_functions/sqrt_one.f90
+++ special_functions/sqrt_one.f90
@@ -1,28 +1,40 @@
 !+
-! function sqrt_one(eps) result (ds1)
+! function sqrt_one(x, nd) result (y)
 !
-! Routine to calculate Sqrt[1+eps] - 1 to machine precision.
+! Routine to calculate Sqrt[1+x] - 1 and derivatives to machine precision.
 !
-! This is usful if eps can be near zero where the direct evaluation of
-! sqrt[1+eps] - 1 is inaccurate.
+! This is usful if x can be near zero where the direct evaluation of
+! sqrt[1+x] - 1 is inaccurate.
 !
 ! Input:
-!   eps -- real(rp):
+!   x   -- real(rp): Number
+!   nd  -- integer, optional: Derivative order. nd = 0 (default) -> compute Sqrt[1+x] - 1.
+!           NOTE: Currently only nd = 0 and nd = 1 are implemented.
 !
 ! Output:
-!   ds1 -- real(rp): Result.
+!   y   -- real(rp): Result.
 !-
 
-elemental function sqrt_one(eps) result (ds1)
+elemental function sqrt_one(x, nd) result (y)
 
-use precision_def
+use utilities_mod
 implicit none
 
-real(rp), intent(in) :: eps
-real(rp) ds1
+real(rp), intent(in) :: x
+real(rp) y, rad
+
+integer, optional, intent(in) :: nd
 
 !
 
-ds1 = eps / (sqrt(1 + eps) + 1)
+rad = sqrt(1 + x) + 1
+
+select case (integer_option(0, nd))
+case (0)
+  y = x / rad
+
+case (1)
+  y = (1 / rad**2 - x) / rad
+end select
 
 end function
