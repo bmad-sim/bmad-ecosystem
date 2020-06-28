ac1f3616b1a2ca92886d45a045b87d6512d1b1cb
John M. Mastroberti <jmm699@cornell.edu>
2020-06-28 16:40:13 +0000

    refactoring

--- converter_element_modeling/simulation/include/bin.hpp
+++ converter_element_modeling/simulation/include/bin.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include <vector>
+#include <cstdio>
 
 struct BinPoint {
   double x, y;
   size_t count;
+  double area;
 };
 
 struct DataPoint { double E, r, dxds, dyds; };
@@ -16,16 +18,19 @@ struct Bin {
     double area;
   private:
     mutable std::vector<xy_point> p_list;
+    mutable std::vector<BinPoint> *binned_ptr;
 
   public:
     Bin();
-    Bin(const Bin&) = default;
-    Bin& operator=(const Bin&) = default;
-    ~Bin() = default;
+    Bin(const Bin&);
+    Bin& operator=(const Bin&);
+    ~Bin();
     void add_point(double dxds, double dyds);
     double density(size_t num_elec_in) const;
-    void bin_momenta(const char * output_dir,
+    void write_momenta(const char * output_dir,
         double E_elec, double T,
-        double E, double r) const;
+        double E, double r, FILE *binlist) const;
+    void binary_write_momenta(FILE *binary_file) const;
+    void bin_momenta() const;
 };
 
