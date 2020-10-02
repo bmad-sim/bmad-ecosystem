ed8674f5bdb626bdfacda59ff046559266e5c9c1
David Sagan <dcs16@cornell.edu>
2020-10-02 02:57:13 +0000

    Made function from subroutine.

--- interface/dn_dpz_from_qmap.f90
+++ interface/dn_dpz_from_qmap.f90
@@ -1,5 +1,5 @@
 !+
-! Subroutine dn_dpz_from_qmap (orb_mat, q_map, dn_dpz)
+! Function dn_dpz_from_qmap (orb_mat, q_map) result (dn_dpz)
 !
 ! Rotine to calculate dn_dpz from th 1-turn 6x6 orbital matrix and the linear quaternian map
 ! This uses the standard SLIM coordinates for the spin motion.
@@ -15,7 +15,7 @@
 !   dn_dpz(3)       -- real(rp): dn_dpz.
 !-
 
-subroutine dn_dpz_from_qmap (orb_mat, q_map, dn_dpz)
+function dn_dpz_from_qmap (orb_mat, q_map) result (dn_dpz)
 
 use sim_utils
 use eigen_mod
@@ -24,9 +24,10 @@ use f95_lapack
 implicit none
 
 real(rp) orb_mat(6,6), q_map(0:3,0:6), dn_dpz(3)
-real(rp) w0(3,3), qv(0:3), n0(0:3), q0(0:3), qq(0:3)
-complex(rp) eval(6), evec(6,6), dd(3,3), gv(3,1), n_eigen(6,3), vv(6,6), aa(6,1)
-integer k, pinfo, ipiv2(2), ipiv6(6)
+real(rp) w0(3,3)
+complex(rp) n0(0:3), q0(0:3), n_eigen(6,3), vv(6,6), aa(6,1)
+complex(rp) eval(6), evec(6,6), qv(0:3), qq(0:3), dd(3,3), gv(3,1)
+integer k, pinfo, ipiv2(3), ipiv6(6)
 logical err
 
 !
@@ -37,7 +38,8 @@ n0 = [0.0_rp, q_map(1:3,0)] / norm2(q_map(1:3,0))
 q0 = q_map(0:3,0)
 
 do k = 1, 6
-  dd = eval(k) - w0
+  call cmplx_mat_make_unit(dd)
+  dd = eval(k) * dd - w0
   qv = matmul(q_map(0:3,1:6), evec(k,:))
   qq = quat_mul(quat_mul(qv, n0), quat_conj(q0)) + quat_mul(quat_mul(q0, n0), quat_conj(qv))
   gv(:,1) = qq(1:3)
@@ -50,4 +52,4 @@ aa(:,1) = [0, 0, 0, 0, 0, 1]
 call zgesv_f95(vv, aa, ipiv6, pinfo)
 dn_dpz = real(matmul(aa(:,1), n_eigen), rp)
 
-end subroutine dn_dpz_from_qmap
+end function dn_dpz_from_qmap
