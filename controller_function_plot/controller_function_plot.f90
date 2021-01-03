99be86555eed55b9579e1e77fc7f25ae18aa1f0b
David Sagan <dcs16@cornell.edu>
2021-01-03 19:21:28 +0000

    More controller function plotting devel.

--- controller_function_plot/controller_function_plot.f90
+++ controller_function_plot/controller_function_plot.f90
@@ -10,36 +10,37 @@ type (lat_struct) lat
 type (ele_pointer_struct), allocatable, target :: eles(:)
 type (ele_struct), pointer :: ele, slave
 type (control_struct), pointer :: ctl
-type (qp_line_struct) line(20)
 
 real(rp) var, var_min, var_max, plot_size(2)
-real(rp), allocatable :: table(:,:)
+real(rp), allocatable :: table(:,:), knots(:,:)
 
-integer n_curve_points, slave_list(20)
+integer n_var_points, slave_list(20)
 integer i, j, n, id, n_loc, ix_var, n_slave, n_slave_max
 
-logical make_plot, make_postscript, err, err_flag, ok
+logical make_plot, err, err_flag, ok, draw_knot_points
 
-character(200) init_file, lat_file, table_file_name, err_str
-character(40) controller_name, control_var
+character(200) init_file, lat_file, table_file_name, err_str, postscript_file
+character(40) controller_name, control_var, ans
 character(40) attrib_name(20)
 
 namelist / params / var_min, var_max, slave_list, table_file_name, make_plot, plot_size, &
-                    controller_name, control_var, lat_file, n_curve_points, make_postscript
+                    controller_name, control_var, lat_file, n_var_points, postscript_file, &
+                    draw_knot_points
 
 ! Read input
 
-init_file = 'controller_function_plot'
+init_file = 'controller_function_plot.init'
 if (cesr_iargc() > 0) call cesr_getarg(1, init_file)
-print '(a)', 'Init file: ', trim(init_file)
+print '(2a)', 'Init file: ', trim(init_file)
 
-slave_list = -1
-n_curve_points = 200
+slave_list = -1                   ! Set some defaults
+n_var_points = 200
 table_file_name = 'table.dat'
 make_plot = .true.
-make_postscript = .false.
+postscript_file = ''
+draw_knot_points = .true.
 
-open (1, file = init_file)
+open (1, file = init_file, status = 'old')
 read (1, nml = params)
 close (1)
 
@@ -101,7 +102,7 @@ if (n_slave == 0) then
   enddo
 endif
 
-allocate (table(0:n_slave, 0:n_curve_points))
+allocate (table(0:n_var_points, 0:n_slave))
 
 if (var_min == var_max) then
   if (ele%control%type == expression$) then
@@ -115,12 +116,19 @@ if (var_min == var_max) then
   var_max = ele%control%x_knot(n)
 endif
 
+! Start knot table
+
+if (ele%control%type == expression$) then
+  draw_knot_points = .false.
+elseif (draw_knot_points) then
+  allocate (knots(size(ele%control%x_knot), 0:n_slave))
+  knots(:,0) = ele%control%x_knot
+endif
+
 ! Make table
 
-
-
-do i = 0, n_curve_points
-  var = var_min + i * (var_max - var_min) / n_curve_points
+do i = 0, n_var_points
+  var = var_min + i * (var_max - var_min) / n_var_points
   ele%control%var(ix_var)%value = var
   table(i,0) = var
 
@@ -130,6 +138,8 @@ do i = 0, n_curve_points
     else
       slave => pointer_to_slave(ele, slave_list(j), ctl)
     endif
+
+    if (draw_knot_points .and. i == 0) knots(:,j) = ctl%y_knot
 
     if (ele%control%type == expression$) then
       call evaluate_expression_stack(ctl%stack, ctl%value, err_flag, err_str, ele%control%var, .false.)
@@ -145,28 +155,64 @@ enddo
 ! Write table
 
 if (table_file_name /= '') then
-  open (1, file = table_file_name, status = 'NEW')
+  open (1, file = table_file_name)
   write (1, '(a, 20a16)') '#  Ix   Var    ',  (attrib_name(j), j = 1, n_slave)
-  do i = 0, n_curve_points
+  do i = 0, n_var_points
     write (1, '(i5, 21es16.8)') i, (table(i,j), j = 0, n_slave)
   enddo
 endif
 
 ! Plotting
 
-if (make_plot) then
-  line(1:6)%color = [character(16):: 'blue', 'red', 'green', 'cyan', 'magenta', 'yellow']
+if (make_plot) call make_this_plot ('X')
+if (postscript_file /= '') call make_this_plot ('PS')
+
+!------------------------------------------------------
+contains
+
+subroutine make_this_plot(who)
+
+type (qp_line_struct) line(20)
+type (qp_symbol_struct) symbol(20)
+
+character(*) who
+
+!
+
+line(1:6)%color = [character(16):: 'blue', 'red', 'green', 'cyan', 'magenta', 'yellow']
+symbol(1:6)%color = [character(16):: 'blue', 'red', 'green', 'cyan', 'magenta', 'yellow']
+
+if (who == 'X') then
   call qp_open_page ('X', id, plot_size(1), plot_size(2), 'POINTS')
-  call qp_set_page_border (0.02_rp, 0.02_rp, 0.02_rp, 0.02_rp, '%PAGE')
-  call qp_set_box (1, 1, 1, 1)
-  call qp_calc_and_set_axis ('X', var_min, var_max, 4, 8, 'GENERAL')
-  call qp_calc_and_set_axis ('Y', minval(table(:,1:)), maxval(table(:,1:)), 4, 8, 'GENERAL')
-  call qp_draw_axes (ele%control%var(ix_var)%name, 'Slave Value')
-  do j = 1, n_slave
-    call qp_set_line_attrib ('PLOT', color = line(j)%color)
-    call qp_draw_data (table(:,0), table(:,j), .true., 0)
-  enddo
-  call qp_draw_curve_legend (0.1_rp, 0.1_rp, '%/GRAPH/LT', line(1:n_slave), text = attrib_name(1:n_slave))
+elseif (plot_size(1) > plot_size(2)) then
+  call qp_open_page ('PS-L', id, plot_size(1), plot_size(2), 'POINTS', plot_file = postscript_file)
+else
+  call qp_open_page ('PS', id, plot_size(1), plot_size(2), 'POINTS', plot_file = postscript_file)
 endif
+
+call qp_set_page_border (0.02_rp, 0.02_rp, 0.02_rp, 0.02_rp, '%PAGE')
+call qp_set_margin (0.07_rp, 0.02_rp, 0.05_rp, 0.01_rp, '%PAGE')
+call qp_set_box (1, 1, 1, 1)
+call qp_calc_and_set_axis ('X', var_min, var_max, 4, 8, 'GENERAL')
+call qp_calc_and_set_axis ('Y', minval(table(:,1:)), maxval(table(:,1:)), 4, 8, 'GENERAL')
+call qp_draw_axes (ele%control%var(ix_var)%name, 'Slave Value')
+do j = 1, n_slave
+  call qp_set_line_attrib ('PLOT', color = line(j)%color)
+  call qp_draw_data (table(:,0), table(:,j), .true., 0)
+  if (draw_knot_points) then
+    call qp_set_symbol(symbol(j))
+    call qp_draw_symbols (knots(:,0), knots(:,j))
+  endif
+enddo
+call qp_draw_curve_legend (0.1_rp, -0.1_rp, '%/GRAPH/LT', line(1:n_slave), text = attrib_name(1:n_slave))
+
+if (who == 'X') then
+  write (*, '(a)', advance = 'NO') 'Hit any key to continue.'
+  read (*, '(a)'), ans
+endif
+
+call qp_close_page()
+
+end subroutine
 
 end program
