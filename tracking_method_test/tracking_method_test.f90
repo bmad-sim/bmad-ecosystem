f88d3bb98e4b6179c5ead15647e1fe478c0967d8
David Sagan <dcs16@cornell.edu>
2021-01-14 12:12:52 +0000

    More ramping devel.

--- tracking_method_test/tracking_method_test.f90
+++ tracking_method_test/tracking_method_test.f90
@@ -10,7 +10,7 @@ type (lat_struct), target :: lat
 character(200) :: line(10), line_debug(10)
 character(100) :: lat_file  = 'tracking_method_test.bmad'
 character(46) :: out_str, fmt, track_method
-integer :: i, j, ib, nargs, isn
+integer :: nargs
 
 logical debug_mode
  
@@ -58,31 +58,35 @@ if (debug_mode) then
 endif
 
 call track_it (lat, 1, 1)
-if (.not. debug_mode) call track_it (lat, 1, -1)
+if (debug_mode) stop
+call track_it (lat,  1, -1)
+call track_it (lat, -1,  1)
+call track_it (lat, -1, -1)
 
 close(1)
 
 !------------------------------------------------
 contains
 
-subroutine track_it(lat, d_sign, p_sign)
+subroutine track_it(lat, ele_o_sign, orb_dir_sign)
 
 type (lat_struct), target :: lat
 type (coord_struct) start_orb, end_orb, end_bs, end_ptc
 type (ele_struct), pointer :: ele
 type (branch_struct), pointer :: branch
 type (track_struct) track
-integer d_sign, p_sign
+integer ele_o_sign, orb_dir_sign
+integer ib, i, j, isn
 
 !
 
 do ib = 0, ubound(lat%branch, 1)
   branch => lat%branch(ib)
-  if (branch%param%particle == photon$ .and. p_sign == -1) cycle
+  if (branch%param%particle == photon$ .and. (orb_dir_sign == -1 .or. ele_o_sign == -1)) cycle
 
   do i = 1, branch%n_ele_max - 1
     ele => branch%ele(i)
-    if (p_sign == -1 .and. ele%key == e_gun$) cycle
+    if (ele_o_sign == -1 .and. ele%key == e_gun$) cycle
     ele%spin_tracking_method = tracking$
 
     isn = 0
@@ -93,7 +97,7 @@ do ib = 0, ubound(lat%branch, 1)
       if (j == custom$) cycle
       if (j == mad$) cycle   ! Ignore MAD
       if (j == taylor$ .and. lat%particle_start%direction == -1) cycle
-      if (p_sign == -1 .and. (j == taylor$ .or. j == linear$)) cycle
+      if ((orb_dir_sign == -1 .or. ele_o_sign == -1) .and. (j == taylor$ .or. j == linear$)) cycle
       ele%tracking_method = j
 
       if (ele%key == e_gun$ .and. (j == runge_kutta$ .or. j == fixed_step_runge_kutta$)) cycle
@@ -117,17 +121,17 @@ do ib = 0, ubound(lat%branch, 1)
         ele%tracking_method = j
       endif
 
-      if (ele%key /= sbend$ .and. ele%key /= lcavity$ .and. ele%key /= rfcavity$) ele%orientation = p_sign
+      if (ele%key /= sbend$ .and. ele%key /= lcavity$ .and. ele%key /= rfcavity$) ele%orientation = ele_o_sign
 
       start_orb = lat%particle_start
 
-      if (p_sign == -1) then
+      if (orb_dir_sign == -1) then
         start_orb%direction = -1
-        start_orb%species = antiparticle(start_orb%species)
         lat%absolute_time_tracking = .true.
-      else
-        start_orb%species = default_tracking_species(branch%param)
       endif
+
+      start_orb%species = default_tracking_species(branch%param)
+      if (start_orb%direction*ele%orientation == -1) start_orb%species = antiparticle(start_orb%species)
 
       call init_coord (start_orb, start_orb, ele, start_end$, start_orb%species, start_orb%direction, E_photon = ele%value(p0c$) * 1.006)
 
@@ -140,12 +144,14 @@ do ib = 0, ubound(lat%branch, 1)
         call track1 (start_orb, ele, branch%param, end_orb)
       endif
 
-      if (p_sign == 1) then
-        out_str = trim(ele%name) // ':' // trim(tracking_method_name(j))
-        if (debug_mode) out_str = trim(ele%name) // ': ' // trim(tracking_method_name(j))
-
+      if (orb_dir_sign == 1 .and. ele_o_sign == 1) then
+        out_str = trim(ele%name) // ': ' // trim(tracking_method_name(j))
+      elseif (orb_dir_sign == 1 .and. ele_o_sign == -1) then
+        out_str = trim(ele%name) // '-Anti_O: ' // trim(tracking_method_name(j))
+      elseif (orb_dir_sign == -1 .and. ele_o_sign == 1) then
+        out_str = trim(ele%name) // '-Anti_D: ' // trim(tracking_method_name(j))
       else
-        out_str = trim(ele%name) // '-Anti:' // trim(tracking_method_name(j))
+        out_str = trim(ele%name) // '-Anti_OD: ' // trim(tracking_method_name(j))
       endif
 
       if (ele%key == e_gun$) then
@@ -168,13 +174,13 @@ do ib = 0, ubound(lat%branch, 1)
       if (j == bmad_standard$ .or. j == runge_kutta$ .or. j == symp_lie_ptc$ .or. j == time_runge_kutta$ .or. j == taylor$) then
         isn = isn + 1
         out_str = trim(out_str) // ' dSpin'
-        write (line(isn), '(a, t49, a,  3f14.9, 4x, f14.9)') '"' // trim(out_str) // '"', tolerance_spin(out_str), &
+        write (line(isn), '(a, t50, a,  3f14.9, 4x, f14.9)') '"' // trim(out_str) // '"', tolerance_spin(out_str), &
               end_orb%spin-start_orb%spin, norm2(end_orb%spin) - norm2(start_orb%spin)
         if (debug_mode) write(line_debug(isn), '(a40, 3f14.9, 4x, f14.9)') out_str, end_orb%spin-start_orb%spin, norm2(end_orb%spin) - norm2(start_orb%spin)
       endif
 
       if (branch%param%particle == photon$) then
-        write (1, '(3a, t49, a, 2es18.10)') '"', trim(ele%name), ':E_Field"', 'REL 1E-07', end_orb%field
+        write (1, '(3a, t50, a, 2es18.10)') '"', trim(ele%name), ':E_Field"', 'REL 1E-07', end_orb%field
       endif
     end do
 
@@ -198,66 +204,17 @@ end subroutine track_it
 
 !--------------------------------------------------------------------------------------
 ! contains
-  
+
 character(10) function tolerance(instr)
 character(*) :: instr
 
-  select case (instr)
-    case('CRYSTAL2:Bmad_Standard')                     ; tolerance = 'ABS 1e-13'
-    case('AC_KICKER2:Time_Runge_Kutta')                ; tolerance = 'ABS 1e-11'
-    case('QUADRUPOLE1:Time_Runge_Kutta')               ; tolerance = 'ABS 5e-12'
-    case('QUADRUPOLE2:Time_Runge_Kutta')               ; tolerance = 'ABS 2e-11'
-    case('QUADRUPOLE4:Time_Runge_Kutta')               ; tolerance = 'ABS 2e-11'
-    case('QUADRUPOLE5:Time_Runge_Kutta')               ; tolerance = 'ABS 1e-11'
-    case('RFCAVITY1:Runge_Kutta')                      ; tolerance = 'ABS 1e-13'
-    case('RFCAVITY1:Time_Runge_Kutta')                 ; tolerance = 'ABS 2e-11'
-    case('RFCAVITY2:Runge_Kutta')                      ; tolerance = 'ABS 1e-13'
-    case('RFCAVITY2:Time_Runge_Kutta')                 ; tolerance = 'ABS 2e-11'
-    case('SBEND2:Time_Runge_Kutta')                    ; tolerance = 'ABS 1e-11'
-    case('SBEND4:Bmad_Standard')                       ; tolerance = 'ABS 1e-11'
-    case('SBEND4:Linear')                              ; tolerance = 'ABS 1e-11'
-    case('SBEND4:Taylor')                              ; tolerance = 'ABS 1e-12'
-    case('SBEND4:Time_Runge_Kutta')                    ; tolerance = 'ABS 4e-13'
-    case('SBEND5:Bmad_Standard')                       ; tolerance = 'ABS 5e-13'
-    case('SBEND5:Linear')                              ; tolerance = 'ABS 5e-13'
-    case('SBEND6:Taylor')                              ; tolerance = 'ABS 2e-11'
-    case('SBEND7:Bmad_Standard')                       ; tolerance = 'ABS 2e-13'
-    case('SBEND7:Linear')                              ; tolerance = 'ABS 2e-13'
-    case('SOLENOID1:Time_Runge_Kutta')                 ; tolerance = 'ABS 1e-11'
-    case('SOLENOID2:Symp_Lie_Bmad')                    ; tolerance = 'ABS 2e-14'
-    case('SOL_QUAD1:Time_Runge_Kutta')                 ; tolerance = 'ABS 1e-11'
-    case('SOL_QUAD2:Time_Runge_Kutta')                 ; tolerance = 'ABS 2e-10'
-    case('LCAVITY1:Bmad_Standard')                     ; tolerance = 'ABS 2e-12'
-    case('LCAVITY1:Time_Runge_Kutta')                  ; tolerance = 'ABS 3e-11'
-    case('LCAVITY1:Runge_Kutta')                       ; tolerance = 'ABS 1e-13'
-    case('LCAVITY2:Time_Runge_Kutta')                  ; tolerance = 'ABS 2e-13'
-    case('LCAVITY2:Runge_Kutta')                       ; tolerance = 'ABS 1e-13'
-    case('LCAVITY3:Runge_Kutta')                       ; tolerance = 'ABS 1e-13'
-    case('LCAVITY3:Time_Runge_Kutta')                  ; tolerance = 'ABS 2e-11'
-    case('WIGGLER_MAP1:Time_Runge_Kutta')              ; tolerance = 'ABS 2e-13'
-    case('WIGGLER_MAP1:Runge_Kutta')                   ; tolerance = 'ABS 1e-13'
-    case('WIGGLER_PLANAR1:Bmad_Standard')              ; tolerance = 'ABS 5e-14'
-    case('WIGGLER_PLANAR1:Runge_Kutta')                ; tolerance = 'ABS 1e-12'
-    case('WIGGLER_PLANAR1:Time_Runge_Kutta')           ; tolerance = 'ABS 2e-12'
-    case('WIGGLER_HELICAL1:Runge_Kutta')               ; tolerance = 'ABS 1e-12'
-    case('WIGGLER_HELICAL1:Time_Runge_Kutta')          ; tolerance = 'ABS 2e-12'
+! There can be differences between debug and non-debug output.
 
-    case("OCTUPOLE1-Anti:Runge_Kutta")                 ; tolerance = 'ABS 1e-13'
-    case("LCAVITY3-Anti:Runge_Kutta")                  ; tolerance = 'ABS 2E-13'
-    case("RFCAVITY1-Anti:Runge_Kutta")                 ; tolerance = 'ABS 4E-10'
-    case("RFCAVITY1-Anti:Time_Runge_Kutta")            ; tolerance = 'ABS 2e-10'
-    case("RFCAVITY2-Anti:Runge_Kutta")                 ; tolerance = 'ABS 4E-10'
-    case("RFCAVITY2-Anti:Time_Runge_Kutta")            ; tolerance = 'ABS 2E-10'
-    case("SOL_QUAD1-Anti:Symp_Lie_Bmad")               ; tolerance = 'ABS 1E-13'
-    case("SOL_QUAD1-Anti:Time_Runge_Kutta")            ; tolerance = 'ABS 2e-12'
-    case("SBEND4-Anti:Bmad_Standard")                  ; tolerance = 'ABS 2e-13'
-    case("WIGGLER_MAP1-Anti:Runge_Kutta")              ; tolerance = 'ABS 1e-13'
-    case("WIGGLER_PLANAR1-Anti:Runge_Kutta")           ; tolerance = 'ABS 5e-13'
-    case("WIGGLER_PLANAR1-Anti:Bmad_Standard")         ; tolerance = 'ABS 5e-13'
-    case("WIGGLER_PLANAR1-Anti:Time_Runge_Kutta")      ; tolerance = 'ABS 2e-13'                  
-    case("WIGGLER_PLANAR1-Anti:Symp_Lie_Bmad")         ; tolerance = 'ABS 2e-14'                  
-    case("WIGGLER_HELICAL1-Anti:Runge_Kutta")          ; tolerance = 'ABS 5e-13'
-    case("WIGGLER_HELICAL1-Anti:Time_Runge_Kutta")     ; tolerance = 'ABS 2e-13'                  
+  select case (instr)
+    case("RFCAVITY1-Anti_O: Time_Runge_Kutta")          ; tolerance = 'ABS 4E-10'
+    case("WIGGLER_HELI1-Anti_D: Runge_Kutta")           ; tolerance = 'ABS 4e-13'
+    case("WIGGLER_HELI1-Anti_O: Runge_Kutta")           ; tolerance = 'ABS 4e-13'                  
+    case("WIGGLER_HELI1-Anti_O: Time_Runge_Kutta")      ; tolerance = 'ABS 4e-13'                  
 
     case default 
       if (index(instr, 'Runge_Kutta') /= 0) then
