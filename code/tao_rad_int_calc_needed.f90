ed6a5aa12b0acc07ed2b0f42756dadbb108e1abc
David Sagan <dcs16@cornell.edu>
2021-03-01 00:38:55 +0000

    Minor coding change.

--- code/tao_rad_int_calc_needed.f90
+++ code/tao_rad_int_calc_needed.f90
@@ -10,6 +10,7 @@
 
 function tao_rad_int_calc_needed (data_type, data_source) result (do_rad_int)
 
+use tao_interface, dummy => tao_rad_int_calc_needed
 
 implicit none
 
@@ -32,5 +33,7 @@ if (data_type(1:11) == 'expression:') then
   if (index(data_type, 'emit.') /= 0) do_rad_int = .true.
   if (index(data_type, 'rad_int') /= 0) do_rad_int = .true.
 endif
+
+if (tao_beam_sigma_calc_needed(data_type, data_source)) do_rad_int = .true.
 
 end function tao_rad_int_calc_needed
