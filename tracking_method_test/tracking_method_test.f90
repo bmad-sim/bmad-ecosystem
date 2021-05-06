0b4e91db77bf4666ecefc0c1b9a1b8d0dce4c4d3
David Sagan <dcs16@cornell.edu>
2021-05-06 22:42:33 +0000

    more...

--- tracking_method_test/tracking_method_test.f90
+++ tracking_method_test/tracking_method_test.f90
@@ -215,9 +215,12 @@ character(*) :: instr
 
   select case (instr)
     case("RFCAVITY1: Time_Runge_Kutta")                 ; tolerance = 'ABS 2E-12'
+    case("WIGGLER_FLAT1: Runge_Kutta")                  ; tolerance = 'ABS 2E-13'
+    case("WIGGLER_FLAT1: Time-Runge_Kutta")             ; tolerance = 'ABS 2E-13'
     case("RFCAVITY1-Anti_D: Time_Runge_Kutta")          ; tolerance = 'ABS 1E-12'
     case("RFCAVITY1-Anti_O: Time_Runge_Kutta")          ; tolerance = 'ABS 4E-10'
     case("RFCAVITY1-Anti_OD: Time_Runge_Kutta")         ; tolerance = 'ABS 1E-12'
+    case("WIGGLER_FLAT1-Anti_OD: Runge_Kutta")          ; tolerance = 'ABS 2E-13'
     case("WIGGLER_HELI1-Anti_D: Runge_Kutta")           ; tolerance = 'ABS 4e-13'
     case("WIGGLER_HELI1-Anti_O: Runge_Kutta")           ; tolerance = 'ABS 4e-13'                  
     case("WIGGLER_HELI1-Anti_O: Time_Runge_Kutta")      ; tolerance = 'ABS 4e-13'                  
