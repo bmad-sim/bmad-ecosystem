bf6b41375c51fcd8d87bbe20acb670071ce3cdb2
David Sagan <dcs16@cornell.edu>
2021-05-19 17:08:26 +0000

    Added to attribute ignore list.

--- mad_to_bmad/mad8_to_bmad.py
+++ mad_to_bmad/mad8_to_bmad.py
@@ -131,7 +131,7 @@ ele_type_translate = {
 }
 
 ignore_mad8_param = ['lrad', 'slot_id', 'aper_tol', 'apertype', 'assembly_id', 
-                     'mech_sep', 'betrf', 'tfill', 'shunt', 'pg', 'eloss']
+                     'mech_sep', 'betrf', 'tfill', 'shunt', 'pg', 'eloss', 'lfile', 'tfile']
 
 bmad_param_name = {
     'deltae':   'voltage',
