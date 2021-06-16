45d45e7d19361d09a778be64681a410cfb606cf7
David Sagan <dcs16@cornell.edu>
2021-06-16 21:13:17 +0000

    Added nametable init to init_lat.

--- low_level/init_lat.f90
+++ low_level/init_lat.f90
@@ -1,27 +1,27 @@
-!----------------------------------------------------------------------------
-!----------------------------------------------------------------------------
-!----------------------------------------------------------------------------
 !+
-! Subroutine init_lat (lat, n)
+! Subroutine init_lat (lat, n, init_beginning_ele)
 !
-! Subroutine to initialize a Bmad lat.
+! Subroutine to initialize a Bmad lat_struct.
 ! 
 ! Input:
-!   n    -- Integer, optional: Upper bound lat%ele(0:) array is initialized to.
+!   n                  -- integer, optional: Upper bound lat%ele(0:) array is initialized to.
+!   init_beginning_ele -- logical, optional: Init lat%ele(0)? Default is False.
 !
 ! Output:
 !   lat -- lat_struct: Initialized lat.
 !-
 
-subroutine init_lat (lat, n)
+subroutine init_lat (lat, n, init_beginning_ele)
 
 use equal_mod, dummy => init_lat
 
 implicit none
 
-type (lat_struct)  lat
+type (lat_struct), target :: lat
+type (ele_struct), pointer :: ele
 
 integer, optional :: n
+logical, optional :: init_beginning_ele
 
 !
 
@@ -52,7 +52,15 @@ lat%n_ic_max = 0
 lat%input_taylor_order = 0
 lat%version = -1
 lat%absolute_time_tracking   = bmad_com%absolute_time_tracking_default
-lat%nametable%n_max = -1
+call nametable_init(lat%nametable, 0)
+
+if (logic_option(.false., init_beginning_ele)) then
+  ele => lat%ele(0)
+  call init_ele(ele, beginning_ele$, 0, 0, lat%branch(0))
+  ele%name = 'BEGINNING'
+  call set_ele_defaults (ele)   ! Defaults for beginning_ele element
+  call nametable_add (lat%nametable, ele%name, 0)
+endif
 
 !----------------------------------------
 contains
