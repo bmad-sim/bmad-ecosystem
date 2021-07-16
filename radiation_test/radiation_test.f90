1b326f3e9d6385846fc6dfc306c28a00486ea714
David Sagan <dcs16@cornell.edu>
2021-07-16 02:35:28 +0000

    More...

--- radiation_test/radiation_test.f90
+++ radiation_test/radiation_test.f90
@@ -5,14 +5,63 @@ use bmad
 implicit none
 
 type (lat_struct), target :: lat
+type (branch_struct), pointer :: branch
 type (ele_struct), pointer :: ele
 type (coord_struct) orb_start, orb_end
+type (coord_struct), allocatable :: orbit(:)
+type (normal_modes_struct) mode
+type (rad_int_all_ele_struct), target :: ri_cache, ri_no_cache
+type (rad_int1_struct), pointer :: rie1(:), rie2(:), rie3(:), rie4(:)
+
+real(rp) vec6(6)
+integer i, j, n, ib, ix_cache
 
 !
 
 open (1, file = 'output.now', recl = 200)
 
+!
+
 call bmad_parser('radiation_test.bmad', lat)
+
+branch => lat%branch(1)
+call reallocate_coord(orbit, branch%n_ele_max)
+vec6 = 0
+do i = 0, branch%n_ele_max
+  call init_coord(orbit(i), vec6, branch%ele(i), upstream_end$)
+enddo
+
+call twiss_propagate_all(lat, 1)
+call twiss_propagate_all(lat, 2)
+
+ix_cache = 0
+call radiation_integrals (lat, orbit, mode, ix_cache, 1, ri_cache)
+call radiation_integrals (lat, orbit, mode, -2, 1, ri_no_cache)
+
+ix_cache = 0
+call radiation_integrals (lat, orbit, mode, ix_cache, 2, ri_cache)
+call radiation_integrals (lat, orbit, mode, -2, 2, ri_no_cache)
+
+rie1 => ri_cache%branch(1)%ele
+rie2 => ri_no_cache%branch(1)%ele
+rie3 => ri_cache%branch(2)%ele
+rie4 => ri_no_cache%branch(2)%ele
+
+do i = 1, branch%n_ele_max
+  write (1, '(a, 4es14.6)') '"i0-' // int_str(i) // '" REL 1E-6', rie1(i)%i0, rie2(i)%i0, rie3(i)%i0, rie4(i)%i0
+  write (1, '(a, 4es14.6)') '"i1-' // int_str(i) // '" REL 1E-6', rie1(i)%i1, rie2(i)%i1, rie3(i)%i1, rie4(i)%i1
+  write (1, '(a, 4es14.6)') '"i2-' // int_str(i) // '" REL 1E-6', rie1(i)%i2, rie2(i)%i2, rie3(i)%i2, rie4(i)%i2
+  write (1, '(a, 4es14.6)') '"i3-' // int_str(i) // '" REL 1E-6', rie1(i)%i3, rie2(i)%i3, rie3(i)%i3, rie4(i)%i3
+  write (1, '(a, 4es14.6)') '"i4a-' // int_str(i) // '" REL 1E-6', rie1(i)%i4a, rie2(i)%i4a, rie3(i)%i4a, rie4(i)%i4a
+  write (1, '(a, 4es14.6)') '"i4b-' // int_str(i) // '" REL 1E-6', rie1(i)%i4b, rie2(i)%i4b, rie3(i)%i4b, rie4(i)%i4b
+  write (1, '(a, 4es14.6)') '"i5a-' // int_str(i) // '" REL 1E-6', rie1(i)%i5a, rie2(i)%i5a, rie3(i)%i5a, rie4(i)%i5a
+  write (1, '(a, 4es14.6)') '"i5b-' // int_str(i) // '" REL 1E-6', rie1(i)%i5b, rie2(i)%i5b, rie3(i)%i5b, rie4(i)%i5b
+  write (1, *)
+enddo
+
+
+!
+
 ele => lat%ele(1)
 
 call init_coord (orb_start, lat%particle_start, ele, upstream_end$)
