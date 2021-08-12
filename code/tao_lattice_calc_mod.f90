7add1b6063c5c61774fc2a0f7d38edfcb19406e6
David Sagan <dcs16@cornell.edu>
2021-08-12 04:05:51 +0000

    Work around for gfortran bug.

--- code/tao_lattice_calc_mod.f90
+++ code/tao_lattice_calc_mod.f90
@@ -677,11 +677,11 @@ do
   if (err .and. print_err) then
     sig = bunch_params%sigma
     if (any(sig /= 0)) then
-      call out_io (s_error$, r_name, [character(80):: &
+      call out_io (s_error$, r_name, &
             'Beam parameters not computed at: ' // trim(ele%name) // '  ' // ele_loc_name(ele, parens = '()') , &
             'Singular sigma matrix is:', &
             '  \6es15.7\', '  \6es15.7\', '  \6es15.7\', '  \6es15.7\', '  \6es15.7\', '  \6es15.7\', &
-            'Will not print any more singular sigma matrices for this track...'], &
+            'Will not print any more singular sigma matrices for this track...', &
             r_array = [sig(1,:), sig(2,:), sig(3,:), sig(4,:), sig(5,:), sig(6,:)])
       print_err = .false.  
     endif
