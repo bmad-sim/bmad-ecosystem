f1e6c135e39fcfbbee6e194d4565c036ebd05eb2
David Sagan <dcs16@cornell.edu>
2021-09-03 02:08:48 +0000

    Removed unused routines gaussian_dist, histogram, and writefile.

--- code/bsim_interface.f90
+++ code/bsim_interface.f90
@@ -1,38 +1,6 @@
 module bsim_interface
   
   interface
-     subroutine writefile(in_file, parameters)
-       use precision_def
-       implicit none
-       real(rp), dimension(1:,1:), intent(in) ::  parameters
-       character(*), intent(in) ::  in_file
-     end subroutine writefile
-  end interface
-  
-  interface
-     subroutine gaussian_dist (ele, mode, coupling, min_sig, coord)
-       use bmad_struct, only: normal_modes_struct, coord_struct, ele_struct, rp
-       implicit none
-       type (normal_modes_struct) mode
-       type (coord_struct), allocatable :: coord(:)
-       type (ele_struct) ele
-       real(rp) min_sig
-       real(rp) coupling
-     end subroutine gaussian_dist
-  end interface
-  
-  interface
-     subroutine histogram (ele, coord, in_file, sig,a_out)
-       use bmad_struct, only: coord_struct, ele_struct, rp
-       implicit none
-       type (coord_struct) coord(:)
-       type (ele_struct) ele
-       real(rp) sig(3), a_out(3)
-       character(*) in_file
-     end subroutine histogram
-  end interface
-
-  interface
      subroutine set_tune3(lat, target_tunes, everything_ok, verbose_in, write_out_in, regex_in)
        use bmad_struct, only: lat_struct, rp
        implicit none
