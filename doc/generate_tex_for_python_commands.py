30bed0065e21d692e2e79fee3948256ced7b5865
David Sagan <dcs16@cornell.edu>
2021-10-09 04:59:36 +0000

    Reformatting python subcommand in manual.

--- doc/generate_tex_for_python_commands.py
+++ doc/generate_tex_for_python_commands.py
@@ -7,11 +7,9 @@
 # 
 # It extracts a formatted comment above each case statement
 
-# In[8]:
-
-
 import json
 import os
+import re
 from string import Formatter
 
 #F90_FILE = os.path.join(os.environ['ACC_ROOT_DIR'], 'tao/code/tao_python_cmd.f90')
@@ -27,90 +25,100 @@ LINES = open(F90_FILE).readlines()
 
 # In[9]:
 
-
 def get_params(command_str):
     return [fn for _, fn, _, _ in Formatter().parse(command_str) if fn is not None]
-
-
-# In[10]:
-
 
 # Collect commands with the header like:
 #      !%% beam ----------------
 command = None
 inside = False
-doclines = {}
+jsonlines = {}
+texdoc = []
+where = ''
+
 for i, line in enumerate(LINES):
     if line.startswith('!%%'):   
         command = line.split()[1]
         inside = True
-        doclines[command] = {'description':''}
+        jsonlines[command] = {'description':''}
+        texdoc.append({'command':command, 'overview':'', 'notes':''})
+        thisdoc = texdoc[-1]
+        where = 'overview'
+
     elif inside and line.startswith('!'):
         line = line[1:]
         if line[0]==' ':
             line = line[1:]
         # Extract command syntax from the next line
         if 'Command syntax' in line:
+            where = 'syntax'
             cmd_str = LINES[i+1].strip('!').strip()
+            thisdoc['command_str'] = LINES[i+1][1:]
             j = 2
             # Allow continued line
             while cmd_str.endswith('^^'):
                 cmd_str+= LINES[i+j].strip('!').strip()
+                thisdoc['command_str'] += LINES[i+j][1:]
                 j+=1 
-            doclines[command]['command_str'] = cmd_str
-            doclines[command]['parameters'] = get_params(cmd_str)
-        doclines[command]['description']+=line
+            jsonlines[command]['command_str'] = cmd_str
+            jsonlines[command]['parameters'] = get_params(cmd_str)
+        #
+        if where == 'syntax' and j == 0: where = 'post-syntax'
+        if where == 'syntax': j -= 1
+        if line.strip() == 'Notes': where = 'notes'
+        if line.strip() == 'Parameters': where = 'parameters'
+        if line.strip() == 'Returns': where = 'other'
+        #
+        jsonlines[command]['description']+=line
+        if where == 'overview': thisdoc['overview'] += line
+        if where == 'post-syntax': thisdoc['notes'] += line
+
     else:
         inside = False
-doclines['datum_create']['command_str']
 
+# Write to LaTeX file
 
-# In[11]:
-
-
-def tex_from_lines(lines, label):
-    text = '\\subsection{python '+label+'}\n'
-    text += '\\begin{example}\n'
-    text +=  lines
-    text += '\n\\end{example}\n'
-    return text
-#print(tex_from_lines(doclines['beam'], 'X'))
-
-
-# In[12]:
-
-
-# Individual command 
-def tex_from_lines(lines, label):
-    text = '\\item['+label+'] \\Newline'
-    text += '\\begin{example}\n'
-    text += lines
-    text += '\n\\end{example}\n'
-    return text
-#print(tex_from_lines(doclines['beam'], 'X'))
-
-
-# In[13]:
-
-
-# Write to file
-
-
+re_pe = re.compile(r'((^| )\S+%\S+( |\n|$))')
 
 with open(TEXFILE, 'w') as f:
     f.write('% WARNING: this is automatically generated. DO NOT EDIT.\n')
-    f.write('\\begin{description}\n')
-    for k, lines in doclines.items():
-        tex = tex_from_lines(lines['description'], k)
-        f.write(tex)
-    f.write('\\end{description}\n')  
+    for doc in texdoc:
+        cmd = doc["command"]
+        cmd2 = cmd.replace('_', '.')
+        f.write(f'''
+%% python {cmd} ------------------------------------
+\\subsection{{python {cmd}}}
+\\index{{python!{cmd}}}
+\\label{{p:{cmd2}}}
+
+''')
+        f.write(doc['overview'].replace('&', '\\&'))
+        f.write('\\begin{example}\n')
+        f.write(doc['command_str'])
+        f.write('\\end{example}\n')
+        f.write('\\begin{verbatim}\n')
+        noff = 0
+        #notes = doc['notes'].replace('{','\\{').replace('}','\}').replace('^', '\\^{}') \
+        #                                        .replace('&', '\\&').replace('#', '\\#').replace('$', '\\$')
+        notes = doc['notes']
+        for match in re.finditer(re_pe, notes):
+          m = match.group()
+          ix0 = 0
+          ix1 = len(m)
+          if m[0] == ' ': ix0 = 1
+          if m[-1] == ' ' or m[-1] == '\n': ix1 -= 1
+          iz0 = match.start() + noff
+          iz1 = match.end() + noff
+          notes = notes[:iz0] + m[:ix0] + '\\vn{' + m[ix0:ix1] + '}' + m[ix1:] + notes[iz1:]
+          noff += 5  # Number of characters added
+        #
+        f.write(notes)
+        f.write('\\end{verbatim}\n')
 
 print('Written:', TEXFILE)
 
+# Write to json file
 
-# In[14]:
-
-
-json.dump(doclines, open(JSONFILE, 'w'))
+json.dump(jsonlines, open(JSONFILE, 'w'))
 print('Written:', JSONFILE)
 
