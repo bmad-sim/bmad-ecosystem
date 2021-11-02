7b32cbe05daf57a72be7a463324cf8049efa7737
David Sagan <dcs16@cornell.edu>
2021-11-02 03:19:02 +0000

    New nonlin_test.

---
+++ nonlin_test/nonlin_test.f90
@@ -0,0 +1,28 @@
+program nonlin_test
+
+use bmad
+
+implicit none
+
+type (lat_struct) lat
+type (coord_struct), allocatable :: orb(:)
+
+real(rp) ch_x, ch_y
+integer status
+logical err
+
+!
+
+open (1, file = 'output.now', recl = 200)
+
+call bmad_parser('small_ring.bmad', lat)
+call twiss_and_track (lat, orb, status)
+
+call chrom_tune (lat, 1.0e-4_rp, 1.0_rp, 2.0_rp, 0.05_rp, err)
+call chrom_calc (lat, 1.0e-4_rp, ch_x, ch_y, err)
+
+write (1, '(a, 2f14.8)') '"Chrom" ABS 1e-7', ch_x, ch_y
+
+close(1)
+
+end program
