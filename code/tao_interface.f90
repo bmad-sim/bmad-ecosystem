18a154b478169bdd7382ab9c36746b970cb061bd
David Sagan <dcs16@cornell.edu>
2021-11-02 17:26:43 +0000

    Fix tao_hook_curve_s_pt interface.

--- code/tao_interface.f90
+++ code/tao_interface.f90
@@ -359,13 +359,13 @@ subroutine tao_hook_command (command_line, found)
   logical found
 end subroutine
  
-function tao_hook_curve_s_pt (s_default, ix_now, curve, tao_lat) result (s_pt)
+function tao_hook_curve_s_pt (s_default, ix_now, x1, x2, n_pts, tao_lat, curve) result (s_pt)
   import
   implicit none
   type (tao_curve_struct) curve
   type (tao_lattice_struct) tao_lat
-  real(rp) s_default, s_pt
-  integer ix_now
+  real(rp) s_default, x1, x2, s_pt
+  integer ix_now, n_pts
 end function
  
 subroutine tao_hook_draw_floor_plan (plot, graph)
