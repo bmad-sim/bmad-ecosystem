6d23d1eea0fc34a0b0b1af3b2a35184f990108ca
David Sagan <dcs16@cornell.edu>
2021-11-15 14:04:55 +0000

    Coppy pdf to bmad-doc.

--- doc/2www.pl
+++ doc/2www.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
 
 `pdflatex lux`; 
-`cp lux.pdf /home/dcs16/public_html/bmad/manuals`;
+`cp lux.pdf /home/dcs16/public_html/bmad/manuals/`;
+`cp lux.pdf ../../bmad-doc/other_manuals/`;
