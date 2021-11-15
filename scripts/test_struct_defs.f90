78744b08edd98ab711aaaef4fb7d2cb1e28cc5f6
David Sagan <dcs16@cornell.edu>
2021-11-15 14:05:51 +0000

    minor devel.

---
+++ scripts/test_struct_defs.f90
@@ -0,0 +1,11 @@
+type wake_lr_struct
+  character(200) :: file = ''
+  real(rp) :: t_ref = 0             ! time reference value for computing the wake amplitude.
+                                    !  This is used to prevent value overflow with long trains.
+  real(rp) :: freq_spread = 0       ! Random frequency spread of long range modes.
+end type
+
+type wake_struct
+  type (wake_lr_struct) :: sr = wake_sr_struct('', null(), null(), 0.0_rp, 0.0_rp, 0.0_rp, 1.0_rp, 1.0_rp, .true.) ! Short-range wake
+end type
+
