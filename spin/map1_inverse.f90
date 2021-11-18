ea742db6af85ed55d315079aa866e46223a540a8
David Sagan <dcs16@cornell.edu>
2021-11-18 06:15:49 +0000

    Fix map1_inverse algorithm.

--- spin/map1_inverse.f90
+++ spin/map1_inverse.f90
@@ -17,13 +17,21 @@ implicit none
 
 type (spin_orbit_map1_struct) map1, inv_map1
 
+real(rp) mq(0:3,6)
+integer i
+
 !
 
 call mat_inverse(map1%orb_mat, inv_map1%orb_mat)
 inv_map1%vec0 = -matmul(inv_map1%orb_mat, map1%vec0)
 
-inv_map1%spin_q(0,:)  =  map1%spin_q(0,:)
-inv_map1%spin_q(1:,:) = -map1%spin_q(1:,:)
+inv_map1%spin_q(0,0)   =  map1%spin_q(0,0)
+inv_map1%spin_q(1:3,0) = -map1%spin_q(1:3,0)
+
+mq = matmul(map1%spin_q(:,1:6), inv_map1%orb_mat)
+do i = 1, 6
+  inv_map1%spin_q(:,i) = -quat_mul(quat_mul(inv_map1%spin_q(:,0), mq(:,i)), inv_map1%spin_q(:,0))
+enddo
 
 end function
 
