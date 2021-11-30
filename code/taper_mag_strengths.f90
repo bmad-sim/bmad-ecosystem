a6827e00972e566c77f068ba0a008ab97b454075
David Sagan <dcs16@cornell.edu>
2021-11-30 14:29:06 +0000

    Added err_flag arg to taper_map_strengths.

--- code/taper_mag_strengths.f90
+++ code/taper_mag_strengths.f90
@@ -1,5 +1,5 @@
 !+
-! Subroutine taper_mag_strengths (lat, ref_lat, except)
+! Subroutine taper_mag_strengths (lat, ref_lat, except, err_flag)
 !
 ! Routine to "correct" magnet strengths in a ring to counteract local radiation damping energy offsets.
 !
@@ -18,9 +18,10 @@
 !
 ! Output:
 !   lat         -- lat_struct: Lattice with magnet strengths varied.
+!   err         -- logical, optional: Set True if there is an error.
 !-
 
-subroutine taper_mag_strengths (lat, ref_lat, except)
+subroutine taper_mag_strengths (lat, ref_lat, except, err_flag)
 
 use bmad, dummy => taper_mag_strengths
 
@@ -34,13 +35,16 @@ type (coord_struct), allocatable :: closed_orb(:)
 type (ele_pointer_struct), allocatable :: eles(:)
 
 real(rp) tol, max_change, pz_ave, weight
-integer k, ib, ie, n_loc
-logical err_flag
+integer k_loop, ib, ie, n_loc
+logical, optional :: err_flag
+logical err
 
 character(*), optional :: except
 character(*), parameter :: r_name = 'taper_mag_strengths'
 
 !
+
+if (present(err_flag)) err_flag = .false.
 
 do ib = 0, ubound(lat%branch,1)
   lat%branch(ib)%ele%select = .true.
@@ -66,11 +70,12 @@ branch_loop: do ib = 0, ubound(lat%branch, 1)
   branch0 => lat0%branch(ib)
   if (branch%param%geometry == open$) cycle
 
-  do k = 1, 10
-    call closed_orbit_calc (lat, closed_orb, 6, +1, ib, err_flag)
-    if (err_flag) then
-      call out_io (s_error$, r_name, 'CLOSED ORBIT CALC FAILED ON LOOP: ' // int_str(k))
-      return
+  do k_loop = 1, 10
+    call closed_orbit_calc (lat, closed_orb, 6, +1, ib, err)
+    if (err) then
+      call out_io (s_error$, r_name, 'CLOSED ORBIT CALC FAILED ON LOOP: ' // int_str(k_loop))
+      if (present(err_flag)) err_flag = .true.
+      cycle branch_loop
     endif
 
     pz_ave = 0
@@ -89,10 +94,11 @@ branch_loop: do ib = 0, ubound(lat%branch, 1)
     enddo
 
     call lattice_bookkeeper(lat)
-    if (max_change < tol .and. k > 1) cycle branch_loop
+    if (max_change < tol .and. k_loop > 1) cycle branch_loop
   enddo
 
   call out_io (s_warn$, r_name, 'CALCULATION NOT CONVERGING. MAX CHANGE ON LAST LOOP IS: ' // real_str(max_change, 3))
+  if (present(err_flag)) err_flag = .true.
 enddo branch_loop
 
 !-----------------------------------------------
@@ -127,19 +133,19 @@ if (.not. ele%select) return
 if (ele%slave_status == multipass_slave$) ele => pointer_to_lord(ele, 1)
 ele0 => pointer_to_ele(lat0, ele)
 
-if (has_attribute(ele, 'DG'))    change = max(change, taper_this_attrib(ele, ele%value(dg$), ele0%value(dg$), orb, pz_ave, ele%value(g$)))
-if (has_attribute(ele, 'K1'))    change = max(change, taper_this_attrib(ele, ele%value(k1$), ele0%value(k1$), orb, pz_ave))
-if (has_attribute(ele, 'K2'))    change = max(change, taper_this_attrib(ele, ele%value(k2$), ele0%value(k2$), orb, pz_ave))
-if (has_attribute(ele, 'K3'))    change = max(change, taper_this_attrib(ele, ele%value(k3$), ele0%value(k3$), orb, pz_ave))
-if (has_attribute(ele, 'HKICK')) change = max(change, taper_this_attrib(ele, ele%value(hkick$), ele0%value(hkick$), orb, pz_ave))
-if (has_attribute(ele, 'VKICK')) change = max(change, taper_this_attrib(ele, ele%value(vkick$), ele0%value(vkick$), orb, pz_ave))
-if (has_attribute(ele, 'KICK'))  change = max(change, taper_this_attrib(ele, ele%value(kick$), ele0%value(kick$), orb, pz_ave))
-if (has_attribute(ele, 'KS'))    change = max(change, taper_this_attrib(ele, ele%value(ks$), ele0%value(ks$), orb, pz_ave))
+if (has_attribute(ele, 'DG'))    change = max(change, taper_this_attrib(ele, 'DG', ele%value(dg$), ele0%value(dg$), orb, pz_ave, ele%value(g$)))
+if (has_attribute(ele, 'K1'))    change = max(change, taper_this_attrib(ele, 'K1', ele%value(k1$), ele0%value(k1$), orb, pz_ave))
+if (has_attribute(ele, 'K2'))    change = max(change, taper_this_attrib(ele, 'K2', ele%value(k2$), ele0%value(k2$), orb, pz_ave))
+if (has_attribute(ele, 'K3'))    change = max(change, taper_this_attrib(ele, 'K3', ele%value(k3$), ele0%value(k3$), orb, pz_ave))
+if (has_attribute(ele, 'HKICK')) change = max(change, taper_this_attrib(ele, 'HKICK', ele%value(hkick$), ele0%value(hkick$), orb, pz_ave))
+if (has_attribute(ele, 'VKICK')) change = max(change, taper_this_attrib(ele, 'VKICK', ele%value(vkick$), ele0%value(vkick$), orb, pz_ave))
+if (has_attribute(ele, 'KICK'))  change = max(change, taper_this_attrib(ele, 'KICK', ele%value(kick$), ele0%value(kick$), orb, pz_ave))
+if (has_attribute(ele, 'KS'))    change = max(change, taper_this_attrib(ele, 'KS', ele%value(ks$), ele0%value(ks$), orb, pz_ave))
 
 if (associated(ele%a_pole)) then
   do ip = 0, ubound(ele%a_pole, 1)
-    change = max(change, taper_this_attrib(ele, ele%a_pole(ip), ele0%a_pole(ip), orb, pz_ave))
-    change = max(change, taper_this_attrib(ele, ele%b_pole(ip), ele0%b_pole(ip), orb, pz_ave))
+    change = max(change, taper_this_attrib(ele, 'A' // int_str(ip), ele%a_pole(ip), ele0%a_pole(ip), orb, pz_ave))
+    change = max(change, taper_this_attrib(ele, 'B' // int_str(ip), ele%b_pole(ip), ele0%b_pole(ip), orb, pz_ave))
   enddo
 endif
 
@@ -148,18 +154,26 @@ end function taper_this_ele
 !-----------------------------------------------
 ! contains
 
-function taper_this_attrib(ele, attrib, attrib0, orb, pz_ave, g0) result (change)
+function taper_this_attrib(ele, attrib_name, attrib, attrib0, orb, pz_ave, g0) result (change)
 
 type (ele_struct) :: ele
 type (coord_struct) orb
 real(rp), optional :: g0
 real(rp) attrib, attrib0, pz_ave, change, base
 real(rp) f, a_new
+character(*) attrib_name
 
 !
 
 change = 0
 base = real_option(0.0_rp, g0)
+
+if (.not. attribute_free (ele, attrib_name, .false., dependent_attribs_free = .true.)) then
+  if (k_loop == 1) call out_io (s_warn$, r_name, 'This attribute controlled by an Overlay: ' // trim(ele%name) // &
+                                                     '[' // attrib_name // ']  ' // ele_loc_name(ele, parens = '()'), &
+                                                 'No tapering applied for this attribute.')
+  return
+endif
 
 if (attrib0 + base == 0) return
 a_new = (attrib0 + base) * (1 + orb%vec(6)) / (1 + pz_ave) - base
