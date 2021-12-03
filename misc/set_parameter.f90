12f56a6b6f131151ed8a5a85118e3d3cb94a211e
David Sagan <dcs16@cornell.edu>
2021-12-03 03:39:10 +0000

    Expanded set_parameter to include integer and logical sets.

--- misc/set_parameter.f90
+++ misc/set_parameter.f90
@@ -3,16 +3,21 @@
 !
 ! Routine to simultaneously set a parameter and return the old parameter value.
 !
+! This is the overloaded name for:
+!   Function set_parameter_real  (param_val, set_val) result (save_val)
+!   Function set_parameter_int   (param_val, set_val) result (save_val)
+!   Function set_parameter_logic (param_val, set_val) result (save_val)
+!
 ! Input:
-!   set_val     -- real(rp): Value to set to.
-!   param_val   -- real(rp): Parameter with some current value to save.
+!   set_val     -- real(rp)/integer/logical: Value to set to.
+!   param_val   -- real(rp)/integer/logical: Parameter with some current value to save.
 !
 ! Output:
-!   param_val   -- real(rp): Parameter with value set to set_val.
-!   save_val    -- real(rp): Old value of param_val.
+!   param_val   -- real(rp)/integer/logical: Parameter with value set to set_val.
+!   save_val    -- real(rp)/integer/logical: Old value of param_val.
 !-
 
-function set_parameter (param_val, set_val) result (save_val)
+function set_parameter_real (param_val, set_val) result (save_val)
 
 use precision_def
 implicit none
@@ -21,4 +26,30 @@ real(rp) param_val, set_val, save_val
 save_val = param_val
 param_val = set_val
 
-end function set_parameter
+end function set_parameter_real
+
+!--------------------------------------------------------------------------------
+
+function set_parameter_int (param_val, set_val) result (save_val)
+
+use precision_def
+implicit none
+integer param_val, set_val, save_val
+
+save_val = param_val
+param_val = set_val
+
+end function set_parameter_int
+
+!--------------------------------------------------------------------------------
+
+function set_parameter_logic (param_val, set_val) result (save_val)
+
+use precision_def
+implicit none
+logical param_val, set_val, save_val
+
+save_val = param_val
+param_val = set_val
+
+end function set_parameter_logic
