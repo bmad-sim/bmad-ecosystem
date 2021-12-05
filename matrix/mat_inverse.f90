05c044ce8c403ba08f7a5898f4f6e277a8f2454b
David Sagan <dcs16@cornell.edu>
2021-12-05 19:25:21 +0000

    More NR removal.

--- matrix/mat_inverse.f90
+++ matrix/mat_inverse.f90
@@ -1,58 +1,46 @@
 !+
 ! Subroutine mat_inverse (mat, mat_inv, ok, print_error)
 !
-! Takes the inverse of a square matrix using LU Decomposition from
-! Numerical Recipes.
+! Takes the inverse of a square matrix using LU Decomposition.
 !
 ! Input:
-!   mat(:,:)     -- Real(rp): Input matrix array
-!   print_err    -- Logical, optional: If True then the subroutine will type out
+!   mat(:,:)     -- real(rp): Input matrix array
+!   print_err    -- logical, optional: If True then the subroutine will type out
 !                         a warning message. Default is False.
 ! Output:
-!   mat_inv(:,:) -- Real(rp): inverse of mat1
-!   ok           -- Logical, optional: Set False for a singular input matrix.
+!   mat_inv(:,:) -- real(rp): inverse of mat1
+!   ok           -- logical, optional: Set False for a singular input matrix.
 !-
 
 subroutine mat_inverse (mat, mat_inv, ok, print_err)
 
 use output_mod, except => mat_inverse
-use nr, only: ludcmp, lubksb
+use f95_lapack, only: la_getrf, la_getri
 
 implicit none
 
-real(rp) :: mat(:,:)
-real(rp) :: mat_inv(:,:)
+real(rp) :: mat(:,:), mat_inv(:,:)
+real(rp) rcond
 
-real(rp) :: mat2(size(mat, 1), size(mat, 1)), vec(size(mat, 1))
 integer :: indx(size(mat, 1))
-real(rp) d
 
-integer n, i
+integer info1, info2
 logical, optional :: ok, print_err
-character(16) :: r_name = 'mat_inverse'
+character(*), parameter :: r_name = 'mat_inverse'
 
 !
 
-n = size (mat, 1)
+mat_inv = mat
 
-mat2 = mat  ! use temp mat so as to not change mat
+call la_getrf (mat_inv, indx, rcond, info = info1)
+call la_getri (mat_inv, indx, info2)
 
-vec(1:n) = maxval(abs(mat), dim = 2)
-if (any(vec(1:n) == 0)) then
+if (info1 /= 0 .or. info2 /= 0) then
   if (logic_option(.false., print_err)) call out_io (s_error$, r_name, 'SINGULAR MATRIX.')
   if (present(ok)) ok = .false.
-  return
+else
+  if (present(ok)) ok = .true.
+  if (rcond < 1e-13 .and. logic_option(.false., print_err)) call out_io (s_warn$, r_name, 'NEARLY SINGULAR MATRIX.')
 endif
-if (present(ok)) ok = .true.
-
-call ludcmp (mat2, indx, d)
-
-mat_inv(1:n,1:n) = 0
-forall (i = 1:n) mat_inv(i,i) = 1
-
-do i = 1, n
-  call lubksb (mat2, indx, mat_inv(1:n,i))
-enddo
 
 end subroutine
-
