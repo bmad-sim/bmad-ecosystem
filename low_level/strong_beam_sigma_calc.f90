78146d929a6c0c0661c93edc5916d2404c438117
David Sagan <dcs16@cornell.edu>
2022-05-03 18:17:23 +0000

    Better alpha calc.

--- low_level/strong_beam_sigma_calc.f90
+++ low_level/strong_beam_sigma_calc.f90
@@ -21,6 +21,7 @@ use bmad_routine_interface, dummy => strong_beam_sigma_calc
 implicit none
 
 type (ele_struct) ele
+type (ele_struct), pointer :: ele0
 
 real(rp) s_pos, z_strong, bbi_const, x_center, y_center, sig_x, sig_y
 real(rp) beta, sig_x0, sig_y0, beta_a0, beta_b0, alpha_a0, alpha_b0, r
@@ -31,16 +32,18 @@ sig_x0 = ele%value(sig_x$)
 sig_y0 = ele%value(sig_y$)
 
 if (ele%value(beta_a_strong$) == 0) then
+  ele0 => pointer_to_next_ele(ele, -1)
   beta_a0 = ele%a%beta
-  alpha_a0 = ele%a%alpha
+  alpha_a0 = 0.5_rp * (ele0%a%alpha + ele%a%alpha)
 else
   beta_a0 = ele%value(beta_a_strong$)
   alpha_a0 = ele%value(alpha_a_strong$)
 endif
 
 if (ele%value(beta_b_strong$) == 0) then
+  ele0 => pointer_to_next_ele(ele, -1)
   beta_b0 = ele%b%beta
-  alpha_b0 = ele%b%alpha
+  alpha_b0 = 0.5_rp * (ele0%b%alpha + ele%b%alpha)
 else
   beta_b0 = ele%value(beta_b_strong$)
   alpha_b0 = ele%value(alpha_b_strong$)
