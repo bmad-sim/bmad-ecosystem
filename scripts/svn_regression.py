3600f16ff1c056130801dae3a96e00456ebb547b
David Sagan <dcs16@cornell.edu>
2022-08-18 03:15:47 +0000

    Change due to change in PTC.

--- scripts/svn_regression.py
+++ scripts/svn_regression.py
@@ -18,8 +18,8 @@ svn_repo = {
   'https://accserv.classe.cornell.edu/svn/packages/openmpi' :              '51988',
   'https://accserv.classe.cornell.edu/svn/packages/plplot' :               '50726',
   'https://accserv.classe.cornell.edu/svn/packages/xraylib' :              '45012',
-  'https://accserv.classe.cornell.edu/svn/trunk/util' :                    '52804',
-  'https://accserv.classe.cornell.edu/svn/trunk/build_system' :            '51314',
+#  'https://accserv.classe.cornell.edu/svn/trunk/util' :                    '52804',
+#  'https://accserv.classe.cornell.edu/svn/trunk/build_system' :            '51314',
 }
 
 all_pass = True
