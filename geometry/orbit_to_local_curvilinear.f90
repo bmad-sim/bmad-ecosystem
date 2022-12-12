c0841df42a8df0115e7317b6672845fbe9cec2d1
David Sagan <dcs16@cornell.edu>
2022-12-12 07:34:05 +0000

    Fix geometry with ele%orientation = -1

--- geometry/orbit_to_local_curvilinear.f90
+++ geometry/orbit_to_local_curvilinear.f90
@@ -1,5 +1,5 @@
 !+
-! Function orbit_to_local_curvilinear (orbit, ele) result (local_position)
+! Function orbit_to_local_curvilinear (orbit, ele, z_direction) result (local_position)
 !
 ! Routine to return the local curvilinear position and orientation of a particle.
 ! The orbit is in laboratory coords so element misalignments do not affect the calculation.
@@ -7,12 +7,14 @@
 ! Input:
 !   orbit         -- coord_struct: Particle orbit in laboratory (not body) coordinates.
 !   ele           -- ele_struct: Lattice element particle is in.
+!   z_direction   -- integer, optional: Set to +1 or -1.  Z-direction of particle velocity
+!                     relative to element z-axis. Default is ele%orientation * orbit%direction.
 !
 ! Output:
 !   local_position  -- floor_position_struct: Position in local coordinates.
 !-
 
-function orbit_to_local_curvilinear (orbit, ele) result (local_position)
+function orbit_to_local_curvilinear (orbit, ele, z_direction) result (local_position)
 
 use bmad_routine_interface, dummy => orbit_to_local_curvilinear
 
@@ -23,15 +25,19 @@ type (ele_struct) ele
 type (floor_position_struct) local_position
 
 real(rp) px, py, pz, r2
+integer, optional :: z_direction
 
 !
 
-local_position%r = [orbit%vec(1), orbit%vec(3), orbit%s - ele%s_start]
+select case (ele%orientation)
+case (+1);  local_position%r = [orbit%vec(1), orbit%vec(3), orbit%s - ele%s_start]
+case (-1);  local_position%r = [orbit%vec(1), orbit%vec(3), ele%s - orbit%s]
+end select
 
 px = orbit%vec(2) / (1 + orbit%vec(6))
 py = orbit%vec(4) / (1 + orbit%vec(6))
 r2 = 1.0_rp - px**2 - py**2
-pz = ele%orientation * orbit%direction * sqrt(max(0.0_rp, r2))
+pz = integer_option(ele%orientation * orbit%direction, z_direction) * sqrt(max(0.0_rp, r2))
 
 local_position%phi = atan2(py, sqrt(px**2  + py**2))
 local_position%theta = atan2(px, pz)
