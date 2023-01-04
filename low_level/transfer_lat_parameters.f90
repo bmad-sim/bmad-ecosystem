c08a1ba5eb0460bbee5a4f128cc7c70b299a6e4c
David Sagan <dcs16@cornell.edu>
2023-01-04 16:39:39 +0000

    Fix for gfortran pickyness.

--- low_level/transfer_lat_parameters.f90
+++ low_level/transfer_lat_parameters.f90
@@ -28,8 +28,6 @@ lat_out%lattice                   = lat_in%lattice
 lat_out%machine                   = lat_in%machine
 lat_out%input_file_name           = lat_in%input_file_name
 lat_out%title                     = lat_in%title
-lat_out%print_str                 = lat_in%print_str
-lat_out%constant                  = lat_in%constant
 lat_out%a                         = lat_in%a
 lat_out%b                         = lat_in%b
 lat_out%z                         = lat_in%z
@@ -39,16 +37,45 @@ lat_out%particle_start            = lat_in%particle_start
 lat_out%beam_init                 = lat_in%beam_init
 lat_out%pre_tracker               = lat_in%pre_tracker
 lat_out%nametable                 = lat_in%nametable
-lat_out%custom                    = lat_in%custom
 lat_out%version                   = lat_in%version
 lat_out%n_ele_track               = lat_in%n_ele_track
 lat_out%n_ele_max                 = lat_in%n_ele_max
 lat_out%n_control_max             = lat_in%n_control_max
-lat_out%control                   = lat_in%control
 lat_out%n_ic_max                  = lat_in%n_ic_max
-lat_out%ic                        = lat_in%ic
 lat_out%input_taylor_order        = lat_in%input_taylor_order
 lat_out%photon_type               = lat_in%photon_type
 lat_out%creation_hash             = lat_in%creation_hash
+
+! Allocatable components
+
+if (allocated(lat_in%print_str)) then
+  lat_out%print_str = lat_in%print_str
+else
+  if (allocated(lat_out%print_str)) deallocate(lat_out%print_str)
+endif
+
+if (allocated(lat_in%constant)) then
+  lat_out%constant = lat_in%constant
+else
+  if (allocated(lat_out%constant)) deallocate(lat_out%constant)
+endif
+
+if (allocated(lat_in%control)) then
+  lat_out%control = lat_in%control
+else
+  if (allocated(lat_out%control)) deallocate(lat_out%control)
+endif
+
+if (allocated(lat_in%custom)) then
+  lat_out%custom = lat_in%custom
+else
+  if (allocated(lat_out%custom)) deallocate(lat_out%custom)
+endif
+
+if (allocated(lat_in%ic)) then
+  lat_out%ic = lat_in%ic
+else
+  if (allocated(lat_out%ic)) deallocate(lat_out%ic)
+endif
 
 end subroutine transfer_lat_parameters
