260e32a2a622e774e149efaec64449a371d60193
David Sagan <dcs16@cornell.edu>
2023-01-21 19:04:29 +0000

    Fix to use complex arguments.

--- matrix/cplx_mat_inverse.f90
+++ matrix/cplx_mat_inverse.f90
@@ -13,23 +13,18 @@
 !   ok           -- Logical, optional: Set False for a singular input matrix.
 !-
 
-subroutine cplx_mat_inverse (mat_r, mat_i, inv_r, inv_i, ok, print_err)
+subroutine cplx_mat_inverse (mat, mat_inv, ok, print_err)
 
 use output_mod, except => cplx_mat_inverse
 
 implicit none
 
-real(rp) :: mat_r(:,:)
-real(rp) :: mat_i(:,:)
-real(rp) :: inv_r(:,:)
-real(rp) :: inv_i(:,:)
+complex(rp) :: mat(:,:)
+complex(rp) :: mat_inv(:,:)
 
-complex(rp) :: mat(size(mat_r,1),size(mat_r,1))
-complex(rp) :: mat_inv(size(mat_r,1),size(mat_r,1))
+complex(rp) :: mat2(size(mat, 1), size(mat, 2))
+integer :: indx(size(mat, 1))
 
-complex(rp) :: mat2(size(mat_r, 1), size(mat_r, 1))
-real(rp) :: vec(size(mat_r, 1))
-integer :: indx(size(mat_r, 1))
 real(rp) d
 
 integer n, i
@@ -38,14 +33,10 @@ character(16) :: r_name = 'mat_inverse'
 
 !
 
-mat = CMPLX(mat_r,mat_i)
-
 n = size (mat, 1)
-
 mat2 = mat  ! use temp mat so as to not change mat
 
-vec(1:n) = maxval(abs(mat), dim = 2)
-if (any(vec(1:n) == 0)) then
+if (any(maxval(abs(mat), dim = 2) == 0)) then
   if (logic_option(.false., print_err)) call out_io (s_error$, r_name, 'SINGULAR MATRIX.')
   if (present(ok)) ok = .false.
   return
@@ -60,9 +51,6 @@ forall (i = 1:n) mat_inv(i,i) = (1,0)
 do i = 1, n
   call cplx_lubksb (mat2, indx, mat_inv(1:n,i))
 enddo
-
-inv_r = REAL(mat_inv)
-inv_i = AIMAG(mat_inv)
 
 end subroutine
 
