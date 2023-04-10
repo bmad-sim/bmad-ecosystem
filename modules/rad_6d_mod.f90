c0738648b8d2f52382009876de9da398c3a63197
David Sagan <dcs16@cornell.edu>
2023-04-10 04:04:33 +0000

    Corrected radiation calc with element with misalignments.

--- modules/rad_6d_mod.f90
+++ modules/rad_6d_mod.f90
@@ -444,14 +444,15 @@ if (ele%key == sbend$ .or. ele%key == rf_bend$) then
   endif
 endif
 
-! Reference position for constructing the matrices is the upstream end but
+! Reference position for constructing the matrices in this routine is the upstream end but
 ! the reference of the output is the downstream end. So need to correct for this.
 
 mat0_inv = mat_symp_conj(mat0)   ! mat0 is transport matrix through the upstream edge
-rad_map%damp_dmat = matmul(matmul(matmul(ele%mat6, mat0_inv), damp_dmat1), mat0)
-rad_map%xfer_damp_vec = matmul(matmul(matmul(ele%mat6, mat0_inv), damp_vec1), mat0)
 
-rad_map%stoc_mat = matmul(matmul(mat0_inv, stoc_var_mat1), transpose(mat0_inv))
+rad_map%xfer_damp_vec = matmul(matmul(ele%mat6, mat0_inv), damp_vec1)
+rad_map%damp_dmat     = matmul(matmul(matmul(ele%mat6, mat0_inv), damp_dmat1), mat0)
+
+stoc_var_mat1    = matmul(matmul(mat0_inv, stoc_var_mat1), transpose(mat0_inv))
 rad_map%stoc_mat = matmul(matmul(ele%mat6, stoc_var_mat1), transpose(ele%mat6))
 
 bmad_com = bmad_com_save
@@ -475,6 +476,7 @@ logical include_opening_angle, save_orb_mat, err_flag
 ! So g will have a factor of (1 + pz) but the equations for the mats was developed for g of the zero pz particle
 
 call create_element_slice (runt, ele, z_pos, 0.0_rp, ele%branch%param, .false., .false., err_flag, pointer_to_next_ele(ele, -1))
+call zero_ele_offsets(runt)
 call track1(orb0, runt, ele%branch%param, orbz)
 call make_mat6(runt, ele%branch%param, orb0, orbz)
 if (save_orb_mat) then
