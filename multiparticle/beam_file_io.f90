d3d23ef0443c12124299f12b69b9555244e9ad17
David Sagan <dcs16@cornell.edu>
2023-07-19 17:48:15 +0000

    Fix track-to-t code.

--- multiparticle/beam_file_io.f90
+++ multiparticle/beam_file_io.f90
@@ -91,7 +91,7 @@ end subroutine write_beam_file
 !-----------------------------------------------------------------------------
 !-----------------------------------------------------------------------------
 !+
-! Subroutine read_beam_file (file_name, beam, beam_init, err_flag, ele, print_p0c_shift_warning, conserve_momentum)
+! Subroutine read_beam_file (file_name, beam, beam_init, err_flag, ele, print_mom_shift_warning, conserve_momentum)
 !
 ! Subroutine to read in a beam definition file.
 ! If non_zero, the following components of beam_init are used to rescale the beam:
@@ -106,7 +106,7 @@ end subroutine write_beam_file
 !   file_name   -- character(*): Name of beam file.
 !   beam_init   -- beam_init_struct: See above.
 !   ele         -- ele_struct, optional: Element with reference energy, etc.
-!   print_p0c_shift_warning   -- logical, optional: Default is True. See hdf5_read_beam doc. Only used when reading hdf5 file.
+!   print_mom_shift_warning   -- logical, optional: Default is True. See hdf5_read_beam doc. Only used when reading hdf5 file.
 !   shift_momentum            -- logical, optional: Default is True. See hdf5_read_beam doc. Only used when reading hdf5 file.
 !
 ! Output:
@@ -114,7 +114,7 @@ end subroutine write_beam_file
 !   err_flag    -- Logical: Set True if there is an error. False otherwise.
 !+ 
 
-subroutine read_beam_file (file_name, beam, beam_init, err_flag, ele, print_p0c_shift_warning, conserve_momentum)
+subroutine read_beam_file (file_name, beam, beam_init, err_flag, ele, print_mom_shift_warning, conserve_momentum)
 
 type (beam_struct), target :: beam
 type (beam_init_struct) beam_init
@@ -138,7 +138,7 @@ character(8) file_type
 character(*), parameter :: r_name = 'read_beam_file'
 
 logical err_flag, error, in_parens, valid
-logical, optional :: print_p0c_shift_warning, conserve_momentum
+logical, optional :: print_mom_shift_warning, conserve_momentum
 
 !
 
@@ -154,7 +154,7 @@ endif
 
 n = len_trim(full_name)
 if (full_name(max(1,n-4):n) == '.hdf5' .or. full_name(max(1,n-2):n) == '.h5') then
-  call hdf5_read_beam (full_name, beam, err_flag, ele, pmd_header, print_p0c_shift_warning, conserve_momentum)
+  call hdf5_read_beam (full_name, beam, err_flag, ele, pmd_header, print_mom_shift_warning, conserve_momentum)
   if (err_flag) return
 
   np = beam_init%n_particle
