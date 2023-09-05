3da08058417dfa6452714de5a0cc39eccab1a350
David Sagan <dcs16@cornell.edu>
2023-09-05 17:31:17 +0000

    More...

--- scripts/svn_regression.py
+++ scripts/svn_regression.py
@@ -8,18 +8,18 @@
 import subprocess
 svn_repo = {
   "https://accserv.classe.cornell.edu/svn/trunk/src/open_spacecharge"  : "50675",
-  "https://accserv.classe.cornell.edu/svn/packages/PGPLOT"             : "50204",
+  "https://accserv.classe.cornell.edu/svn/packages/PGPLOT"             : "55094",  #  Change from 50204
   "https://accserv.classe.cornell.edu/svn/packages/fftw"               : "54912",  #  Change from 54142
   "https://accserv.classe.cornell.edu/svn/packages/fgsl"               : "54914",  #  Change from 54143
   "https://accserv.classe.cornell.edu/svn/packages/gnu_utilities_src"  : "51479",
   "https://accserv.classe.cornell.edu/svn/packages/gsl"                : "54913",  #  Change from 53177
-  "https://accserv.classe.cornell.edu/svn/packages/hdf5"               : "54917",  #  Change from 53179
+  "https://accserv.classe.cornell.edu/svn/packages/hdf5"               : "55115",  #  Change from 54917
   "https://accserv.classe.cornell.edu/svn/packages/lapack"             : "54915",  #  Change from 53213
   "https://accserv.classe.cornell.edu/svn/packages/lapack95"           : "53051",
   "https://accserv.classe.cornell.edu/svn/packages/openmpi"            : "51988",
-  "https://accserv.classe.cornell.edu/svn/packages/plplot"             : "54925",  #  Change from 54269
+  "https://accserv.classe.cornell.edu/svn/packages/plplot"             : "55075",  #  Change from 54925
   "https://accserv.classe.cornell.edu/svn/packages/xraylib"            : "54916",  #  Change from 53181
-  "https://accserv.classe.cornell.edu/svn/trunk/util"                  : "54987",  #  Change from 54619
+  "https://accserv.classe.cornell.edu/svn/trunk/util"                  : "55117",  #  Change from 54987
   "https://accserv.classe.cornell.edu/svn/trunk/build_system"          : "54615",
 }
 
