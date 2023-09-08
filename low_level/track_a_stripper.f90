0bbed9fcc5b2da66ba5aa0400360098a77980443
David Sagan <dcs16@cornell.edu>
2023-09-08 02:47:47 +0000

    Stripper devel.

--- low_level/track_a_stripper.f90
+++ low_level/track_a_stripper.f90
@@ -17,9 +17,8 @@
 subroutine track_a_stripper (orbit, ele, param, mat6, make_matrix)
 
 use bmad_interface, except_dummy => track_a_stripper
+use xraylib, dummy => r_e
 use random_mod
-use super_recipes_mod
-use spline_mod
 
 implicit none
 
@@ -28,12 +27,45 @@ type (ele_struct), target :: ele
 type (lat_param_struct) :: param
 
 real(rp), optional :: mat6(6,6)
+real(rp) x0, xx0, sigma, z, rnd(2), density, p
+real(rp), parameter :: S2 = 13.6, epsilon = 0.088
 
+integer material, atomic_num
 
 logical, optional :: make_matrix
 
 character(*), parameter :: r_name = 'track_a_stripper'
 
 !
+
+material = species_id(ele%component_name)
+atomic_num = atomic_number(material)
+x0 = x0_radiation_length(material)   ! kg/m^2
+if (x0 == real_garbage$) then
+  call out_io(s_error$, r_name, 'CANNOT HANDLE NON-ATOMIC MATERIAL_TYPE: ' // species_name(material))
+  orbit%state = lost$
+  return
+endif
+
+!
+
+z = atomic_number(orbit%species)
+density = ElementDensity(atomic_num) * 1e3_rp  ! Convert to kg/m^3
+xx0 = ele%value(thickness$) / (x0 / density)
+p = (1.0_rp + orbit%vec(6)) * orbit%p0c
+! Factor of 1e6 is due to original formula using MeV/c for momentum
+sigma = 1.0e6_rp * S2 * z * sqrt(xx0) / (p * orbit%beta) * (1.0_rp + epsilon * log10(xx0*z*z/orbit%beta**2))
+
+!
+
+call ran_gauss(rnd)
+orbit%vec(2) = orbit%vec(2) + rnd(1) * sigma
+orbit%vec(4) = orbit%vec(4) + rnd(2) * sigma
+
+!
+
+if (logic_option(.false., make_matrix)) then
+  call mat_make_unit(mat6)
+endif
 
 end subroutine track_a_stripper
