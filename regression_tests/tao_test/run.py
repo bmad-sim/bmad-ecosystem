7c63c744fa3f2084b7b4e3aba50afe6bc01f93d6
David Sagan <dcs16@cornell.edu>
2024-10-15 22:45:59 +0200

    Fix Tao `show spin` command. (#1239)

--- regression_tests/tao_test/run.py
+++ regression_tests/tao_test/run.py
@@ -2,35 +2,10 @@ import subprocess
 import os
 import sys
 
-out_file = open('output.now', 'w')
-
-if os.path.isabs(sys.argv[1]):
-  exe = sys.argv[1] + 'tao'
-else:
-  exe = '../' + sys.argv[1] + 'tao'
+exe = sys.argv[1] + 'tao'
 
 #-----------
 
-print ('Test1...')
-os.chdir('test1')
-results = subprocess.run([exe, '-noplot', '-lat', 'lat.bmad'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-os.chdir('..')
-
-if 'contact DCS' in results or 'FATAL' in results:
-  out_file.write ('"Bookkeeper1" STR  "BAD"\n')
-  print(results)
-else:
-  out_file.write ('"Bookkeeper1" STR  "GOOD"\n')
-
-#-----------
-
-print ('Test2...')
-os.chdir('test2')
-results = subprocess.run([exe, '-noplot', '-lat', 'lat.bmad'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-os.chdir('..')
-
-if 'contact DCS' in results or 'FATAL' in results:
-  out_file.write ('"Bookkeeper2" STR  "BAD"\n')
-  print(results)
-else:
-  out_file.write ('"Bookkeeper2" STR  "GOOD"\n')
+print ('Test...')
+#os.chdir('test1')
+results = subprocess.run([exe, '-noplot', '-lat', 'small_ring.bmad'], stdout=subprocess.PIPE).stdout.decode('utf-8')
