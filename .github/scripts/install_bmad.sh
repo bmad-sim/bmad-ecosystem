13865044c38d7137281e4bfe043211b5c263339a
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2025-03-07 11:19:08 -0800

    CI: add workflow to build/test bmad with just conda

--- .github/scripts/install_bmad.sh
+++ .github/scripts/install_bmad.sh
@@ -11,7 +11,7 @@ echo "**** Setup Preferences"
 
 echo "Number of processors: $(nproc)"
 
-cat <<EOF >> ./util/dist_prefs
+cat <<EOF >>./util/dist_prefs
 export DIST_F90_REQUEST="gfortran"
 export ACC_PLOT_PACKAGE="pgplot"
 export ACC_PLOT_DISPLAY_TYPE="X"
@@ -25,6 +25,23 @@ export ACC_ENABLE_FPIC="Y"
 export ACC_ENABLE_PROFILING="N"
 export ACC_SET_GMAKE_JOBS="$(nproc)"
 EOF
+
+if [ "$USE_CONDA" == "1" ]; then
+  [ -z "$CONDA_PREFIX" ] && {
+    echo "CONDA_PREFIX unset?"
+    exit 1
+  }
+
+  echo "* Using conda to build: $CONDA_PREFIX"
+
+  cat <<EOF >>./util/dist_prefs
+export ACC_CONDA_BUILD="Y"
+export ACC_CONDA_BUILD_TESTS="Y"
+export ACC_CONDA_PATH="$CONDA_PREFIX"
+export ACC_USE_MACPORTS="N"
+EOF
+
+fi
 
 echo "**** Invoking dist_source_me"
 source ./util/dist_source_me
