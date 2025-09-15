cec68a3adeb2a9eb5788e98b3f7720bca0a0e159
David Sagan <dcs16@cornell.edu>
2025-09-15 18:43:29 -0400

    Increased bmad_inc_version$.

--- bmad/modules/bmad_struct.f90
+++ bmad/modules/bmad_struct.f90
@@ -19,7 +19,7 @@ private next_in_branch
 ! IF YOU CHANGE THE LAT_STRUCT OR ANY ASSOCIATED STRUCTURES YOU MUST INCREASE THE VERSION NUMBER !!!
 ! THIS IS USED BY BMAD_PARSER TO MAKE SURE DIGESTED FILES ARE OK.
 
-integer, parameter :: bmad_inc_version$ = 343
+integer, parameter :: bmad_inc_version$ = 344
 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
