8179f29d7cd5ddaa5d25d01b7469f50ece46685e
David Sagan <dcs16@cornell.edu>
2025-10-15 23:59:48 -0400

    correct.

--- regression_tests/autoscale_test/autoscale_test.f90
+++ regression_tests/autoscale_test/autoscale_test.f90
@@ -20,6 +20,6 @@ call track1(orb_start, ele, lat%param, orb_end)
 write (1, '(a, f16.12)') '"Auto_Phase" ABS 2e-4', ele%value(phi0_autoscale$)
 write (1, '(a, es18.10)') '"Auto_Amp"   REL 1e-6', ele%value(field_autoscale$)
 write (1, '(a, es18.10)') '"z" ABS 4e-10', orb_end%vec(5)
-write (1, '(a, es18.10)') '"pz" ABS 1e-8', orb_end%vec(6)
+write (1, '(a, es18.10)') '"pz" ABS 2e-8', orb_end%vec(6)
 
 end program
