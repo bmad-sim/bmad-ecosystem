89b058eec71ea5e0169db14cb9458aa09983a653
Christopher M. Pierce <contact@chris-pierce.com>
2025-12-09 21:25:36 -0800

    cleanup

--- regression_tests/pytao/smoke_tests/test_lattices.py
+++ regression_tests/pytao/smoke_tests/test_lattices.py
@@ -8,8 +8,8 @@ lattices_dir = Path(__file__).parent / "lattices"
 lattice_files = sorted(lattices_dir.glob("*.bmad"))
 
 
-@pytest.mark.parametrize("lat_path", lattice_files, ids=lambda p: p.name)
-def test_load_lattice(lat_path):
+@pytest.mark.parametrize("lattice_file", lattice_files, ids=lambda p: p.name)
+def test_load_lattice(lattice_file):
     """Test that each lattice file can be loaded without errors."""
-    with SubprocessTao(lattice_file=lat_path, noplot=True):
+    with SubprocessTao(lattice_file=lattice_file, noplot=True):
         pass
