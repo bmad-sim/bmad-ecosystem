a2e3284ea2cdd5932e385990b5964c6d539576b1
David Sagan <dcs16@cornell.edu>
2026-01-07 22:26:47 -0500

    Fix bug when passed in character string has to small a length.

--- tao/code/tao_srdt_calc_needed.f90
+++ tao/code/tao_srdt_calc_needed.f90
@@ -12,6 +12,7 @@ use srdt_mod
 implicit none
 
 character(*) data_type, data_source
+character(20) data_typ
 integer do_srdt
 
 !
@@ -19,10 +20,11 @@ integer do_srdt
 do_srdt = 0
 if (data_source /= 'lat') return
 
-if (data_type(1:5)  == 'srdt.') then
-  if(any(data_type(6:11) == srdt_first)) then
+data_typ = data_type  ! Needed in case data_type in calling routine has less than 11 characters.
+if (data_typ(1:5)  == 'srdt.') then
+  if(any(data_typ(6:11) == srdt_first)) then
     do_srdt = 1
-  elseif(any(data_type(6:11) == srdt_second)) then
+  elseif(any(data_typ(6:11) == srdt_second)) then
     do_srdt = 2
   endif
 endif
